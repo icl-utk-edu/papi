f6d52c159bf1c887e6f4005343f778c1d7cc19bb
deane <deane>
1999-02-13 15:21:29 +0000

    *** empty log message ***

---
+++ src/papi.h
@@ -0,0 +1,152 @@
+/* file: papi.h */
+
+/*platform specific defintions*/
+#include "StdEventDefs.h" 
+
+/*
+Return Codes
+
+All of the functions contained in the PerfAPI return standardized error codes.
+Values greater than or equal to zero indicate success, less than zero indicates
+failure. 
+*/
+
+
+#define PAPI_OK_MPX    1  /*No error, multiplexing has been enabled 
+                            and is now active*/
+#define PAPI_OK        0  /*No error*/
+#define PAPI_EINVAL   -1  /*Invalid argument*/
+#define PAPI_ENOMEM   -2  /*Insufficient memory*/
+#define PAPI_ESYS     -3  /*A System C library call failed, please check errno*/
+#define PAPI_ESBSTR   -4  /*Substrate returned an error, 
+			    usually the result of an unimplemented feature*/
+#define PAPI_ECLOST   -5  /*Access to the counters was lost or interrupted*/
+#define PAPI_EBUG     -6  /*Internal error, please send mail to the developers*/
+#define PAPI_ENOEVNT  -7  /*Hardware Event does not exist*/
+#define PAPI_ECNFLCT  -8  /*Hardware Event exists, but cannot be counted 
+                            due to counter resource limitations*/ 
+#define PAPI_ENOTRUN  -9  /*No Events or EventSets are currently counting*/
+
+/*
+Constants
+
+All of the functions in the PerfAPI should use the following set of constants.
+*/
+
+#define PAPI_NULL       -1    /*A nonexistent hardware event used as a placeholder*/ 
+#define PAPI_USER        0    /*Counts are accumulated for events occuring in the 
+				the user context*/
+#define PAPI_KERNEL	 1    /*Counts are accumulated for events occurring in the
+				the kernel context*/
+#define PAPI_SYSTEM	 2    /*Counts are accumulated for events occuring in
+			 	either the user context or the kernel context*/
+
+#define PAPI_PER_THR     0    /*Counts are accumulated on a per kernel thread basis*/ 	
+#define PAPI_PER_PROC    1    /*Counts are accumulated on a per process basis*/
+#define PAPI_PER_CPU     2    /*Counts are accumulated on a per cpu basis*/
+#define PAPI_PER_NODE    3    /*Counts are accumulated on a per node or 
+				processor basis*/ 
+
+#define PAPI_RUNNING     1    /*EventSet is running*/
+#define PAPI_STOPPED     2    /*EventSet is stopped*/ 
+#define PAPI_PAUSED      3    /*EventSet is temporarily disabled by the library*/
+#define PAPI_NOT_INIT    4    /*EventSet defined, but has not yet been initiated*/
+			      /* :::::PAPI_NOT_INIT NOT IN STANDARD:::::   	
+				The constant PAPI_NOT_INIT is not in the standard, 
+				but this definition is reserved for future use.*/  
+
+#define PAPI_QUIET       0    /*Option to not do any automatic error reporting 
+				to stderr*/
+#define PAPI_VERB_ECONT  1    /*Option to automatically report any return codes <0 
+				to stderr [error-continue]*/ 
+#define PAPI_VERB_ESTOP  2    /*Option to automatically report any error codes < 0 
+				to stderr and call exit(PAPI_ERROR) [error-stop]*/
+
+#define PAPI_SET_MPXRES  1    /*Option to enable and set the resolution of the 
+				multiplexing hardware*/
+#define PAPI_GET_MPXRES  2    /*Option to query the status of the 
+                                multiplexing software*/
+#define PAPI_DEF_MPXRES  1000 /*Default resolution in microseconds of the 
+				multiplexing software*/
+
+#define PAPI_DEBUG	 3    /*Option to turn on debugging features of 
+				the PerfAPI library*/
+#define PAPI_SET_OVERFLO 4    /*Option to turn on the overflow reporting software*/
+#define PAPI_GET_OVERFLO 5    /*Option to query the status of the overflow
+				reporting software*/
+
+#define PAPI_ONESHOT	 1    /*Option to have the overflow handler called once*/
+#define PAPI_RANDOMIZE	 2    /*Option to have the threshold of the overflow
+				handler randomized*/
+
+#define PAPI_MAX_EVNTS   16   /*The maximum number of spontaneous events 
+				countable by the platform specific hardware 
+				without multiplexing*/
+#define PAPI_ERROR	 123  /*Exit code for PerfAPI executables that have 
+				PAPI_VERB_ESTOP option set*/
+
+
+#define PAPI_MAX_ERRMS   16   /*Number of internal error messages.*/
+
+#define PAPI_MAX_ERROR   10   /*Highest number of defined error messages.*/ 	
+
+/*
+The Low Level API
+
+The following functions represent the low level portion of the PerfAPI. These
+functions provide greatly increased efficiency and functionality over the high
+level API presented in the next section. All of the following functions are callable
+from both C and Fortran except where noted. As mentioned in the introduction,
+the low level API is only as powerful as the substrate upon which it is built. Thus
+some features may not be available on every platform. The converse may also be
+true, that more advanced features may be available and defined in the header file.
+The user is encouraged to read the documentation carefully. 
+*/
+
+
+int PAPI_set_granularity(int granularity);
+int PAPI_set_context(int context);
+int PAPI_perror(int code, char *destination, int length);
+
+int PAPI_add_event(int *EventSet, int Event);
+int PAPI_add_events(int *EventSet, int *Events, int number);
+int PAPI_add_pevent(int *EventSet, int code, void *inout);
+int PAPI_rem_event(int EventSet, int Event); 
+int PAPI_rem_events(int EventSet, int *Events, int number); 
+int PAPI_list_events(int EventSet, int *Events, int *number);
+
+int PAPI_start(int EventSet);
+int PAPI_stop(int EventSet, long long *values);
+int PAPI_read(int EventSet, long long *values);
+int PAPI_accum(int EventSet, long long *values);
+int PAPI_write(int EventSet, long long *values);
+
+int PAPI_reset(int EventSet);
+int PAPI_cleanup(int *EventSet);
+
+int PAPI_state(int EventSet, int *status);
+int PAPI_set_opt(int option, int value, PAPI_option_t *ptr); 
+
+void (*handler)(int signal, siginfo_t *si, void *ucontext, 
+              int EventSet, int Event, int count); 
+int PAPI_get_opt(int option, int *value, PAPI_option_t *ptr);
+
+int PAPI_num_events(void);
+
+/*
+The High Level API
+
+The simple interface implemented by the following four routines
+allows the user to access and count specific hardware events from
+both C and Fortran. It should be noted that this API can be used in
+conjunction with the low level API. If counter multiplexing is
+enabled by the user, the high level API is only able to access those
+events countable simultaneously by the underlying hardware. 
+
+*/
+
+int PAPI_start_counters(int *events, int array_len);
+int PAPI_read_counters(long long *values, int array_len);
+int PAPI_stop_counters(long long *values, int array_len);
+
+
--- src/papiStdEventDefs.h
+++ src/papiStdEventDefs.h
@@ -11,35 +11,35 @@ your platform.
  
 */
 
-#define PAPI_L1_DCM  0x80010000 /*Level 1 data cache misses*/
-#define PAPI_L1_ICM  0x80010001 /*Level 1 instruction cache misses*/ 
-#define PAPI_L2_DCM  0x80010002 /*Level 2 data cache misses*/
-#define PAPI_L2_ICM  0x80010003 /*Level 2 instruction cache misses*/ 
-#define PAPI_L3_DCM  0x80010004 /*Level 3 data cache misses*/
-#define PAPI_L3_ICM  0x80010005 /*Level 3 instruction cache misses*/ 
+#define PAPI_L1_DCM  0x80000000 /*Level 1 data cache misses*/
+#define PAPI_L1_ICM  0x80000001 /*Level 1 instruction cache misses*/ 
+#define PAPI_L2_DCM  0x80000002 /*Level 2 data cache misses*/
+#define PAPI_L2_ICM  0x80000003 /*Level 2 instruction cache misses*/ 
+#define PAPI_L3_DCM  0x80000004 /*Level 3 data cache misses*/
+#define PAPI_L3_ICM  0x80000005 /*Level 3 instruction cache misses*/ 
 
-#define PAPI_CA_SHR  0x80011000 /*Request for access to shared cache line (SMP)*/
-#define PAPI_CA_CLN  0x80011001 /*Request for access to clean  cache line (SMP)*/
-#define PAPI_CA_INV  0x80011002 /*Cache Line Invalidation (SMP)*/
+#define PAPI_CA_SHR  0x8000000A /*Request for access to shared cache line (SMP)*/
+#define PAPI_CA_CLN  0x8000000B /*Request for access to clean  cache line (SMP)*/
+#define PAPI_CA_INV  0x8000000C /*Cache Line Invalidation (SMP)*/
 
-#define PAPI_TBL_DM  0x80020000 /*Data translation lookaside buffer misses*/
-#define PAPI_TBL_IM  0x80020001 /*Instruction translation lookaside buffer misses*/
-#define PAPI_TBL_SD  0x80021000 /*Translation lookaside buffer shootdowns (SMP)*/
+#define PAPI_TBL_DM  0x80000014 /*Data translation lookaside buffer misses*/
+#define PAPI_TBL_IM  0x80000015 /*Instruction translation lookaside buffer misses*/
+#define PAPI_TBL_SD  0x8000001E /*Translation lookaside buffer shootdowns (SMP)*/
 
-#define PAPI_BRI_MSP 0x80030000 /*Branch instructions mispredicted*/
-#define PAPI_BRI_TKN 0x80030001 /*Branch instructions taken*/
-#define PAPI_BRI_NTK 0x80030002 /*Branch instructions not taken*/
+#define PAPI_BRI_MSP 0x80000028 /*Branch instructions mispredicted*/
+#define PAPI_BRI_TKN 0x80000029 /*Branch instructions taken*/
+#define PAPI_BRI_NTK 0x8000002A /*Branch instructions not taken*/
 
-#define PAPI_TOT_INS 0x80100000 /*Total instructions executed*/
-#define PAPI_INT_INS 0x80100001 /*Integer instructions executed*/
-#define PAPI_FP_INS  0x80100002 /*Floating point instructions executed*/
-#define PAPI_LD_INS  0x80100003 /*Load instructions executed*/
-#define PAPI_SR_INS  0x80100004 /*Store instructions executed*/
-#define PAPI_CND_INS 0x80100005 /*Branch instructions executed*/
-#define PAPI_VEC_INS 0x80100006 /*Vector/SIMD instructions executed*/
-#define PAPI_FLOPS   0x80100007 /*Floating Point instructions per second*/ 
+#define PAPI_TOT_INS 0x80000032 /*Total instructions executed*/
+#define PAPI_INT_INS 0x80000033 /*Integer instructions executed*/
+#define PAPI_FP_INS  0x80000034 /*Floating point instructions executed*/
+#define PAPI_LD_INS  0x80000035 /*Load instructions executed*/
+#define PAPI_SR_INS  0x80000036 /*Store instructions executed*/
+#define PAPI_CND_INS 0x80000037 /*Branch instructions executed*/
+#define PAPI_VEC_INS 0x80000038 /*Vector/SIMD instructions executed*/
+#define PAPI_FLOPS   0x80000039 /*Floating Point instructions per second*/ 
 
-#define PAPI_TOT_CYC 0x80200000 /*Total cycles*/
-#define PAPI_MIPS    0x80200001 /*Millions of instructions executed per second*/
+#define PAPI_TOT_CYC 0x8000003C /*Total cycles*/
+#define PAPI_MIPS    0x8000003D /*Millions of instructions executed per second*/
 
 
