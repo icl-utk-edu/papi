9dc5e1cf2af881cde443a77c0c93b4bc80653fbb
deane <deane>
1999-02-13 23:33:20 +0000

    I have put the function _papi_expandDA into papi.c
    There are massive comments, but the code is short.
    
    I have compiled this function and tested it with a c program that
    calls the function.
    
    The code for my tester is:
    
    #include "papi.h"
    int main(int argc, char *argv[]) {
    
    DynamicArray *dDA;
    int n=-3;
    
    dDA=(DynamicArray *)malloc(sizeof(DynamicArray));
    printf("\n BEFORE _papi_expandDA: dDA->totalSlots=%d",dDA->totalSlots);
    printf("\n BEFORE _papi_expandDA: dDA->availSlots=%d",dDA->availSlots);
    printf("\n BEFORE _papi_expandDA: dDA->lowestEmptySlot=%d",dDA->lowestEmptySlot);
    
    n=_papi_expandDA(dDA);
    printf("\n AFTER #1  n=%d",n);
    printf("\n AFTER #1  _papi_expandDA: dDA->totalSlots=%d",dDA->totalSlots);
    printf("\n AFTER #1  _papi_expandDA: dDA->availSlots=%d",dDA->availSlots);
    printf("\n AFTER #1  _papi_expandDA: dDA->lowestEmptySlot=%d",dDA->lowestEmptySlot);
    
    n=_papi_expandDA(dDA);
    printf("\n AFTER #2  n=%d",n);
    printf("\n AFTER #2  _papi_expandDA: dDA->totalSlots=%d",dDA->totalSlots);
    printf("\n AFTER #2  _papi_expandDA: dDA->availSlots=%d",dDA->availSlots);
    printf("\n AFTER #2  _papi_expandDA: dDA->lowestEmptySlot=%d",dDA->lowestEmptySlot);
    
    printf("\n\n normal termination %s\n",__FILE__);
    }/*end main*/
    
    
    cheers!
    cricket!

---
+++ src/papi.c
@@ -0,0 +1,102 @@
+/* file: papi.c */ 
+/*file: PAPI_expandDA.c*/
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <siginfo.h>
+
+#include "papi.h"
+
+
+/*========================================================================*/
+/*========================================================================*/
+/*========================================================================*/
+/* begin function: int _papi_expandDA(DynamicArray *DA);                  */
+/*========================================================================*/
+
+----------------------------------------------------------------------------
+the _dynamic_array struct defined in papi_internal.h
+typedef struct _dynamic_array {
+	void   **dataSlotArray; ** ptr to array of ptrs to EventSets      **
+	int    totalSlots;      ** number of slots in dataSlotArrays      **
+	int    availSlots;      ** number of open slots in dataSlotArrays **
+	int    lowestEmptySlot; ** index of lowest empty dataSlotArray    **
+} DynamicArray;
+----------------------------------------------------------------------------
+The function _papi_expandDA initializes or expands the global 
+data structure DA, which is type DynamicArray defined above.
+
+The function _papi_expandDA takes one argument:
+	DynamicArray *DA
+
+If the initialization case is detected [ if(DA->totalSlots==0) ],
+the DA structure is initialized.
+
+If the expansion case is detected [ if (DA->totalSlots!=0) ],
+the DA->dataSlotArray is expanded. 
+
+The DA structure holds the dataSlotArray.  Each element of dataSlotArray  
+is a pointer to an EventSetInfo structure.  The zero element of the
+dataSlotArray is reserved and is set during initilization [see below].
+
+DA->dataSlotArray[1] holds ptr to EventSetInfo number 1 
+DA->dataSlotArray[2] holds ptr to EventSetInfo number 2 
+DA->dataSlotArray[3] holds ptr to EventSetInfo number 3 
+  ...
+  ...
+DA->dataSlotArray[N] holds ptr to EventSetInfo number N,
+where 
+	N<DA->totalSlots
+
+The function _papi_expandDA is only called when one of
+the following is detected by a function seeking to
+add an EventSet to the list:
+
+	1. initialization condition 
+	2. N==DA->totalSlots
+
+The function _papi_expandDA returns a pointer to the
+expanded DA structure.
+
+	return (1) indicates failure
+	return (0) indicates success
+----------------------------------------------------------------------------
+*/
+
+
+int _papi_expandDA(DynamicArray *DA) {
+
+int          prevTotal;	
+
+if(DA->totalSlots==0) { /*initialization of first DA structure*/
+	
+	DA->totalSlots = 64;
+	DA->availSlots = 63;
+	DA->lowestEmptySlot = 1;
+	DA->dataSlotArray=(void **)malloc(DA->totalSlots*sizeof(void *));
+
+	}/***/
+
+else	{/* expansion of existing DA structure */
+	prevTotal           = DA->totalSlots; 
+	DA->totalSlots     += prevTotal;
+	DA->availSlots      = prevTotal;
+	DA->lowestEmptySlot = prevTotal;
+
+	/*realloc existing array*/
+        if( (void **)realloc(DA->dataSlotArray,DA->totalSlots*sizeof(void *))
+        ==NULL) return(1);   
+
+        /* would anyone prefer:  return(PAPI_ENOMEM);
+           instead of:           return(1);
+         */  
+
+	}/***/
+
+return(0);
+
+}
+/*========================================================================*/
+/*end function: _papi_expandDA                                            */
+/*========================================================================*/
+/*========================================================================*/
