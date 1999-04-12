9c29e3b5b7a83285bd7099bf9631d8894303dc39
gho <gho>
1999-04-12 22:00:38 +0000

    added first.c to EXAMPLES in Makefile, first.c is the first test file to use Low-Level API the way it will be used by a User.

---
+++ src/ctests/first.c
@@ -0,0 +1,29 @@
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include "papiStdEventDefs.h"
+#include "papi.h"
+#include "papi_internal.h"
+
+void main() {
+  int r, i;
+  double a, b, c;
+  unsigned long long  ct[3];
+  int EventSet = PAPI_NULL;
+
+r=PAPI_add_event(&EventSet, PAPI_FP_INS);
+r=PAPI_add_event(&EventSet, PAPI_TOT_INS);
+r=PAPI_add_event(&EventSet, PAPI_TOT_CYC);
+r=PAPI_reset(EventSet);
+r=PAPI_start(EventSet);
+
+  a = 0.5;
+  b = 6.2;
+  for (i=0; i < 50000000; i++) {
+    c = a*b;
+  }
+
+r=PAPI_stop(EventSet, ct);
+}
