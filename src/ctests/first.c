631972da85be79c10b08bc4e08878ed682d70496
Philip Mucci <mucci@eecs.utk.edu>
1999-05-24 17:31:41 +0000

    Added Makefile for AIX. Make at the top level must be done with make -f Makefile.aix now.
    Also, finished a nice rewrite of first.c. Everyone take notice! This is
    the new reference test program. See how clean it is?

--- src/ctests/first.c
+++ src/ctests/first.c
@@ -5,34 +5,63 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <memory.h>
+#include <assert.h>
 #include "papiStdEventDefs.h"
 #include "papi.h"
 #include "papi_internal.h"
+
+#define TESTNUM 10000000
 
 void main() 
 {
   int r, i;
   double a, b, c;
-  unsigned long long ct[3];
+  unsigned long long *ct;
   int EventSet = PAPI_NULL;
-  PAPI_option_t options;
+
+  i = PAPI_num_events();
+  assert(i>=2);
+
+  ct = (unsigned long long *)malloc(i*sizeof(unsigned long long));
+  assert(ct!=NULL);
+  memset(ct,0x00,i*sizeof(unsigned long long));
+
+  r=PAPI_add_event(&EventSet, PAPI_TOT_CYC);
+  assert(r>=PAPI_OK);
+
+  r=PAPI_add_event(&EventSet, PAPI_TOT_INS);
+  assert(r>=PAPI_OK); 
 
   r=PAPI_add_event(&EventSet, PAPI_FP_INS);
-  r=PAPI_add_event(&EventSet, PAPI_TOT_INS);
-  r=PAPI_add_event(&EventSet, PAPI_TOT_CYC);
+  assert(r>=PAPI_OK);
 
-  options.domain.eventset=1;
-  options.domain.domain=PAPI_DOM_DEFAULT;
-
-  r=PAPI_set_opt(PAPI_SET_DOMAIN, &options);
-  r=PAPI_reset(EventSet);
   r=PAPI_start(EventSet);
+  assert(r>=PAPI_OK);
 
   a = 0.5;
   b = 6.2;
-  for (i=0; i < 50000000; i++) {
+  for (i=0; i < TESTNUM; i++) {
     c = a*b;
   }
   
   r=PAPI_stop(EventSet, ct);
+  assert(r>=PAPI_OK);
+
+  r=PAPI_rem_event(&EventSet, PAPI_FP_INS);
+  assert(r>=PAPI_OK);
+
+  r=PAPI_rem_event(&EventSet, PAPI_TOT_INS);
+  assert(r>=PAPI_OK);
+
+  r=PAPI_rem_event(&EventSet, PAPI_TOT_CYC);
+  assert(r>=PAPI_OK); 
+
+  PAPI_shutdown();
+
+  printf("%d iterations of c = a*b\n",TESTNUM);
+  printf("%lld cycles\n%lld instructions\n%lld floating point instructions\n",ct[0],ct[1],ct[2]);
+  printf("%f IPC\n%f FPC\n",(float)ct[1]/(float)ct[0],(float)ct[2]/(float)ct[0]);
+
+  free(ct);
+  exit(0);
 }
