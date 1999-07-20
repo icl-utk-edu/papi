6937bcc0ad87c943f67203e69fbd23c1e802e3ce
gho <gho>
1999-07-20 18:39:17 +0000

    new example files that Phil added. I tweaked the src/ files a little to fix some minor bugs these examples brought out.
    -george

---
+++ src/ctests/overflow.c
@@ -0,0 +1,58 @@
+/* $Id$ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <memory.h>
+#include <malloc.h>
+#include <assert.h>
+#include "papiStdEventDefs.h"
+#include "papi.h"
+#include "papi_internal.h"
+
+#define TESTNUM 100000000
+
+void handler(int eventcode, unsigned long long value, unsigned long long *threshold, void *context)
+{
+  static int count = 0;
+  count++;
+
+  fprintf(stderr,"User overflow handler %d: %x, %llu, %llu, %p\n",count,eventcode,value,*threshold,context);
+  return;
+}
+
+int main() 
+{
+  int i, n = 2;
+  double a, b, c;
+  unsigned long long *ct;
+  int EventSet = PAPI_NULL;
+
+  ct = (unsigned long long *)malloc(n*sizeof(unsigned long long));
+  assert(ct!=NULL);
+  memset(ct,0x00,n*sizeof(unsigned long long));
+
+  assert(PAPI_num_events() >= PAPI_OK);
+  assert(PAPI_query_event(PAPI_TOT_CYC) == PAPI_OK);
+  assert(PAPI_add_event(&EventSet, PAPI_TOT_CYC) == PAPI_OK);
+  assert(PAPI_query_event(PAPI_FP_INS) == PAPI_OK);
+  assert(PAPI_add_event(&EventSet, PAPI_FP_INS) == PAPI_OK);
+  assert(PAPI_overflow(EventSet, PAPI_FP_INS, 1000000, 0, handler) == PAPI_OK);
+  assert(PAPI_start(EventSet) == PAPI_OK);
+
+  a = 0.5;
+  b = 6.2;
+  for (i=0; i < TESTNUM; i++) {
+    c = a*b;
+  }
+  
+  assert(PAPI_stop(EventSet, ct) == PAPI_OK);
+  assert(PAPI_rem_event(&EventSet, PAPI_FP_INS) == PAPI_OK);
+  assert(PAPI_rem_event(&EventSet, PAPI_TOT_CYC) == PAPI_OK);
+  PAPI_shutdown();
+
+  free(ct);
+  
+  exit(0);
+}
---
+++ src/ctests/tests/fourth.c
@@ -0,0 +1,106 @@
+/* $Id$ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <memory.h>
+#include <assert.h>
+#include <malloc.h>
+#include "papiStdEventDefs.h"
+#include "papi.h"
+#include "papi_internal.h"
+
+#define TESTNUM 10000000
+
+int main(int argc, char **argv) 
+{
+  int r, i, n = 3;
+  double a, b, c;
+  unsigned long long *cr,*cs,*ct, *cu;
+  int EventSet1 = PAPI_NULL;
+  int EventSet2 = PAPI_NULL;
+
+  r = PAPI_num_events();
+  assert(r>=PAPI_OK);
+
+  r=PAPI_add_event(&EventSet1, PAPI_TOT_CYC);
+  assert(r>=PAPI_OK);
+  r=PAPI_add_event(&EventSet1, PAPI_TOT_INS);
+  assert(r>=PAPI_OK);
+
+  r=PAPI_add_event(&EventSet2, PAPI_FP_INS);
+  assert(r>=PAPI_OK);
+  r=PAPI_add_event(&EventSet2, PAPI_TOT_CYC);
+  assert(r>=PAPI_OK);
+  
+  ct = (unsigned long long *)malloc(n*sizeof(unsigned long long));
+  cr = (unsigned long long *)malloc(n*sizeof(unsigned long long));
+  cs = (unsigned long long *)malloc(n*sizeof(unsigned long long));
+  cu = (unsigned long long *)malloc(n*sizeof(unsigned long long));
+  assert(ct!=NULL);
+  assert(cr!=NULL);
+  assert(cs!=NULL);
+  assert(cu!=NULL);
+  memset(ct,0x00,n*sizeof(unsigned long long));
+  memset(cr,0x00,n*sizeof(unsigned long long));
+  memset(cs,0x00,n*sizeof(unsigned long long));
+  memset(cu,0x00,n*sizeof(unsigned long long));
+
+  r=PAPI_start(EventSet1);
+  assert(r>=PAPI_OK);
+
+  a = 0.5;
+  b = 6.2;
+  for (i=0; i < TESTNUM; i++) {
+    c = a*b;
+  }
+  
+  r=PAPI_reset(EventSet1);
+  assert(r>=PAPI_OK);
+
+  r=PAPI_start(EventSet2);
+  assert(r>=PAPI_OK);
+
+  a = 0.5;
+  b = 6.2;
+  for (i=0; i < TESTNUM; i++) {
+    c = a*b;
+  }
+
+  r=PAPI_read(EventSet1, cr);
+  assert(r>=PAPI_OK);
+  fprintf(stderr,"(flops)(reset)(flops) read of eventset 1 after eventset 2 started\n");
+
+  r=PAPI_read(EventSet2, cs);
+  assert(r>=PAPI_OK);
+  fprintf(stderr,"(flops) read of eventset 2 after both started\n");
+
+  a = 0.5;
+  b = 6.2;
+  for (i=0; i < TESTNUM; i++) {
+    c = a*b;
+  }
+
+  r=PAPI_stop(EventSet2, ct);
+  assert(r>=PAPI_OK);
+  fprintf(stderr,"(flops) stop of eventset 2 after both started\n");
+
+  r=PAPI_stop(EventSet1, cu);
+  assert(r>=PAPI_OK);
+  fprintf(stderr,"stop of eventset 1 after eventset 1 stopped\n");
+
+  PAPI_shutdown();
+
+  printf("(flops) = %d iterations of c = a*b\n",TESTNUM);
+  printf("Cycles: %llu %llu %llu %llu\n",cr[0],cs[1],ct[0],cu[1]);
+  printf("Flinst: %llu %llu %llu %llu\n",0ULL,cs[1],0ULL,cu[1]);
+  printf("Instrs: %llu %llu %llu %llu\n",cr[1],0ULL,ct[1],0ULL);
+
+  free(cr);
+  free(cs);
+  free(ct);
+  free(cu);
+  
+  exit(0);
+}
