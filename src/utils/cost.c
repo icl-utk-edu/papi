a12002ded25f6974c81f4f1b544f38b31248da46
Dan Terpstra <terpstra@eecs.utk.edu>
2001-07-20 19:37:46 +0000

    Added warning that this test takes a while... Fixed %lld to %I64d format problems for Windows.

--- src/utils/cost.c
+++ src/utils/cost.c
@@ -1,5 +1,13 @@
 #include "papi_test.h"
 
+#ifdef _WIN32
+	char format_string1[] = {"%I64d total cyc,\n%I64d total ins,\n%f %s,\n%f %s\n"};
+	char format_string2[] = {"%I64d total cyc,\n%f %s\n"};
+#else
+	char format_string1[] = {"%lld total cyc,\n%lld total ins,\n%f %s,\n%f %s\n"};
+	char format_string2[] = {"%lld total cyc,\n%f %s\n"};
+#endif
+
 void err_exit(int code, char *str)
 {
   char out[PAPI_MAX_STR_LEN];
@@ -12,6 +20,9 @@ int main()
 {
    int i, retval, EventSet = PAPI_NULL, CostEventSet = PAPI_NULL;
    long_long totcyc, values[2], readvalues[2];
+
+	printf("Cost of execution for PAPI start/stop and PAPI read.\n");
+	printf("This test takes a while. Please be patient...\n");
 
    if ((retval = PAPI_library_init(PAPI_VER_CURRENT)) != PAPI_VER_CURRENT)
      err_exit(retval, "PAPI_library_init(PAPI_VER_CURRENT)");
@@ -59,11 +70,12 @@ int main()
    if ((retval = PAPI_reset(CostEventSet)) != PAPI_OK)
      err_exit(retval, "PAPI_reset(CostEventSet)");
 
+   printf("Performing start/stop test...\n");
    totcyc = PAPI_get_real_cyc();
    if ((retval = PAPI_start(CostEventSet)) != PAPI_OK)
      err_exit(retval, "PAPI_start(CostEventSet)");
 
-   for (i=0;i<1000000;i++)
+	for (i=0;i<1000000;i++)
      {
        PAPI_start(EventSet);
        PAPI_stop(EventSet, values);
@@ -72,16 +84,16 @@ int main()
    if ((retval = PAPI_stop(CostEventSet, values)) != PAPI_OK)
      err_exit(retval, "PAPI_stop(CostEventSet, values)");
    totcyc = PAPI_get_real_cyc() - totcyc;
+   printf("\n");
 
    printf("User level cost for PAPI_start/stop(2 counters) over 1000000 iterations\n");
-   printf("%lld total cyc, %lld total ins, %f cyc/call pair, %f ins/call pair\n",
-	  values[0],values[1],((float)values[0])/1000000.0,((float)values[1])/1000000.0);
+   printf(format_string1,values[0],values[1],((float)values[0])/1000000.0,"cyc/call pair",
+	   ((float)values[1])/1000000.0,"ins/call pair");
    printf("\nTotal cost for PAPI_start/stop(2 counters) over 1000000 iterations\n");
-   printf("%lld total cyc, %f cyc/call pair\n",
-	  totcyc,((float)totcyc)/1000001.0);
+   printf(format_string2,totcyc,((float)totcyc)/1000001.0,"cyc/call pair");
 
    /* Start the read eval */
-
+   printf("\n\nPerforming read test...\n");
    if ((retval = PAPI_start(EventSet)) != PAPI_OK)
      err_exit(retval, "PAPI_start(EventSet)");
    if( (retval = PAPI_reset(CostEventSet)) != PAPI_OK)
@@ -90,7 +102,7 @@ int main()
    if ((retval = PAPI_start(CostEventSet)) != PAPI_OK)
      err_exit(retval, "PAPI_start(CostEventSet)");
 
-   for (i=0;i<1000000;i++)
+	for (i=0;i<1000000;i++)
      {
        PAPI_read(EventSet, values);
      }
@@ -100,11 +112,10 @@ int main()
    totcyc = PAPI_get_real_cyc() - totcyc;
 
    printf("\nUser level cost for PAPI_read(2 counters) over 1000000 iterations\n");
-   printf("%lld total cyc, %lld total ins, %f cyc/call, %f ins/call\n",
-	  values[0],values[1],((float)values[0])/1000000.0,((float)values[1])/1000000.0);
+   printf(format_string1,values[0],values[1],((float)values[0])/1000000.0,"cyc/call",
+	   ((float)values[1])/1000000.0,"ins/call");
    printf("\nTotal cost for PAPI_read(2 counters) over 1000000 iterations\n");
-   printf("%lld total cyc, %f cyc/call\n",
-	  totcyc,((float)totcyc)/1000001.0);
+   printf(format_string2,totcyc,((float)totcyc)/1000001.0,"cyc/call");
 
    exit(0);
 }
