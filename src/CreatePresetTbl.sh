89d86cf0d0459d073974e41fc45576e3172e3d30
Nils Smeds <nils.smeds@gmail.com>
2001-08-20 09:57:40 +0000

    PAPI_preset.3 sorted alphabetically for more easy reading.

--- src/CreatePresetTbl.sh
+++ src/CreatePresetTbl.sh
@@ -8,5 +8,6 @@ box, tab(&);
 lt | lw(50).
 =
 EOF
-./tests/avail | sed 's/(.*)//g' | awk '/Name/,/-----/ { printf("%s&T{\n", $1); for(i=5;i<=NF;i++) { printf("%s ",$i) } ; printf("\nT}\n_\n") }' 
+./tests/avail | grep 'PAPI_' | sed 's/(.*)//g' | sort | \
+   awk '{ printf("%s&T{\n", $1); for(i=5;i<=NF;i++) { printf("%s ",$i) } ; printf("\nT}\n_\n") }' 
 echo ".TE"
