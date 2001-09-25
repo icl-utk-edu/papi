5564dc07fed75a29952fcff55fa61afc05dc3223
zlong <zlong>
2001-09-25 23:17:33 +0000

    Added papif_query_all_event_verbose, and papif_query_event_verbose in ftests_wrappers.c Rewrite avail.F so that its output matches avail.c
    -long

--- src/ftests/ftests_wrappers.c
+++ src/ftests/ftests_wrappers.c
@@ -31,7 +31,7 @@ PAPI_FCALL(papif_get_inherit, PAPIF_GET_INHERIT, (int *inherit, int *check))
 }
 #endif
 
-PAPI_FCALL(papif_get_granularity, PAPIf_GET_GRANULARITY, 
+PAPI_FCALL(papif_get_granularity, PAPIF_GET_GRANULARITY, 
            (int *eventset, int *granularity, int *mode, int *check))
 {
   PAPI_option_t g;
@@ -51,7 +51,7 @@ PAPI_FCALL(papif_get_granularity, PAPIf_GET_GRANULARITY,
   }
 }
 
-PAPI_FCALL(papif_get_domain, PAPIf_GET_DOMAIN, (int *eventset, int *domain, int *mode, int *check))
+PAPI_FCALL(papif_get_domain, PAPIF_GET_DOMAIN, (int *eventset, int *domain, int *mode, int *check))
 {
   PAPI_option_t d;
 
@@ -70,7 +70,7 @@ PAPI_FCALL(papif_get_domain, PAPIf_GET_DOMAIN, (int *eventset, int *domain, int 
   }
 }
 
-PAPI_FCALL(papif_get_exe_info, PAPIf_GET_EXE_INFO, (char *fullname, char *name, int *text_start, int *text_end, 
+PAPI_FCALL(papif_get_exe_info, PAPIF_GET_EXE_INFO, (char *fullname, char *name, int *text_start, int *text_end, 
            int *data_start, int *data_end, int *bss_start, int *bss_end, char *lib_preload_env, int *check))
 {
   PAPI_option_t e;
@@ -89,7 +89,7 @@ PAPI_FCALL(papif_get_exe_info, PAPIf_GET_EXE_INFO, (char *fullname, char *name, 
 }
 
 #if 0
-PAPI_FCALL(papif_set_inherit, PAPIf_SET_INHERIT, (int *inherit, int *check))
+PAPI_FCALL(papif_set_inherit, PAPIF_SET_INHERIT, (int *inherit, int *check))
 {
   PAPI_option_t i;
 
@@ -98,7 +98,7 @@ PAPI_FCALL(papif_set_inherit, PAPIf_SET_INHERIT, (int *inherit, int *check))
 }
 #endif
 
-PAPI_FCALL(papif_set_domain1, PAPIf_SET_DOMAIN1, (int *es, int *domain, int *check))
+PAPI_FCALL(papif_set_domain1, PAPIF_SET_DOMAIN1, (int *es, int *domain, int *check))
 {
   PAPI_option_t d;
 
@@ -107,7 +107,7 @@ PAPI_FCALL(papif_set_domain1, PAPIf_SET_DOMAIN1, (int *es, int *domain, int *che
   *check = PAPI_set_opt(PAPI_SET_DOMAIN, &d);
 }
 
-PAPI_FCALL(papif_profil, PAPIf_PROFIL, (unsigned short *buf, unsigned *bufsiz, unsigned long *offset, unsigned *scale, unsigned *eventset, 
+PAPI_FCALL(papif_profil, PAPIF_PROFIL, (unsigned short *buf, unsigned *bufsiz, unsigned long *offset, unsigned *scale, unsigned *eventset, 
            unsigned *eventcode, unsigned *threshold, unsigned *flags, unsigned *check))
 {
   *check = PAPI_profil(buf, *bufsiz, *offset, *scale, *eventset, *eventcode, *threshold, *flags);
@@ -116,4 +116,51 @@ PAPI_FCALL(papif_profil, PAPIf_PROFIL, (unsigned short *buf, unsigned *bufsiz, u
 PAPI_FCALL(papif_get_clockrate, PAPIF_GET_CLOCKRATE, (int *cr))
 {
   *cr = PAPI_get_opt(PAPI_GET_CLOCKRATE, NULL);
+}
+
+PAPI_FCALL(papif_query_event_verbose, PAPIF_QUERY_EVENT_VERBOSE, (unsigned int *EventCode, char* event_name, char *event_descr, char *event_label, int *avail, char *event_note, int *flags, unsigned *check))
+{
+  PAPI_preset_info_t info;
+  
+  if ((*check = PAPI_query_event_verbose(*EventCode, &info))==PAPI_OK){
+    strncpy(event_name, info.event_name, PAPI_MAX_STR_LEN);
+    strncpy(event_descr, info.event_descr, PAPI_MAX_STR_LEN);
+    strncpy(event_label, info.event_label, PAPI_MAX_STR_LEN);
+    *avail = info.avail;
+    strncpy(event_note, info.event_note, PAPI_MAX_STR_LEN);
+    *flags = info.flags;
+  }
+}
+
+PAPI_FCALL(papif_query_all_event_verbose, PAPIF_QUERY_ALL_EVENT_VERBOSE, (unsigned int *EventCode, char* event_name, char *event_descr, char *event_label, int *avail, char *event_note, int *flags, unsigned *check))
+{
+  const PAPI_preset_info_t *info;
+  int index;
+  
+  if((info = PAPI_query_all_events_verbose())==NULL){
+    *check = PAPI_EINVAL;
+    return;
+  }
+
+  index = *EventCode - 0x80000000;
+
+  if (!info[index].event_name) *check = PAPI_ENOEVNT;
+  else {
+    strncpy(event_name, info[index].event_name, PAPI_MAX_STR_LEN);
+    if(info[index].event_descr)
+      strncpy(event_descr, info[index].event_descr, PAPI_MAX_STR_LEN);
+    else
+      strncpy(event_descr, "",  PAPI_MAX_STR_LEN);
+    if(info[index].event_label)
+      strncpy(event_label, info[index].event_label, PAPI_MAX_STR_LEN);
+    else
+      strncpy(event_label, "",  PAPI_MAX_STR_LEN);
+    *avail = info[index].avail;
+    if(info[index].event_note)
+      strncpy(event_note, info[index].event_note, PAPI_MAX_STR_LEN);
+    else
+      strncpy(event_note, "",  PAPI_MAX_STR_LEN);
+    *flags = info[index].flags;
+    *check = PAPI_OK;
+  }
 }
