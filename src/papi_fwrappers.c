f0565cb5154de3bcc860bdd59194bf81415eff40
zlong <zlong>
2001-09-26 19:27:54 +0000

    Moved ftests/ftests_wrappers.c into papi_fwrappers.c
    -long

--- src/papi_fwrappers.c
+++ src/papi_fwrappers.c
@@ -391,3 +391,303 @@ PAPI_FCALL(papif_get_virt_cyc,PAPIF_GET_VIRT_CYC,(long_long *virt_cyc))
 {
   *virt_cyc = PAPI_get_virt_cyc();
 }
+
+
+#if defined ( _CRAYT3E )
+PAPI_FCALL(papif_get_preload, PAPIF_GET_PRELOAD, (_fcd lib_preload_env_fcd, int *check))
+#elif defined(_FORTRAN_STRLEN_AT_END)
+PAPI_FCALL(papif_get_preload, PAPIF_GET_PRELOAD, (char *lib_preload_env, int *check, int lib_preload_env_len))
+#else
+PAPI_FCALL(papif_get_preload, PAPIF_GET_PRELOAD, (char *lib_preload_env, int *check))
+#endif
+{
+  PAPI_option_t p;
+#if defined( _CRAYT3E ) || defined(_FORTRAN_STRLEN_AT_END)
+#if defined( _CRAYT3E )
+  int lib_preload_env_len=_fcdlen(lib_preload_env_fcd);
+  char *lib_preload_env=_fcdtocp(lib_preload_env_fcd);
+#endif
+  int i;
+
+  if ((*check = PAPI_get_opt(PAPI_GET_PRELOAD, &p))==PAPI_OK){
+    strncpy(lib_preload_env, p.preload.lib_preload_env, lib_preload_env_len);
+    for(i=strlen(p.preload.lib_preload_env); i<lib_preload_env_len; lib_preload_env[i++]=' ');
+  }
+#else
+  if ((*check = PAPI_get_opt(PAPI_GET_PRELOAD, &p))==PAPI_OK){
+    strncpy(lib_preload_env, p.preload.lib_preload_env, PAPI_MAX_STR_LEN);
+  }
+#endif
+}
+
+#if 0
+PAPI_FCALL(papif_get_inherit, PAPIF_GET_INHERIT, (int *inherit, int *check))
+{
+  PAPI_option_t i;
+
+  if ((*check = PAPI_get_opt(PAPI_GET_INHERIT, &i))==PAPI_OK){
+    *inherit = i.inherit.inherit;
+  }
+}
+#endif
+
+PAPI_FCALL(papif_get_granularity, PAPIF_GET_GRANULARITY, 
+           (int *eventset, int *granularity, int *mode, int *check))
+{
+  PAPI_option_t g;
+
+  if (*mode == PAPI_GET_DEFGRN){
+    *granularity = PAPI_get_opt(*mode, &g);
+    *check = PAPI_OK;
+  }
+  else if (*mode == PAPI_GET_GRANUL){
+    g.granularity.eventset = *eventset;
+    if ((*check = PAPI_get_opt(*mode, &g))==PAPI_OK){
+      *granularity = g.granularity.granularity;
+    }
+  }
+  else{
+    *check = PAPI_EINVAL;
+  }
+}
+
+PAPI_FCALL(papif_get_domain, PAPIF_GET_DOMAIN, (int *eventset, int *domain, int *mode, int *check))
+{
+  PAPI_option_t d;
+
+  if (*mode == PAPI_GET_DEFDOM){
+    *domain = PAPI_get_opt(*mode, NULL);
+    *check = PAPI_OK;
+  }
+  else if(*mode == PAPI_GET_DOMAIN){
+    d.domain.eventset = *eventset;
+    if ((*check = PAPI_get_opt(*mode, &d))==PAPI_OK){
+      *domain = d.domain.domain;
+    }
+  }
+  else{
+    *check = PAPI_EINVAL;
+  }
+}
+
+#if defined ( _CRAYT3E )
+PAPI_FCALL(papif_get_exe_info, PAPIF_GET_EXE_INFO, (_fcd fullname_fcd, _fcd name_fcd, int *text_start, int *text_end, 
+           int *data_start, int *data_end, int *bss_start, int *bss_end, _fcd lib_preload_env_fcd, int *check))
+#elif defined(_FORTRAN_STRLEN_AT_END)
+PAPI_FCALL(papif_get_exe_info, PAPIF_GET_EXE_INFO, (char *fullname, char *name, int *text_start, int *text_end, 
+           int *data_start, int *data_end, int *bss_start, int *bss_end, char *lib_preload_env, int *check,
+	   int fullname_len, int name_len, int lib_preload_env_len))
+#else
+PAPI_FCALL(papif_get_exe_info, PAPIF_GET_EXE_INFO, (char *fullname, char *name, int *text_start, int *text_end, 
+           int *data_start, int *data_end, int *bss_start, int *bss_end, char *lib_preload_env, int *check))
+#endif
+{
+  PAPI_option_t e;
+
+#if defined( _CRAYT3E ) || defined(_FORTRAN_STRLEN_AT_END)
+#if defined( _CRAYT3E )
+  int fullname_len=_fcdlen(fullname_fcd);
+  char *fullname=_fcdtocp(fullname_fcd);
+  int name_len=_fcdlen(name_fcd);
+  char *name=_fcdtocp(name_fcd);
+  int lib_preload_env_len=_fcdlen(lib_preload_env_fcd);
+  char *lib_preload_env=_fcdtocp(lib_preload_env_fcd);
+#endif
+  int i;
+  if ((*check = PAPI_get_opt(PAPI_GET_EXEINFO, &e))==PAPI_OK){
+    strncpy(fullname, e.exe_info->fullname, fullname_len);
+    for(i=strlen(e.exe_info->fullname);i<fullname_len;fullname[i++]=' ');
+    strncpy(name, e.exe_info->name, name_len);
+    for(i=strlen(e.exe_info->name);i<name_len;name[i++]=' ');
+    *text_start = (int)e.exe_info->text_start;
+    *text_end = (int)e.exe_info->text_end;
+    *data_start = (int)e.exe_info->data_start;
+    *data_end = (int)e.exe_info->data_end;
+    *bss_start = (int)e.exe_info->bss_start;
+    *bss_end = (int)e.exe_info->bss_end;
+    strncpy(lib_preload_env, e.exe_info->lib_preload_env, lib_preload_env_len);
+    for(i=strlen(e.exe_info->lib_preload_env);i<lib_preload_env_len;lib_preload_env[i++]=' ');
+  }
+#else
+  if ((*check = PAPI_get_opt(PAPI_GET_EXEINFO, &e))==PAPI_OK){
+    strncpy(fullname, e.exe_info->fullname, PAPI_MAX_STR_LEN);
+    strncpy(name, e.exe_info->name, PAPI_MAX_STR_LEN);
+    *text_start = (int)e.exe_info->text_start;
+    *text_end = (int)e.exe_info->text_end;
+    *data_start = (int)e.exe_info->data_start;
+    *data_end = (int)e.exe_info->data_end;
+    *bss_start = (int)e.exe_info->bss_start;
+    *bss_end = (int)e.exe_info->bss_end;
+    strncpy(lib_preload_env, e.exe_info->lib_preload_env, PAPI_MAX_STR_LEN);
+  }
+#endif
+}
+
+#if 0
+PAPI_FCALL(papif_set_inherit, PAPIF_SET_INHERIT, (int *inherit, int *check))
+{
+  PAPI_option_t i;
+
+  i.inherit.inherit = *inherit;
+  *check = PAPI_set_opt(PAPI_SET_INHERIT, &i);
+}
+#endif
+
+PAPI_FCALL(papif_set_domain1, PAPIF_SET_DOMAIN1, (int *es, int *domain, int *check))
+{
+  PAPI_option_t d;
+
+  d.domain.domain = *domain;
+  d.domain.eventset = *es;
+  *check = PAPI_set_opt(PAPI_SET_DOMAIN, &d);
+}
+
+PAPI_FCALL(papif_profil, PAPIF_PROFIL, (unsigned short *buf, unsigned *bufsiz, unsigned long *offset, unsigned *scale, unsigned *eventset, 
+           unsigned *eventcode, unsigned *threshold, unsigned *flags, unsigned *check))
+{
+  *check = PAPI_profil(buf, *bufsiz, *offset, *scale, *eventset, *eventcode, *threshold, *flags);
+}
+
+PAPI_FCALL(papif_get_clockrate, PAPIF_GET_CLOCKRATE, (int *cr))
+{
+  *cr = PAPI_get_opt(PAPI_GET_CLOCKRATE, NULL);
+}
+
+#if defined ( _CRAYT3E )
+PAPI_FCALL(papif_query_event_verbose, PAPIF_QUERY_EVENT_VERBOSE, (unsigned int *EventCode, _fcd event_name_fcd, _fcd event_descr_fcd, _fcd event_label_fcd, int *avail, _fcd event_note_fcd, int *flags, unsigned *check))
+#elif defined(_FORTRAN_STRLEN_AT_END)
+PAPI_FCALL(papif_query_event_verbose, PAPIF_QUERY_EVENT_VERBOSE, (unsigned int *EventCode, char* event_name, char *event_descr, char *event_label, int *avail, char *event_note, int *flags, unsigned *check, int event_name_len, int event_descr_len, int event_label_len, int event_note_len))
+#else
+PAPI_FCALL(papif_query_event_verbose, PAPIF_QUERY_EVENT_VERBOSE, (unsigned int *EventCode, char* event_name, char *event_descr, char *event_label, int *avail, char *event_note, int *flags, unsigned *check))
+#endif
+{
+  PAPI_preset_info_t info;
+  
+#if defined( _CRAYT3E )
+  int event_name_len=_fcdlen(event_name_fcd);
+  char *event_name=_fcdtocp(event_name_fcd);
+  int event_descr_len=_fcdlen(event_descr_fcd);
+  char *event_descr=_fcdtocp(event_descr_fcd);
+  int event_label_len=_fcdlen(event_label_fcd);
+  char *event_label=_fcdtocp(event_label_fcd);
+  int event_note_len=_fcdlen(event_note_fcd);
+  char *event_note =_fcdtocp(event_note_fcd);
+#endif
+#if defined( _CRAYT3E ) || defined(_FORTRAN_STRLEN_AT_END)
+  int i;
+#endif
+
+#if defined( _CRAYT3E ) || defined(_FORTRAN_STRLEN_AT_END)
+  if ((*check = PAPI_query_event_verbose(*EventCode, &info))==PAPI_OK){
+    strncpy(event_name, info.event_name, event_name_len);
+    for(i=strlen(info.event_name);i<event_name_len;event_name[i++]=' ');
+    strncpy(event_descr, info.event_descr, event_descr_len);
+    for(i=strlen(info.event_descr);i<event_descr_len;event_descr[i++]=' ');
+    strncpy(event_label, info.event_label, event_label_len);
+    for(i=strlen(info.event_label);i<event_label_len;event_label[i++]=' ');
+    *avail = info.avail;
+    strncpy(event_note, info.event_note, event_note_len);
+    for(i=strlen(info.event_note);i<event_note_len;event_note[i++]=' ');
+    *flags = info.flags;
+  }
+#else
+  if ((*check = PAPI_query_event_verbose(*EventCode, &info))==PAPI_OK){
+    strncpy(event_name, info.event_name, PAPI_MAX_STR_LEN);
+    strncpy(event_descr, info.event_descr, PAPI_MAX_STR_LEN);
+    strncpy(event_label, info.event_label, PAPI_MAX_STR_LEN);
+    *avail = info.avail;
+    strncpy(event_note, info.event_note, PAPI_MAX_STR_LEN);
+    *flags = info.flags;
+  }
+#endif
+}
+
+#if defined ( _CRAYT3E )
+PAPI_FCALL(papif_query_all_event_verbose, PAPIF_QUERY_ALL_EVENT_VERBOSE, (unsigned int *EventCode, _fcd event_name_fcd, _fcd event_descr_fcd, _fcd event_label_fcd, int *avail, _fcd event_note_fcd, int *flags, unsigned *check))
+#elif defined(_FORTRAN_STRLEN_AT_END)
+PAPI_FCALL(papif_query_all_event_verbose, PAPIF_QUERY_ALL_EVENT_VERBOSE, (unsigned int *EventCode, char* event_name, char *event_descr, char *event_label, int *avail, char *event_note, int *flags, unsigned *check, int event_name_len, int event_descr_len, int event_label_len, int event_note_len))
+#else
+PAPI_FCALL(papif_query_all_event_verbose, PAPIF_QUERY_ALL_EVENT_VERBOSE, (unsigned int *EventCode, char* event_name, char *event_descr, char *event_label, int *avail, char *event_note, int *flags, unsigned *check))
+#endif
+{
+  const PAPI_preset_info_t *info;
+  int index;
+
+#if defined( _CRAYT3E )
+  int event_name_len=_fcdlen(event_name_fcd);
+  char *event_name=_fcdtocp(event_name_fcd);
+  int event_descr_len=_fcdlen(event_descr_fcd);
+  char *event_descr=_fcdtocp(event_descr_fcd);
+  int event_label_len=_fcdlen(event_label_fcd);
+  char *event_label=_fcdtocp(event_label_fcd);
+  int event_note_len=_fcdlen(event_note_fcd);
+  char *event_note =_fcdtocp(event_note_fcd);
+#endif
+#if defined( _CRAYT3E ) || defined(_FORTRAN_STRLEN_AT_END)
+  int i;
+#endif
+
+  if((info = PAPI_query_all_events_verbose())==NULL){
+    *check = PAPI_EINVAL;
+    return;
+  }
+
+  index = *EventCode - 0x80000000;
+
+#if defined( _CRAYT3E ) || defined(_FORTRAN_STRLEN_AT_END)
+  if (!info[index].event_name) *check = PAPI_ENOEVNT;
+  else {
+    strncpy(event_name, info[index].event_name, event_name_len);
+    for(i=strlen(info[index].event_name);i<event_name_len;event_name[i++]=' ');
+
+    if(info[index].event_descr){
+      strncpy(event_descr, info[index].event_descr, event_descr_len);
+      for(i=strlen(info[index].event_descr);i<event_descr_len;event_descr[i++]=' ');
+    }
+    else{
+      for(i=0;i<event_descr_len;event_descr[i++]=' ');
+    }
+
+    if(info[index].event_label){
+      strncpy(event_label, info[index].event_label, event_label_len);
+      for(i=strlen(info[index].event_label);i<event_label_len;event_label[i++]=' ');
+    }
+    else{
+      for(i=0;i<event_label_len;event_label[i++]=' ');
+    }
+
+    *avail = info[index].avail;
+
+    if(info[index].event_note){
+      strncpy(event_note, info[index].event_note, event_note_len);
+      for(i=strlen(info[index].event_note);i<event_note_len;event_note[i++]=' ');
+    }
+    else{
+      for(i=0;i<event_note_len;event_note[i++]=' ');
+    }
+
+    *flags = info[index].flags;
+    *check = PAPI_OK;
+  }
+#else
+  if (!info[index].event_name) *check = PAPI_ENOEVNT;
+  else {
+    strncpy(event_name, info[index].event_name, PAPI_MAX_STR_LEN);
+    if(info[index].event_descr)
+      strncpy(event_descr, info[index].event_descr, PAPI_MAX_STR_LEN);
+    else
+      strncpy(event_descr, "",  PAPI_MAX_STR_LEN);
+    if(info[index].event_label)
+      strncpy(event_label, info[index].event_label, PAPI_MAX_STR_LEN);
+    else
+      strncpy(event_label, "",  PAPI_MAX_STR_LEN);
+    *avail = info[index].avail;
+    if(info[index].event_note)
+      strncpy(event_note, info[index].event_note, PAPI_MAX_STR_LEN);
+    else
+      strncpy(event_note, "",  PAPI_MAX_STR_LEN);
+    *flags = info[index].flags;
+    *check = PAPI_OK;
+  }
+#endif
+}
