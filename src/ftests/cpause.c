384f636b422bb48913b31725ea988b957ddc2227
Dan Terpstra <terpstra@eecs.utk.edu>
2001-11-06 15:53:35 +0000

    Parameter type corrections on strtest; Mods to ftests_util to allow Windows to pause for a keypress before exiting; Addition of cpause.c for Windows only, to implement the pause on exit.

---
+++ src/ftests/cpause.c
@@ -0,0 +1,18 @@
+#include <windows.h>
+#include <stdio.h>
+
+void CPAUSE(void)
+{
+	HANDLE hStdIn;
+	BOOL bSuccess;
+	INPUT_RECORD inputBuffer;
+	DWORD dwInputEvents; /* number of events actually read */
+	
+		printf("Press any key to continue...\n");
+		hStdIn = GetStdHandle(STD_INPUT_HANDLE);
+		do { bSuccess = ReadConsoleInput(hStdIn, &inputBuffer, 
+			1, &dwInputEvents);
+		} while (!(inputBuffer.EventType == KEY_EVENT &&
+ 			inputBuffer.Event.KeyEvent.bKeyDown));
+}
+
