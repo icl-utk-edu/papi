4700e5ec92035947b77639a85cbf6266297cc8e9
Dan Terpstra <terpstra@eecs.utk.edu>
2002-01-10 21:17:30 +0000

    Changes to handle native event initialization on AIX.

---
+++ src/ftests/addAIXevents.c
@@ -0,0 +1,59 @@
+/*
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <memory.h>
+#include <malloc.h>
+#include "papiStdEventDefs.h"
+#include "papi.h"
+#include "test_utils.h"
+*/
+
+#include "papiStdEventDefs.h"
+#include "papi.h"
+
+void addaixevents(int *EventSet, int *retval)
+{
+  int native;
+
+  if (*EventSet == PAPI_NULL) {
+	  *retval = PAPI_ENOEVST;
+	  return;
+  }
+
+#if defined(_AIX)
+      native = 0 | 5 << 8  | 0; /* ICM */
+      if ( (*retval = PAPI_add_event(EventSet, native))!=PAPI_OK) return;
+      native = 0 | 35 << 8 | 1; /* FPU1CMPL */
+      if ( (*retval = PAPI_add_event(EventSet, native))!=PAPI_OK) return;
+      native = 0 | 5 << 8  | 2; /* LDCM */
+      if ( (*retval = PAPI_add_event(EventSet, native))!=PAPI_OK) return;
+      native = 0 | 5 << 8  | 3; /* LDCMPL */
+      if ( (*retval = PAPI_add_event(EventSet, native))!=PAPI_OK) return;
+      native = 0 | 5 << 8  | 4; /* FPU0CMPL */
+      if ( (*retval = PAPI_add_event(EventSet, native))!=PAPI_OK) return;
+      native = 0 | 12 << 8 | 5; /* CYC */
+      if ( (*retval = PAPI_add_event(EventSet, native))!=PAPI_OK) return;
+      native = 0 | 9 << 8  | 6; /* FMA */
+      if ( (*retval = PAPI_add_event(EventSet, native))!=PAPI_OK) return;
+      native = 0 | 0 << 8  | 7; /* TLB */
+      if ( (*retval = PAPI_add_event(EventSet, native))!=PAPI_OK) return;
+#endif
+}
+
+void ADDAIXEVENTS(int *EventSet, int *retval)
+{
+	addaixevents(EventSet, retval);
+}
+
+void addaixevents_(int *EventSet, int *retval)
+{
+	addaixevents(EventSet, retval);
+}
+
+void addaixevents__(int *EventSet, int *retval)
+{
+	addaixevents(EventSet, retval);
+}
