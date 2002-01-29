3452598de33a15ce79747a5a0422424c6b428bee
Shirley Moore <shirley@eecs.utk.edu>
2002-01-29 15:41:53 +0000

    bug fix, second PAPI_TOT_IIS should be added to CostEventSet

--- src/utils/cost.c
+++ src/utils/cost.c
@@ -48,7 +48,7 @@ int main(int argc, char **argv)
 	test_fail(__FILE__,__LINE__,"PAPI_add_event", retval );
 
    if ((retval = PAPI_add_event(&CostEventSet, PAPI_TOT_INS)) != PAPI_OK)
-      if ((retval = PAPI_add_event(&EventSet, PAPI_TOT_IIS)) != PAPI_OK)
+      if ((retval = PAPI_add_event(&CostEventSet, PAPI_TOT_IIS)) != PAPI_OK)
 	test_fail(__FILE__,__LINE__,"PAPI_add_event", retval );
 
    /* Make sure no errors */
