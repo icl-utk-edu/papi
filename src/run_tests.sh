8ea900f88a295844840f314a76180426e0bb9c14
Nils Smeds <nils.smeds@gmail.com>
2002-02-21 19:35:26 +0000

    Added the possibility to use the command line option -v to run_tests.sh to run the tests in verbose mode. Also added a clause to skip the long-running test to overflow the counter (+30min)

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -7,6 +7,12 @@
 # Mods:    Kevin London
 #          london@cs.utk.edu
 
+if [ "$1" = "-v" ]; then
+  shift ; TESTS_QUIET=""
+else
+  TESTS_QUIET="TESTS_QUIET"
+fi
+
 CTESTS=`find tests -perm -u+x -type f`;
 FTESTS=`find ftests -perm -u+x -type f`;
 ALLTESTS="$FTESTS $CTESTS";
@@ -20,8 +26,12 @@ echo "Running C Tests\n";
 for i in $CTESTS;
 do
 if [ -x $i ]; then
+if [ "$i" = "tests/timer_overflow" ]; then
+  echo Skipping test $i, it takes too long...
+else
 echo -n "Running $i: ";
-./$i TESTS_QUIET
+./$i $TESTS_QUIET
+fi;
 fi;
 done
 
@@ -30,6 +40,6 @@ for i in $FTESTS;
 do
 if [ -x $i ]; then
 echo -n "Running $i: ";
-./$i TESTS_QUIET
+./$i $TESTS_QUIET
 fi;
 done
