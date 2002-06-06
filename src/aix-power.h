0bf4d530b9c8b80e4e195ef48f3a810372660dc2
Dan Terpstra <terpstra@eecs.utk.edu>
2002-06-06 17:39:32 +0000

    New Power3/Power4/AIX4/AIX5 combined code base! - Fixed a bug in Power3 code that caused a crash for native events; - Patched the event names to support AIX 5 changes on Power3; - Changed directory pathing for AIX 5; - Merged code support for Power4 into h and c files:   - several routines are now conditionally compiled for P3 vs. P4; - Added 2 new make files for aix5-power3 and for power4. If someone more knowledgable than me wants to merge these make files, do so with my full blessing! Note that the _POWER4 define is required for conditional compiliation on POWER4. IBM may provide a system define, but I couldn't find one. Also, the P4 event table is still very incomplete. Extra help here would also be most welcome.

--- src/aix-power.h
+++ src/aix-power.h
@@ -13,6 +13,9 @@
 
 #include "pmapi.h"
 #define POWER_MAX_COUNTERS MAX_COUNTERS
+#define GROUP_INTS 2
+#define MAX_GROUPS (GROUP_INTS * 32)
+#define INVALID_EVENT -2
 
 #include "papi.h"
 
@@ -34,17 +37,33 @@ typedef struct hwd_control_state {
 
 typedef struct hwd_preset {
   /* Is this event derived? */
-  unsigned char derived;   
+  unsigned char derived;
+  
+#ifdef _POWER4
+  /* How many metrics? */
+  unsigned char metric_count;
+  /* Bits encode the groups this event lives in */
+  unsigned int gps[GROUP_INTS];  
+#else
   /* Which counters can be used? Bits encode counters available
       Separate selectors for each metric in a derived event;
       Rank determines how many counters carry each metric */
   unsigned char selector[POWER_MAX_COUNTERS];  
-  unsigned char rank[POWER_MAX_COUNTERS];  
+  unsigned char rank[POWER_MAX_COUNTERS];
+#endif
+
   /* Buffers containing counter cmds for each possible metric */
   unsigned char counter_cmd[POWER_MAX_COUNTERS][POWER_MAX_COUNTERS];
   /* If it exists, then this is the description of this event */
   char note[PAPI_MAX_STR_LEN];
 } hwd_preset_t;
+
+typedef struct hwd_groups {
+  /* group number from the pmapi pm_groups_t struct */
+  int group_id;
+  /* Buffer containing counter cmds for this group */
+  unsigned char counter_cmd[POWER_MAX_COUNTERS];
+} hwd_groups_t;
 
 typedef struct pmapi_search {
   /* Preset code */
