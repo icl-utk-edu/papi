229eb70966c12a2d0e20781a30530acee3d1a5c9
Philip Mucci <mucci@eecs.utk.edu>
2002-09-10 21:04:27 +0000

    Added -a option for printing available eventsets suitable for parsing

--- src/ctests/avail.c
+++ src/ctests/avail.c
@@ -8,10 +8,17 @@ int main(int argc, char **argv)
 {
   int i;
   int retval;
+  int print_avail_only = 0;
   const PAPI_preset_info_t *info = NULL;
   const PAPI_hw_info_t *hwinfo = NULL;
-
+  
   tests_quiet(argc, argv); /* Set TESTS_QUIET variable */
+  for(i=0;i<argc;i++)
+    if (argv[i])
+      {
+	if (strstr(argv[i],"-a"))
+	  print_avail_only = 1;
+      }
 
   retval = PAPI_library_init(PAPI_VER_CURRENT);
   if ( retval != PAPI_VER_CURRENT)  test_fail(__FILE__, __LINE__, "PAPI_library_init", retval);
@@ -33,23 +40,47 @@ int main(int argc, char **argv)
 	printf("-------------------------------------------------------------------------\n");
 	printf("Vendor string and code   : %s (%d)\n",hwinfo->vendor_string,hwinfo->vendor);
 	printf("Model string and code    : %s (%d)\n",hwinfo->model_string,hwinfo->model);
-	printf("CPU revision             : %f\n",hwinfo->revision);
+	printf("CPU Revision             : %f\n",hwinfo->revision);
 	printf("CPU Megahertz            : %f\n",hwinfo->mhz);
-	printf("CPU's in an SMP node     : %d\n",hwinfo->ncpu);
-	printf("Nodes in the system      : %d\n",hwinfo->nnodes);
-	printf("Total CPU's in the system: %d\n",hwinfo->totalcpus);
+	printf("CPU's in this Node       : %d\n",hwinfo->ncpu);
+	printf("Nodes in this System     : %d\n",hwinfo->nnodes);
+	printf("Total CPU's              : %d\n",hwinfo->totalcpus);
+	printf("Number Hardware Counters : %d\n",PAPI_get_opt(PAPI_GET_MAX_HWCTRS,NULL));
+	printf("Max Multiple Counters    : %d\n",PAPI_MPX_DEF_DEG);
 	printf("-------------------------------------------------------------------------\n");
+
+	if (print_avail_only == 0)
+	  {
 	printf("Name\t\tCode\t\tAvail\tDeriv\tDescription (Note)\n");
+
 	for (i=0;i<PAPI_MAX_PRESET_EVENTS;i++)
-	if (info[i].event_name)
-	  printf("%s\t0x%x\t%s\t%s\t%s (%s)\n",
-		 info[i].event_name,
-		 info[i].event_code,
-		 (info[i].avail ? "Yes" : "No"),
-		 (info[i].flags & PAPI_DERIVED ? "Yes" : "No"),
-		 info[i].event_descr,
-		 (info[i].event_note ? info[i].event_note : ""));
+	  if (info[i].event_name)
+	    {
+	      printf("%s\t0x%x\t%s\t%s\t%s (%s)\n",
+		     info[i].event_name,
+		     info[i].event_code,
+		     (info[i].avail ? "Yes" : "No"),
+		     (info[i].flags & PAPI_DERIVED ? "Yes" : "No"),
+		     info[i].event_descr,
+		     (info[i].event_note ? info[i].event_note : ""));
+	    }
 	printf("-------------------------------------------------------------------------\n");
+	  }
+	else
+	  {
+	printf("Name\t\tDerived\tDescription (Mgr. Note)\n");
+
+	for (i=0;i<PAPI_MAX_PRESET_EVENTS;i++)
+	  if ((info[i].event_name) && (info[i].avail))
+	    {
+	      printf("%s\t%s\t%s (%s)\n",
+		     info[i].event_name,
+		     (info[i].flags & PAPI_DERIVED ? "Yes" : "No"),
+		     info[i].event_descr,
+		     (info[i].event_note ? info[i].event_note : ""));
+	    }
+	exit(0);
+	  }
   }
   test_pass(__FILE__, NULL, 0);
   exit(1);
