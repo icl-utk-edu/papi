34f25455eef8602c2c3f9468d351e29f789007bc
Shirley Moore <shirley@eecs.utk.edu>
2002-09-25 20:13:32 +0000

    Add back previous changes.

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -7,6 +7,12 @@
 # Mods:    Kevin London
 #          london@cs.utk.edu
 
+if [ "$1" = "-v" ]; then
+  shift ; TESTS_QUIET=""
+else
+  TESTS_QUIET="TESTS_QUIET"
+fi
+
 CTESTS=`find tests -perm -u+x -type f`;
 FTESTS=`find ftests -perm -u+x -type f`;
 ALLTESTS="$FTESTS $CTESTS";
@@ -21,8 +27,12 @@ echo "Running C Tests\n";
 for i in $CTESTS;
 do
 if [ -x $i ]; then
+if [ "$i" = "tests/timer_overflow" ]; then
+  echo Skipping test $i, it takes too long...
+else
 echo -n "Running $i: ";
 $CWD/$i TESTS_QUIET
+fi;
 fi;
 done
 
