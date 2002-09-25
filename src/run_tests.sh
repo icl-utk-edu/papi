958e897e5a93dc721f916e55414045639d12d8a7
Shirley Moore <shirley@eecs.utk.edu>
2002-09-25 19:38:52 +0000

    added full pathname for test executables (needed by DADD on Alpha)

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -7,16 +7,11 @@
 # Mods:    Kevin London
 #          london@cs.utk.edu
 
-if [ "$1" = "-v" ]; then
-  shift ; TESTS_QUIET=""
-else
-  TESTS_QUIET="TESTS_QUIET"
-fi
-
 CTESTS=`find tests -perm -u+x -type f`;
 FTESTS=`find ftests -perm -u+x -type f`;
 ALLTESTS="$FTESTS $CTESTS";
 x=0;
+CWD=`pwd`
 
 echo "The following test cases will be run";
 echo $ALLTESTS;
@@ -26,12 +21,8 @@ echo "Running C Tests\n";
 for i in $CTESTS;
 do
 if [ -x $i ]; then
-if [ "$i" = "tests/timer_overflow" ]; then
-  echo Skipping test $i, it takes too long...
-else
 echo -n "Running $i: ";
-./$i $TESTS_QUIET
-fi;
+$CWD/$i TESTS_QUIET
 fi;
 done
 
@@ -40,6 +31,6 @@ for i in $FTESTS;
 do
 if [ -x $i ]; then
 echo -n "Running $i: ";
-./$i $TESTS_QUIET
+$CWD/$i TESTS_QUIET
 fi;
 done
