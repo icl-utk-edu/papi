e452ba9f034b9859773e2c7203e7f13ece5f2df6
london <london>
2002-10-23 17:05:29 +0000

    Extended range on error checking for total cycles

--- src/ctests/overflow.c
+++ src/ctests/overflow.c
@@ -150,8 +150,8 @@ int main(int argc, char **argv)
 		test_fail(__FILE__, __LINE__, event_name, 1);
   }
 #ifndef _CRAYT3E
-  min = (long_long)(values[0][0]*.9);
-  max = (long_long)(values[0][0]*1.1);
+  min = (long_long)(values[0][0]*.8);
+  max = (long_long)(values[0][0]*1.2);
   if ( values[1][0] > max || values[1][0] < min )
   	test_fail(__FILE__, __LINE__, "PAPI_TOT_CYC", 1);
 #endif
