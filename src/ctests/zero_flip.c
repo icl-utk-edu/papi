94c8ec6d600d661bb184bc13ec90cd7495db2475
london <london>
2002-11-13 21:10:21 +0000

    Changed events from TOT_INS & FP_INS to FP_INS/TOT_CYC or TOT_INS/TOT_CYC because p4 can't handle TOT_INS & FP_INS yet......

--- src/ctests/zero_flip.c
+++ src/ctests/zero_flip.c
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
 
   /* query and set up the right instruction to monitor */
   if (PAPI_query_event(PAPI_FP_INS) == PAPI_OK) PAPI_event = PAPI_FP_INS;
-  else PAPI_event = PAPI_TOT_CYC;
+  else PAPI_event = PAPI_TOT_INS;
 
   retval = PAPI_event_code_to_name(PAPI_event, event_name);
   if (retval != PAPI_OK) test_fail(__FILE__, __LINE__, "PAPI_event_code_to_name", retval);
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
   retval = PAPI_add_event(&EventSet1,PAPI_event);
   if (retval != PAPI_OK) test_fail(__FILE__, __LINE__, "PAPI_add_event", retval);
 
-  retval = PAPI_add_event(&EventSet1,PAPI_TOT_INS);
+  retval = PAPI_add_event(&EventSet1,PAPI_TOT_CYC);
   if (retval != PAPI_OK) test_fail(__FILE__, __LINE__, "PAPI_add_event", retval);
 
   /* Add them reversed to EventSet2 */
