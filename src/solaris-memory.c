bd8c2c5c376b58c0f1444c97606633819b8173af
london <london>
2003-01-31 18:42:40 +0000

    Added support for dynamic memory information for solaris systems. It is possible that this will break on some solaris systems if it doesn't follow the /proc filesystem that is like linux.  All, our systems have this, so hopefully this won't be a problem.  In addition since I read in a binary file with sizeof(struct psinfo), if you compile this on one system and move it to a system with a slightly different psinfo structure, the results will be undefined (IE most likely crash and burn :) )

--- src/solaris-memory.c
+++ src/solaris-memory.c
@@ -1,4 +1,7 @@
 #include "papi.h"
+#include <stdio.h>
+#include <procfs.h>
+#include <fcntl.h>
 
 int get_memory_info( PAPI_mem_info_t * mem_info ){
    int retval = 0;
@@ -6,4 +9,51 @@ int get_memory_info( PAPI_mem_info_t * mem_info ){
 }
 
 long _papi_hwd_get_dmem_info(int option){
+   FILE *fd;
+   struct psinfo psi;
+   long pgsz=getpagesize();
+
+   if ((fd=fopen("/proc/self/psinfo","r"))==NULL) {
+	DBG((stderr,"PAPI_get_dmem_info can't open /proc/self\n"));
+	return(PAPI_ESYS);
+   }
+ 
+   fread((void *) &psi, sizeof(struct psinfo), 1, fd);
+   fclose(fd);
+
+   switch(option){
+     case PAPI_GET_RESSIZE:
+        return(((1024*psi.pr_size)/pgsz));
+     case PAPI_GET_SIZE:
+        return(((1024*psi.pr_rssize)/pgsz));
+     default:
+        return(PAPI_EINVAL);
+    }
+
+/*  Depending on OS we may need this, so going to leave
+ *  the code here for now. -KSL
+   pid_t pid = getpid();
+   psinfo_t info;
+   char pfile[256];
+   long pgsz=getpagesize();
+   int fd;
+
+   sprintf(pfile, "/proc/%05d", pid);
+   if((fd=open(pfile,O_RDONLY)) <0 ) {
+        DBG((stderr,"PAPI_get_dmem_info can't open /proc/%d\n",pid));
+        return(PAPI_ESYS);
+   }
+   if(ioctl(fd, PIOCPSINFO,  &info)<0){
+        return(PAPI_ESYS);
+   }
+   close(fd);
+ switch(option){
+   case PAPI_GET_RESSIZE:
+        return(((1024*info.pr_rssize)/pgsz));
+   case PAPI_GET_SIZE:
+        return(((1024*info.pr_size)/pgsz));
+   default:
+        return(PAPI_EINVAL);
+  }
+  */
 }
