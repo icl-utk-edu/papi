14f77399c07ff20aec45e27562a695c52a0ab75c
Haihang You <you@eecs.utk.edu>
2003-02-12 20:32:13 +0000

    allocate.* contain our new counter allocation functions. remove those functions from aix-power.c, and did some modification in aix-power.h and Makefile.inc to make it compile as well

--- src/aix-power.h
+++ src/aix-power.h
@@ -122,3 +122,9 @@ extern _edata;
 extern _end;
 extern _data;
 extern int (*thread_kill_fn)(int, int);
+
+/* globals */
+pm_info_t pminfo;
+hwd_preset_t preset_map[PAPI_MAX_PRESET_EVENTS] = { 0 };
+
+#include "allocate.h"
