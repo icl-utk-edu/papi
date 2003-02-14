0c3e28f90acd067192cdcad39c88cfc6fe106055
london <london>
2003-02-14 20:14:05 +0000

    EV7 was not being determined correctly, and missing a function. Thanks Paul.

--- src/dadd-alpha.c
+++ src/dadd-alpha.c
@@ -152,7 +152,7 @@ static int get_system_info(void)
       strcat(_papi_system_info.hw_info.model_string,"21164");
       _papi_system_info.num_cntrs = 47;
     }
-  else if (family == 4)
+  else if (family == 3) /* EV7, thanks Paul */
     {
       strcat(_papi_system_info.hw_info.model_string,"21364");
       _papi_system_info.num_cntrs = 47;
@@ -456,11 +456,19 @@ int _papi_hwd_set_overflow(EventSetInfo *ESI, EventSetOverflowInfo_t *overflow_o
   return(PAPI_ESBSTR);
 }
 
-int _papi_hwd_set_profile(EventSetInfo *ESI, EventSetProfileInfo_t *profile_option)
+int _papi_hwd_set_profile(EventSetInfo *ESI, EventSetProfileInfo_t *overflow_opt
+ion)
 {
   /* This function is not used and shouldn't be called. */
 
-  return(PAPI_ESBSTR);
+  abort();
+}
+
+int _papi_hwd_stop_profiling(EventSetInfo *ESI, EventSetInfo *master)
+{
+  /* This function is not used and shouldn't be called. */
+
+  abort();
 }
 
 void *_papi_hwd_get_overflow_address(void *context)
