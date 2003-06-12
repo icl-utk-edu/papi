83ac0516417cff56a0c36a03d56bdb5b1ee763c2
Haihang You <you@eecs.utk.edu>
2003-06-12 14:17:43 +0000

    This file will get compiled and run at compile time. It will generate native.h and native.c files which contain native events info. Currently it only support power3 and power4.

---
+++ src/nativegen.c
@@ -0,0 +1,215 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "pmapi.h"
+/*#include "papi.h"*/
+
+#define MAX_NATIVE_EVENT 256
+#define MAX_COUNTERS 8
+#define NATIVE_MASK 0x40000000
+#define PAPI_MAX_STR_LEN 129
+#define GROUP_INTS 2
+
+typedef struct native_event_entry{
+  /* assigned index for the event */
+  /*unsigned char index;*/
+  /* indicate which counters this event can live on */
+  unsigned int selector;
+  /* Buffers containing counter cmds for each possible metric */
+  int counter_cmd[MAX_COUNTERS];
+  /* If it exists, then this is the name of this event */
+  char name[PAPI_MAX_STR_LEN];
+#ifdef _POWER4
+  /* which group this event belongs */
+  unsigned int group[GROUP_INTS];
+#endif
+  /* If it exists, then this is the description of this event */
+  char *description;
+} native_event_entry_t;  
+
+/* globals */
+native_event_entry_t native_table[MAX_NATIVE_EVENT] = { 0 };
+pm_info_t pminfo;
+#ifdef _AIXVERSION_510
+  pm_groups_info_t pmgroups;
+#endif
+
+/* to initialize the native_table */
+void initialize_native_table()
+{
+  int i, j;
+  
+  for(i=0;i<MAX_NATIVE_EVENT;i++){
+	for(j=0;j<MAX_COUNTERS;j++)
+		native_table[i].counter_cmd[j]=-1;
+  }
+}
+
+#ifdef _POWER4
+/* to setup native_table group value */
+void setup_gps(int total)
+{
+  int i, j, gnum;
+  
+  for(i=0;i<total;i++){
+  	for(j=0;j<MAX_COUNTERS;j++){
+		if(native_table[i].selector & (1<<j)){
+			for(gnum=0;gnum<pmgroups.maxgroups;gnum++){
+  				if(native_table[i].counter_cmd[j]==pmgroups.event_groups[gnum].events[j]){
+					/* could use gnum instead of pmgroups.event_groups[gnum].group_id */
+					native_table[i].group[pmgroups.event_groups[gnum].group_id/32] |= \
+				   	  1<<(pmgroups.event_groups[gnum].group_id%32);
+				}
+			}
+		}
+	}
+  }
+}
+#endif
+
+/* to setup native_table values, and return number of entries */
+int setup_native_table()
+{
+  pm_events_t *wevp;
+  pm_info_t *info;
+  int pmc, ev, i, index;
+  
+  info=&pminfo;
+  index=0;
+  for (pmc = 0; pmc < info->maxpmcs; pmc++){
+  	wevp = info->list_events[pmc];
+  	for (ev = 0; ev < info->maxevents[pmc]; ev++, wevp++){
+ 		for(i=0;i<index;i++){
+			if (strcmp(wevp->short_name, native_table[i].name) == 0){
+				native_table[i].selector |= 1<<pmc;
+				native_table[i].counter_cmd[pmc]=wevp->event_id;
+				break;
+			}
+		}
+		if(i==index){
+			/*native_table[i].index=i;*/
+			native_table[i].selector |= 1<<pmc;
+			native_table[i].counter_cmd[pmc]=wevp->event_id;
+			strcpy(native_table[i].name, wevp->short_name);
+			native_table[i].description=strdup(wevp->description);
+			index++;
+		}
+	}
+  }
+
+#ifdef _POWER4
+  setup_gps(index);
+#endif
+
+  return index;
+}  
+
+void main()
+{
+  FILE *fp[2];
+  int retval, total, i, j, k;
+  char *str, *tmp;
+
+#ifdef _POWER4
+  #define PM_INIT_FLAGS PM_VERIFIED|PM_UNVERIFIED|PM_CAVEAT|PM_GET_GROUPS
+#else
+  #define PM_INIT_FLAGS PM_VERIFIED|PM_UNVERIFIED|PM_CAVEAT
+#endif
+
+/* get counter information from system*/
+#ifdef _AIXVERSION_510
+  /*DBG((stderr,"Calling AIX 5 version of pm_init...\n"));*/
+  retval = pm_init(PM_INIT_FLAGS, &pminfo, &pmgroups);
+#else
+  /*DBG((stderr,"Calling AIX 4 version of pm_init...\n"));*/
+  retval = pm_init(PM_INIT_FLAGS,&pminfo);
+#endif
+  
+  fp[0]=fopen("native.h", "w");
+  if(fp[0]==NULL){
+  	perror("open");
+	exit(1);
+  }
+  fp[1]=fopen("native.c", "w");
+  if(fp[1]==NULL){
+  	perror("open");
+	exit(1);
+  }
+  
+  initialize_native_table();
+  total=setup_native_table();
+  
+  /* write into native.h */
+  fprintf(fp[0], "#ifndef _PAPI_NATIVE  /* _PAPI_NATIVE */\n");
+  fprintf(fp[0], "#define _PAPI_NATIVE\n\n");
+  fprintf(fp[0], "#include SUBSTRATE\n\n");
+  fprintf(fp[0], "#define MAX_NATIVE_EVENT %d\n", total);
+#ifdef _POWER4
+  fprintf(fp[0], "#define GROUP_INTS 2\n");
+  fprintf(fp[0], "#define MAX_GROUPS (GROUP_INTS * 32)\n");
+#endif
+  fprintf(fp[0], "\ntypedef struct native_event_entry{\n");
+  fprintf(fp[0], "  /* indicate which counters this event can live on */\n");
+  fprintf(fp[0], "  unsigned int selector;\n");
+  fprintf(fp[0], "  /* Buffers containing counter cmds for each possible metric */\n");
+  fprintf(fp[0], "  int counter_cmd[MAX_COUNTERS];\n");
+  fprintf(fp[0], "  /* If it exists, then this is the name of this event */\n");
+  fprintf(fp[0], "  char name[PAPI_MAX_STR_LEN];\n");
+#ifdef _POWER4
+  fprintf(fp[0], "  /* which group this event belongs */\n");
+  fprintf(fp[0], "  unsigned int group[GROUP_INTS];\n");
+#endif
+  fprintf(fp[0], "  /* If it exists, then this is the description of this event */\n");
+  fprintf(fp[0], "  char *description;\n");
+  fprintf(fp[0], "} native_event_entry_t;\n\n"); 
+
+  fprintf(fp[0], "extern native_event_entry_t native_table[MAX_NATIVE_EVENT];\n\n"); 
+
+  for(i=0;i<total;i++){
+  	fprintf(fp[0], "#define PNE_%-40s 0x%x\n", native_table[i].name, NATIVE_MASK+i);
+  }
+  fprintf(fp[0], "\n#endif /* _PAPI_NATIVE */\n");
+    
+  if(fclose(fp[0])!=0){
+  	perror("fclose");
+	exit(1);
+  } 
+
+  /* write into native.c */
+  fprintf(fp[1], "#include \"native.h\"\n\n");
+  fprintf(fp[1], "native_event_entry_t native_table[MAX_NATIVE_EVENT] = {");
+  for(i=0;i<total;i++){
+  	if(i==0)
+		fprintf(fp[1], "\n	{0x%x, {%d", native_table[i].selector, native_table[i].counter_cmd[0]);
+	else
+	  	fprintf(fp[1], ",\n	{0x%x, {%d", native_table[i].selector, native_table[i].counter_cmd[0]);
+    for(j=1;j<MAX_COUNTERS;j++){
+		fprintf(fp[1], ",%d", native_table[i].counter_cmd[j]);
+	}
+	fprintf(fp[1], "}, \"%s\", ", native_table[i].name);
+
+  #ifdef _POWER4
+  	fprintf(fp[1], "{ 0x%x", native_table[i].group[0]);
+  	for(k=1;k<GROUP_INTS;k++){
+		fprintf(fp[1], ", 0x%x", native_table[i].group[k]);
+	}
+	fprintf(fp[1], "}, ");
+  #endif
+  	fprintf(fp[1], "\"");
+	str=strdup(native_table[i].description);
+	while((tmp=strchr(str, '\n'))!=NULL){
+		*tmp='\0';
+		fprintf(fp[1], "%s\\n", str);
+		str=tmp+1;
+	}		
+	fprintf(fp[1], "\"}");
+  }
+  fprintf(fp[1], "};\n");
+  
+  if(fclose(fp[1])!=0){
+  	perror("fclose");
+	exit(1);
+  } 
+
+}  
+  
