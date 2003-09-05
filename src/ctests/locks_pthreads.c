615f80c33b015c51aadbd452332c41ef300eb541
london <london>
2003-09-05 17:11:01 +0000

    Typos fixed

--- src/ctests/locks_pthreads.c
+++ src/ctests/locks_pthreads.c
@@ -20,12 +20,12 @@ void *Master(void *arg)
   int *result = (int *) arg;
   int i;
   usleep(SLEEP_VALUE);
-  PAPI_lock(PAPI_USR1_LCK); 
+  PAPI_lock(PAPI_USR1_LOCK); 
   /* Make sure Slaves are not sleeping */
   for(i=0;i<LOOPS;i++){
-	count= 2*count/3
+	count= 2*count/3;
   }
-  PAPI_unlock(PAPI_USR1_LCK);
+  PAPI_unlock(PAPI_USR1_LOCK);
   pthread_exit(NULL);
 }
 
@@ -33,13 +33,13 @@ void *Slave(void *arg)
 {
   int i;
   
-  PAPI_lock(PAPI_USR2_LCK);
-  PAPI_lock(PAPI_USR1_LCK); 
+  PAPI_lock(PAPI_USR2_LOCK);
+  PAPI_lock(PAPI_USR1_LOCK); 
   for(i=0;i<LOOPS;i++){
 	count+=i;
   }
-  PAPI_unlock(PAPI_USR1_LCK);
-  PAPI_unlock(PAPI_USR2_LCK);
+  PAPI_unlock(PAPI_USR1_LOCK);
+  PAPI_unlock(PAPI_USR2_LOCK);
   pthread_exit(NULL);
 }
 
@@ -50,6 +50,7 @@ main(int argc, char **argv )
   pthread_t master;
   pthread_t slave1;
   int result_m,result_s,rc,i;
+  int retval;
 
   /* Setup a random number so compilers can't optimize it out */
   count = rand();
@@ -59,7 +60,7 @@ main(int argc, char **argv )
   tests_quiet(argc, argv); /* Set TESTS_QUIET variable */
                                                                                         
   for(i=0;i<LOOPS;i++){
-	result_m= 2*result_m/3
+	result_m= 2*result_m/3;
   }
   result_s = result_m;
  
@@ -74,25 +75,25 @@ main(int argc, char **argv )
     if ((retval=PAPI_set_debug(PAPI_VERB_ECONT))!= PAPI_OK)
         test_fail(__FILE__,__LINE__,"PAPI_set_debug",retval);
 
-  PAPI_lock(PAPI_USR2_LCK);
-  rc = pthread_create(&master, NULL, Master, (void *) &result);
+  PAPI_lock(PAPI_USR2_LOCK);
+  rc = pthread_create(&master, NULL, Master, NULL);
   if (rc){
         retval=PAPI_ESYS;
         test_fail(__FILE__,__LINE__,"pthread_create",retval);
   }
-  rc = pthread_create(&slave1, NULL, Slave, (void *) &result);
+  rc = pthread_create(&slave1, NULL, Slave, NULL);
   if (rc){
         retval=PAPI_ESYS;
         test_fail(__FILE__,__LINE__,"pthread_create",retval);
   }
   pthread_join(master, NULL);
-  if ( !TESTS_QUIET ){
+  if ( !TESTS_QUIET )
 	printf("Master: Expected: %d  Recieved: %d\n", result_m,count);
   if ( result_m != count) 
 	test_fail(__FILE__,__LINE__,"Thread Locks",1);
-  PAPI_unlock(PAPU_USR2_LCK); 
+  PAPI_unlock(PAPI_USR2_LOCK); 
   pthread_join(slave1, NULL);
-  if ( !TESTS_QUIET ){
+  if ( !TESTS_QUIET )
 	printf("Slave: Expected: %d  Recieved: %d\n", result_s,count);
   if ( result_s != count)
 	test_fail(__FILE__,__LINE__,"Thread Locks",1);
