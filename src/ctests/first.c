7f71a85e4be9a15c8d10f51386af67ba04a74f17
Zhou Min <min@cs.utk.edu>
2003-11-04 01:44:54 +0000

    delete useless statement added by me

--- src/ctests/first.c
+++ src/ctests/first.c
@@ -51,17 +51,13 @@ int main(int argc, char **argv)
    }
 
    /* query and set up the right instruction to monitor */
-/*
    if (PAPI_query_event(PAPI_FP_INS) == PAPI_OK) {
       PAPI_event = PAPI_FP_INS;
       mask = MASK_FP_INS | MASK_TOT_CYC;
    } else {
-*/
       PAPI_event = PAPI_TOT_INS;
       mask = MASK_TOT_INS | MASK_TOT_CYC;
-/*
    }
-*/
 
    retval = PAPI_event_code_to_name(PAPI_event, event_name);
    if (retval != PAPI_OK)
