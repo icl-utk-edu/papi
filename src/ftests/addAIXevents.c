4ab46abb51a7eb4c4265a9e0409c15d465220856
Dan Terpstra <terpstra@eecs.utk.edu>
2003-11-17 20:20:59 +0000

    Another stupid syntax error.

--- src/ftests/addAIXevents.c
+++ src/ftests/addAIXevents.c
@@ -134,9 +134,9 @@ void addaixevents__(int *EventSet, int *retval)
 void pmtoolkit12(int *retval)
 {
 #ifdef PMTOOLKIT_1_2
-  return(1);
+  *retval = 1;
 #else
-  return(0);
+  *retval = 0;
 #endif
 }
 
