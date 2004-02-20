9009a443e7d875a9a09f00fb3d115919e249f666
Philip Mucci <mucci@eecs.utk.edu>
2004-02-20 21:50:56 +0000

    Edited the function name handling again. There is a better one than __FUNCTION__ which is the GNU standard. C99 defined __func__ as the CPP keyword of choice.
    If you are on a platform where the build fails due to this, please
    add -DNO_FUNCTION_MACRO to your platform's makefile and check it in.
    
    Added MACROS file which is there to document (for developers) all the MACRO's
    we use. We will have to remove all of these when we go to GNU autoconf
    anyways, so documenting them now will help us much later on. Please don't
    forget to consult this.

--- src/papi_internal.h
+++ src/papi_internal.h
@@ -20,14 +20,16 @@
 #ifndef PAPI_INTERNAL_H
 #define PAPI_INTERNAL_H
 
-#ifndef __FUNCTION__
-  #define __FUNCTION__ "unknown"
-#endif 
+#if defined(NO_FUNCTION_MACRO) 
+#define __func__ "?"
+#endif
+
+/* Could be __func__ as spec'd by C99 standard? */
 
 #ifndef NO_VARARG_MACRO         /* Has variable arg macro support */
-#define error_return(retval, format, args...){ fprintf(stderr, "Error in file %s, function %s, line %d: ", __FILE__,__FUNCTION__,__LINE__); fprintf(stderr, format, ## args); fprintf(stderr, "\n"); return(retval); }
+#define error_return(retval, format, args...){ fprintf(stderr, "Error %s:%s:%d: ", __FILE__,__func__,__LINE__); fprintf(stderr, format, ## args); fprintf(stderr, "\n"); return(retval); }
 #else
-#define error_return(retval, format){ fprintf(stderr, "Error in file %s, function %s, line %d: ", __FILE__,__FUNCTION__,__LINE__); fprintf(stderr, format); fprintf(stderr, "\n"); return(retval); }
+#define error_return(retval, format){ fprintf(stderr, "Error %s:%s:%d: ", __FILE__,__func__,__LINE__); fprintf(stderr, format); fprintf(stderr, "\n"); return(retval); }
 #endif
 
 #ifdef DEBUG
@@ -44,9 +46,9 @@
 
   /* Please get rid of the DBG macro from your code */
 
-#define DBG(a) { extern int _papi_hwi_debug; if (_papi_hwi_debug) { fprintf(stderr,"DEBUG:%s:%s:%d: ",__FILE__,__FUNCTION__,__LINE__); fprintf a; } }
+#define DBG(a) { extern int _papi_hwi_debug; if (_papi_hwi_debug) { fprintf(stderr,"DEBUG:%s:%s:%d: ",__FILE__,__func__,__LINE__); fprintf a; } }
 
-#define DEBUGLABEL(a) fprintf(stderr, "%s:%s:%s:%d: ",a,__FILE__, __FUNCTION__, __LINE__)
+#define DEBUGLABEL(a) fprintf(stderr, "%s:%s:%s:%d: ",a,__FILE__, __func__, __LINE__)
 
 #define DEBUGLEVEL(a) ((a&DEBUG_SUBSTRATE)?"SUBSTRATE":(a&DEBUG_API)?"API":(a&DEBUG_INTERNAL)?"INTERNAL":(a&DEBUG_THREADS)?"THREADS":(a&DEBUG_MULTIPLEX)?"MULTIPLEX":(a&DEBUG_OVERFLOW)?"OVERFLOW":"UNKNOWN")
 
