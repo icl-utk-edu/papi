9e5dec7828c5f2d155a025bb8040d3a564fff1e7
Philip Mucci <mucci@eecs.utk.edu>
2004-02-25 09:11:23 +0000

    Added a real PAPI_get_execuable_info test case.
    Edited the shlib test case to reflect real usage.
    
    Test cases both pass on linux. Do they pass on your platform?
    
    Phil

--- src/ctests/shlib.c
+++ src/ctests/shlib.c
@@ -42,12 +42,12 @@ int main(int argc, char **argv)
    map = shinfo->map;
    for (i=0;i<shinfo->count;i++)
      {
-       printf("Library: %s\n",map->mapname);
+       printf("Library: %s\n",map->name);
        printf("Text start: %p, Text end: %p\n",map->text_start,map->text_end);
        printf("Data start: %p, Data end: %p\n",map->data_start,map->data_end);
        printf("Bss start: %p, Bss end: %p\n",map->bss_start,map->bss_end);
 
-       if ((map->mapname == NULL) || (strlen(map->mapname) == 0))
+       if ((map->name == NULL) || (strlen(map->name) == 0))
 	 test_fail(__FILE__, __LINE__, "PAPI_get_shared_lib_info",1);
        if ((map->text_start == 0x0) || (map->text_end == 0x0) ||
 	   (map->text_start >= map->text_end))
@@ -55,14 +55,17 @@ int main(int argc, char **argv)
        if ((map->data_start == 0x0) || (map->data_end == 0x0) ||
 	   (map->data_start >= map->data_end))
 	 test_fail(__FILE__, __LINE__, "PAPI_get_shared_lib_info",1);
-       if ((map->bss_start == 0x0) || (map->bss_end == 0x0) ||
-	   (map->bss_start >= map->bss_end))
+       if (((map->bss_start) && (!map->bss_end)) ||
+	   ((!map->bss_start) && (map->bss_end)) ||
+	   (map->bss_start > map->bss_end))
 	 test_fail(__FILE__, __LINE__, "PAPI_get_shared_lib_info",1);
 
        map++;
      }
 
-#ifdef NO_DLFCN
+   sleep(1); /* Needed for debugging, so you can ^Z and stop the process, inspect /proc to see if it's right */
+
+#ifndef NO_DLFCN
    {
      char *libname = 
 #ifdef _AIX
@@ -73,9 +76,9 @@ int main(int argc, char **argv)
      void *handle = dlopen("libm.so", RTLD_LAZY);
      double (*cosine)(double);
      char *error;
-     int oldcount, foundit = 0;
+     int oldcount;
 
-     printf("Loading %s with dlopen().\n",libname);
+     printf("\nLoading %s with dlopen().\n",libname);
 
      handle = dlopen (libname, RTLD_NOW);
      if (!handle) {
@@ -89,7 +92,7 @@ int main(int argc, char **argv)
        test_fail(__FILE__, __LINE__, "dlsym", 1);
      }
      
-     printf ("cos(2.0) = %f\n", (*cosine)(2.0));
+     printf ("cos(2.0) = %f\n\n", (*cosine)(2.0));
  
    oldcount = shinfo->count;
 
@@ -97,23 +100,25 @@ int main(int argc, char **argv)
       test_fail(__FILE__, __LINE__, "PAPI_get_shared_lib_info", 1);
    }
 
+   sleep(1); /* Needed for debugging, so you can ^Z and stop the process, inspect /proc to see if it's right */
+
    if ((shinfo->count == 0) && (shinfo->map)) {
+      test_fail(__FILE__, __LINE__, "PAPI_get_shared_lib_info", 1);
+   }
+
+   if (shinfo->count <= oldcount) {
       test_fail(__FILE__, __LINE__, "PAPI_get_shared_lib_info", 1);
    }
 
    map = shinfo->map;
    for (i=0;i<shinfo->count;i++)
      {
-       if (strstr(map->mapname,libname))
-	 {
-	   printf("New Library: %s\n",map->mapname);
+	   printf("Library: %s\n",map->name);
 	   printf("Text start: %p, Text end: %p\n",map->text_start,map->text_end);
 	   printf("Data start: %p, Data end: %p\n",map->data_start,map->data_end);
 	   printf("Bss start: %p, Bss end: %p\n",map->bss_start,map->bss_end);
-	   foundit = 1;
-	 }
 
-       if ((map->mapname == NULL) || (strlen(map->mapname) == 0))
+       if ((map->name == NULL) || (strlen(map->name) == 0))
 	 test_fail(__FILE__, __LINE__, "PAPI_get_shared_lib_info",1);
        if ((map->text_start == 0x0) || (map->text_end == 0x0) ||
 	   (map->text_start >= map->text_end))
@@ -121,15 +126,15 @@ int main(int argc, char **argv)
        if ((map->data_start == 0x0) || (map->data_end == 0x0) ||
 	   (map->data_start >= map->data_end))
 	 test_fail(__FILE__, __LINE__, "PAPI_get_shared_lib_info",1);
-       if ((map->bss_start == 0x0) || (map->bss_end == 0x0) ||
-	   (map->bss_start >= map->bss_end))
+       if (((map->bss_start) && (!map->bss_end)) ||
+	   ((!map->bss_start) && (map->bss_end)) ||
+	   (map->bss_start > map->bss_end))
 	 test_fail(__FILE__, __LINE__, "PAPI_get_shared_lib_info",1);
 
        map++;
      }
    
-   if (foundit == 0)
-     test_fail(__FILE__, __LINE__, "PAPI_get_shared_lib_info",1);
+   sleep(1); /* Needed for debugging, so you can ^Z and stop the process, inspect /proc to see if it's right */
 
    dlclose(handle);
    }
