72531965c9d9dce028bf6fee147eaa34d8a4d853
london <london>
2004-03-05 22:26:00 +0000

    Thanks to Stephane we have L1,L2 and L3 memory information.  I will add the TLB stuff shortly.

--- src/linux-ia64-memory.c
+++ src/linux-ia64-memory.c
@@ -13,30 +13,165 @@
 #endif
 #include SUBSTRATE
 #include <stdio.h>
+#include <string.h>
 
 inline void get_cpu_info(unsigned int *rev, unsigned int *model, unsigned int *family, unsigned int *archrev);
+void fline ( FILE *fp, char *buf );
+int get_number(char *buf);
+
+#define C_INSTRUCTION 1
+#define C_DATA        2
+#define C_COMBINED    3
 
 int _papi_hwd_get_memory_info(PAPI_hw_info_t * mem_info, int cpu_type)
 {
    unsigned int rev,model,family,archrev;
    int retval = 0;
+   FILE *f;
+   int clevel=0, ctype=0;
+   char buf[1024];
+   int num;
+
+   f = fopen("/proc/pal/cpu0/cache_info","r");
+
+   if (!f)
+      error_return(PAPI_ESYS, "fopen(/proc/pal/cpu0/cache_info returned < 0");
+
+   while (!feof(f)) {
+        fline(f, buf);
+        if ( buf[0] == '\0' ) break;
+        if (  !strncmp(buf, "Data Cache", 10) ) {
+  	   ctype = C_DATA;
+	   clevel = get_number( buf );
+        }
+        else if ( !strncmp(buf, "Instruction Cache", 17) ){
+   	   ctype = C_INSTRUCTION;
+	   clevel = get_number( buf );
+        }
+	else if ( !strncmp(buf, "Data/Instruction Cache", 22)){
+           ctype = C_COMBINED;
+	   clevel = get_number( buf );
+        }
+        else {
+           if ( (clevel == 0 || clevel > 3) && ctype != 0)
+	     error_return(PAPI_EBUG, "Cache type could not be recognized, send /proc/pal/cpu0/cache_info");
+           if ( !strncmp(buf, "Size", 4) ){
+		num = get_number( buf );
+		switch(clevel){
+		  case 1:
+		    if ( ctype == C_INSTRUCTION ){
+			num = num/1024;
+       			mem_info->L1_size += num;
+       			mem_info->L1_icache_size = num;
+		    }
+		    else if ( ctype == C_DATA ){
+			num = num/1024;
+       			mem_info->L1_size += num;
+       			mem_info->L1_dcache_size = num;
+		    }
+		    else if ( ctype == C_COMBINED ){
+			num = num/1024;
+       			mem_info->L1_size = num;
+       			mem_info->L1_dcache_size = 0;
+       			mem_info->L1_icache_size = 0;
+		    }
+		    break;
+		  case 2:
+			mem_info->L2_cache_size = num/1024;
+		    break;
+		  case 3:
+			mem_info->L3_cache_size = num/1024;
+		    break;
+		  default:
+		    break;
+		}
+           }
+  	   else if ( !strncmp(buf, "Associativity", 13) ) {
+	   	num = get_number( buf );
+		switch(clevel){
+		  case 1:
+		    if ( ctype == C_INSTRUCTION ){
+       			mem_info->L1_icache_assoc = num;
+		    }
+		    else if ( ctype == C_DATA ){
+       			mem_info->L1_dcache_assoc = num;
+		    }
+		    else if ( ctype == C_COMBINED ){
+       			mem_info->L1_icache_assoc = num;
+       			mem_info->L1_dcache_assoc = num;
+		    }
+		    break;
+		  case 2:
+			mem_info->L2_cache_assoc = num;
+		    break;
+		  case 3:
+			mem_info->L3_cache_assoc = num;
+		    break;
+		  default:
+		    break;
+		}
+	   }
+	   else if ( !strncmp(buf, "Line size", 9) ) {
+	   	num = get_number( buf );
+		switch(clevel){
+		  case 1:
+		    if ( ctype == C_INSTRUCTION ){
+		        mem_info->L1_icache_linesize = num;
+			mem_info->L1_icache_lines = (mem_info->L1_icache_size*1024)/num;
+		    }
+		    else if ( ctype == C_DATA ){
+		        mem_info->L1_dcache_linesize = num;
+			mem_info->L1_dcache_lines = (mem_info->L1_icache_size*1024)/num;
+		    }
+		    else if ( ctype == C_COMBINED ){
+		        mem_info->L1_icache_linesize = num;
+		        mem_info->L1_dcache_linesize = num;
+			mem_info->L1_icache_lines = (mem_info->L1_icache_size*1024)/num;
+			mem_info->L1_dcache_lines = (mem_info->L1_icache_size*1024)/num;
+		    }
+		    break;
+		  case 2:
+			mem_info->L2_cache_linesize = num;
+			mem_info->L2_cache_lines = (mem_info->L2_cache_size*1024)/num;
+		    break;
+		  case 3:
+			mem_info->L3_cache_linesize = num;
+			mem_info->L3_cache_lines = (mem_info->L2_cache_size*1024)/num;
+		    break;
+		  default:
+		    break;
+		}
+	   }
+	   else
+	     continue;
+        }
+   } 
 
    get_cpu_info(&rev,&model,&family,&archrev);
 
-   if ( family == 6 ) {  /* Itanium */
-       mem_info->L1_size = 32;
-   }
-   else if ( family == 31 ) {  /* Itanium 2 */
-       mem_info->L1_size = 64;
-       mem_info->L1_icache_size = 32;
-       mem_info->L1_dcache_size = 32;
-       mem_info->L2_cache_size = 256;
-   }
-   else{
-	SUBDBG("Family %d not found\n", family);
-	return -1;
-   }
    return retval;
+}
+
+int get_number( char *buf ){
+   char numbers[] = "0123456789";
+   int num;
+   char *tmp, *end;
+
+printf("Buf: %s\n", buf);
+   tmp = strpbrk(buf, numbers);
+printf("%s:%s\n", tmp, buf);
+   if ( tmp != NULL ){
+	end = tmp;
+	while(isdigit(*end)) end++;
+	*end='\0';
+        num = atoi(tmp);
+printf("Num: %d\n", num);
+        return(num);
+    }
+    else {
+        error_return(PAPI_EBUG, "Cache type could not be recognized, send /proc/pal/cpu0/cache_info");
+    }
+  return(-1);
 }
 
 long _papi_hwd_get_dmem_info(int option)
@@ -72,6 +207,37 @@ long _papi_hwd_get_dmem_info(int option)
 }
 
 
+void fline ( FILE *fp, char *rline ) {
+  char *tmp,*end,c;
+
+  tmp = rline;
+  end = &rline[1023];
+   
+  memset(rline, '\0', 1024);
+
+  do {
+    if ( feof(fp))  return;
+    c = getc(fp);
+  } while (isspace(c) || c == '\n' || c == '\r');
+
+  ungetc( c, fp);
+
+  for(;;) {
+    if ( feof(fp) ) {
+       return;
+    }
+    c = getc( fp);
+    if ( c == '\n' || c == '\r' )
+      break;
+    *tmp++ = c;
+    if ( tmp == end ) {
+       *tmp = '\0';
+       return;
+    }
+  }
+  return;
+} 
+ 
 inline void get_cpu_info(unsigned int *rev, unsigned int *model, unsigned int *family, unsigned int *archrev)
 {
         unsigned long r;
