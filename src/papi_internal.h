e6ebd1c5948b42f11f7cf6cfc6563fbd89c74759
Dan Terpstra <terpstra@eecs.utk.edu>
2004-06-02 16:06:55 +0000

    Looks like irix needs the same __inline definition as AIX.

--- src/papi_internal.h
+++ src/papi_internal.h
@@ -23,7 +23,9 @@
 /* Windows has it's own way of declaring inline static routines */
 #ifdef _WIN32
    #define inline_static static __inline
-#elif defined(_AIX) /* don't know how to declare inline for xlc */
+#elif defined(_AIX) /* different way to declare inline for xlc */
+   #define inline_static __inline
+#elif ( defined(mips) && defined(sgi) && defined(unix) )
    #define inline_static __inline
 #else
    #define inline_static inline static
