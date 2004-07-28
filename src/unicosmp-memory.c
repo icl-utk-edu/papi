2908d7e78c423aeb1123eae57c125d72dfa455ea
london <london>
2004-07-28 16:44:51 +0000

    Ooops forgot to add this file

---
+++ src/unicosmp-memory.c
@@ -0,0 +1,49 @@
+/*
+* File:    unicos-memory.c
+* Author:  Kevin London
+*          london@cs.utk.edu
+*
+* Mods:    <your name here>
+*          <your email address>
+*/
+
+#include "papi.h"
+#include SUBSTRATE
+
+
+int get_memory_info(PAPI_hw_info_t * mem_info)
+{
+   inventory_t *curr;
+   PAPI_mh_level_t *L = mem_info->mem_hierarchy.level;
+
+   return PAPI_OK;
+}
+
+long _papi_hwd_get_dmem_info(int option)
+{
+   pid_t pid = getpid();
+   prpsinfo_t info;
+   char pfile[256];
+   int fd;
+
+   sprintf(pfile, "/proc/%05d", (int) pid);
+   if ((fd = open(pfile, O_RDONLY)) < 0) {
+      DBG((stderr, "PAPI_get_dmem_info can't open /proc/%d\n", (int) pid));
+      return (PAPI_ESYS);
+   }
+   if (ioctl(fd, PIOCPSINFO, &info) < 0) {
+      return (PAPI_ESYS);
+   }
+   close(fd);
+
+    switch(option) {
+	case PAPI_GET_RESSIZE:
+  	  return (info.pr_rssize);
+	case PAPI_GET_SIZE:
+          return (info.pr_size);
+	case PAPI_GET_PAGESIZE:
+	  return(getpagesize());
+	default:
+	  return(PAPI_EINVAL);
+    }
+}
