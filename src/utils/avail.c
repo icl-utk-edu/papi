6fd7516e454b85f4555a22dfab6fdc8cdf4cf10b
Dan Terpstra <terpstra@eecs.utk.edu>
2004-08-17 16:17:09 +0000

    Beefed up avail.c to make it work more like a tool and less like a test. Try typing avail -h. New options to display in the old tabular format (-t), and the new full detail format (-f). As well as the event detail format (-e).

--- src/utils/avail.c
+++ src/utils/avail.c
@@ -3,21 +3,36 @@
 #include "papi_test.h"
 extern int TESTS_QUIET;         /* Declared in test_utils.c */
 
+static void print_help(void)
+{
+   printf("This is the PAPI avail program.\n");
+   printf("It provides availability and detail information\nfor PAPI preset and native events.  Usage:\n\n");
+   printf("    avail [options] [event name]\n");
+   printf("    avail TESTS_QUIET\n\n");
+   printf("Options:\n\n");
+   printf("  -a            display only available PAPI preset events\n");
+   printf("  -t            display PAPI preset event info in tabular format (default)\n");
+   printf("  -h            print this help message\n");
+   printf("  -f            display PAPI preset event info in full detail format\n");
+   printf("  -e EVENTNAME  display full detail for named preset or native event\n");
+   printf("\n");
+}
 
 int main(int argc, char **argv)
 {
    int i,j, k;
    int retval;
-   int print_full_info = 0;
+   int print_event_info = 0;
    char *name = NULL;
    int print_avail_only = 0;
+   int print_tabular = 1;
    PAPI_event_info_t info;
    const PAPI_hw_info_t *hwinfo = NULL;
 
    tests_quiet(argc, argv);     /* Set TESTS_QUIET variable */
    if (argv[1]) {
-      if (strstr(argv[1], "-f")) {
-         print_full_info = 1;
+      if (strstr(argv[1], "-e")) {
+         print_event_info = 1;
          name = argv[2];
       }
    }
@@ -25,6 +40,12 @@ int main(int argc, char **argv)
       if (argv[i]) {
          if (strstr(argv[i], "-a"))
             print_avail_only = PAPI_PRESET_ENUM_AVAIL;
+         if (strstr(argv[i], "-f"))
+            print_tabular = 0;
+         if (strstr(argv[i], "-h")) {
+            print_help();
+            exit(1);
+         }
       }
 
    retval = PAPI_library_init(PAPI_VER_CURRENT);
@@ -41,7 +62,7 @@ int main(int argc, char **argv)
       test_fail(__FILE__, __LINE__, "PAPI_get_hardware_info", 2);
 
    if (!TESTS_QUIET) {
-      printf("Test case 8: Available events and hardware information.\n");
+      printf("Test case avail.c: Available events and hardware information.\n");
       printf
           ("-------------------------------------------------------------------------\n");
       printf("Vendor string and code   : %s (%d)\n", hwinfo->vendor_string,
@@ -57,7 +78,7 @@ int main(int argc, char **argv)
       printf
           ("-------------------------------------------------------------------------\n");
 
-      if (print_full_info) {
+      if (print_event_info) {
          if (PAPI_event_name_to_code(name, &i) == PAPI_OK) {
             if (PAPI_get_event_info(i, &info) == PAPI_OK) {
                PAPI_event_info_t n_info;
@@ -90,25 +111,46 @@ int main(int argc, char **argv)
          else printf("Sorry, an event by the name '%s' could not be found. Is it typed correctly?\n\n",name);
       }
       else {
-         printf("The following correspond to fields in the PAPI_event_info_t structure.\n");
-         
-         printf("Symbol\tEvent Code\tCount\n |Short Description|\n |Long Description|\n |Developer's Notes|\n Derived|\n |PostFix|\n");
-         printf("The count field indicates whether it is a) available (count >= 1) and b) derived (count > 1)\n");
-
          i = PAPI_PRESET_MASK;
+         if (print_tabular) {
+            if (print_avail_only) {
+               printf("Name\t\tDerived\tDescription (Mgr. Note)\n");
+            } else {
+               printf("Name\t\tCode\t\tAvail\tDeriv\tDescription (Note)\n");
+            }
+         }
+         else {
+            printf("The following correspond to fields in the PAPI_event_info_t structure.\n");
+            printf("Symbol\tEvent Code\tCount\n |Short Description|\n |Long Description|\n |Developer's Notes|\n Derived|\n |PostFix|\n");
+         }
          do {
-            if (PAPI_get_event_info(i, &info) == PAPI_OK) 
-	         {
-	            printf("%s\t0x%x\t%d\n |%s|\n |%s|\n |%s|\n |%s|\n |%s|\n",
-		            info.symbol,
-		            info.event_code,
-		            info.count,
-		            info.short_descr,
-		            info.long_descr,
-		            info.note,
-                  info.derived,
-                  info.postfix);
-               for (j=0;j<info.count;j++) printf(" |Native Code[%d]: 0x%x  %s|\n",j,info.code[j], info.name[j]);
+            if (PAPI_get_event_info(i, &info) == PAPI_OK) {
+               if (print_tabular) {
+                  if (print_avail_only) {
+		               printf("%s\t%s\t%s (%s)\n",
+			               info.symbol,
+			               (info.count > 1 ? "Yes" : "No"),
+			               info.long_descr, (info.note ? info.note : ""));
+                  } else {
+		               printf("%s\t0x%x\t%s\t%s\t%s (%s)\n",
+		                     info.symbol,
+		                     info.event_code,
+		                     (info.count ? "Yes" : "No"),
+		                     (info.count > 1 ? "Yes" : "No"),
+		                     info.long_descr, (info.note ? info.note : ""));
+                  }
+               } else {
+	               printf("%s\t0x%x\t%d\n |%s|\n |%s|\n |%s|\n |%s|\n |%s|\n",
+		               info.symbol,
+		               info.event_code,
+		               info.count,
+		               info.short_descr,
+		               info.long_descr,
+		               info.note,
+                     info.derived,
+                     info.postfix);
+                  for (j=0;j<info.count;j++) printf(" |Native Code[%d]: 0x%x  %s|\n",j,info.code[j], info.name[j]);
+               }
 	         }
          } while (PAPI_enum_event(&i, print_avail_only) == PAPI_OK);
       }
