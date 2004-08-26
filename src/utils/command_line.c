f264c02445a11272ba9019fd8ef342456a1f18c7
Dan Terpstra <terpstra@eecs.utk.edu>
2004-08-26 18:27:32 +0000

    This is a waay cool little utility. But it was segfaulting on PAPI_perror(). And it was failing to count if an event didn't add. I fixed it. Thanks, Kevin!

--- src/utils/command_line.c
+++ src/utils/command_line.c
@@ -5,19 +5,15 @@
 
 #include "papi_test.h"
 
-extern int TESTS_QUIET;         /* Declared in test_utils.c */
-
 int main(int argc, char **argv)
 {
    int retval;
    int num_events;
-
-   #define NUM_EVENTS 4
    long_long *values;
-   unsigned int *Events;
+   char *success;
    int EventSet = PAPI_NULL;
-   char errstr[PAPI_MAX_STR_LEN];
-   int i,preset;
+   int i, j, event;
+   char errstr[PAPI_HUGE_STR_LEN];
 
    tests_quiet(argc, argv);     /* Set TESTS_QUIET variable */
 
@@ -39,32 +35,27 @@ int main(int argc, char **argv)
    if ( num_events == 0 ) 
      test_pass(__FILE__, NULL, 0);
 
-   Events = (unsigned int *) malloc(sizeof(unsigned int)*num_events);
    values = (long_long *) malloc(sizeof(long_long)*num_events);
+   success = (char *) malloc(argc);
 
-   if ( Events == NULL || values == NULL ) 
+   if ( success == NULL || values == NULL ) 
       test_fail(__FILE__, __LINE__, "malloc", PAPI_ESYS);
 
    for ( ;i<argc; i++ ){
-     if ( (retval = PAPI_event_name_to_code(argv[i], &preset)) != PAPI_OK )
+     if ( (retval = PAPI_event_name_to_code(argv[i], &event)) != PAPI_OK )
        test_fail(__FILE__, __LINE__, "PAPI_event_name_to_code", retval);
 
-     if ( (retval = PAPI_add_event(EventSet, preset)) != PAPI_OK ) {
-         if ( !TESTS_QUIET ) {
-            printf("Failed adding: %s\n", argv[i] );
-         }
-         if ( !TESTS_QUIET ) {
-            PAPI_perror(retval, errstr, 1024 );
-            printf("Failed because: %s\n", errstr);
-         }
+     if ( (retval = PAPI_add_event(EventSet, event)) != PAPI_OK ) {
+         PAPI_perror(retval, errstr, 1024 );
+         printf("Failed adding: %s\nbecause: %s\n", argv[i], errstr );
+        success[i] = 0;
      }
      else {
-         if ( !TESTS_QUIET ) 
-           printf("Sucessfully added: %s\n", argv[i]);
+        success[i] = 1;
+        printf("Sucessfully added: %s\n", argv[i]);
      }
    }
-   if ( !TESTS_QUIET )
-     printf("\n");
+   printf("\n");
 
    if ((retval = PAPI_start(EventSet)) != PAPI_OK)
       test_fail(__FILE__, __LINE__, "PAPI_start", retval);
@@ -74,18 +65,16 @@ int main(int argc, char **argv)
    if ((retval = PAPI_stop(EventSet, values)) != PAPI_OK)
       test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
 
-   if (!TESTS_QUIET){
-      for(i=1; i< argc; i++ ){
-        printf("%s : \t%lld\n", argv[i], values[i-1]);
+   for(i=1, j=0; i<argc; i++ ) {
+      if (success[i]) {
+         printf("%s : \t%lld\n", argv[i], values[j++]);
+      } else {
+         printf("%s : \t---------\n", argv[i]);
       }
    }
 
-   if (!TESTS_QUIET) {
-      printf("\n----------------------------------\n");
-      printf("Verification: There is no verification, this is a utility that allows the ability\n");
-      printf("to add events in different orders by the command line interface to determine if.\n");
-      printf("there are problems adding events in different orders.\n");
-   }
+   printf("\n----------------------------------\n");
+   printf("Verification: None.\n This utility lets you add events from the command line interface to see if they work.\n");
    test_pass(__FILE__, NULL, 0);
    exit(1);
 }
