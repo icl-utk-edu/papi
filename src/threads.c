8188f179cfe967059c0361c6113397cb9340c3f9
Philip Mucci <mucci@eecs.utk.edu>
2004-09-05 12:37:07 +0000

    Entirely new thread handling library, required for unregister thread and other goods. Now uses __thread keyword with GCC 3 for superfast TLS lookups (no searching, constant time!)
    New entry points to thread lookup..._papi_hwi_lookup_thread()

--- src/threads.c
+++ src/threads.c
@@ -13,183 +13,374 @@
 
 /* This file contains thread allocation and bookkeeping functions */
 
-/* The threads are now stored in a circular link-list, thus there is
- * no more locking on the lookup or insert function, but, one must
- * make sure if a remove function is put in that we again think about
- * locking, the shutdown/cleanup still lock
- */
-
 #include "papi.h"
-#include SUBSTRATE
 #include "papi_internal.h"
-#include "papi_protos.h"
 
-extern unsigned long int (*_papi_hwi_thread_id_fn) (void);
+/*****************/
+/* BEGIN GLOBALS */
+/*****************/
 
-#ifdef ANY_THREAD_GETS_SIGNAL
-extern int (*_papi_hwi_thread_kill_fn) (int, int);
+/* The following globals get initialized and cleared by:
+   extern int _papi_hwi_init_global_threads(void);
+   extern int _papi_hwi_shutdown_thread(ThreadInfo_t *thread); */
+
+/* list of threads, gets initialized to master process with TID of getpid() */
+
+ThreadInfo_t *_papi_hwi_thread_head;
+
+/* If we have TLS, this variable ALWAYS points to our thread descriptor. It's like magic! */
+
+#if defined(HAVE_THREAD_LOCAL_STORAGE)
+THREAD_LOCAL_STORAGE_KEYWORD ThreadInfo_t *_papi_hwi_my_thread;
 #endif
 
-static ThreadInfoList_t *head = NULL;
+/* Function that returns and unsigned long int thread identifier */
 
-/*
- * Calls _papi_hwd_shutdown on every element of the thread list
- * but does not free up the memory.
- */
-void _papi_hwi_shutdown_the_thread_list(void)
+unsigned long int (*_papi_hwi_thread_id_fn)(void);
+
+/* Function that sends a signal to other threads */
+
+#ifdef ANY_THREAD_GETS_SIGNAL
+int (*_papi_hwi_thread_kill_fn)(int, int);
+#endif
+
+/*****************/
+/*  END  GLOBALS */
+/*****************/
+
+static int lookup_and_set_thread_symbols(void)
 {
-   ThreadInfoList_t *tmp;
-   ThreadInfoList_t *nxt;
+#if defined(ANY_THREAD_GETS_SIGNAL)
+   int retval;
+   char *error_ptc = NULL, *error_ptk = NULL;
+   void *symbol_ptc = NULL, *symbol_ptk = NULL, *handle = NULL;
 
-   _papi_hwd_lock(PAPI_INTERNAL_LOCK);
-   if (head == NULL) {
-      _papi_hwd_unlock(PAPI_INTERNAL_LOCK);
-      return;
-   }
+   handle = dlopen(NULL, RTLD_LAZY);
+   if (handle == NULL)
+     {
+       THRDBG("Error from dlopen(NULL, RTLD_LAZY): %s\n",dlerror());
+       return(PAPI_MISC);
+     }
 
-   tmp = head->next;
-   while (tmp != head) {
-      nxt = tmp->next;
-      THRDBG("%llu:%s:%d:0x%lx:Shutting down master thread %lx at %p\n", _papi_hwd_get_real_usec(), __FILE__, __LINE__, (*_papi_hwi_thread_id_fn) (), tmp->master->tid, tmp);
-      _papi_hwd_shutdown(&tmp->master->context);
-      tmp = nxt;
-   }
-   if (tmp) {
-      THRDBG("%lld:%s:%d:0x%lx:Shutting down master thread %lx at %p\n", _papi_hwd_get_real_usec(), __FILE__, __LINE__, (*_papi_hwi_thread_id_fn) (), tmp->master->tid, tmp);
-      _papi_hwd_shutdown(&tmp->master->context);
-   }
-   _papi_hwd_unlock(PAPI_INTERNAL_LOCK);
+   symbol_ptc = dlsym(handle, "pthread_self");
+   if (symbol_ptc == NULL)
+     {
+       error_ptc = dlerror();
+       THRDBG("dlsym(%p,pthread_self) returned NULL: %s\n",(error_ptc ? error_ptc : "No error, NULL symbol!"));
+       dlclose(handle);
+       return(PAPI_EMISC);
+     }
+   
+   symbol_ptk = dlsym(handle, "pthread_kill");
+   if (symbol_ptk == NULL)
+     {
+       error_ptk = dlerror();
+       THRDBG("dlsym(%p,pthread_kill) returned NULL: %s\n",(error_ptk ? error_ptk : "No error, NULL symbol!"));
+       dlclose(handle);
+       return(PAPI_EMISC);
+     }
+	  
+   dlclose(handle);
+
+   _papi_hwi_thread_kill_fn = symbol_ptk;
+   _papi_hwi_thread_id_fn = symbol_ptc;
+#endif
+   return(PAPI_OK);
 }
 
-/*
- * Free's up any memory associated with the list
- */
-void _papi_hwi_cleanup_thread_list(void)
+static ThreadInfo_t *allocate_thread(void)
 {
-   ThreadInfoList_t *tmp;
-   ThreadInfoList_t *nxt;
-   ThreadInfoList_t *prev;
+   ThreadInfo_t *thread;
 
-   _papi_hwd_lock(PAPI_INTERNAL_LOCK);
-   if (head == NULL) {
-      _papi_hwd_unlock(PAPI_INTERNAL_LOCK);
-      return;
-   }
+   /* The Thread EventSet is special. It is not in the EventSet list, but is pointed
+      to by each EventSet of that particular thread. */
 
-   prev = head;
-   tmp = head->next;
-   while (tmp != head) {
-      nxt = tmp->next;
-      THRDBG("%lld:%s:%d:0x%lx:Freeing master thread 0x%lx at %p\n", _papi_hwd_get_real_usec(), __FILE__, __LINE__, (*_papi_hwi_thread_id_fn) (), tmp->master->tid, tmp); 
-      THRDBG( "%p->%p: %p->%p: %p->%p", prev, prev->next, tmp, tmp->next, nxt, nxt->next);
-      free(tmp);
-      prev->next = nxt;
-      tmp = nxt;
-   }
-   THRDBG( "%lld:%s:%d:0x%lx:Freeing master thread 0x%lx at %p\n", _papi_hwd_get_real_usec(), __FILE__, __LINE__, (*_papi_hwi_thread_id_fn) (), tmp->master->tid, tmp);
-   free(tmp);
-   head = NULL;
-   _papi_hwd_unlock(PAPI_INTERNAL_LOCK);
+   thread = (ThreadInfo_t *) malloc(sizeof(ThreadInfo_t));
+   if (thread == NULL)
+      return (NULL);
+   memset(thread, 0x00, sizeof(ThreadInfo_t));
+
+   if (_papi_hwi_thread_id_fn)
+     thread->tid = (*_papi_hwi_thread_id_fn)();
+   else
+     thread->tid = getpid();
+
+   THRDBG("Allocated thread 0x%lx at %p\n",thread->tid,thread);
+
+   return (thread);
 }
 
-int _papi_hwi_insert_in_thread_list(ThreadInfo_t * ptr)
+static void free_thread(ThreadInfo_t ** thread)
 {
-   ThreadInfoList_t *entry = (ThreadInfoList_t *) malloc(sizeof(ThreadInfoList_t));
-   int i;
+   THRDBG("Freeing thread 0x%lx at %p\n",(*thread)->tid,*thread);
 
-   if (entry == NULL)
-      return (PAPI_ENOMEM);
-   THRDBG( "%lld:%s:%d:0x%lx:(%p): New entry is at %p\n", _papi_hwd_get_real_usec(), __FILE__, __LINE__, (*_papi_hwi_thread_id_fn) (), ptr, entry);
-   entry->master = ptr;
+   memset(*thread, 0x00, sizeof(ThreadInfo_t));
+   free(*thread);
+   *thread = NULL;
+}
 
-   /* Thread specific data */
-   for (i = 0; i < PAPI_MAX_THREAD_STORAGE; i++)
-      ptr->thread_storage[0] = NULL;
+static void insert_thread (ThreadInfo_t * entry)
+{
+  _papi_hwi_lock (THREADS_LOCK);	
+  
+  if (_papi_hwi_thread_head == NULL) /* 0 elements */
+    {
+      THRDBG ("_papi_hwi_thread_head is NULL\n");
+      entry->next = entry;
+    }
+  else if (_papi_hwi_thread_head->next == _papi_hwi_thread_head) /* 1 elements */
+    {
+      THRDBG ("_papi_hwi_thread_head was thread 0x%lx at %p\n",_papi_hwi_thread_head->tid,_papi_hwi_thread_head);
+      _papi_hwi_thread_head->next = entry;
+      entry->next = _papi_hwi_thread_head;
+    }
+  else /* 2+ elements */
+    {
+      THRDBG ("_papi_hwi_thread_head was thread 0x%lx at %p\n",_papi_hwi_thread_head->tid,_papi_hwi_thread_head);
+      entry->next = _papi_hwi_thread_head->next;
+      _papi_hwi_thread_head->next = entry;
+    }
 
-  _papi_hwd_lock(PAPI_INTERNAL_LOCK); /* KSL */
-   if (head == NULL) {
-      head = entry;
-      head->next = head;
-   } else {
-      /* Since we are no longer locking the entry has to be setup BEFORE
-       * inserting it into the circular list -KSL
-       */
-      entry->next = head->next;
-      head->next = entry;
-      head = entry;
-   }
-   THRDBG("%lld:0x%lx:(%p): Old head is at %p\n", _papi_hwd_get_real_usec(), (*_papi_hwi_thread_id_fn) (), ptr, entry->next);
-   THRDBG("%lld:0x%lx:(%p): New head is at %p\n", _papi_hwd_get_real_usec(), (*_papi_hwi_thread_id_fn) (), ptr, head);
-  _papi_hwd_unlock(PAPI_INTERNAL_LOCK); /* KSL */
+  _papi_hwi_thread_head = entry;
 
+  THRDBG("_papi_hwi_thread_head now thread 0x%lx at %p\n",_papi_hwi_thread_head->tid,_papi_hwi_thread_head);
+
+  _papi_hwi_unlock (THREADS_LOCK);	
+
+#if defined(HAVE_THREAD_LOCAL_STORAGE)
+  _papi_hwi_my_thread = entry;
+  THRDBG("TLS for thread 0x%lx is now %p\n",entry->tid,_papi_hwi_my_thread);
+#endif
+}
+
+static int remove_thread (ThreadInfo_t * entry)
+{
+  ThreadInfo_t *tmp = NULL, *prev = NULL;
+
+  _papi_hwi_lock (THREADS_LOCK);	
+
+  THRDBG ("_papi_hwi_thread_head was thread 0x%lx at %p\n",_papi_hwi_thread_head->tid,_papi_hwi_thread_head);
+
+  /* Find the preceding element and the matched element,
+     short circuit if we've seen the head twice */
+
+  for (tmp = _papi_hwi_thread_head; (entry != tmp) || (prev == NULL); tmp = tmp->next)
+    {
+      prev = tmp;
+    }
+
+  if (tmp != entry)
+    {
+      THRDBG("Thread 0x%lx at %p was not found in the thread list!\n",entry->tid,entry);
+      return(PAPI_EBUG);
+    }
+
+  /* Only 1 element in list */
+
+  if (prev == tmp)
+    {
+      _papi_hwi_thread_head = NULL;
+      tmp->next = NULL;
+      THRDBG("_papi_hwi_thread_head now NULL\n");
+    }
+  else
+    {
+      prev->next = tmp->next;
+      /* If we're removing the head, better advance it! */
+      if (_papi_hwi_thread_head == tmp)
+	{
+	  _papi_hwi_thread_head = tmp->next;
+	  THRDBG("_papi_hwi_thread_head now thread 0x%lx at %p\n",_papi_hwi_thread_head->tid,_papi_hwi_thread_head);
+	}
+      THRDBG("Removed thread %p from list\n",tmp);
+    }
+
+  _papi_hwi_unlock (THREADS_LOCK);	
+
+#if defined(HAVE_THREAD_LOCAL_STORAGE)
+  _papi_hwi_my_thread = NULL;
+  THRDBG("TLS for thread 0x%lx is now %p\n",entry->tid,_papi_hwi_my_thread);
+#endif
+
+  return (PAPI_OK);
+}
+
+int _papi_hwi_initialize_thread(ThreadInfo_t ** dest)
+{
+   int retval;
+   ThreadInfo_t *thread;
+
+   if ((thread = allocate_thread()) == NULL)
+     {
+       *dest = NULL;
+       return(PAPI_ENOMEM);
+     }
+   
+   /* Call the substrate to fill in anything special. */
+
+   retval = _papi_hwd_init(&thread->context);
+   if (retval) 
+     {
+       free_thread(&thread);
+       *dest = NULL;
+       return (retval);
+     }
+
+   insert_thread(thread);
+
+   *dest = thread;
    return (PAPI_OK);
 }
 
-ThreadInfo_t *_papi_hwi_lookup_in_thread_list(void)
-{
-   if (_papi_hwi_thread_id_fn == NULL)
-      return (default_master_thread);
-   else {
-      unsigned long int id_to_find = (*_papi_hwi_thread_id_fn) ();
-      ThreadInfoList_t *tmp;
+#if defined(ANY_THREAD_GETS_SIGNAL)
 
-      _papi_hwd_lock(PAPI_INTERNAL_LOCK); /* KSL */
-      tmp = head;
-      while (tmp != NULL) {
-         THRDBG("%lld:%s:%d:0x%lx:Examining master at %p,tid 0x%lx.\n", _papi_hwd_get_real_usec(), __FILE__, __LINE__, (*_papi_hwi_thread_id_fn) (), tmp->master, tmp->master->tid);
-         if (tmp->master->tid == id_to_find) {
-            head = tmp;
-	    _papi_hwd_unlock(PAPI_INTERNAL_LOCK);
-            return (tmp->master);
-         }
-         tmp = tmp->next;
-         if (tmp == head)
-            break;
-      }
-      THRDBG("%lld:%s:%d:0x%lx:I'm not in the list at %p.\n", _papi_hwd_get_real_usec(), __FILE__, __LINE__, (*_papi_hwi_thread_id_fn) (), head);
-      _papi_hwd_unlock(PAPI_INTERNAL_LOCK);/* KSL */
-      return (NULL);
-   }
+/* This is ONLY defined for systems that enable ANY_THREAD_GETS_SIGNAL
+   since we must forward signals sent to non-PAPI threads.
+
+   This is NOT compatible with thread local storage, since to broadcast
+   the signal, we need a list of threads. */
+
+int _papi_hwi_broadcast_signal(unsigned int mytid)
+{
+  int retval, didsomething = 0;
+  ThreadInfo_t *foo = NULL;
+
+  _papi_hwi_lock(THREADS_LOCK);
+
+  for (foo = _papi_hwi_thread_head; foo != NULL; foo = foo->next)
+    {
+      if ((foo->tid != mytid) && (foo->running_eventset) && (foo->wants_a_signal))
+	{
+	  THRDBG("Thread 0x%lx sending signal %d to thread 0x%lx\n",mytid,foo->tid,PAPI_SIGNAL);
+	  retval = (*_papi_hwi_thread_kill_fn)(foo->tid, PAPI_SIGNAL);
+	  if (retval != 0)
+	    return(PAPI_EMISC);
+	}
+      if (foo->next == _papi_hwi_thread_head)
+	break;
+    }
+  _papi_hwi_unlock (THREADS_LOCK);
+
+  return(PAPI_OK);
 }
 
-/*  * Return a pointer to the hwd_context_t.
- */
-int  _papi_hwi_get_thr_context(void ** ptr)
-{  
-   ThreadInfo_t *thread;
-   thread = _papi_hwi_lookup_in_thread_list();
-   *ptr = &thread->context; 
-   return(PAPI_OK);
-}  
+#else
 
+/* This is undefined for systems that enable ANY_THREAD_GETS_SIGNAL
+   since we always must enable threads for safety. */
 
-#ifdef ANY_THREAD_GETS_SIGNAL
-void _papi_hwi_broadcast_overflow_signal(unsigned int mytid)
+int _papi_hwi_set_thread_id_fn(unsigned long int (*id_fn) (void))
 {
-   int retval, didsomething = 0;
-   ThreadInfoList_t *foo = NULL;
-   _papi_hwd_lock(PAPI_INTERNAL_LOCK);
-   for (foo = head; foo != NULL; foo = foo->next) {
-      if ((foo->master->event_set_overflowing) && (foo->master->tid != mytid)) {
-#ifdef OVERFLOW_DEBUG_TIMER
-         fprintf(stderr, "%lld:%s:%d:0x%x:I'm forwarding signal to thread %x\n",
-                 _papi_hwd_get_real_usec(), __FILE__, __LINE__, (*_papi_hwi_thread_id_fn) (),
-                 foo->master->tid);
+#if !defined(ANY_THREAD_GETS_SIGNAL)
+  /* Check for multiple threads still in the list, if so, we can't change it */
+
+  if (_papi_hwi_thread_head->next != _papi_hwi_thread_head)
+    return(PAPI_EINVAL);
+  
+  /* We can't change the thread id function from one to another, 
+     only NULL to non-NULL and vice versa. */
+
+  if ((id_fn != NULL) && (_papi_hwi_thread_id_fn != NULL))
+    return(PAPI_EINVAL);
+  
+  _papi_hwi_thread_id_fn = id_fn;
+
+  THRDBG("Set new thread id function to %p\n",id_fn);
+
+  if (id_fn)
+    _papi_hwi_thread_head->tid = (*_papi_hwi_thread_id_fn)();
+  else
+    _papi_hwi_thread_head->tid = getpid();
+
+  THRDBG("New master tid is 0x%lx\n",_papi_hwi_thread_head->tid);
 #endif
-         retval = (*_papi_hwi_thread_kill_fn) (foo->master->tid, PAPI_SIGNAL);
-         assert(retval == 0);
-         didsomething++;
-      } else {
-#ifdef OVERFLOW_DEBUG_TIMER
-         fprintf(stderr, "%lld:%s:%d:0x%x:I'm NOT forwarding signal to thread %x\n",
-                 _papi_hwd_get_real_usec(), __FILE__, __LINE__, (*_papi_hwi_thread_id_fn) (),
-                 foo->master->tid);
-#endif
-      }
-      if (foo->next == head)
-         break;
-   }
-   _papi_hwd_unlock(PAPI_INTERNAL_LOCK);
-   assert(didsomething);
+  return(PAPI_OK);
 }
 #endif
+
+int _papi_hwi_shutdown_thread(ThreadInfo_t *thread)
+{
+   int retval = PAPI_OK;
+   unsigned long int tid;
+
+   if (_papi_hwi_thread_id_fn)
+     tid = (*_papi_hwi_thread_id_fn)();
+   else
+     tid = getpid();
+
+   if (thread->tid == tid)
+     {
+       remove_thread(thread);
+       THRDBG ("Shutting down thread 0x%lx at %p\n",thread->tid,thread);
+       retval = _papi_hwd_shutdown(&thread->context);
+       free_thread(&thread);
+       return(retval);
+     }
+
+   THRDBG("Skipping shutdown thread 0x%lx at %p, thread 0x%lx not owner!\n",thread->tid,thread,tid);
+   return (PAPI_EBUG);
+}
+
+/* THESE MUST BE CALLED WITH A GLOBAL LOCK */
+
+int _papi_hwi_shutdown_global_threads(void)
+{
+  int err;
+  ThreadInfo_t *tmp = _papi_hwi_lookup_thread();
+
+  if (tmp == NULL)
+    {
+      THRDBG("Did not find my thread for shutdown!\n");      
+      err = PAPI_EBUG;
+    }
+  else
+    {
+      err = _papi_hwi_shutdown_thread(tmp);
+    }
+
+#ifdef DEBUG
+  if (ISLEVEL(DEBUG_THREADS))
+    {
+      if (_papi_hwi_thread_head)
+	{
+	  THRDBG("Thread head %p still exists!\n",_papi_hwi_thread_head);
+	}
+    }
+#endif
+
+#if defined(HAVE_THREAD_LOCAL_STORAGE)
+  _papi_hwi_my_thread = NULL;
+#endif
+  _papi_hwi_thread_head = NULL;
+  _papi_hwi_thread_id_fn = NULL;
+#if defined(ANY_THREAD_GETS_SIGNAL)
+  _papi_hwi_thread_kill_fn = NULL;
+#endif
+
+  return(err);
+}
+
+int _papi_hwi_init_global_threads(void)
+{
+  int retval;
+  ThreadInfo_t *tmp;
+
+  _papi_hwi_lock(GLOBAL_LOCK);
+  
+#if defined(HAVE_THREAD_LOCAL_STORAGE)
+  _papi_hwi_my_thread = NULL;
+#endif
+  _papi_hwi_thread_head = NULL;
+  _papi_hwi_thread_id_fn = NULL;
+#if defined(ANY_THREAD_GETS_SIGNAL)
+  _papi_hwi_thread_kill_fn = NULL;
+#endif
+
+  retval = _papi_hwi_initialize_thread(&tmp);
+  if (retval == PAPI_OK)
+    retval = lookup_and_set_thread_symbols();
+
+  _papi_hwi_unlock(GLOBAL_LOCK);
+
+  return(retval);
+}
