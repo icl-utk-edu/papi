8f3dcddec828ab76ecce04259eef90620ce96b08
Dan Terpstra <terpstra@eecs.utk.edu>
2004-09-08 01:32:06 +0000

    Reworked this test to repeat the basic test 10 times and compute min, max, and sum. This gives better statistics and much smaller errors. It now passes every time on itanium.

--- src/ctests/tenth.c
+++ src/ctests/tenth.c
@@ -19,9 +19,9 @@
 #define EVT1		  PAPI_L2_TCM
 #define EVT2		  PAPI_L2_TCA
 #define EVT3		  PAPI_L2_TCH
-#define EVT1_STR	  "PAPI_L2_TCM"
-#define EVT2_STR	  "PAPI_L2_TCA"
-#define EVT3_STR	  "PAPI_L2_TCH"
+#define EVT1_STR	  "PAPI_L2_TCM: "
+#define EVT2_STR	  "PAPI_L2_TCA: "
+#define EVT3_STR	  "PAPI_L2_TCH: "
 #define MASK1		  MASK_L2_TCM
 #define MASK2		  MASK_L2_TCA
 #define MASK3		  MASK_L2_TCH
@@ -31,9 +31,9 @@
 #define EVT1		  PAPI_L1_DCA
 #define EVT2		  PAPI_L1_DCW
 #define EVT3		  PAPI_L1_DCR
-#define EVT1_STR	  "PAPI_L1_DCA"
-#define EVT2_STR	  "PAPI_L1_DCW"
-#define EVT3_STR	  "PAPI_L1_DCR"
+#define EVT1_STR	  "PAPI_L1_DCA: "
+#define EVT2_STR	  "PAPI_L1_DCW: "
+#define EVT3_STR	  "PAPI_L1_DCR: "
 #define MASK1		  MASK_L1_DCA
 #define MASK2		  MASK_L1_DCW
 #define MASK3		  MASK_L1_DCR
@@ -42,9 +42,9 @@
 #define EVT1		  PAPI_L1_TCM
 #define EVT2		  PAPI_L1_ICM
 #define EVT3		  PAPI_L1_DCM
-#define EVT1_STR	  "PAPI_L1_TCM"
-#define EVT2_STR	  "PAPI_L1_ICM"
-#define EVT3_STR	  "PAPI_L1_DCM"
+#define EVT1_STR	  "PAPI_L1_TCM: "
+#define EVT2_STR	  "PAPI_L1_ICM: "
+#define EVT3_STR	  "PAPI_L1_DCM: "
 #define MASK1		  MASK_L1_TCM
 #define MASK2		  MASK_L1_ICM
 #define MASK3		  MASK_L1_DCM
@@ -57,7 +57,7 @@ extern int TESTS_QUIET;         /* Declared in test_utils.c */
 
 int main(int argc, char **argv)
 {
-   int retval, num_tests = 3, tmp;
+   int retval, num_tests = 30, tmp;
    int EventSet1;
    int EventSet2;
    int EventSet3;
@@ -68,6 +68,10 @@ int main(int argc, char **argv)
    int num_events2;
    int num_events3;
    long_long **values;
+   int i, j;
+   long_long min[3];
+   long_long max[3];
+   long_long sum[3];
 
    tests_quiet(argc, argv);     /* Set TESTS_QUIET variable */
 
@@ -102,42 +106,55 @@ int main(int argc, char **argv)
    values = allocate_test_space(num_tests, 1);
 
    /* Warm me up */
-
    do_l1misses(ITERS);
 
-   retval = PAPI_start(EventSet1);
-   if (retval != PAPI_OK)
-      test_fail(__FILE__, __LINE__, "PAPI_start", retval);
+   for (i=0; i<10; i++) {
+      retval = PAPI_start(EventSet1);
+      if (retval != PAPI_OK)
+         test_fail(__FILE__, __LINE__, "PAPI_start", retval);
 
-   do_l1misses(ITERS);
+      do_l1misses(ITERS);
 
-   retval = PAPI_stop(EventSet1, values[0]);
-   if (retval != PAPI_OK)
-      test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
+      retval = PAPI_stop(EventSet1, values[(i*3)+0]);
+      if (retval != PAPI_OK)
+         test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
 
-   retval = PAPI_start(EventSet2);
-   if (retval != PAPI_OK)
-      test_fail(__FILE__, __LINE__, "PAPI_start", retval);
+      retval = PAPI_start(EventSet2);
+      if (retval != PAPI_OK)
+         test_fail(__FILE__, __LINE__, "PAPI_start", retval);
 
-   do_l1misses(ITERS);
+      do_l1misses(ITERS);
 
-   retval = PAPI_stop(EventSet2, values[1]);
-   if (retval != PAPI_OK)
-      test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
+      retval = PAPI_stop(EventSet2, values[(i*3)+1]);
+      if (retval != PAPI_OK)
+         test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
 
-   retval = PAPI_start(EventSet3);
-   if (retval != PAPI_OK)
-      test_fail(__FILE__, __LINE__, "PAPI_start", retval);
+      retval = PAPI_start(EventSet3);
+      if (retval != PAPI_OK)
+         test_fail(__FILE__, __LINE__, "PAPI_start", retval);
 
-   do_l1misses(ITERS);
+      do_l1misses(ITERS);
 
-   retval = PAPI_stop(EventSet3, values[2]);
-   if (retval != PAPI_OK)
-      test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
+      retval = PAPI_stop(EventSet3, values[(i*3)+2]);
+      if (retval != PAPI_OK)
+         test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
+   }
 
    remove_test_events(&EventSet1, mask1);
    remove_test_events(&EventSet2, mask2);
    remove_test_events(&EventSet3, mask3);
+
+   for (j=0;j<3;j++) {
+      min[j] = 65535;
+      max[j] = sum[j] = 0;
+   }
+   for (i=0;i<10;i++) {
+      for (j=0;j<3;j++) {
+         if (min[j] > values[(i*3)+j][0]) min[j] = values[(i*3)+j][0];
+         if (max[j] < values[(i*3)+j][0]) max[j] = values[(i*3)+j][0];
+         sum[j] += values[(i*3)+j][0];
+      }
+   }
 
    if (!TESTS_QUIET) {
       printf("Test case 10: start, stop for derived event %s.\n", CACHE_LEVEL);
@@ -146,46 +163,47 @@ int main(int argc, char **argv)
       printf("Default domain is: %d (%s)\n", tmp, stringify_domain(tmp));
       tmp = PAPI_get_opt(PAPI_DEFGRN, NULL);
       printf("Default granularity is: %d (%s)\n", tmp, stringify_granularity(tmp));
-      printf("Using %d iterations of c += a*b\n", NUM_FLOPS);
+      printf("Using %d iterations of c += a*b\n", ITERS);
+      printf("Repeated 10 times\n");
       printf
           ("-------------------------------------------------------------------------\n");
-
-      printf("Test type   : %12s%12s%12s\n", "1", "2", "3");
+/* 
+      for (i=0;i<10;i++) {
+         printf("Test type   : %12s%13s%13s\n", "1", "2", "3");
+         printf(TAB3, EVT1_STR, values[(i*3)+0][0], (long_long)0, (long_long)0);
+         printf(TAB3, EVT2_STR, (long_long)0, values[(i*3)+1][0], (long_long)0);
+         printf(TAB3, EVT3_STR, (long_long)0, (long_long)0, values[(i*3)+2][0]);
+         printf
+            ("-------------------------------------------------------------------------\n");
+      }
+*/
+      printf("Test type   : %12s%13s%13s\n", "min", "max", "sum");
+      printf(TAB3, EVT1_STR, min[0], max[0], sum[0]);
+      printf(TAB3, EVT2_STR, min[1], max[1], sum[1]);
+      printf(TAB3, EVT3_STR, min[2], max[2], sum[2]);
+      printf
+         ("-------------------------------------------------------------------------\n");
+      printf("Verification:\n");
 #if defined(sun) && defined(sparc)
-      printf(TAB3, "PAPI_L2_TCM : ", (values[0])[0], (long_long) 0, (long_long) 0);
-      printf(TAB3, "PAPI_L2_TCA : ", (long_long) 0, (values[1])[0], (long_long) 0);
-      printf(TAB3, "PAPI_L2_TCH : ", (long_long) 0, (long_long) 0, (values[2])[0]);
-      printf
-          ("-------------------------------------------------------------------------\n");
-
-      printf("Verification:\n");
-      printf(TAB1, "First number row 1 approximately equals (2,2) - (3,3) or",
-             (values[1])[0] - (values[2])[0]);
+      printf(TAB1, "Sum 1 approximately equals sum 2 - sum 3 or", (sum[1] - sum[2]));
 #else
-      printf(TAB3, "PAPI_L1_TCM : ", (values[0])[0], (long_long) 0, (long_long) 0);
-      printf(TAB3, "PAPI_L1_ICM : ", (long_long) 0, (values[1])[0], (long_long) 0);
-      printf(TAB3, "PAPI_L1_DCM : ", (long_long) 0, (long_long) 0, (values[2])[0]);
-      printf
-          ("-------------------------------------------------------------------------\n");
-
-      printf("Verification:\n");
-      printf(TAB1, "First number row 1 approximately equals (2,2) + (3,3) or",
-             (values[1])[0] + (values[2])[0]);
+      printf(TAB1, "Sum 1 approximately equals sum 2 + sum 3 or", (sum[1] + sum[2]));
 #endif
    }
 
    {
-      long_long min, max;
+      long_long tmin, tmax;
 
 #if defined(sun) && defined(sparc)
-      max = (long_long) ((values[1])[0] - (values[2])[0]);
+      tmax = (long_long) (sum[1] - sum[2]);
 #else
-      max = (long_long) ((values[1])[0] + (values[2])[0]);
+      tmax = (long_long) (sum[1] + sum[2]);
 #endif
 
-      min = (long_long) (max * 0.8);
-      max = (long_long) (max * 1.2);
-      if (values[0][0] > max || values[0][0] < min)
+      printf ("percent error: %f\n", (float)(abs(tmax - sum[0])*100)/sum[0]);
+      tmin = (long_long) (tmax * 0.8);
+      tmax = (long_long) (tmax * 1.2);
+      if (sum[0] > tmax || sum[0] < tmin)
          test_fail(__FILE__, __LINE__, CACHE_LEVEL, 1);
    }
    test_pass(__FILE__, values, num_tests);
