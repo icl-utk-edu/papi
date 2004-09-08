57eced9e36631ba30bd07e2137068ba4fb3e9bab
london <london>
2004-09-08 14:20:06 +0000

    Skip PAPI_L1_ICM and PAPI_L2_ICM to make sure we have counts, as we don't always have an instruction cache miss and currently we don't have a loop to force them.

--- src/ctests/multiplex2.c
+++ src/ctests/multiplex2.c
@@ -52,6 +52,9 @@ int case1(void)
       test_fail(__FILE__, __LINE__, "PAPI_set_multiplex", retval);
 
    for (i = 0; i < PAPI_MAX_PRESET_EVENTS; i++) {
+      if ( (i|PAPI_PRESET_MASK) == PAPI_L1_ICM ) continue;
+      if ( (i|PAPI_PRESET_MASK) == PAPI_L2_ICM ) continue;
+
       retval = PAPI_get_event_info(i | PAPI_PRESET_MASK, &pset);
       if (retval != PAPI_OK)
          test_fail(__FILE__, __LINE__, "PAPI_get_event_info", retval);
