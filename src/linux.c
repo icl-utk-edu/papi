cdd76d5fbc0353a08181ebe2272b62c4f3afe6e2
Philip Mucci <mucci@eecs.utk.edu>
2005-04-28 12:31:29 +0000

    New version of update_SHLIB_info that now does the 'right' thing on all systems including IA64, PPC/32/64 and x86, x86_64.

---
+++ src/linux.c
@@ -0,0 +1,538 @@
+/* 
+* File:    linunx.c
+* CVS:     $Id$
+* Author:  Philip Mucci
+*          mucci@cs.utk.edu
+* Mods:    <your name here>
+*          <your email address>
+*/
+
+#include "papi.h"
+#include "papi_internal.h"
+
+/* This should be in a linux.h header file maybe. */
+#define FOPEN_ERROR "fopen(%s) returned NULL"
+
+/****************************/
+/* BEGIN LOCAL DECLARATIONS */
+/****************************/
+
+/**************************/
+/* END LOCAL DECLARATIONS */
+/**************************/
+
+/******************************/
+/* BEGIN STOLEN/MODIFIED CODE */
+/******************************/
+
+#ifdef _WIN32
+
+int _papi_hwd_update_shlib_info(void)
+{
+   return PAPI_ESBSTR;
+}
+
+
+// split the filename from a full path
+// roughly equivalent to unix basename()
+static void splitpath(const char *path, char *name)
+{
+	short i = 0, last = 0;
+	
+	while (path[i]) {
+		if (path[i] == '\\') last = i;
+		i++;
+	}
+	name[0] = 0;
+	i = i - last;
+	if (last > 0) {
+		last++;
+		i--;
+	}
+	strncpy(name, &path[last], i);
+	name[i] = 0;
+}
+
+int _papi_hwd_get_system_info(void)
+{
+  struct wininfo win_hwinfo;
+  HMODULE hModule;
+  DWORD len;
+  long i = 0;
+
+  /* Path and args */
+  _papi_hwi_system_info.pid = getpid();
+
+  hModule = GetModuleHandle(NULL); // current process
+  len = GetModuleFileName(hModule,_papi_hwi_system_info.exe_info.fullname,PAPI_MAX_STR_LEN);
+  if (len) splitpath(_papi_hwi_system_info.exe_info.fullname, _papi_hwi_system_info.exe_info.address_info.name);
+  else return(PAPI_ESYS);
+
+  SUBDBG("Executable is %s\n",_papi_hwi_system_info.exe_info.address_info.name);
+  SUBDBG("Full Executable is %s\n",_papi_hwi_system_info.exe_info.fullname);
+
+  /* Hardware info */
+  if (!init_hwinfo(&win_hwinfo))
+    return(PAPI_ESYS);
+
+  _papi_hwi_system_info.hw_info.ncpu = win_hwinfo.ncpus;
+  _papi_hwi_system_info.hw_info.nnodes = win_hwinfo.nnodes;
+  _papi_hwi_system_info.hw_info.totalcpus = win_hwinfo.total_cpus;
+
+  _papi_hwi_system_info.hw_info.vendor = win_hwinfo.vendor;
+  _papi_hwi_system_info.hw_info.revision = (float)win_hwinfo.revision;
+  strcpy(_papi_hwi_system_info.hw_info.vendor_string,win_hwinfo.vendor_string);
+
+  /* initialize the model to something */
+  _papi_hwi_system_info.hw_info.model = PERFCTR_X86_GENERIC;
+
+  if (IS_P3(&win_hwinfo) || IS_P3_XEON(&win_hwinfo) || IS_CELERON(&win_hwinfo))
+    _papi_hwi_system_info.hw_info.model = PERFCTR_X86_INTEL_PIII;
+
+  if (IS_MOBILE(&win_hwinfo))
+    _papi_hwi_system_info.hw_info.model = PERFCTR_X86_INTEL_PENTM;
+
+  if (IS_P4(&win_hwinfo)) {
+    if (win_hwinfo.model >= 2)
+      /* this is a guess for Pentium 4 Model 2 */
+      _papi_hwi_system_info.hw_info.model = PERFCTR_X86_INTEL_P4M2;
+    else
+      _papi_hwi_system_info.hw_info.model = PERFCTR_X86_INTEL_P4;
+  }
+
+  if (IS_AMDDURON(&win_hwinfo) || IS_AMDATHLON(&win_hwinfo))
+    _papi_hwi_system_info.hw_info.model = PERFCTR_X86_AMD_K7;
+
+  strcpy(_papi_hwi_system_info.hw_info.model_string,win_hwinfo.model_string);
+
+  _papi_hwi_system_info.num_cntrs = win_hwinfo.nrctr;
+  _papi_hwi_system_info.num_gp_cntrs = _papi_hwi_system_info.num_cntrs;
+
+  _papi_hwi_system_info.hw_info.mhz = (float)win_hwinfo.mhz; 
+
+  return(PAPI_OK);
+}
+
+#else
+#ifdef __CATAMOUNT__
+
+int _papi_hwd_update_shlib_info(void)
+{
+   /* Catamount doesn't support shared libraries */
+   return PAPI_ESBSTR;
+}
+
+int _papi_hwd_get_system_info(void)
+{
+   pid_t pid;
+
+   /* Software info */
+
+   /* Path and args */
+
+   pid = getpid();
+   if (pid < 0)
+     { PAPIERROR("getpid() returned < 0"); return(PAPI_ESYS); }
+   _papi_hwi_system_info.pid = pid;
+
+   /* executable name is hardcoded for Catamount */
+   sprintf(_papi_hwi_system_info.exe_info.fullname,"/home/a.out");
+	sprintf(_papi_hwi_system_info.exe_info.address_info.name,"%s",
+                  basename(_papi_hwi_system_info.exe_info.fullname));
+
+   /* Hardware info */
+
+  _papi_hwi_system_info.hw_info.ncpu = 1;
+  _papi_hwi_system_info.hw_info.nnodes = 1;
+  _papi_hwi_system_info.hw_info.totalcpus = 1;
+  _papi_hwi_system_info.hw_info.vendor = 2;
+
+	sprintf(_papi_hwi_system_info.hw_info.vendor_string,"AuthenticAMD");
+	_papi_hwi_system_info.hw_info.revision = 1;
+
+  return(PAPI_OK);
+}
+
+#else
+
+int _papi_hwd_update_shlib_info(void)
+{
+   char fname[PAPI_HUGE_STR_LEN];
+   PAPI_address_map_t *tmp, *tmp2;
+   FILE *f;
+   char find_data_mapname[PAPI_HUGE_STR_LEN] = "";
+   int upper_bound = 0, i, index = 0, find_data_index = 0, count = 0;
+   char buf[PAPI_HUGE_STR_LEN + PAPI_HUGE_STR_LEN], perm[5], dev[6], mapname[PAPI_HUGE_STR_LEN];
+   unsigned long begin, end, size, inode, foo;
+
+   sprintf(fname, "/proc/%ld/maps", (long)_papi_hwi_system_info.pid);
+   f = fopen(fname, "r");
+   if (!f)
+     { 
+	 PAPIERROR("fopen(%s) returned < 0", fname); 
+	 return(PAPI_OK); 
+     }
+
+   /* First count up things that look kinda like text segments, this is an upper bound */
+
+   while (1)
+     {
+      if (fgets(buf, sizeof(buf), f) == NULL)
+	{
+	  if (ferror(f))
+	    {
+	      PAPIERROR("fgets(%s, %d) returned < 0", fname, sizeof(buf)); 
+	      fclose(f);
+	      return(PAPI_OK); 
+	    }
+	  else
+	    break;
+	}
+
+      sscanf(buf, "%lx-%lx %4s %lx %5s %ld %s", &begin, &end, perm, &foo, dev, &inode, mapname);
+
+      if (strlen(mapname) && (perm[0] == 'r') && (perm[1] != 'w') && (perm[2] == 'x') && (inode != 0))
+	{
+	  upper_bound++;
+	}
+     }
+   if (upper_bound == 0)
+     {
+       PAPIERROR("No segments found with r-x, inode != 0 and non-NULL mapname"); 
+       fclose(f);
+       return(PAPI_OK); 
+     }
+
+   /* Alloc our temporary space */
+
+   tmp = (PAPI_address_map_t *) calloc(upper_bound, sizeof(PAPI_address_map_t));
+   if (tmp == NULL)
+     {
+       PAPIERROR("calloc(%d) failed", upper_bound*sizeof(PAPI_address_map_t));
+       fclose(f);
+       return(PAPI_OK);
+     }
+      
+   rewind(f);
+   while (1)
+     {
+      if (fgets(buf, sizeof(buf), f) == NULL)
+	{
+	  if (ferror(f))
+	    {
+	      PAPIERROR("fgets(%s, %d) returned < 0", fname, sizeof(buf)); 
+	      fclose(f);
+	      free(tmp);
+	      return(PAPI_OK); 
+	    }
+	  else
+	    break;
+	}
+
+      sscanf(buf, "%lx-%lx %4s %lx %5s %ld %s", &begin, &end, perm, &foo, dev, &inode, mapname);
+      size = end - begin;
+
+      if (strlen(mapname) == 0)
+	continue;
+
+      if ((strcmp(find_data_mapname,mapname) == 0) && (perm[0] == 'r') && (perm[1] == 'w') && (inode != 0))
+	{
+	  tmp[find_data_index].data_start = (caddr_t) begin;
+	  tmp[find_data_index].data_end = (caddr_t) (begin + size);
+	  find_data_mapname[0] = '\0';
+	}
+      else if ((perm[0] == 'r') && (perm[1] != 'w') && (perm[2] == 'x') && (inode != 0))
+	{
+	  /* Text segment, check if we've seen it before, if so, ignore it. Some entries
+	     have multiple r-xp entires. */
+
+	  for (i=0;i<upper_bound;i++)
+	    {
+	      if (strlen(tmp[i].name))
+		{
+		  if (strcmp(mapname,tmp[i].name) == 0)
+		    break;
+		}
+	      else
+		{
+		  /* Record the text, and indicate that we are to find the data segment, following this map */
+		  strcpy(tmp[i].name,mapname);
+		  tmp[i].text_start = (caddr_t) begin;
+		  tmp[i].text_end = (caddr_t) (begin + size);
+		  count++;
+		  strcpy(find_data_mapname,mapname);
+		  find_data_index = i;
+		  break;
+		}
+	    }
+	}
+     }
+   if (count == 0)
+     {
+       PAPIERROR("No segments found with r-x, inode != 0 and non-NULL mapname"); 
+       fclose(f);
+       free(tmp);
+       return(PAPI_OK); 
+     }
+   fclose(f);
+
+   /* Now condense the list and update exe_info */
+   tmp2 = (PAPI_address_map_t *) calloc(count, sizeof(PAPI_address_map_t));
+   if (tmp2 == NULL)
+     {
+       PAPIERROR("calloc(%d) failed", count*sizeof(PAPI_address_map_t));
+       free(tmp);
+       fclose(f);
+       return(PAPI_OK);
+     }
+
+   for (i=0;i<count;i++)
+     {
+       if (strcmp(tmp[i].name,_papi_hwi_system_info.exe_info.fullname) == 0)
+	 {
+	   _papi_hwi_system_info.exe_info.address_info.text_start = tmp[i].text_start;
+	   _papi_hwi_system_info.exe_info.address_info.text_end = tmp[i].text_end;
+	   _papi_hwi_system_info.exe_info.address_info.data_start = tmp[i].data_start;
+	   _papi_hwi_system_info.exe_info.address_info.data_end = tmp[i].data_end;
+	 }
+       else
+	 {
+	   strcpy(tmp2[index].name,tmp[i].name);
+	   tmp2[index].text_start = tmp[i].text_start;
+	   tmp2[index].text_end = tmp[i].text_end;
+	   tmp2[index].data_start = tmp[i].data_start;
+	   tmp2[index].data_end = tmp[i].data_end;
+	   index++;
+	 }
+     }
+   free(tmp);
+
+   if (_papi_hwi_system_info.shlib_info.map)
+     free(_papi_hwi_system_info.shlib_info.map);
+   _papi_hwi_system_info.shlib_info.map = tmp2;
+   _papi_hwi_system_info.shlib_info.count = index;
+
+   return (PAPI_OK);
+}
+
+/****************************/
+/* END STOLEN/MODIFIED CODE */
+/****************************/
+
+static char *search_cpu_info(FILE * f, char *search_str, char *line)
+{
+   /* This code courtesy of our friends in Germany. Thanks Rudolph Berrendorf! */
+   /* See the PCL home page for the German version of PAPI. */
+
+   char *s;
+
+   while (fgets(line, 256, f) != NULL) {
+      if (strncmp(line, search_str, strlen(search_str)) == 0) {
+         /* ignore all characters in line up to : */
+         for (s = line; *s && (*s != ':'); ++s);
+         if (*s)
+            return (s);
+      }
+   }
+   return (NULL);
+
+   /* End stolen code */
+}
+/* Pentium III
+ * processor  : 1
+ * vendor     : GenuineIntel
+ * arch       : IA-64
+ * family     : Itanium 2
+ * model      : 0
+ * revision   : 7
+ * archrev    : 0
+ * features   : branchlong
+ * cpu number : 0
+ * cpu regs   : 4
+ * cpu MHz    : 900.000000
+ * itc MHz    : 900.000000
+ * BogoMIPS   : 1346.37
+ * */
+/* IA64
+ * processor       : 1
+ * vendor_id       : GenuineIntel
+ * cpu family      : 6
+ * model           : 7
+ * model name      : Pentium III (Katmai)
+ * stepping        : 3
+ * cpu MHz         : 547.180
+ * cache size      : 512 KB
+ * physical id     : 0
+ * siblings        : 1
+ * fdiv_bug        : no
+ * hlt_bug         : no
+ * f00f_bug        : no
+ * coma_bug        : no
+ * fpu             : yes
+ * fpu_exception   : yes
+ * cpuid level     : 2
+ * wp              : yes
+ * flags           : fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 mmx fxsr sse
+ * bogomips        : 1091.17
+ * */
+
+int _papi_hwd_get_system_info(void)
+{
+   int tmp, retval;
+   char maxargs[PAPI_HUGE_STR_LEN], *t, *s;
+   pid_t pid;
+   float mhz = 0.0;
+   FILE *f;
+
+   /* Software info */
+
+   /* Path and args */
+
+   pid = getpid();
+   if (pid < 0)
+     { PAPIERROR("getpid() returned < 0"); return(PAPI_ESYS); }
+   _papi_hwi_system_info.pid = pid;
+
+   sprintf(maxargs, "/proc/%d/exe", (int) pid);
+   if (readlink(maxargs, _papi_hwi_system_info.exe_info.fullname, PAPI_HUGE_STR_LEN) < 0)
+   { 
+       PAPIERROR("readlink(%s) returned < 0", maxargs); 
+       strcpy(_papi_hwi_system_info.exe_info.fullname,"");
+       strcpy(_papi_hwi_system_info.exe_info.address_info.name,"");
+   }
+   else
+   {
+   /* basename can modify it's argument */
+   strcpy(maxargs,_papi_hwi_system_info.exe_info.fullname);
+   strcpy(_papi_hwi_system_info.exe_info.address_info.name, basename(maxargs));
+   }
+
+   /* Executable regions, may require reading /proc/pid/maps file */
+
+   retval = _papi_hwd_update_shlib_info();
+
+   /* PAPI_preload_option information */
+
+   strcpy(_papi_hwi_system_info.preload_info.lib_preload_env, "LD_PRELOAD");
+   _papi_hwi_system_info.preload_info.lib_preload_sep = ' ';
+   strcpy(_papi_hwi_system_info.preload_info.lib_dir_env, "LD_LIBRARY_PATH");
+   _papi_hwi_system_info.preload_info.lib_dir_sep = ':';
+
+   SUBDBG("Executable is %s\n", _papi_hwi_system_info.exe_info.address_info.name);
+   SUBDBG("Full Executable is %s\n", _papi_hwi_system_info.exe_info.fullname);
+   SUBDBG("Text: Start %p, End %p, length %d\n",
+          _papi_hwi_system_info.exe_info.address_info.text_start,
+          _papi_hwi_system_info.exe_info.address_info.text_end,
+          (int)(_papi_hwi_system_info.exe_info.address_info.text_end -
+          _papi_hwi_system_info.exe_info.address_info.text_start));
+   SUBDBG("Data: Start %p, End %p, length %d\n",
+          _papi_hwi_system_info.exe_info.address_info.data_start,
+          _papi_hwi_system_info.exe_info.address_info.data_end,
+          (int)(_papi_hwi_system_info.exe_info.address_info.data_end -
+          _papi_hwi_system_info.exe_info.address_info.data_start));
+   SUBDBG("Bss: Start %p, End %p, length %d\n",
+          _papi_hwi_system_info.exe_info.address_info.bss_start,
+          _papi_hwi_system_info.exe_info.address_info.bss_end,
+          (int)(_papi_hwi_system_info.exe_info.address_info.bss_end -
+          _papi_hwi_system_info.exe_info.address_info.bss_start));
+
+   /* Hardware info */
+
+   _papi_hwi_system_info.hw_info.ncpu = sysconf(_SC_NPROCESSORS_ONLN);
+   _papi_hwi_system_info.hw_info.nnodes = 1;
+   _papi_hwi_system_info.hw_info.totalcpus = sysconf(_SC_NPROCESSORS_CONF);
+   _papi_hwi_system_info.hw_info.vendor = -1;
+
+   if ((f = fopen("/proc/cpuinfo", "r")) == NULL)
+     { PAPIERROR("fopen(/proc/cpuinfo) errno %d",errno); return(PAPI_ESYS); }
+
+   /* All of this information maybe overwritten by the substrate */ 
+
+   /* MHZ */
+
+   rewind(f);
+   s = search_cpu_info(f, "cpu MHz", maxargs);
+   if (s)
+      sscanf(s + 1, "%f", &mhz);
+   _papi_hwi_system_info.hw_info.mhz = mhz;
+
+   /* Vendor Name */
+
+   rewind(f);
+   s = search_cpu_info(f, "vendor_id", maxargs);
+   if (s && (t = strchr(s + 2, '\n'))) 
+     {
+      *t = '\0';
+      strcpy(_papi_hwi_system_info.hw_info.vendor_string, s + 2);
+     }
+   else 
+     {
+       rewind(f);
+       s = search_cpu_info(f, "vendor", maxargs);
+       if (s && (t = strchr(s + 2, '\n'))) {
+	 *t = '\0';
+	 strcpy(_papi_hwi_system_info.hw_info.vendor_string, s + 2);
+       }
+     }
+       
+   /* Revision */
+
+   rewind(f);
+   s = search_cpu_info(f, "stepping", maxargs);
+   if (s)
+      {
+	sscanf(s + 1, "%d", &tmp);
+	_papi_hwi_system_info.hw_info.revision = (float) tmp;
+      }
+   else
+     {
+       rewind(f);
+       s = search_cpu_info(f, "revision", maxargs);
+       if (s)
+	 {
+	   sscanf(s + 1, "%d", &tmp);
+	   _papi_hwi_system_info.hw_info.revision = (float) tmp;
+	 }
+     }
+
+   /* Model Name */
+
+   rewind(f);
+   s = search_cpu_info(f, "family", maxargs);
+   if (s && (t = strchr(s + 2, '\n'))) 
+     {
+       *t = '\0';
+       strcpy(_papi_hwi_system_info.hw_info.model_string, s + 2);
+     }
+   else 
+     {
+       rewind(f);
+       s = search_cpu_info(f, "vendor", maxargs);
+       if (s && (t = strchr(s + 2, '\n'))) 
+	 {
+	   *t = '\0';
+	   strcpy(_papi_hwi_system_info.hw_info.vendor_string, s + 2);
+	 }
+     }
+
+   rewind(f);
+   s = search_cpu_info(f, "model", maxargs);
+   if (s)
+      {
+	sscanf(s + 1, "%d", &tmp);
+	_papi_hwi_system_info.hw_info.model = tmp;
+      }
+
+   fclose(f);
+
+   SUBDBG("Found %d %s(%d) %s(%d) CPU's at %f Mhz.\n",
+          _papi_hwi_system_info.hw_info.totalcpus,
+          _papi_hwi_system_info.hw_info.vendor_string,
+          _papi_hwi_system_info.hw_info.vendor,
+          _papi_hwi_system_info.hw_info.model_string,
+          _papi_hwi_system_info.hw_info.model, _papi_hwi_system_info.hw_info.mhz);
+
+   return (PAPI_OK);
+}
+#endif /* __CATAMOUNT__ */
+#endif /* _WIN32 */
