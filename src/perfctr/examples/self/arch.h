338a57a9c6eff43bba1ae6c50b664ed12383d9cd
Philip Mucci <mucci@eecs.utk.edu>
2005-07-22 18:03:52 +0000

    This directory should have been emptied long ago.

--- src/perfctr/examples/self/arch.h
+++
@@ -1,8 +0,0 @@
-/* $Id$
- * Architecture-specific support code.
- *
- * Copyright (C) 2004  Mikael Pettersson
- */
-
-extern void do_setup(const struct perfctr_info *info,
-		     struct perfctr_cpu_control *cpu_control);
