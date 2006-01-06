07d3d15f7729b742383d0e91b53cf87c571e7ddc
Dan Terpstra <terpstra@eecs.utk.edu>
2006-01-06 21:36:05 +0000

    Added support for the myrinet mx substrate.

--- src/papi_table.c
+++ src/papi_table.c
@@ -8,10 +8,18 @@ char *  const init_str[] = {"proc_substrate"
 #ifdef HAS_ACPI
 	,"acpi"
 #endif
+#ifdef HAS_MYRINET_MX
+	,"myrinet_mx"
+#endif
   , NULL
 };
 
+#ifdef HAS_ACPI
 int _papi_hwd_init_acpi_substrate(papi_vectors_t *vtable, int idx);
+#endif
+#ifdef HAS_MYRINET_MX
+int _papi_hwd_init_myrinet_mx_substrate(papi_vectors_t *vtable, int idx);
+#endif
 
 InitPtr _papi_hwi_find_init(char *name){
 
@@ -20,6 +28,9 @@ InitPtr _papi_hwi_find_init(char *name){
   if (!strcmp(name, "proc_substrate")) return _papi_hwd_init_substrate;
 #ifdef HAS_ACPI
   if ( !strcmp(name, "acpi")) return _papi_hwd_init_acpi_substrate;
+#endif
+#ifdef HAS_MYRINET_MX
+  if ( !strcmp(name, "myrinet_mx")) return _papi_hwd_init_myrinet_mx_substrate;
 #endif
 
   INTDBG("No substrate named: %s found!\n", name);
