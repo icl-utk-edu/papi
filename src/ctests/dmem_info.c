fa721cc200a2165e0d99345b5a4b8af98e1e177c
Dan Terpstra <terpstra@eecs.utk.edu>
2006-05-10 20:18:40 +0000

    Reworked for new implementation of dynamic memory stuff based on Phil's new model for linux.

--- src/ctests/dmem_info.c
+++ src/ctests/dmem_info.c
@@ -8,77 +8,60 @@
 #include "papi_test.h"
 
 #define ALLOCMEM 200000
+static void dump_memory_info(FILE *output, PAPI_dmem_info_t *d)
+{
+  fprintf(output,"\n--------\n");
+  fprintf(output,"Mem Size:\t\t%lld\n",d->size);
+  fprintf(output,"Mem Resident:\t\t%lld\n",d->resident);
+  fprintf(output,"Mem High Water Mark:\t%lld\n",d->high_water_mark);
+  fprintf(output,"Mem Shared:\t\t%lld\n",d->shared);
+  fprintf(output,"Mem Text:\t\t%lld\n",d->text);
+  fprintf(output,"Mem Library:\t\t%lld\n",d->library);
+  fprintf(output,"Mem Heap:\t\t%lld\n",d->heap);
+  fprintf(output,"Mem Locked:\t\t%lld\n",d->locked);
+  fprintf(output,"Mem Stack:\t\t%lld\n",d->stack);
+  fprintf(output,"Mem Pagesize:\t\t%lld\n",d->pagesize);
+  fprintf(output,"--------\n\n");
+}
 
 int main(int argc, char **argv)
 {
-#ifdef PAPI_MEM_INFO
-   long value[7];
-   int retval, i;
-   double *a, *b, *c, *d, *e, *f;
-
+   PAPI_dmem_info_t dmem;
+   long_long value[7];
+   int retval, i = 0, j = 0;
+   double *m[7];
+ 
    tests_quiet(argc, argv);     /* Set TESTS_QUIET variable */
    retval = PAPI_library_init(PAPI_VER_CURRENT);
    if (retval != PAPI_VER_CURRENT)
       test_fail(__FILE__, __LINE__, "PAPI_library_init", retval);
 
-   if ((value[0] = PAPI_get_dmem_info(PAPI_GET_RESSIZE)) < 0)
-      test_fail(__FILE__, __LINE__, "PAPI_get_dmem_info", value[0]);
-
-   a = (double *) malloc(ALLOCMEM * sizeof(double));
-   touch_dummy(a, ALLOCMEM);
-   if ((value[1] = PAPI_get_dmem_info(PAPI_GET_RESSIZE)) < 0)
-      test_fail(__FILE__, __LINE__, "PAPI_get_dmem_info", value[1]);
-
-   b = (double *) malloc(ALLOCMEM * sizeof(double));
-   touch_dummy(b, ALLOCMEM);
-   if ((value[2] = PAPI_get_dmem_info(PAPI_GET_RESSIZE)) < 0)
-      test_fail(__FILE__, __LINE__, "PAPI_get_dmem_info", value[2]);
-
-   c = (double *) malloc(ALLOCMEM * sizeof(double));
-   touch_dummy(c, ALLOCMEM);
-   if ((value[3] = PAPI_get_dmem_info(PAPI_GET_RESSIZE)) < 0)
-      test_fail(__FILE__, __LINE__, "PAPI_get_dmem_info", value[3]);
-
-   d = (double *) malloc(ALLOCMEM * sizeof(double));
-   touch_dummy(d, ALLOCMEM);
-   if ((value[4] = PAPI_get_dmem_info(PAPI_GET_RESSIZE)) < 0)
-      test_fail(__FILE__, __LINE__, "PAPI_get_dmem_info", value[4]);
-
-   e = (double *) malloc(ALLOCMEM * sizeof(double));
-   touch_dummy(e, ALLOCMEM);
-   if ((value[5] = PAPI_get_dmem_info(PAPI_GET_RESSIZE)) < 0)
-      test_fail(__FILE__, __LINE__, "PAPI_get_dmem_info", value[5]);
-
-   f = (double *) malloc(ALLOCMEM * sizeof(double));
-   touch_dummy(f, ALLOCMEM);
-   if ((value[6] = PAPI_get_dmem_info(PAPI_GET_RESSIZE)) < 0)
-      test_fail(__FILE__, __LINE__, "PAPI_get_dmem_info", value[6]);
-
+   for (i=0;i<7;i++) {
+	   retval = PAPI_get_dmem_info(&dmem);
+	   if (retval != PAPI_OK)
+		  test_fail(__FILE__, __LINE__, "PAPI_get_dmem_info", retval);
+//	   dump_memory_info(stdout,&dmem);
+	   value[i] = dmem.size;
+	   m[i] = (double *) malloc(ALLOCMEM * sizeof(double));
+	   touch_dummy(m[j], ALLOCMEM);
+   }
 
    if (!TESTS_QUIET) {
       printf("Test case:  Dynamic Memory Information.\n");
+	  dump_memory_info(stdout,&dmem);
       printf
           ("------------------------------------------------------------------------\n");
       for (i = 0; i < 7; i++)
-         printf("Malloc additional: %d KB  Resident Size in Pages: %ld\n",
+         printf("Malloc additional: %d KB  Memory Size in KB: %lld\n",
                 ((sizeof(double) * ALLOCMEM) / 1024), value[i]);
       printf
           ("------------------------------------------------------------------------\n");
-      printf("Resident Size in Pages: %ld  Size in Pages: %ld Total Malloc size: %d KB\n",
-             value[6], PAPI_get_dmem_info(PAPI_GET_SIZE),
-             ((6 * sizeof(double) * ALLOCMEM) / 1024));
-      printf("Pagesize in bytes: %ld  Total Resident Memory: %ld KB\n",
-             PAPI_get_dmem_info(PAPI_GET_PAGESIZE),
-             (PAPI_get_dmem_info(PAPI_GET_PAGESIZE) * value[6] / 1024));
    }
    if (value[6] >= value[5] && value[5] >= value[4] && value[4] >= value[3]
        && value[3] >= value[2] && value[2] >= value[1] && value[1] >= value[0])
       test_pass(__FILE__, NULL, 0);
    else
       test_fail(__FILE__, __LINE__, "Calculating Resident Memory", value[6]);
+
    exit(1);
-#else
-   test_skip(__FILE__, __LINE__, "PAPI_dmem_info has been deprecated pending a redesign", 0);
-   exit(1);
-#endif
 }
