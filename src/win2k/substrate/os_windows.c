cf905ef9b140daf182a42d4a088cecc8121719e2
Dan Terpstra <terpstra@eecs.utk.edu>
2006-08-11 19:53:33 +0000

    Updates for PAPI 3.3 and Opteron support.

--- src/win2k/substrate/os_windows.c
+++ src/win2k/substrate/os_windows.c
@@ -102,38 +102,56 @@ static void lock_release(void)
 
 #ifndef PAPI_NO_VECTOR
 papi_svector_t _windows_os_table[] = {
- #ifndef __CATAMOUNT__
-   {(void (*)())_papi_hwd_update_shlib_info, VEC_PAPI_HWD_UPDATE_SHLIB_INFO},
- #endif
  {(void (*)())_papi_hwd_init, VEC_PAPI_HWD_INIT},
-/****WIN32: this callback requires a totally different structure for Windows
- {(void (*)())_papi_hwd_dispatch_timer, VEC_PAPI_HWD_DISPATCH_TIMER},
-*/
+ {(void (*)())_papi_hwd_timer_callback, VEC_PAPI_HWD_DISPATCH_TIMER},
  {(void (*)())_papi_hwd_ctl, VEC_PAPI_HWD_CTL},
  {(void (*)())_papi_hwd_get_real_usec, VEC_PAPI_HWD_GET_REAL_USEC},
  {(void (*)())_papi_hwd_get_real_cycles, VEC_PAPI_HWD_GET_REAL_CYCLES},
- {(void (*)())_papi_hwd_get_virt_cycles, VEC_PAPI_HWD_GET_VIRT_CYCLES},
  {(void (*)())_papi_hwd_get_virt_usec, VEC_PAPI_HWD_GET_VIRT_USEC},
  { NULL, VEC_PAPI_END}
 };
 #endif
 
+HANDLE pmc_dev = NULL;	// device handle for kernel driver
+
+/* Called once per process. */
+
+int _papi_hwd_shutdown_global(void) {
+  pmc_close(pmc_dev);
+  pmc_dev = NULL;
+  lock_release();
+   return (PAPI_OK);
+}
+
+int _papi_hwd_init(hwd_context_t *ctx)
+{
+   /* Initialize our thread/process pointer. */
+   if ((ctx->self = pmc_dev = pmc_open()) == NULL) {
+      PAPIERROR("pmc_open() returned NULL"); 
+      return(PAPI_ESYS);
+   }
+   SUBDBG("_papi_hwd_init pmc_open() = %p\n", ctx->self);
+
+   /* Linux makes sure that each thread has a virtualized TSC here.
+      This makes no sense on Windows, since the counters aren't
+      saved at context switch.
+   */
+
+   return(PAPI_OK);
+}
+
 int _papi_hwd_init_substrate(papi_vectors_t *vtable)
 {
   int retval;
   int cpu_type;
-  char abiv[PAPI_MIN_STR_LEN];
+  HANDLE dh;	// device handle for kernel driver
+
 
   /* Setup the vector entries that the OS knows about */
 #ifndef PAPI_NO_VECTOR
   retval = _papi_hwi_setup_vector_table( vtable, _windows_os_table);
   if ( retval != PAPI_OK ) return(retval);
 #endif
-
-// **** Phil's Changes
-//   retval = mdi_init();
-//   if ( retval ) 
-//     return(retval);
 
   /* Fill in what we can of the papi_system_info. */
   retval = _papi_hwd_get_system_info();
@@ -145,9 +163,6 @@ int _papi_hwd_init_substrate(papi_vectors_t *vtable)
    retval = _papi_hwd_get_memory_info(&_papi_hwi_system_info.hw_info, cpu_type);
    if (retval)
       return (retval);
-
-// **** Phil's Changes
-//   strcpy(_papi_hwi_system_info.sub_info.name, "$Id$");
 
    /* Setup presets */
    if ( check_p4(cpu_type) ){
@@ -163,47 +178,26 @@ int _papi_hwd_init_substrate(papi_vectors_t *vtable)
    if ( retval ) 
      return(retval);
 
-   // **** Phil's Changes
-   // This stuff needs to be carefully examined in the Windows context
    strcpy(_papi_hwi_system_info.sub_info.name, "$Id$");
-   strcpy(_papi_hwi_system_info.sub_info.version, "$Revision$");
-//   sprintf(abiv,"0x%08X",info.abi_version);
-   strcpy(_papi_hwi_system_info.sub_info.support_version, abiv);
-//   strcpy(_papi_hwi_system_info.sub_info.kernel_version, info.driver_version);
-//   _papi_hwi_system_info.sub_info.num_cntrs = PERFCTR_CPU_NRCTRS(&info);
-//   _papi_hwi_system_info.sub_info.fast_counter_read = (info.cpu_features & PERFCTR_FEATURE_RDPMC) ? 1 : 0;
+   strcpy(_papi_hwi_system_info.sub_info.version, "$Revision$"); // cvs revision of this file
+   dh = pmc_open();
+   strcpy(_papi_hwi_system_info.sub_info.support_version, pmc_kernel_version(dh));
+   pmc_close(dh);
+
+   strcpy(_papi_hwi_system_info.sub_info.kernel_version, pmc_revision()); // cvs revision # of pmclib.c
+   _papi_hwi_system_info.sub_info.fast_counter_read = 1; // built into WinPMC driver
    _papi_hwi_system_info.sub_info.fast_real_timer = 1;
    _papi_hwi_system_info.sub_info.fast_virtual_timer = 1;
    _papi_hwi_system_info.sub_info.default_domain = PAPI_DOM_USER;
    _papi_hwi_system_info.sub_info.available_domains = PAPI_DOM_USER|PAPI_DOM_KERNEL;
    _papi_hwi_system_info.sub_info.default_granularity = PAPI_GRN_THR;
    _papi_hwi_system_info.sub_info.available_granularities = PAPI_GRN_THR;
-//   _papi_hwi_system_info.sub_info.hardware_intr =
-//       (info.cpu_features & PERFCTR_FEATURE_PCINT) ? 1 : 0;
+   /****WIN32: can we figure out how to get Windows to support hdw interrupts on
+    counter overflow? The hardware supports it; does the OS? */
+   _papi_hwi_system_info.sub_info.hardware_intr = 0; // no hardware interrupt on Windows
 
    SUBDBG("Hardware/OS %s support counter generated interrupts\n",
           _papi_hwi_system_info.sub_info.hardware_intr ? "does" : "does not");
-
-//   strcpy(_papi_hwi_system_info.hw_info.model_string, PERFCTR_CPU_NAME(&info));
-
-
-   /* Fixup stuff from os_windows.c */
-
-/****WIN32: all this stuff comes out of the info structure;
-	we should emulate it if we need it.
-
-   strcpy(_papi_hwi_system_info.hw_info.model_string, PERFCTR_CPU_NAME(&info));
-
-   _papi_hwi_system_info.supports_hw_overflow =
-       (info.cpu_features & PERFCTR_FEATURE_PCINT) ? 1 : 0;
-   SUBDBG("Hardware/OS %s support counter generated interrupts\n",
-          _papi_hwi_system_info.supports_hw_overflow ? "does" : "does not");
-
-   _papi_hwi_system_info.num_cntrs = PERFCTR_CPU_NRCTRS(&info);
-   _papi_hwi_system_info.num_gp_cntrs = PERFCTR_CPU_NRCTRS(&info);
-   _papi_hwi_system_info.hw_info.model = info.cpu_type;
-   _papi_hwi_system_info.hw_info.vendor = xlate_cpu_type_to_vendor(cpu_type);
-*/
 
    lock_init();
 
@@ -255,212 +249,6 @@ void CALLBACK _papi_hwd_timer_callback(UINT wTimerID, UINT msg,
 	OVERFLOW_MASK, GEN_OVERFLOW, &master); 
 }
 
-HANDLE pmc_dev;	// device handle for kernel driver
-
-/* Called once per process. */
-
-int _papi_hwd_shutdown_global(void) {
-  pmc_close(pmc_dev);
-  lock_release();
-   return (PAPI_OK);
-}
-
-int _papi_hwd_init(hwd_context_t *ctx)
-{
-   /* Initialize our thread/process pointer. */
-   if ((ctx->self = pmc_dev = pmc_open()) == NULL) {
-      PAPIERROR("pmc_open() returned NULL"); 
-      return(PAPI_ESYS);
-   }
-   SUBDBG("_papi_hwd_init pmc_open() = %p\n", ctx->self);
-
-   /* Linux makes sure that each thread has a virtualized TSC here.
-      This makes no sense on Windows, since the counters aren't
-      saved at context switch.
-   */
-
-   return(PAPI_OK);
-}
-
-// **** Phil's  Changes
-/* Initialize the system-specific settings */
-/* Machine info structure. -1 is unused. */
-/*
-static int mdi_init() {
-     /* Name of the substrate we're using */
-/*
-   _papi_hwi_system_info.sub_info.supports_hw_overflow = 0;
-   _papi_hwi_system_info.supports_64bit_counters = 1;
-   _papi_hwi_system_info.supports_inheritance = 0;
-   _papi_hwi_system_info.supports_real_usec = 1;
-   _papi_hwi_system_info.supports_real_cyc = 1;
-   _papi_hwi_system_info.supports_virt_usec = 0;
-   _papi_hwi_system_info.supports_virt_cyc = 0;
-
-    return (PAPI_OK);
-}
-*/
-int _papi_hwd_update_shlib_info(void)
-{
-   char fname[PAPI_HUGE_STR_LEN];
-   PAPI_address_map_t *tmp, *tmp2;
-   FILE *f;
-   char find_data_mapname[PAPI_HUGE_STR_LEN] = "";
-   int upper_bound = 0, i, index = 0, find_data_index = 0, count = 0;
-   char buf[PAPI_HUGE_STR_LEN + PAPI_HUGE_STR_LEN], perm[5], dev[6], mapname[PAPI_HUGE_STR_LEN];
-   unsigned long begin, end, size, inode, foo;
-
-   sprintf(fname, "/proc/%ld/maps", (long)_papi_hwi_system_info.pid);
-   f = fopen(fname, "r");
-
-   if (!f)
-     { 
-	 PAPIERROR("fopen(%s) returned < 0", fname); 
-	 return(PAPI_OK); 
-     }
-
-   /* First count up things that look kinda like text segments, this is an upper bound */
-
-   while (1)
-     {
-      if (fgets(buf, sizeof(buf), f) == NULL)
-	{
-	  if (ferror(f))
-	    {
-	      PAPIERROR("fgets(%s, %d) returned < 0", fname, sizeof(buf)); 
-	      fclose(f);
-	      return(PAPI_OK); 
-	    }
-	  else
-	    break;
-	}
-
-      sscanf(buf, "%lx-%lx %4s %lx %5s %ld %s", &begin, &end, perm, &foo, dev, &inode, mapname);
-
-      if (strlen(mapname) && (perm[0] == 'r') && (perm[1] != 'w') && (perm[2] == 'x') && (inode != 0))
-	{
-	  upper_bound++;
-	}
-     }
-   if (upper_bound == 0)
-     {
-       PAPIERROR("No segments found with r-x, inode != 0 and non-NULL mapname"); 
-       fclose(f);
-       return(PAPI_OK); 
-     }
-
-   /* Alloc our temporary space */
-
-   tmp = (PAPI_address_map_t *) papi_calloc(upper_bound, sizeof(PAPI_address_map_t));
-   if (tmp == NULL)
-     {
-       PAPIERROR("calloc(%d) failed", upper_bound*sizeof(PAPI_address_map_t));
-       fclose(f);
-       return(PAPI_OK);
-     }
-      
-   rewind(f);
-   while (1)
-     {
-      if (fgets(buf, sizeof(buf), f) == NULL)
-	{
-	  if (ferror(f))
-	    {
-	      PAPIERROR("fgets(%s, %d) returned < 0", fname, sizeof(buf)); 
-	      fclose(f);
-	      papi_free(tmp);
-	      return(PAPI_OK); 
-	    }
-	  else
-	    break;
-	}
-
-      sscanf(buf, "%lx-%lx %4s %lx %5s %ld %s", &begin, &end, perm, &foo, dev, &inode, mapname);
-      size = end - begin;
-
-      if (strlen(mapname) == 0)
-	continue;
-
-      if ((strcmp(find_data_mapname,mapname) == 0) && (perm[0] == 'r') && (perm[1] == 'w') && (inode != 0))
-	{
-	  tmp[find_data_index].data_start = (caddr_t) begin;
-	  tmp[find_data_index].data_end = (caddr_t) (begin + size);
-	  find_data_mapname[0] = '\0';
-	}
-      else if ((perm[0] == 'r') && (perm[1] != 'w') && (perm[2] == 'x') && (inode != 0))
-	{
-	  /* Text segment, check if we've seen it before, if so, ignore it. Some entries
-	     have multiple r-xp entires. */
-
-	  for (i=0;i<upper_bound;i++)
-	    {
-	      if (strlen(tmp[i].name))
-		{
-		  if (strcmp(mapname,tmp[i].name) == 0)
-		    break;
-		}
-	      else
-		{
-		  /* Record the text, and indicate that we are to find the data segment, following this map */
-		  strcpy(tmp[i].name,mapname);
-		  tmp[i].text_start = (caddr_t) begin;
-		  tmp[i].text_end = (caddr_t) (begin + size);
-		  count++;
-		  strcpy(find_data_mapname,mapname);
-		  find_data_index = i;
-		  break;
-		}
-	    }
-	}
-     }
-   if (count == 0)
-     {
-       PAPIERROR("No segments found with r-x, inode != 0 and non-NULL mapname"); 
-       fclose(f);
-       papi_free(tmp);
-       return(PAPI_OK); 
-     }
-   fclose(f);
-
-   /* Now condense the list and update exe_info */
-   tmp2 = (PAPI_address_map_t *) papi_calloc(count, sizeof(PAPI_address_map_t));
-   if (tmp2 == NULL)
-     {
-       PAPIERROR("calloc(%d) failed", count*sizeof(PAPI_address_map_t));
-       papi_free(tmp);
-       fclose(f);
-       return(PAPI_OK);
-     }
-
-   for (i=0;i<count;i++)
-     {
-       if (strcmp(tmp[i].name,_papi_hwi_system_info.exe_info.fullname) == 0)
-	 {
-	   _papi_hwi_system_info.exe_info.address_info.text_start = tmp[i].text_start;
-	   _papi_hwi_system_info.exe_info.address_info.text_end = tmp[i].text_end;
-	   _papi_hwi_system_info.exe_info.address_info.data_start = tmp[i].data_start;
-	   _papi_hwi_system_info.exe_info.address_info.data_end = tmp[i].data_end;
-	 }
-       else
-	 {
-	   strcpy(tmp2[index].name,tmp[i].name);
-	   tmp2[index].text_start = tmp[i].text_start;
-	   tmp2[index].text_end = tmp[i].text_end;
-	   tmp2[index].data_start = tmp[i].data_start;
-	   tmp2[index].data_end = tmp[i].data_end;
-	   index++;
-	 }
-     }
-   papi_free(tmp);
-
-   if (_papi_hwi_system_info.shlib_info.map)
-     papi_free(_papi_hwi_system_info.shlib_info.map);
-   _papi_hwi_system_info.shlib_info.map = tmp2;
-   _papi_hwi_system_info.shlib_info.count = index;
-
-   return (PAPI_OK);
-}
-
 // split the filename from a full path
 // roughly equivalent to unix basename()
 static void splitpath(const char *path, char *name)
@@ -498,6 +286,7 @@ int _papi_hwd_get_system_info(void)
 
   SUBDBG("Executable is %s\n",_papi_hwi_system_info.exe_info.address_info.name);
   SUBDBG("Full Executable is %s\n",_papi_hwi_system_info.exe_info.fullname);
+
   /* Hardware info */
   if (!init_hwinfo(&win_hwinfo))
     return(PAPI_ESYS);
@@ -533,7 +322,6 @@ int _papi_hwd_get_system_info(void)
   strcpy(_papi_hwi_system_info.hw_info.model_string,win_hwinfo.model_string);
 
   _papi_hwi_system_info.sub_info.num_cntrs = win_hwinfo.nrctr;
-//  _papi_hwi_system_info.num_gp_cntrs = _papi_hwi_system_info.num_cntrs;
 
   _papi_hwi_system_info.hw_info.mhz = (float)win_hwinfo.mhz;
 
@@ -556,13 +344,24 @@ long_long _papi_hwd_get_real_cycles(void) {
    return((long_long)get_cycles());
 }
 
-long_long _papi_hwd_get_virt_cycles(const hwd_context_t * ctx)
-{
-   return(PAPI_ESBSTR); // Windows can't read virtual cycles...
-}
+//long_long _papi_hwd_get_virt_cycles(const hwd_context_t * ctx)
+//{
+//   return(PAPI_ESBSTR); // Windows can't read virtual cycles...
+//}
 
 long_long _papi_hwd_get_virt_usec(const hwd_context_t * ctx)
 {
-   return(PAPI_ESBSTR); // Windows can't read virtual seconds...
-}
+    HANDLE p;
+    BOOL ret;
+    FILETIME Creation, Exit, Kernel, User;
+    long_long virt;
 
+    p = GetCurrentProcess();
+    ret = GetProcessTimes(p, &Creation, &Exit, &Kernel, &User);
+    if (ret) {
+	virt = (((long_long)(Kernel.dwHighDateTime + User.dwHighDateTime))<<32)
+	     + Kernel.dwLowDateTime + User.dwLowDateTime;
+	return(virt/1000);
+    }
+    else return(PAPI_ESBSTR);
+}
