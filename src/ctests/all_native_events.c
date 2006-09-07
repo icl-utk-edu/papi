ef5ac9fd60768e0c9c559b8aa46e3c81f038b288
Dan Terpstra <terpstra@eecs.utk.edu>
2006-09-07 14:54:46 +0000

    Major refactoring and code cleanup. Tested on ppc64/power4, Pentium4, Opteron, Core Duo.

--- src/ctests/all_native_events.c
+++ src/ctests/all_native_events.c
@@ -6,26 +6,38 @@
  *          <Your email here>
  */
 
-/* This file performs the following test: hardware info and which native events are available */
+/* This file hardware info and performs the following test:
+     Attempts to add all available native events to an event set.
+     This is a good preliminary way to validate native event tables.
+*/
 
 #include "papi_test.h"
 extern int TESTS_QUIET;         /* Declared in test_utils.c */
 
+static int add_remove_event(int EventSet, int event_code, char *name) {
+    int retval;
+
+    retval = PAPI_add_event(EventSet, event_code);
+    if (retval != PAPI_OK) {
+	printf("Error adding %s\n", name);
+	return(0);
+    } else printf("Added %s successfully.\n", name);
+    retval = PAPI_remove_event(EventSet, event_code);
+    if (retval != PAPI_OK)
+	test_fail(__FILE__, __LINE__, "PAPI_remove_event", retval);
+    else printf("Removed %s successfully.\n", name);
+    return(1);
+}
+
 int main(int argc, char **argv)
 {
-   /*
-   int loop;
-   long_long values;
-   */
-   int i, j, EventSet=PAPI_NULL, count=0;
+   int i, EventSet=PAPI_NULL, add_count=0, err_count=0;
    int retval;
    PAPI_event_info_t info;
    const PAPI_hw_info_t *hwinfo = NULL;
-#ifdef _POWER4
-   int group = 0, event_code;
-#endif
+   int event_code;
 #ifdef PENTIUM4
-   int k, l;
+   int k;
 #endif
 
    tests_quiet(argc, argv);     /* Set TESTS_QUIET variable */
@@ -44,7 +56,7 @@ int main(int argc, char **argv)
 
    if (!TESTS_QUIET) {
       printf
-          ("Test case NATIVE_AVAIL: Available native events and hardware information.\n");
+          ("Test case ALL_NATIVE_EVENTS: Available native events and hardware information.\n");
       printf
           ("-------------------------------------------------------------------------\n");
       printf("Vendor string and code   : %s (%d)\n", hwinfo->vendor_string,
@@ -61,139 +73,49 @@ int main(int argc, char **argv)
           ("-------------------------------------------------------------------------\n");
    }
    i = 0 | PAPI_NATIVE_MASK;
-   j = 0;
 #ifdef __crayx1
    PAPI_enum_event(&i, 0);
 #endif
    do {
-#ifdef _POWER4
-      group = (i & 0x00FF0000) >> 16;
-      if (group) {
-         if (!TESTS_QUIET)
-            printf("%10d", group - 1);
-      } else {
-         if (!TESTS_QUIET)
-            printf("\n\n");
-#endif
-         j++;
-	 /*memset(&info, 0, sizeof(PAPI_event_info_t));*/
-         retval = PAPI_get_event_info(i, &info);
+        retval = PAPI_get_event_info(i, &info);
 
-	 printf("\n%s\t0x%x  \n%s\n",
+	printf("\n%s\t0x%x  \n%s\n",
 		info.symbol,
 		info.event_code,
 		info.long_descr);
 
-#ifdef _POWER4
-         if (!TESTS_QUIET)
-            printf("Groups: ");
-      }
-#endif
 #ifdef PENTIUM4
-      k = i;
-      if (PAPI_enum_event(&k, PAPI_PENT4_ENUM_BITS) == PAPI_OK) {
-         l = strlen(info.long_descr);
-         do {
-            j++;
-            retval = PAPI_get_event_info(k, &info);
-            retval = PAPI_add_event(EventSet, info.event_code);       /* JT */
-            if (retval != PAPI_OK) {
-               if (!TESTS_QUIET)
-                  printf("Error adding %s\n", info.symbol);
-               test_fail(__FILE__, __LINE__, "PAPI_add_event", retval);
-            } else { 
-               if (!TESTS_QUIET) 
-                  printf("Added %s successful\n", info.symbol);
-               count++;
-            }
-			/*
-            retval = PAPI_start(EventSet);
-            if (retval != PAPI_OK)
-               test_fail(__FILE__, __LINE__, "PAPI_start", retval);
-			   
-			for(loop=0;loop<10;loop++);
-			   
-            retval = PAPI_stop(EventSet, &values);
-            if (retval != PAPI_OK)
-               test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
-            */
-            retval = PAPI_remove_event(EventSet, info.event_code);
-            if (retval != PAPI_OK)
-               test_fail(__FILE__, __LINE__, "PAPI_remove_event", retval);
-         } while (PAPI_enum_event(&k, PAPI_PENT4_ENUM_BITS) == PAPI_OK);
-	  }
-      if (!TESTS_QUIET && retval == PAPI_OK)
-         printf("\n");
-   } while (PAPI_enum_event(&i, PAPI_PENT4_ENUM_GROUPS) == PAPI_OK);
+	k = i;
+	if (PAPI_enum_event(&k, PAPI_PENT4_ENUM_BITS) == PAPI_OK) {
+	    do {
+		retval = PAPI_get_event_info(k, &info);
+		event_code = info.event_code;
+		if (add_remove_event(EventSet, event_code, info.symbol))
+		    add_count++;
+		else err_count++;
+	    } while (PAPI_enum_event(&k, PAPI_PENT4_ENUM_BITS) == PAPI_OK);
+	}
+	if (!TESTS_QUIET && retval == PAPI_OK)
+	    printf("\n");
+    } while (PAPI_enum_event(&i, PAPI_PENT4_ENUM_GROUPS) == PAPI_OK);
 #else
-#if defined(_POWER4)
-      event_code = info.event_code & 0xff00ffff;
-      retval = PAPI_add_event(EventSet, event_code);       /* JT */
-      if (retval != PAPI_OK) {
-         if (!TESTS_QUIET)
-            printf("Error adding %s\n", info.symbol);
-         /*test_fail(__FILE__, __LINE__, "PAPI_add_event", retval);*/ continue;
-      } else { 
-         if (!TESTS_QUIET) 
-            printf("Added %s successfully\n", info.symbol);
-         count++;
-      }
-	  /*
-      retval = PAPI_start(EventSet);
-      if (retval != PAPI_OK)
-         test_fail(__FILE__, __LINE__, "PAPI_start", retval);
-      retval = PAPI_stop(EventSet, &values);
-      if (retval != PAPI_OK)
-         test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
-      */
-      retval = PAPI_remove_event(EventSet, event_code);
-      if (retval != PAPI_OK)
-         test_fail(__FILE__, __LINE__, "PAPI_remove_event", retval);
-/* this function would return the next native event code.
-    modifer = PAPI_ENUM_ALL
-		 it simply returns next native event code
-    modifer = PAPI_PWR4_ENUM_GROUPS
-		 it would return information of groups this native event lives
-                 0x400000ed is the native code of PM_FXLS_FULL_CYC,
-		 before it returns 0x400000ee which is the next native event's
-		 code, it would return *EventCode=0x400400ed, the digits 16-23
-		 indicate group number
-   function return value:
-     PAPI_OK successful, next event is valid
-     PAPI_ENOEVNT  fail, next event is invalid
-*/
-   } while (PAPI_enum_event(&i, PAPI_PWR4_ENUM_GROUPS) == PAPI_OK);
+#ifdef _POWER4
+	  event_code = info.event_code & 0xff00ffff;
 #else
-      retval = PAPI_add_event(EventSet, info.event_code);       /* JT */
-      if (retval != PAPI_OK) {
-         if (!TESTS_QUIET)
-            printf("Error adding %s\n", info.symbol);
-         /*test_fail(__FILE__, __LINE__, "PAPI_add_event", retval);*/ continue;
-      } else { 
-         if (!TESTS_QUIET) 
-            printf("Added %s successfully\n", info.symbol);
-         count++;
-      }
-	  /*
-      retval = PAPI_start(EventSet);
-      if (retval != PAPI_OK)
-         test_fail(__FILE__, __LINE__, "PAPI_start", retval);
-      retval = PAPI_stop(EventSet, &values);
-      if (retval != PAPI_OK)
-         test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
-      */
-      retval = PAPI_remove_event(EventSet, info.event_code);
-      if (retval != PAPI_OK)
-         test_fail(__FILE__, __LINE__, "PAPI_remove_event", retval);
+	  event_code = info.event_code;
+#endif
+	  if (add_remove_event(EventSet, event_code, info.symbol))
+	      add_count++;
+	  else err_count++;
    } while (PAPI_enum_event(&i, PAPI_ENUM_ALL) == PAPI_OK);
 #endif
-#endif
 
-   if (!TESTS_QUIET)
-      printf("\nSuccessfully added,started and stopped %d events.\n", count);
-   if ( count > 0 )
+    printf("\n\nSuccessfully found, added, and removed %d events.\n", add_count);
+    if (err_count)
+    printf("\nFailed to add %d events.\n", err_count);
+    if ( add_count > 0 )
       test_pass(__FILE__, NULL, 0);
-   else
+    else
       test_fail(__FILE__, __LINE__, "No events added", 1);
-   exit(1);
+    exit(1);
 }
