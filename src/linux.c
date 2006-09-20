b5968868c79763b5130cf4101deb8e4819859e73
Philip Mucci <mucci@eecs.utk.edu>
2006-09-20 22:02:32 +0000

    Don't forget to update the vendor map when you add a CPU

--- src/linux.c
+++ src/linux.c
@@ -57,6 +57,9 @@ inline_static int xlate_cpu_type_to_vendor(unsigned perfctr_cpu_type) {
 #ifdef PERFCTR_X86_INTEL_PENTM
    case PERFCTR_X86_INTEL_PENTM:
 #endif
+#ifdef PERFCTR_X86_INTEL_CORE
+   case PERFCTR_X86_INTEL_CORE:
+#endif
       return (PAPI_VENDOR_INTEL);
 #ifdef PERFCTR_X86_AMD_K8
    case PERFCTR_X86_AMD_K8:
