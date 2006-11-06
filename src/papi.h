c4863cccf04278a5384aa97b78ca184a73ddd040
Dan Terpstra <terpstra@eecs.utk.edu>
2006-11-06 19:29:55 +0000

    Remove support files for deprecated Alpha and T3E platforms. Bump papi.h version number to 3.5.0.0.

--- src/papi.h
+++ src/papi.h
@@ -39,7 +39,7 @@
 /* 1st beta version of 3.4.9
     This will culminate in version 3.5.0 when the beta process is complete.
 */
-#define PAPI_VERSION  			PAPI_VERSION_NUMBER(3,4,9,1)
+#define PAPI_VERSION  			PAPI_VERSION_NUMBER(3,5,0,0)
 #define PAPI_VER_CURRENT 		(PAPI_VERSION & 0xffff0000)
 
 #ifdef __cplusplus
