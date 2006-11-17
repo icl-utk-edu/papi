a19920d73a38e5ee94739f0209f693f4625b0b98
Dan Terpstra <terpstra@eecs.utk.edu>
2006-11-17 02:19:47 +0000

    Remove Kevin's private copy pf perfctr leftover in HEAD.

--- src/kev/examples/perfex/ppc.h
+++
@@ -1,9 +0,0 @@
-/* $Id$
- * PPC32-specific declarations.
- *
- * Copyright (C) 2004  Mikael Pettersson
- */
-
-#define ARCH_LONG_OPTIONS	\
-    { "mmcr0", 1, NULL, 1 }, \
-    { "mmcr2", 1, NULL, 2 },
