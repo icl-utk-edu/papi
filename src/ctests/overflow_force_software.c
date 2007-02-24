26faa7b205c4d9a705e67a8acf256ab1639ad311
Dan Terpstra <terpstra@eecs.utk.edu>
2007-02-24 21:30:10 +0000

    Changes to accomodate moving and renaming the sub_info structure. Aliases still provide backward compatibility with older application code.

---
+++ src/ctests/overflow_force_software.c
@@ -0,0 +1,226 @@
+/* 
+* File:    overflow_single_event.c
+* CVS:     $Id$
+* Author:  Kevin London
+*          london@cs.utk.edu
+* Mods:    Maynard Johnson
+*          maynardj@us.ibm.com
+* Mods:    <your name here>
+*          <your email address>
+*/
+
+/* This file performs the following test: overflow dispatch of an eventset
+   with just a single event. Using both Hardware and software overflows
+
+     The Eventset contains:
+     + PAPI_FP_INS (overflow monitor)
+
+   - Start eventset 1
+   - Do flops
+   - Stop and measure eventset 1
+   - Set up overflow on eventset 1
+   - Start eventset 1
+   - Do flops
+   - Stop eventset 1
+   - Set up forced software overflow on eventset 1
+   - Start eventset 1
+   - Do flops
+   - Stop eventset 1
+*/
+
+#include "papi_test.h"
+
+#ifdef _CRAYT3E
+#define OVER_FMT	"handler(%d ) Overflow at %x! overflow_vector=0x%x!\n"
+#define OUT_FMT		"%-12s : %16lld%16d%16lld\n"
+#elif defined(_WIN32)
+#define OVER_FMT	"handler(%d ) Overflow at %p! overflow_vector=0x%x!\n"
+#define OUT_FMT		"%-12s : %16I64d%16d%16I64d\n"
+#else
+#define OVER_FMT	"handler(%d ) Overflow at %p overflow_vector=0x%llx!\n"
+#define OUT_FMT		"%-12s : %16lld%16d%16lld\n"
+#endif
+
+extern int _papi_hwi_error_level;
+static int total[2] = {0,0};                  /* total overflows */
+static int use_total=0;
+
+void handler(int EventSet, void *address, long_long overflow_vector, void *context)
+{
+   if (!TESTS_QUIET) {
+      fprintf(stderr, OVER_FMT, EventSet, address, overflow_vector);
+   }
+
+   if ( use_total )
+      total[1]++;
+   else
+      total[0]++;
+}
+
+int main(int argc, char **argv)
+{
+   int EventSet=PAPI_NULL;
+   long_long values[3] = { 0, 0, 0 };
+   long_long min, max;
+   int save_debug_setting;
+   int num_flops, retval;
+   int PAPI_event=0, mythreshold;
+   char event_name[PAPI_MAX_STR_LEN];
+   PAPI_option_t  opt;
+   PAPI_event_info_t info;
+
+   tests_quiet(argc, argv);     /* Set TESTS_QUIET variable */
+
+   retval = PAPI_library_init(PAPI_VER_CURRENT);
+   if (retval != PAPI_VER_CURRENT)
+      test_fail(__FILE__, __LINE__, "PAPI_library_init", retval);
+
+   retval = PAPI_get_opt(PAPI_COMPONENTINFO, &opt);
+   if ( !opt.cmp_info->hardware_intr )
+      test_skip(__FILE__, __LINE__, "Platform does not support Hardware overflow", 0);
+
+   /* query and set up the right instruction to monitor */
+  if (PAPI_query_event(PAPI_FP_INS) == PAPI_OK) {
+     if (PAPI_query_event(PAPI_FP_INS) == PAPI_OK) {
+        PAPI_get_event_info(PAPI_FP_INS, &info);
+        if ( info.count == 1 || !strcmp(info.derived, "DERIVED_CMPD"))
+           PAPI_event = PAPI_FP_INS;
+     }
+  } 
+  if ( PAPI_event == 0 ) {
+     if (PAPI_query_event(PAPI_FP_OPS) == PAPI_OK) {
+        PAPI_get_event_info(PAPI_FP_OPS, &info);
+        if ( info.count == 1 || !strcmp(info.derived, "DERIVED_CMPD"))
+           PAPI_event = PAPI_FP_OPS;
+     }
+  }
+  if ( PAPI_event == 0 ) {
+     if (PAPI_query_event(PAPI_TOT_INS) == PAPI_OK) {
+        PAPI_get_event_info(PAPI_TOT_INS, &info);
+        if ( info.count == 1 || !strcmp(info.derived, "DERIVED_CMPD"))
+           PAPI_event = PAPI_TOT_INS;
+     }	
+  }
+
+   if ( PAPI_event == 0 )
+      test_fail(__FILE__, __LINE__, "No suitable event for this test found!", 0);
+   
+
+   if (PAPI_event == PAPI_FP_INS )
+      mythreshold = THRESHOLD;
+   else if ( PAPI_event == PAPI_TOT_INS )
+      mythreshold = THRESHOLD * 10;
+   else 
+      mythreshold = THRESHOLD * 2;
+
+   retval = PAPI_create_eventset(&EventSet);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_create_eventset", retval);
+
+   retval = PAPI_add_event(EventSet, PAPI_event);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_add_event", retval);
+
+   retval = PAPI_start(EventSet);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_start", retval);
+
+   do_flops(NUM_FLOPS);
+
+   retval = PAPI_stop(EventSet, &values[0]);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
+
+   retval = PAPI_overflow(EventSet, PAPI_event, mythreshold, 0, handler);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_overflow", retval);
+
+   retval = PAPI_start(EventSet);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_start", retval);
+
+   do_flops(NUM_FLOPS);
+
+   retval = PAPI_stop(EventSet, &values[1]);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
+
+   use_total = 1;
+
+   retval = PAPI_add_event(EventSet, PAPI_TOT_CYC);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_add_event", retval);
+
+   retval = PAPI_overflow(EventSet, PAPI_event, 0, PAPI_OVERFLOW_FORCE_SW, handler);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_overflow", retval);
+
+   retval = PAPI_overflow(EventSet, PAPI_event, mythreshold, PAPI_OVERFLOW_FORCE_SW, handler);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_overflow", retval);
+
+   /* expecting error code, so let's suppress printout of error message */
+   PAPI_get_opt(PAPI_DEBUG, &opt);
+   save_debug_setting = opt.debug.level;
+   opt.debug.level = PAPI_QUIET;
+   PAPI_set_opt(PAPI_DEBUG, &opt);
+   retval = PAPI_overflow(EventSet, PAPI_TOT_CYC, mythreshold, 0, handler);
+   if (retval == PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_overflow: allowed hardware and software overflow", -1);
+   opt.debug.level = save_debug_setting;
+   PAPI_set_opt(PAPI_DEBUG, &opt);
+
+   retval = PAPI_overflow(EventSet, PAPI_TOT_CYC, mythreshold, PAPI_OVERFLOW_FORCE_SW, handler);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_overflow: didn't allow 2 software overflow", -1);
+
+   retval = PAPI_remove_event(EventSet, PAPI_TOT_CYC);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_remove_event", retval);
+
+   retval = PAPI_start(EventSet);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_start", retval);
+
+   do_flops(NUM_FLOPS);
+
+   retval = PAPI_stop(EventSet, &values[2]);
+   if (retval != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
+
+   num_flops = NUM_FLOPS;
+#if defined(linux) || defined(__ia64__) || defined(_WIN32) || defined(_CRAYT3E) || defined(_POWER4)
+   num_flops *= 2;
+#endif
+
+   if (!TESTS_QUIET) {
+      if ((retval = PAPI_event_code_to_name(PAPI_event, event_name)) != PAPI_OK)
+         test_fail(__FILE__, __LINE__, "PAPI_event_code_to_name", retval);
+
+      printf("Test case: Overflow dispatch of 1st event in set with 1 event.\n");
+      printf("--------------------------------------------------------------\n");
+      printf("Threshold for overflow is: %d\n", mythreshold);
+      printf("Using %d iterations of c += a*b\n", NUM_FLOPS);
+      printf("-----------------------------------------------\n");
+
+      printf("Test type    : %16d%16d%16d\n", 1, 2, 3);
+      printf(OUT_FMT, event_name, values[0], 0, values[2]);
+      printf("Overflows    : %16s%16d%16d\n", "", total[0], total[1]);
+      printf("-----------------------------------------------\n");
+
+      printf("Verification:\n");
+     
+      printf("Overflow in Column 2 approximately equals overflows in column 3\n");
+   }
+
+   min = (long_long) ((values[0] * (1.0 - OVR_TOLERANCE)) / (long_long) mythreshold);
+   max = (long_long) ((values[0] * (1.0 + OVR_TOLERANCE)) / (long_long) mythreshold);
+   if (total[0] > max || total[0] < min)
+      test_fail(__FILE__, __LINE__, "Hardware Overflows", 1);
+
+   if (total[1] > max || total[1] < min)
+      test_fail(__FILE__, __LINE__, "Software Overflows", 1);
+
+   test_pass(__FILE__, NULL, 0);
+   exit(1);
+}
---
+++ src/ctests/subinfo.c
@@ -0,0 +1,65 @@
+/* 
+* File:    subinfo.c
+* CVS:     $Id$
+* Author:  Philip Mucci
+*          mucci@cs.utk.edu
+* Mods:    <your name here>
+*          <your email address>
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include "papi_test.h"
+
+int main(int argc, char **argv)
+{
+   int retval;
+
+   const PAPI_component_info_t *cmpinfo;
+
+   tests_quiet(argc, argv);     /* Set TESTS_QUIET variable */
+
+   if ((retval = PAPI_library_init(PAPI_VER_CURRENT)) != PAPI_VER_CURRENT)
+      test_fail(__FILE__, __LINE__, "PAPI_library_init", retval);
+
+   if ((cmpinfo = PAPI_get_component_info()) == NULL) 
+     test_fail(__FILE__, __LINE__, "PAPI_get_substrate_info", retval);
+
+   printf("name: %s\n",cmpinfo->name);
+   printf("substrate_version: %s\n",cmpinfo->version);
+   printf("support_version: %s\n",cmpinfo->support_version);
+   printf("kernel_version: %s\n",cmpinfo->kernel_version);
+   printf("num_cntrs: %d\n",cmpinfo->num_cntrs);
+   printf("num_mpx_cntrs: %d\n",cmpinfo->num_mpx_cntrs);
+   printf("num_preset_events: %d\n",cmpinfo->num_preset_events);           /* Number of counters the substrate supports */
+   printf("num_native_events: %d\n",cmpinfo->num_native_events);           /* Number of counters the substrate supports */
+   printf("default_domain: 0x%x (%s)\n",cmpinfo->default_domain,stringify_all_domains(cmpinfo->default_domain));
+   printf("available_domains: 0x%x (%s)\n",cmpinfo->available_domains,stringify_all_domains(cmpinfo->available_domains));       /* Available domains */ 
+   printf("default_granularity: 0x%x (%s)\n",cmpinfo->default_granularity,stringify_granularity(cmpinfo->default_granularity));
+   /* The default granularity when this substrate is used */
+   printf("available_granularities: 0x%x (%s)\n",cmpinfo->available_granularities,stringify_all_granularities(cmpinfo->available_granularities)); /* Available granularities */
+   printf("multiplex_timer_sig: %d\n",cmpinfo->multiplex_timer_sig);      /* Width of opcode matcher if exists, 0 if not */
+   printf("multiplex_timer_num: %d\n",cmpinfo->multiplex_timer_num);      /* Width of opcode matcher if exists, 0 if not */
+   printf("multiplex_timer_us: %d\n",cmpinfo->multiplex_timer_us);      /* Width of opcode matcher if exists, 0 if not */
+   printf("hardware_intr_sig: %d\n",cmpinfo->hardware_intr_sig);      /* Width of opcode matcher if exists, 0 if not */
+   printf("opcode_match_width: %d\n",cmpinfo->opcode_match_width);      /* Width of opcode matcher if exists, 0 if not */
+/*   printf("reserved_ints[4]: %d\n",cmpinfo->reserved_ints[4]); */
+   printf("hardware_intr: %d\n",cmpinfo->hardware_intr);         /* Needs hw overflow intr to be emulated in software*/
+   printf("precise_intr: %d\n",cmpinfo->precise_intr);          /* Performance interrupts happen precisely */
+   printf("posix1b_timers: %d\n",cmpinfo->posix1b_timers);          /* Performance interrupts happen precisely */
+   printf("kernel_profile: %d\n",cmpinfo->kernel_profile);        /* Needs kernel profile support (buffered interrupts) to be emulated */
+   printf("kernel_multiplex: %d\n",cmpinfo->kernel_multiplex);      /* In kernel multiplexing */
+   printf("data_address_range: %d\n",cmpinfo->data_address_range);    /* Supports data address range limiting */
+   printf("instr_address_range: %d\n",cmpinfo->instr_address_range);   /* Supports instruction address range limiting */
+   printf("fast_counter_read: %d\n",cmpinfo->fast_counter_read);       /* Has a fast counter read */
+   printf("fast_real_timer: %d\n",cmpinfo->fast_real_timer);       /* Has a fast real timer */
+   printf("fast_virtual_timer: %d\n",cmpinfo->fast_virtual_timer);    /* Has a fast virtual timer */
+   printf("attach: %d\n",cmpinfo->attach);    /* Has a fast virtual timer */
+   printf("attach_must_ptrace: %d\n",cmpinfo->attach_must_ptrace);    /* Has a fast virtual timer */
+   printf("edge_detect: %d\n",cmpinfo->edge_detect);    /* Has a fast virtual timer */
+   printf("invert: %d\n",cmpinfo->invert);    /* Has a fast virtual timer */
+   printf("profile_ear: %d\n",cmpinfo->profile_ear);     /* Supports data/instr/tlb miss address sampling */
+   printf("grouped_cntrs: %d\n",cmpinfo->grouped_cntrs);           /* Number of counters the substrate supports */
+
+   test_pass(__FILE__, NULL, 0);
+   exit(0);
+}
