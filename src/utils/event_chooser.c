0b46f6b8aa2808ef9b3de233b2bbec692183ebab
Dan Terpstra <terpstra@eecs.utk.edu>
2008-02-12 19:25:45 +0000

    Fixed an infinite loop in event_chooser. This showed up as a result of the rework of PAPI_enum_events. Thanks to Wyatt Spear. We really should make a file of shared output routines for avail, native_avail and event_chooser. That way common formats would live in one place and actually *look* the same...

--- src/utils/event_chooser.c
+++ src/utils/event_chooser.c
@@ -5,47 +5,59 @@
 int EventSet=PAPI_NULL;
 int retval;
 
+static char *is_derived(PAPI_event_info_t *info)
+{
+	if (strlen(info->derived) == 0)
+		return("No");
+	else if (strcmp(info->derived,"NOT_DERIVED") == 0)
+		return("No");
+	else if (strcmp(info->derived,"DERIVED_CMPD") == 0)
+		return("No");
+	else
+		return("Yes");
+}
+
 void papi_init(int argc, char **argv)
 {
-   const PAPI_hw_info_t *hwinfo = NULL;
-   
-   tests_quiet(argc, argv);     /* Set TESTS_QUIET variable */
-   /*for(i=0;i<argc;i++) */
+	const PAPI_hw_info_t *hwinfo = NULL;
 
-   retval = PAPI_library_init(PAPI_VER_CURRENT);
-   if (retval != PAPI_VER_CURRENT)
-      test_fail(__FILE__, __LINE__, "PAPI_library_init", retval);
+	tests_quiet(argc, argv);     /* Set TESTS_QUIET variable */
+	/*for(i=0;i<argc;i++) */
 
-   retval = PAPI_set_debug(PAPI_VERB_ECONT);
-   if (retval != PAPI_OK)
-      test_fail(__FILE__, __LINE__, "PAPI_set_debug", retval);
+	retval = PAPI_library_init(PAPI_VER_CURRENT);
+	if (retval != PAPI_VER_CURRENT)
+		test_fail(__FILE__, __LINE__, "PAPI_library_init", retval);
 
-   if ((hwinfo = PAPI_get_hardware_info()) == NULL)
-      test_fail(__FILE__, __LINE__, "PAPI_get_hardware_info", 2);
+	retval = PAPI_set_debug(PAPI_VERB_ECONT);
+	if (retval != PAPI_OK)
+		test_fail(__FILE__, __LINE__, "PAPI_set_debug", retval);
 
-   printf
-      ("Test case event_chooser: Available events which can be added with given events.\n");
-   printf
-      ("-------------------------------------------------------------------------\n");
-   printf("Vendor string and code   : %s (%d)\n", hwinfo->vendor_string, hwinfo->vendor);
-   printf("Model string and code    : %s (%d)\n", hwinfo->model_string, hwinfo->model);
-   printf("CPU Revision             : %f\n", hwinfo->revision);
-   printf("CPU Megahertz            : %f\n", hwinfo->mhz);
-   printf("CPU Clock Megahertz      : %d\n", hwinfo->clock_mhz);
-   printf("CPU Clock Ticks / sec    : %d\n", hwinfo->clock_ticks);
-   printf("CPU's in this Node       : %d\n", hwinfo->ncpu);
-   printf("Nodes in this System     : %d\n", hwinfo->nnodes);
-   printf("Total CPU's              : %d\n", hwinfo->totalcpus);
-   printf("Number Hardware Counters : %d\n", PAPI_get_opt(PAPI_MAX_HWCTRS, NULL));
-   printf("Max Multiplex Counters   : %d\n", PAPI_get_opt(PAPI_MAX_MPX_CTRS, NULL));
-   printf
-      ("-------------------------------------------------------------------------\n");
+	if ((hwinfo = PAPI_get_hardware_info()) == NULL)
+		test_fail(__FILE__, __LINE__, "PAPI_get_hardware_info", 2);
 
-   retval=PAPI_create_eventset(&EventSet);
-   if(retval != PAPI_OK){
-      fprintf(stderr, "PAPI_create_eventset error\n");
-      exit(1);
-  }
+	printf
+		("Test case event_chooser: Available events which can be added with given events.\n");
+	printf
+		("-------------------------------------------------------------------------\n");
+	printf("Vendor string and code   : %s (%d)\n", hwinfo->vendor_string, hwinfo->vendor);
+	printf("Model string and code    : %s (%d)\n", hwinfo->model_string, hwinfo->model);
+	printf("CPU Revision             : %f\n", hwinfo->revision);
+	printf("CPU Megahertz            : %f\n", hwinfo->mhz);
+	printf("CPU Clock Megahertz      : %d\n", hwinfo->clock_mhz);
+	printf("CPU Clock Ticks / sec    : %d\n", hwinfo->clock_ticks);
+	printf("CPU's in this Node       : %d\n", hwinfo->ncpu);
+	printf("Nodes in this System     : %d\n", hwinfo->nnodes);
+	printf("Total CPU's              : %d\n", hwinfo->totalcpus);
+	printf("Number Hardware Counters : %d\n", PAPI_get_opt(PAPI_MAX_HWCTRS, NULL));
+	printf("Max Multiplex Counters   : %d\n", PAPI_get_opt(PAPI_MAX_MPX_CTRS, NULL));
+	printf
+		("-------------------------------------------------------------------------\n");
+
+	retval=PAPI_create_eventset(&EventSet);
+	if(retval != PAPI_OK){
+		fprintf(stderr, "PAPI_create_eventset error\n");
+		exit(1);
+	}
 }
 
 int native()
@@ -124,94 +136,66 @@ int native()
 
 int preset()
 {
-   int i,j=0;
-   int retval;
-   int print_avail_only = 1;
-   int print_tabular = 1;
-   PAPI_event_info_t info;
+	int i,j=0;
+	int retval;
+	PAPI_event_info_t info;
 
+	printf("    Name        Code    ");
+	printf("Deriv Description (Note)\n");
 
-         i = PAPI_PRESET_MASK;
-         if (print_tabular) {
-            if (print_avail_only) {
-               printf("Name\t\tDerived\tDescription (Mgr. Note)\n");
-            } else {
-               printf("Name\t\tCode\t\tAvail\tDeriv\tDescription (Note)\n");
-            }
-         }
-         else {
-            printf("The following correspond to fields in the PAPI_event_info_t structure.\n");
-            printf("Symbol\tEvent Code\tCount\n |Short Description|\n |Long Description|\n |Developer's Notes|\n Derived|\n |PostFix|\n");
-         }
-         do {
-           retval=PAPI_add_event(EventSet,i);
-           if(retval == PAPI_OK){
-             if (PAPI_get_event_info(i, &info) == PAPI_OK) {
-               if (print_tabular) {
-                  if (print_avail_only) {
-		               printf("%s\t%s\t%s (%s)\n",
-			               info.symbol,
-			               (info.count > 1 ? "Yes" : "No"),
-			               info.long_descr, (info.note ? info.note : ""));
-                  } else {
-		               printf("%s\t0x%x\t%s\t%s\t%s (%s)\n",
-		                     info.symbol,
-		                     info.event_code,
-		                     (info.count ? "Yes" : "No"),
-		                     (info.count > 1 ? "Yes" : "No"),
-		                     info.long_descr, (info.note ? info.note : ""));
-                  }
-               } else {
-	               printf("%s\t0x%x\t%d\n |%s|\n |%s|\n |%s|\n |%s|\n |%s|\n",
-		               info.symbol,
-		               info.event_code,
-		               info.count,
-		               info.short_descr,
-		               info.long_descr,
-		               info.note,
-                     info.derived,
-                     info.postfix);
-                  for (j=0;j<(int)info.count;j++) printf(" |Native Code[%d]: 0x%x  %s|\n",j,info.code[j], info.name[j]);
-               }
-	        }   
-	        if((retval=PAPI_remove_event(EventSet,i))!=PAPI_OK)
-              printf("Error in PAPI_remove_event\n");
-            j++;
-          }
+	/* For consistency, always ASK FOR the first event */
+	i = 0 | PAPI_PRESET_MASK;
+	PAPI_enum_event(&i, PAPI_ENUM_FIRST);
 
-         } while (PAPI_enum_event(&i, print_avail_only) == PAPI_OK);
-      
-   printf ("-------------------------------------------------------------------------\n");
-   printf("Total events reported: %d\n", j);
-   test_pass(__FILE__, NULL, 0);
-   exit(1);
+	do {
+		retval=PAPI_add_event(EventSet,i);
+		if(retval == PAPI_OK){
+			if (PAPI_get_event_info(i, &info) == PAPI_OK) {
+				printf("%-13s0x%x  %-5s%s",
+					info.symbol,
+					info.event_code,
+					is_derived(&info),
+					info.long_descr);
+				if (info.note[0]) printf(" (%s)", info.note);
+				printf("\n");
+			}
+			if((retval=PAPI_remove_event(EventSet,i))!=PAPI_OK)
+				printf("Error in PAPI_remove_event\n");
+			j++;
+		}
+	} while (PAPI_enum_event(&i, PAPI_PRESET_ENUM_AVAIL) == PAPI_OK);
+
+	printf ("-------------------------------------------------------------------------\n");
+	printf("Total events reported: %d\n", j);
+	test_pass(__FILE__, NULL, 0);
+	exit(1);
 }
 
 int main(int argc, char **argv)
 {
-  int i;
-  int pevent;
+	int i;
+	int pevent;
 
-  if(argc<3) goto use_exit;
+	if(argc<3) goto use_exit;
 
-  papi_init(argc, argv);
+	papi_init(argc, argv);
 
-  for(i=2; i<argc; i++){
-    PAPI_event_name_to_code(argv[i], &pevent);
-    retval=PAPI_add_event(EventSet,pevent);
-    if(retval != PAPI_OK){
-      fprintf(stderr, "Event %s can't be counted with others\n", argv[i]);
-      exit(1);
-    }
-  }
+	for(i=2; i<argc; i++){
+		PAPI_event_name_to_code(argv[i], &pevent);
+		retval=PAPI_add_event(EventSet,pevent);
+		if(retval != PAPI_OK){
+			fprintf(stderr, "Event %s can't be counted with others\n", argv[i]);
+			exit(1);
+		}
+	}
 
-  if(!strcmp("NATIVE", argv[1]))
-    native();
-  else if(!strcmp("PRESET", argv[1]))
-    preset();
-  else goto use_exit;
-  exit(0);
+	if(!strcmp("NATIVE", argv[1]))
+		native();
+	else if(!strcmp("PRESET", argv[1]))
+		preset();
+	else goto use_exit;
+	exit(0);
 use_exit:
-  fprintf(stderr, "Usage: papi_event_chooser NATIVE|PRESET evt1 evt2 ... \n");
-  exit(1);
+	fprintf(stderr, "Usage: papi_event_chooser NATIVE|PRESET evt1 evt2 ... \n");
+	exit(1);
 }
