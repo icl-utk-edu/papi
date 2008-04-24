ffa3736744505642cea02fb60652fd337c927430
Haihang You <you@eecs.utk.edu>
2008-04-24 17:11:01 +0000

    put commented out command back in.

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -65,29 +65,29 @@ do
   done
   if [ $MATCH -ne 1 ]; then
     if [ -x $i ]; then
-    if [ "$i" = "ctests/timer_overflow" ]; then
-      echo Skipping test $i, it takes too long...
-    else
-    if [ "$i" = "ctests/shlib" ]; then
-      echo -n "Running $i: ";
-      if [ "$LD_LIBRARY_PATH" = "" ]; then
-          LD_LIBRARY_PATH=.:./libpfm-3.y/lib:./libpfm-2.x/libpfm
+      if [ "$i" = "ctests/timer_overflow" ]; then
+        echo Skipping test $i, it takes too long...
       else
-          LD_LIBRARY_PATH=.:./libpfm-3.y/lib:./libpfm-2.x/libpfm:"$LD_LIBRARY_PATH"
-      fi
-      export LD_LIBRARY_PATH
-      if [ "$LIBPATH" = "" ]; then
-          LIBPATH=.:./libpfm-3.y/lib:./libpfm-2.x/libpfm
-      else
-          LIBPATH=.:./libpfm-3.y/lib:./libpfm-2.x/libpfm:"$LIBPATH"
-      fi
-      export LIBPATH
-    #  ./$i $TESTS_QUIET
-    else
-    echo -n "Running $i: ";
-    #./$i $TESTS_QUIET
-    fi;
-    fi;
+        if [ "$i" = "ctests/shlib" ]; then
+          echo -n "Running $i: ";
+          if [ "$LD_LIBRARY_PATH" = "" ]; then
+            LD_LIBRARY_PATH=.:./libpfm-3.y/lib:./libpfm-2.x/libpfm
+          else
+            LD_LIBRARY_PATH=.:./libpfm-3.y/lib:./libpfm-2.x/libpfm:"$LD_LIBRARY_PATH"
+          fi
+          export LD_LIBRARY_PATH
+          if [ "$LIBPATH" = "" ]; then
+            LIBPATH=.:./libpfm-3.y/lib:./libpfm-2.x/libpfm
+          else
+            LIBPATH=.:./libpfm-3.y/lib:./libpfm-2.x/libpfm:"$LIBPATH"
+          fi
+          export LIBPATH
+          ./$i $TESTS_QUIET
+        else
+          echo -n "Running $i: ";
+          ./$i $TESTS_QUIET
+        fi;
+      fi;
     fi;
   fi;
   MATCH=0
@@ -110,7 +110,7 @@ do
   if [ $MATCH -ne 1 ]; then
     if [ -x $i ]; then
     echo -n "Running $i: ";
-    #./$i $TESTS_QUIET
+    ./$i $TESTS_QUIET
     fi;
   fi;
   MATCH=0
