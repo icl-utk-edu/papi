5db95e8e5ca18e4d1e5e6f4b9941dd0b65952a50
Haihang You <you@eecs.utk.edu>
2008-05-02 19:39:54 +0000

    comment out #ifdef HAVE_PFM_MSG_TYPE     pfm_msg_t msg; #else     pfarg_msg_t msg; #endif it breaks on brutus.

--- src/perfmon.c
+++ src/perfmon.c
@@ -3868,11 +3868,13 @@ process_smpl_buf(int num_smpl_pmds, int entry_size, ThreadInfo_t **thr)
 void _papi_hwd_dispatch_timer(int n, hwd_siginfo_t * info, void *uc)
 {
     _papi_hwi_context_t ctx;
-#ifdef HAVE_PFM_MSG_TYPE
+/*#ifdef HAVE_PFM_MSG_TYPE
     pfm_msg_t msg;
 #else
     pfarg_msg_t msg;
 #endif
+*/
+    pfarg_msg_t msg;
     int ret, wanted_fd, fd = info->si_fd;
     unsigned long address;
     ThreadInfo_t *thread = _papi_hwi_lookup_thread();
