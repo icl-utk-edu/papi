b445a2601ff2d7bc3d6efbd26ac529b99ca7d8cf
Haihang You <you@eecs.utk.edu>
2009-02-23 17:20:38 +0000

    add this file from head branch

---
+++ src/utils/version.c
@@ -0,0 +1,13 @@
+/* This utility displays the current PAPI version number */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "papi.h"
+
+int main(int argc, char **argv)
+{
+	printf("PAPI Version: %d.%d.%d.%d\n", PAPI_VERSION_MAJOR(PAPI_VERSION),
+		PAPI_VERSION_MINOR(PAPI_VERSION), PAPI_VERSION_REVISION(PAPI_VERSION),
+		PAPI_VERSION_INCREMENT(PAPI_VERSION));
+	exit(0);
+}
