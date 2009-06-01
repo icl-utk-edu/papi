3d60e89effbc677b374a400871eba33c48e542e6
Dan Terpstra <terpstra@eecs.utk.edu>
2009-06-01 20:35:16 +0000

    Restructure for compatibility with current PAPI-C.

--- src/any-null.h
+++ src/any-null.h
@@ -14,63 +14,45 @@
 #ifndef _PAPI_ANY_NULL_H
 #define _PAPI_ANY_NULL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <assert.h>
-#include <ctype.h>
-#include <time.h>
 #if defined(linux)
-#include <signal.h>
-#include <syscall.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/resource.h>
-#include <sys/time.h>
-#include <sys/times.h>
+#include "linux.h"
 #endif
 
-#define MAX_COUNTERS 2
+#define MAX_COUNTERS 4
 #define MAX_COUNTER_TERMS  MAX_COUNTERS
 
 #define inline_static inline static
 
-typedef struct hwd_siginfo {
+typedef struct _any_reg_alloc {
   int placeholder;
-} hwd_siginfo_t;
+} _any_reg_alloc_t;
 
-typedef struct hwd_register {
+typedef struct _any_register {
   int placeholder;
-} hwd_register_t;
+} _any_register_t;
 
-typedef struct hwd_reg_alloc {
+typedef struct _any_control_state {
   int placeholder;
-} hwd_reg_alloc_t;
+} _any_control_state_t;
 
-typedef struct hwd_control_state {
-  int placeholder;
-} hwd_control_state_t;
-
-typedef struct hwd_context {
+typedef struct _any_context {
 #if defined(USE_PROC_PTTIMER)
   int stat_fd;
 #endif
   int placeholder; 
-} hwd_context_t;
+} _any_context_t;
 
-typedef struct hwd_ucontext {
+typedef struct _any_siginfo {
+  int placeholder;
+} _any_siginfo_t;
+
+typedef struct _any_ucontext {
   int placeholder; 
-} hwd_ucontext_t;
+} _any_ucontext_t;
 
-#define _papi_hwd_lock_init() { ; }
-#define _papi_hwd_lock(a) { ; }
-#define _papi_hwd_unlock(a) { ; }
 #define GET_OVERFLOW_ADDRESS(ctx) (ctx)
 
+#define MY_VECTOR _any_vector
+
 #endif /* _PAPI_ANY_NULL_H */
-
-
 
