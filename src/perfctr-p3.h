bfd1eae3be08d51b905f9f44d5a6731b06223dc6
Dan Terpstra <terpstra@eecs.utk.edu>
2009-08-11 19:07:30 +0000

    Shrink the field for event codes from 12 bits (4096 events) to 10 bits (1024 events, and shift the unit mask field down 2 bits to avoid possibility of collision with the component index field. With this change, all_native_events now runs successfully on Nehalem.

--- src/perfctr-p3.h
+++ src/perfctr-p3.h
@@ -63,11 +63,11 @@
 /* Native events consist of a flag field, an event field, and a unit mask field.
  * The next 4 macros define the characteristics of the event and unit mask fields.
  */
-#define PAPI_NATIVE_EVENT_AND_MASK 0x00000fff	/* 12 bits == 4096 max events */
+#define PAPI_NATIVE_EVENT_AND_MASK 0x000003ff	/* 10 bits == 1024 max events */
 #define PAPI_NATIVE_EVENT_SHIFT 0
-#define PAPI_NATIVE_UMASK_AND_MASK 0x0ffff000	/* 16 bits for unit masks */
+#define PAPI_NATIVE_UMASK_AND_MASK 0x03fffc00	/* 16 bits for unit masks */
 #define PAPI_NATIVE_UMASK_MAX 16				/* 16 possible unit masks */
-#define PAPI_NATIVE_UMASK_SHIFT 12
+#define PAPI_NATIVE_UMASK_SHIFT 10
 
 #define PERF_MAX_COUNTERS 5
 #define MAX_COUNTERS PERF_MAX_COUNTERS
