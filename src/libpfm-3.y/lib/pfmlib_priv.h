d459c5a45d46f3a319a0d77c015317e6c5110e51
James Ralph <ralph@eecs.utk.edu>
2009-09-02 17:11:03 +0000

    TODO: Find a good solution for variable arguments in vc++

--- src/libpfm-3.y/lib/pfmlib_priv.h
+++ src/libpfm-3.y/lib/pfmlib_priv.h
@@ -31,7 +31,7 @@
 
 typedef struct {
 	char 		*pmu_name;
-	int		pmu_type;
+	int		pmu_type; /* must remain int, using -1 */
 	unsigned int	pme_count; /* number of events */
  	unsigned int	pmd_count; /* number of PMD registers */
  	unsigned int	pmc_count; /* number of PMC registers */
@@ -45,6 +45,7 @@ typedef struct {
 	unsigned int	(*get_num_event_masks)(unsigned int event_idx);
 	int 		(*dispatch_events)(pfmlib_input_param_t *p, void *model_in, pfmlib_output_param_t *q, void *model_out);
 	int 		(*pmu_detect)(void);
+	int 		(*pmu_init)(void);
 	void		(*get_impl_pmcs)(pfmlib_regmask_t *impl_pmcs);
 	void		(*get_impl_pmds)(pfmlib_regmask_t *impl_pmds);
 	void		(*get_impl_counters)(pfmlib_regmask_t *impl_counters);
@@ -53,6 +54,7 @@ typedef struct {
 	int		(*get_event_mask_desc)(unsigned int event_idx, unsigned int mask_idx, char **buf);
 	int		(*get_cycle_event)(pfmlib_event_t *e);
 	int		(*get_inst_retired_event)(pfmlib_event_t *e);
+	int		(*has_umask_default)(unsigned int i); /* optional */
 } pfm_pmu_support_t;
 
 #define PFMLIB_MULT_CODE_EVENT	0x1	/* more than one code per event (depending on counter) */
@@ -64,6 +66,7 @@ typedef struct {
 typedef struct {
 	pfmlib_options_t	options;
 	pfm_pmu_support_t	*current;
+	int			options_env_set; /* 1 if options set by env variables */
 } pfm_config_t;	
 
 #define PFMLIB_INITIALIZED()	(pfm_config.current != NULL)
@@ -75,15 +78,24 @@ extern pfm_config_t pfm_config;
 #define pfm_current		pfm_config.current
 
 extern void __pfm_vbprintf(const char *fmt,...);
-extern int __pfm_getcpuinfo_attr(const char *attr, char *ret_buf, size_t maxlen);
 extern int __pfm_check_event(pfmlib_event_t *e);
 
+/*
+ * provided by OS-specific module
+ */
+extern int __pfm_getcpuinfo_attr(const char *attr, char *ret_buf, size_t maxlen);
+extern void pfm_init_syscalls(void);
+
 #ifdef PFMLIB_DEBUG
-#define DPRINT(a) \
+#ifdef WIN32
+#define DPRINT(...)
+#else
+#define DPRINT(fmt, a...) \
 	do { \
 		if (pfm_config.options.pfm_debug) { \
-			fprintf(stderr, "%s (%s.%d): ", __FILE__, __func__, __LINE__); printf a; } \
+			fprintf(libpfm_fp, "%s (%s.%d): " fmt, __FILE__, __func__, __LINE__, ## a); } \
 	} while (0)
+#endif
 #else
 #define DPRINT(a)
 #endif
@@ -103,12 +115,15 @@ extern pfm_pmu_support_t i386_pii_support;
 extern pfm_pmu_support_t i386_pm_support;
 extern pfm_pmu_support_t gen_ia32_support;
 extern pfm_pmu_support_t generic_mips64_support;
+extern pfm_pmu_support_t sicortex_support;
 extern pfm_pmu_support_t pentium4_support;
 extern pfm_pmu_support_t coreduo_support;
 extern pfm_pmu_support_t core_support;
 extern pfm_pmu_support_t gen_powerpc_support;
 extern pfm_pmu_support_t sparc_support;
 extern pfm_pmu_support_t cell_support;
+extern pfm_pmu_support_t intel_atom_support;
+extern pfm_pmu_support_t intel_nhm_support;
 
 static inline unsigned int pfm_num_masks(int e)
 {
@@ -117,4 +132,18 @@ static inline unsigned int pfm_num_masks(int e)
 	return pfm_current->get_num_event_masks(e);
 }
 
+extern FILE *libpfm_fp;
+extern int forced_pmu;
+extern int _pfmlib_sys_base; /* syscall base */
+extern int _pfmlib_major_version; /* kernel perfmon major version */
+extern int _pfmlib_minor_version; /* kernel perfmon minor version */
+extern void pfm_init_syscalls(void);
+
+static inline int
+_pfmlib_get_sys_base()
+{
+	if (!_pfmlib_sys_base)
+		pfm_init_syscalls();
+	return _pfmlib_sys_base;
+}
 #endif /* __PFMLIB_PRIV_H__ */
