7ac9a46198527006dae13e64fc8491f56dcf0cc3
Dan Terpstra <terpstra@eecs.utk.edu>
2009-11-10 20:58:19 +0000

    Update from 5 counters(Core2) to 7 counters (i7).

--- src/perfctr-p3.h
+++ src/perfctr-p3.h
@@ -1,29 +1,55 @@
-#ifndef _PAPI_PENTIUM3
-#define _PAPI_PENTIUM3
+#ifndef _PAPI_PENTIUM3_H
+#define _PAPI_PENTIUM3_H
 
-#include <signal.h>
+#ifndef __USE_GNU
+#define __USE_GNU
+#endif
+#ifndef __USE_UNIX98
+#define __USE_UNIX98
+#endif
+#ifndef __USE_XOPEN_EXTENDED
+#define __USE_XOPEN_EXTENDED
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <signal.h>
+
+#ifndef __BSD__ /* #include <malloc.h> */
 #include <malloc.h>
+#endif
+
 #include <assert.h>
 #include <string.h>
 #include <math.h>
 #include <limits.h>
 #include <sys/types.h>
 
-#ifdef _WIN32
-#include <errno.h>
-#include "cpuinfo.h"
-#include "pmclib.h"
-#else
+#ifdef XML
+#include <expat.h>
+#endif
+
+#define inline_static inline static
 #include <unistd.h>
 #include <time.h>
 #include <errno.h>
 #include <ctype.h>
+
+#ifdef __BSD__
+#include <ucontext.h>
+#else
 #include <sys/ucontext.h>
+#endif
+
 #include <sys/times.h>
 #include <sys/time.h>
-#include <linux/unistd.h>
+
+#ifndef __BSD__ /* #include <linux/unistd.h> */
+  #ifndef __CATAMOUNT__
+    #include <linux/unistd.h>	
+  #endif
+#endif
 
 #ifndef CONFIG_SMP
 /* Assert that CONFIG_SMP is set before including asm/atomic.h to 
@@ -33,9 +59,17 @@
 #endif
 #include <inttypes.h>
 #include "libperfctr.h"
-#endif
 
-#define PERF_MAX_COUNTERS 4
+/* Native events consist of a flag field, an event field, and a unit mask field.
+ * The next 4 macros define the characteristics of the event and unit mask fields.
+ */
+#define PAPI_NATIVE_EVENT_AND_MASK 0x00000fff	/* 12 bits == 4096 max events */
+#define PAPI_NATIVE_EVENT_SHIFT 0
+#define PAPI_NATIVE_UMASK_AND_MASK 0x0ffff000	/* 16 bits for unit masks */
+#define PAPI_NATIVE_UMASK_MAX 16				/* 16 possible unit masks */
+#define PAPI_NATIVE_UMASK_SHIFT 12
+
+#define PERF_MAX_COUNTERS 7 /* i7 has 7 counters */
 #define MAX_COUNTERS PERF_MAX_COUNTERS
 #define MAX_COUNTER_TERMS  MAX_COUNTERS
 #define P3_MAX_REGS_PER_EVENT 2
@@ -43,44 +77,6 @@
 #include "papi.h"
 #include "papi_preset.h"
 
-
-#ifdef _WIN32
-#define inline_static static __inline
-
-/* cpu_type values:: lifted from perfctr.h */
-#define PERFCTR_X86_GENERIC	0	/* any x86 with rdtsc */
-#define PERFCTR_X86_INTEL_P5	1	/* no rdpmc */
-#define PERFCTR_X86_INTEL_P5MMX	2
-#define PERFCTR_X86_INTEL_P6	3
-#define PERFCTR_X86_INTEL_PII	4
-#define PERFCTR_X86_INTEL_PIII	5
-#define PERFCTR_X86_CYRIX_MII	6
-#define PERFCTR_X86_WINCHIP_C6	7	/* no rdtsc */
-#define PERFCTR_X86_WINCHIP_2	8	/* no rdtsc */
-#define PERFCTR_X86_AMD_K7	9
-#define PERFCTR_X86_VIA_C3	10	/* no pmc0 */
-#define PERFCTR_X86_INTEL_P4	11	/* model 0 and 1 */
-#define PERFCTR_X86_INTEL_P4M2	12	/* model 2 and above */
-
-/* Lock macros. */
-extern CRITICAL_SECTION lock[PAPI_MAX_LOCK];
-
-#define  _papi_hwd_lock(lck) EnterCriticalSection(&lock[lck])
-#define  _papi_hwd_unlock(lck) LeaveCriticalSection(&lock[lck])
-
-//typedef siginfo_t hwd_siginfo_t;
-typedef int hwd_siginfo_t;
-//typedef ucontext_t hwd_ucontext_t;
-typedef CONTEXT hwd_ucontext_t;
-
-#define GET_OVERFLOW_ADDRESS(ctx) ((caddr_t)(ctx->ucontext->Eip))
-
-/* Windows DOES NOT support hardware overflow */
-#define HW_OVERFLOW 0
-
-#else
-
-#define inline_static inline static
 
 /* Lock macros. */
 extern volatile unsigned int lock[PAPI_MAX_LOCK];
@@ -106,30 +102,22 @@ do                                              \
    __asm__ __volatile__ ("xchg %0,%1" : "=r"(res) : "m"(lock[lck]), "0"(MUTEX_OPEN) : "memory");                                \
 } while(0)
 
-/* Overflow-related defines and declarations */
-typedef struct {
-   siginfo_t *si;
-   struct sigcontext *ucontext;
-} _papi_hwd_context_t;
-
 typedef siginfo_t hwd_siginfo_t;
 typedef ucontext_t hwd_ucontext_t;
 
 /* Overflow macros */
 #ifdef __x86_64__
-#define GET_OVERFLOW_ADDRESS(ctx) (caddr_t)(((struct sigcontext *)(&ctx->ucontext->uc_mcontext))->rip)
+  #ifdef __CATAMOUNT__
+    #define GET_OVERFLOW_ADDRESS(ctx) (caddr_t)(((struct sigcontext *)(&ctx->ucontext->uc_mcontext))->sc_rip)
+  #else
+    #define GET_OVERFLOW_ADDRESS(ctx) ctx->ucontext->uc_mcontext.gregs[REG_RIP]
+  #endif
 #else
-#define GET_OVERFLOW_ADDRESS(ctx) (caddr_t)(((struct sigcontext *)(&ctx->ucontext->uc_mcontext))->eip)
+  #define GET_OVERFLOW_ADDRESS(ctx) ctx->ucontext->uc_mcontext.gregs[REG_EIP]
 #endif
-#define GET_OVERFLOW_CTR_BITS(ctx) ((_papi_hwi_context_t *)ctx)->overflow_vector
-#define HASH_OVERFLOW_CTR_BITS_TO_PAPI_INDEX(bit) _papi_hwi_event_index_map[bit]
 
 /* Linux DOES support hardware overflow */
 #define HW_OVERFLOW 1
-
-#define inline_static inline static
-
-#endif /* _WIN32 */
 
 typedef struct P3_register {
    unsigned int selector;       /* Mask for which counters in use */
@@ -170,34 +158,6 @@ typedef struct native_event_entry {
 typedef P3_reg_alloc_t hwd_reg_alloc_t;
 typedef P3_register_t hwd_register_t;
 
-#ifdef _WIN32
-/* Per eventset data structure for thread level counters */
-
-typedef struct P3_WinPMC_control {
-   hwd_native_t native[MAX_COUNTERS];
-   int native_idx;
-   unsigned char master_selector;
-   P3_register_t allocated_registers;
-   /* Buffer to pass to the kernel to control the counters */
-   struct vpmc_control control;
-   struct pmc_state state;
-} P3_WinPMC_control_t;
-
-/* Per thread data structure for thread level counters */
-
-typedef struct P3_WinPMC_context {
-   /* Handle to the open kernel driver */
-   HANDLE self;
-/*   P3_WinPMC_control_t start; */
-} P3_WinPMC_context_t;
-
-/* typedefs to conform to hardware independent PAPI code. */
-typedef P3_WinPMC_control_t hwd_control_state_t;
-typedef P3_WinPMC_context_t hwd_context_t;
-#define hwd_pmc_control vpmc_control
-
-#else
-
 typedef struct P3_perfctr_control {
    hwd_native_t native[MAX_COUNTERS];
    int native_idx;
@@ -205,6 +165,8 @@ typedef struct P3_perfctr_control {
    P3_register_t allocated_registers;
    struct vperfctr_control control;
    struct perfctr_sum_ctrs state;
+   /* Allow attach to be per-eventset. */
+   struct rvperfctr * rvperfctr;
 } P3_perfctr_control_t;
 
 typedef struct P3_perfctr_context {
@@ -217,15 +179,26 @@ typedef P3_perfctr_control_t hwd_control_state_t;
 typedef P3_perfctr_context_t hwd_context_t;
 #define hwd_pmc_control vperfctr_control
 
-#endif
-
-/* Used in determining on which counters an event can live. */
+/* Used in resources.selector to determine on which counters an event can live. */
 #define CNTR1 0x1
 #define CNTR2 0x2
 #define CNTR3 0x4
 #define CNTR4 0x8
+#define CNTR5 0x10
 #define CNTRS12 (CNTR1|CNTR2)
-#define ALLCNTRS (CNTR1|CNTR2|CNTR3|CNTR4)
+#define ALLCNTRS (CNTR1|CNTR2|CNTR3|CNTR4|CNTR5)
+
+#define HAS_MESI	  0x0100 /* indicates this event supports MESI modifiers */ 
+#define HAS_MOESI	  0x0200 /* indicates this event supports MOESI modifiers */
+#define HAS_UMASK	  0x0400 /* indicates this event has defined unit mask bits */
+#define MOESI_M		  0x1000 /* Modified bit */
+#define MOESI_O		  0x0800 /* Owner bit */
+#define MOESI_E		  0x0400 /* Exclusive bit */
+#define MOESI_S		  0x0200 /* Shared bit */
+#define MOESI_I		  0x0100 /* Invalid bit */
+#define MOESI_M_INTEL	  MOESI_O /* Modified bit on Intel processors */
+#define MOESI_ALL	  0x1F00 /* mask for MOESI bits in event code or counter_cmd */
+#define UNIT_MASK_ALL	  0xFF00 /* mask for unit mask bits in event code or counter_cmd */
 
 /* Masks to craft an eventcode to perfctr's liking */
 #define PERF_CTR_MASK          0xFF000000
@@ -240,19 +213,25 @@ typedef P3_perfctr_context_t hwd_context_t;
 #define PERF_EVNT_MASK         0x000000FF
 
 #define AI_ERROR "No support for a-mode counters after adding an i-mode counter"
-#define VOPEN_ERROR "vperfctr_open() returned NULL"
+#define VOPEN_ERROR "vperfctr_open() returned NULL, please run perfex -i to verify your perfctr installation"
 #define GOPEN_ERROR "gperfctr_open() returned NULL"
 #define VINFO_ERROR "vperfctr_info() returned < 0"
 #define VCNTRL_ERROR "vperfctr_control() returned < 0"
+#define RCNTRL_ERROR "rvperfctr_control() returned < 0"
 #define GCNTRL_ERROR "gperfctr_control() returned < 0"
 #define FOPEN_ERROR "fopen(%s) returned NULL"
 #define STATE_MAL_ERROR "Error allocating perfctr structures"
-#define MODEL_ERROR "This is not a Pentium I,II,III, Athlon or Opteron"
+#define MODEL_ERROR "This is not a supported cpu."
 
 extern native_event_entry_t *native_table;
 extern hwi_search_t *preset_search_map;
-extern unsigned int p3_size, p2_size, ath_size, opt_size, NATIVE_TABLE_SIZE;
-extern char *basename(char *);
-extern caddr_t _start, _init, _etext, _fini, _end, _edata, __bss_start;
+
+#if __CATAMOUNT__
+  extern void _start ( );
+  extern caddr_t _etext[ ], _edata[ ];
+  extern caddr_t __stop___libc_freeres_ptrs[ ];
+#else
+  extern caddr_t _start, _init, _etext, _fini, _end, _edata, __bss_start;
+#endif
 
 #endif /* _PAPI_PENTIUM3 */
