ad0c343f1f2cf95723cf20882b100a35377383db
Brian Sheely <bsheely@utk.edu>
2010-02-24 21:50:37 +0000

    Removed hack to compile without warnings using Wconversion

--- src/linux-memory.c
+++ src/linux-memory.c
@@ -48,23 +48,6 @@ _linux_get_dmem_info( PAPI_dmem_info_t * d )
 	return PAPI_OK;
 }
 #else
-static size_t
-_strspn( const char *s1, const char *s2 )
-{
-	size_t i, j;
-
-	for ( i = 0; *s1; ++i, ++s1 ) {
-		for ( j = 0; s2[j]; ++j )
-			if ( *s1 == s2[j] )
-				break;
-
-		if ( !s2[j] )
-			break;
-	}
-
-	return i;
-}
-
 int
 _linux_get_dmem_info( PAPI_dmem_info_t * d )
 {
@@ -81,47 +64,44 @@ _linux_get_dmem_info( PAPI_dmem_info_t * d )
 		return PAPI_ESBSTR;
 	}
 	while ( 1 ) {
-		/* NOTE: Passing tmp to strspn results in a compiler warning that seems to be
-		   impossible to resolve. Thus, _strspn was implemented and used below.
-		 */
 		if ( fgets( tmp, PATH_MAX, f ) == NULL )
 			break;
-		if ( _strspn( tmp, "VmSize:" ) == strlen( "VmSize:" ) ) {
+		if ( strspn( tmp, "VmSize:" ) == strlen( "VmSize:" ) ) {
 			sscanf( tmp + strlen( "VmSize:" ), "%lld", &sz );
 			d->size = sz;
 			continue;
 		}
-		if ( _strspn( tmp, "VmHWM:" ) == strlen( "VmHWM:" ) ) {
+		if ( strspn( tmp, "VmHWM:" ) == strlen( "VmHWM:" ) ) {
 			sscanf( tmp + strlen( "VmHWM:" ), "%lld", &hwm );
 			d->high_water_mark = hwm;
 			continue;
 		}
-		if ( _strspn( tmp, "VmLck:" ) == strlen( "VmLck:" ) ) {
+		if ( strspn( tmp, "VmLck:" ) == strlen( "VmLck:" ) ) {
 			sscanf( tmp + strlen( "VmLck:" ), "%lld", &lck );
 			d->locked = lck;
 			continue;
 		}
-		if ( _strspn( tmp, "VmRSS:" ) == strlen( "VmRSS:" ) ) {
+		if ( strspn( tmp, "VmRSS:" ) == strlen( "VmRSS:" ) ) {
 			sscanf( tmp + strlen( "VmRSS:" ), "%lld", &res );
 			d->resident = res;
 			continue;
 		}
-		if ( _strspn( tmp, "VmData:" ) == strlen( "VmData:" ) ) {
+		if ( strspn( tmp, "VmData:" ) == strlen( "VmData:" ) ) {
 			sscanf( tmp + strlen( "VmData:" ), "%lld", &dat );
 			d->heap = dat;
 			continue;
 		}
-		if ( _strspn( tmp, "VmStk:" ) == strlen( "VmStk:" ) ) {
+		if ( strspn( tmp, "VmStk:" ) == strlen( "VmStk:" ) ) {
 			sscanf( tmp + strlen( "VmStk:" ), "%lld", &stk );
 			d->stack = stk;
 			continue;
 		}
-		if ( _strspn( tmp, "VmExe:" ) == strlen( "VmExe:" ) ) {
+		if ( strspn( tmp, "VmExe:" ) == strlen( "VmExe:" ) ) {
 			sscanf( tmp + strlen( "VmExe:" ), "%lld", &txt );
 			d->text = txt;
 			continue;
 		}
-		if ( _strspn( tmp, "VmLib:" ) == strlen( "VmLib:" ) ) {
+		if ( strspn( tmp, "VmLib:" ) == strlen( "VmLib:" ) ) {
 			sscanf( tmp + strlen( "VmLib:" ), "%lld", &lib );
 			d->library = lib;
 			continue;
