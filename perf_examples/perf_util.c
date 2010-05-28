20c60482c7cb33c6327a5f5ab4c9ac7dea2ea5f7
Stephane Eranian <eranian@gmail.com>
2010-05-28 15:42:04 +0200

    Revert "fix memory leak in perf_setup_list_events()"
    This reverts commit ffe598954508d922d514ef41a1aa99c09f1b0fba.

--- perf_examples/perf_util.c
+++ perf_examples/perf_util.c
@@ -138,7 +138,6 @@ perf_setup_list_events(const char *ev, perf_event_desc_t **fd, int *num_fds)
 	argv[i] = NULL;
 	ret = perf_setup_argv_events(argv, fd, num_fds);
 	free(argv);
-	free(events);
 	return ret;
 }
 
