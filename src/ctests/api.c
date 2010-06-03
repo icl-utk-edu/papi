5547cd2dd953c36506c0e3a596ad0e513b67a371
Brian Sheely <bsheely@utk.edu>
2010-06-03 18:40:46 +0000

    Initial commit for ctests/api which is not yet implemented

---
+++ src/ctests/api.c
@@ -0,0 +1,20 @@
+/* 
+* File:    api.c
+* CVS:     $Id$
+* Author:  Brian Sheely
+*          bsheely@eecs.utk.edu
+*
+* Description: This test is designed to provide unit testing and complete
+*              coverage for all functions which comprise the "Low Level API" 
+*              and the "High Level API" as defined in papi.h.
+*/
+
+#include "papi.h"
+#include "papi_test.h"
+
+int
+main(  )
+{
+	test_pass( __FILE__, NULL, 0 );
+	exit( 1 );
+}
