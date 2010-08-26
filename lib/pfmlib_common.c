icl-utk-edu/papi

commit f3398129497c38693f0e5b4264c66fee5c467747

use pfmlib_pmus[] instead of pfmlib_pmus_map[] when scanning all PMUs
This is more direct and also respects the order
in which the PMU models are organized in pfmlib_pmus[].
This is important for architecture where there is a
generic PMU (fallback).

For instance without the patch on Intel Westmere:

Detected PMU models:
	[16, ix86arch, "Intel X86 architectural PMU"]
	[50, wsm, "Intel Westmere"]
	[51, wsm_unc, "Intel Westmere uncore"]
Total events: 1865
Event ix86arch::INSTRUCTION_RETIRED:k=0:u=1:e=0:i=0:c=0:t=0:
	PMU: Intel X86 architectural PMU
	IDX: 268435457
	codes[0]=0x5100c0

$ examples/check_events instruction_retired
Event ix86arch::INSTRUCTION_RETIRED:k=0:u=1:e=0:i=0:c=0:t=0:
	PMU: Intel X86 architectural PMU
	IDX: 268435457
	codes[0]=0x5100c0

It picks up the architected PMU (ix86arch) first rather than wsm.
With the patch applied:

$ examples/check_events instruction_retired
Event wsm::INST_RETIRED:ANY_P:k=0:u=1:e=0:i=0:c=0:t=0:
	PMU: Intel Westmere
	IDX: 838860818
	codes[0]=0x5101c0

Which is better as it may offer a bit more modifiers.

Signed-off-by: Stephane Eranian <eranian@gmail.com>

lib/pfmlib_common.c
@@ -136,8 +136,8 @@ static pfmlib_pmu_t *pfmlib_pmus_map[PFM_PMU_MAX];
 #define for_each_pmu_event_attr(u, i) \
 	for((u)=0; (u) < (i)->nattrs; (u) = (u)+1)
 
-#define pfmlib_for_all_pmu(x) \
-	for((x)= 0 ; (x) < PFM_PMU_MAX; (x)++)
+#define pfmlib_for_each_pmu(x) \
+	for((x)= 0 ; (x) < PFMLIB_NUM_PMUS; (x)++)
 
 pfmlib_config_t pfm_cfg;
 
@@ -348,9 +348,6 @@ pfmlib_init_pmus(void)
 	int i, ret, n = 0;
 	int nsuccess = 0;
 	
-	for(i=0; i < PFM_PMU_MAX; i++)
-		pfmlib_pmus_map[i] = NULL;
-
 	if (pfm_cfg.forced_pmu) {
 		char *p;
 		p = strchr(pfm_cfg.forced_pmu, ',');
@@ -425,13 +422,13 @@ void
 pfm_terminate(void)
 {
 	pfmlib_pmu_t *pmu;
-	int pmu_id;
+	int id;
 
 	if (PFMLIB_INITIALIZED() == 0)
 		return;
 
-	pfmlib_for_all_pmu(pmu_id) {
-		pmu = pfmlib_pmus_map[pmu_id];
+	pfmlib_for_each_pmu(id) {
+		pmu = pfmlib_pmus[id];
 		if (!pfmlib_pmu_active(pmu))
 			continue;
 		if (pmu->pmu_terminate)
@@ -647,7 +644,7 @@ pfmlib_parse_event(const char *event, pfmlib_event_desc_t *d)
 	pfm_event_info_t einfo;
 	char *str, *s, *p, *q;
 	pfmlib_pmu_t *pmu;
-	int i, pmu_id;
+	int i, id;
 	const char *pname = NULL;
 	int ret;
 
@@ -684,10 +681,8 @@ pfmlib_parse_event(const char *event, pfmlib_event_desc_t *d)
 	/*
 	 * for each pmu
 	 */
-	pfmlib_for_all_pmu(pmu_id) {
-		pmu = pfmlib_pmus_map[pmu_id];
-		if (!pmu)
-			continue;
+	pfmlib_for_each_pmu(id) {
+		pmu = pfmlib_pmus[id];
 		/*
 		 * if no explicit PMU name is given, then
 		 * only look for active (detected) PMU models
@@ -739,12 +734,10 @@ int
 pfm_get_nevents(void)
 {
 	pfmlib_pmu_t *pmu;
-	int pmu_id, total = 0;
+	int id, total = 0;
 
-	pfmlib_for_all_pmu(pmu_id) {
-		pmu = pfmlib_pmus_map[pmu_id];
-		if (!pmu)
-			continue;
+	pfmlib_for_each_pmu(id) {
+		pmu = pfmlib_pmus[id];
 		total += pmu->pme_count;
 	}
 	return total;
@@ -834,13 +827,11 @@ int
 pfm_get_event_first(void)
 {
 	pfmlib_pmu_t *pmu;
-	int pmu_id, pidx;
+	int id, pidx;
 
 	/* scan all compiled in PMU models */
-	pfmlib_for_all_pmu(pmu_id) {
-		pmu = pfmlib_pmus_map[pmu_id];
-		if (!pmu)
-			continue;
+	pfmlib_for_each_pmu(id) {
+		pmu = pfmlib_pmus[id];
 		pidx = pmu->get_event_first(pmu);
 		if (pidx != -1)
 			return pfmlib_pidx2idx(pmu, pidx);

commit d5bfca1dba77a4a2e79856a8ab48410f6023ae9c

fix invalid pfmlib_get_pmu_next()
commit f339812 introduced a bug in pfmlib_get_pmu_next()
whereby it would not look at certain entries in pfmlib_pmus[].
In the new scheme, we need to scan pfmlib_pmus[] to find the
next pmu.

Signed-off-by: Stephane Eranian <eranian@gmail.com>

lib/pfmlib_common.c
@@ -780,16 +780,19 @@ pfm_get_version(void)
 }
 
 static pfmlib_pmu_t *
-pfmlib_get_pmu_next(pfm_pmu_t i)
+pfmlib_get_pmu_next(pfm_pmu_t p)
 {
 	pfmlib_pmu_t *pmu;
+	int i;
 
-	for(++i; i < PFM_PMU_MAX; i++) {
-		pmu = pfmlib_pmus_map[i];
-		if (pmu)
-			return pmu;
+	for(i = 0; i < PFMLIB_NUM_PMUS; i++) {
+		pmu = pfmlib_pmus[i];
+		if (pmu->pmu == p)
+			goto found;
 	}
 	return NULL;
+found:
+	return i == (PFMLIB_NUM_PMUS-1) ? NULL : pfmlib_pmus[i+1];
 }
 
 int