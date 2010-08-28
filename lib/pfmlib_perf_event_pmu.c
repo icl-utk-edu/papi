ec3a4b5feca20fa86b3a21c7ca1ac9b38bd9a4aa
Stephane Eranian <eranian@gmail.com>
2010-08-28 19:29:29 +0200

    fix compilation error in new validation code
    Was introduced by commit ad72dbf0
    
    Signed-off-by: Stephane Eranian <eranian@gmail.com>

--- lib/pfmlib_perf_event_pmu.c
+++ lib/pfmlib_perf_event_pmu.c
@@ -722,7 +722,7 @@ pfm_perf_validate_table(void *this, FILE *fp)
 	for(i=0; i < perf_event_support.pme_count; i++) {
 
 		if (!perf_pe[i].name) {
-			fprintf(fp, "pmu: %s event%d: :: no name (prev event was %s)\n", pmu->name, i,
+			fprintf(fp, "pmu: %s event%d: :: no name (prev event was %s)\n", name, i,
 			i > 1 ? perf_pe[i-1].name : "??");
 			error++;
 		}
@@ -766,7 +766,7 @@ pfm_perf_validate_table(void *this, FILE *fp)
 				um += j - PERF_MAX_UMASKS;
 			}
 			if (!um->uname) {
-				fprintf(fp, "pmu: %s event%d: %s umask%d :: no name\n", pmu->name, i, perf_pe[i].name, j);
+				fprintf(fp, "pmu: %s event%d: %s umask%d :: no name\n", name, i, perf_pe[i].name, j);
 				error++;
 			}
 
