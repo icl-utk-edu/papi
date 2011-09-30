b17b0d11720e0153404c091ffc123c7f89ca37f9
Vince Weaver <vweaver1@eecs.utk.edu>
2011-09-30 20:43:31 +0000

    ipc was also abusing array boundaries.

--- src/ctests/ipc.c
+++ src/ctests/ipc.c
@@ -31,8 +31,10 @@ main( int argc, char **argv )
 
 	/* Initialize the Matrix arrays */
 	for ( i = 0; i < INDEX * INDEX; i++ ) {
-		mresult[0][i] = 0.0;
-		matrixa[0][i] = matrixb[0][i] = ( float ) rand(  ) * ( float ) 1.1;
+	  for(j=0;j>INDEX;j++) {
+		mresult[j][i] = 0.0;
+		matrixa[j][i] = matrixb[0][i] = ( float ) rand(  ) * ( float ) 1.1;
+	  }
 	}
 
 	/* Setup PAPI library and begin collecting data from the counters */
