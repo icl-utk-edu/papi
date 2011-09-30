5a76f72fb38ef4384d096b69e11d11ef0f8500c7
Vince Weaver <vweaver1@eecs.utk.edu>
2011-09-30 16:01:31 +0000

    Move more code to its proper place.

--- src/aix.c
+++ src/aix.c
@@ -43,7 +43,106 @@ atomic_p lock[PAPI_MAX_LOCK];
 static int maxgroups = 0;
 struct utsname AixVer;
 
-extern hwd_groups_t group_map[];
+native_event_entry_t native_table[PAPI_MAX_NATIVE_EVENTS];
+
+hwd_pminfo_t pminfo;
+pm_groups_info_t pmgroups;
+native_event_entry_t native_table[PAPI_MAX_NATIVE_EVENTS];
+PPC64_native_map_t native_name_map[PAPI_MAX_NATIVE_EVENTS];
+hwd_groups_t group_map[MAX_GROUPS] = { 0 };
+
+/* to initialize the native_table */
+void
+aix_initialize_native_table(  )
+{
+	int i, j;
+
+	memset( native_table, 0,
+			PAPI_MAX_NATIVE_EVENTS * sizeof ( native_event_entry_t ) );
+	memset( native_name_map, 0,
+			PAPI_MAX_NATIVE_EVENTS * sizeof ( PPC64_native_map_t ) );
+	for ( i = 0; i < PAPI_MAX_NATIVE_EVENTS; i++ ) {
+		native_name_map[i].index = -1;
+		for ( j = 0; j < MAX_COUNTERS; j++ )
+			native_table[i].resources.counter_cmd[j] = -1;
+	}
+}
+
+/* to setup native_table group value */
+static void
+aix_ppc64_setup_gps( int total )
+{
+	int i, j, gnum;
+
+	for ( i = 0; i < total; i++ ) {
+		for ( j = 0; j < MAX_COUNTERS; j++ ) {
+			/*      native_table[i].resources.rgg[j]=-1; */
+			if ( native_table[i].resources.selector & ( 1 << j ) ) {
+				for ( gnum = 0; gnum < pmgroups.maxgroups; gnum++ ) {
+					if ( native_table[i].resources.counter_cmd[j] ==
+						 pmgroups.event_groups[gnum].events[j] ) {
+						/* could use gnum instead of pmgroups.event_groups[gnum].group_id */
+						native_table[i].resources.group[pmgroups.
+														event_groups[gnum].
+														group_id / 32] |=
+							1 << ( pmgroups.event_groups[gnum].group_id % 32 );
+					}
+				}
+			}
+		}
+	}
+
+	for ( gnum = 0; gnum < pmgroups.maxgroups; gnum++ ) {
+		for ( i = 0; i < MAX_COUNTERS; i++ ) {
+			/*group_map[gnum].counter_cmd[i] = pmgroups.event_groups[gnum].events[i]; */
+		    if (pmgroups.event_groups[gnum].group_id >=MAX_GROUPS) {
+		       fprintf(stderr,"ERROR, group number trying to go past MAX GROUPS\n");
+		       continue;
+		    } 
+
+			group_map[pmgroups.event_groups[gnum].group_id].counter_cmd[i] =
+				pmgroups.event_groups[gnum].events[i];
+		}
+	}
+}
+
+/* to setup native_table values, and return number of entries */
+int
+aix_ppc64_setup_native_table(  )
+{
+	hwd_pmevents_t *wevp;
+	hwd_pminfo_t *info;
+	int pmc, ev, i, j, index;
+
+	info = &pminfo;
+	index = 0;
+	initialize_native_table(  );
+	for ( pmc = 0; pmc < info->maxpmcs; pmc++ ) {
+		wevp = info->list_events[pmc];
+		for ( ev = 0; ev < info->maxevents[pmc]; ev++, wevp++ ) {
+			for ( i = 0; i < index; i++ ) {
+				if ( strcmp( wevp->short_name, native_table[i].name ) == 0 ) {
+					native_table[i].resources.selector |= 1 << pmc;
+					native_table[i].resources.counter_cmd[pmc] = wevp->event_id;
+					break;
+				}
+			}
+			if ( i == index ) {
+				/*native_table[i].index=i; */
+				native_table[i].resources.selector |= 1 << pmc;
+				native_table[i].resources.counter_cmd[pmc] = wevp->event_id;
+				native_table[i].name = wevp->short_name;
+				native_table[i].description = wevp->description;
+				native_name_map[i].name = native_table[i].name;
+				native_name_map[i].index = i;
+				index++;
+			}
+		}
+	}
+	ppc64_setup_gps( index );
+
+	return index;
+}
 
 /* Reports the elements of the hwd_register_t struct as an array of names and a matching array of values.
    Maximum string length is name_len; Maximum number of values is count.
@@ -684,7 +783,7 @@ _aix_init_substrate( int cidx )
 			_papi_hwi_system_info.hw_info.mhz );
 
 	MY_VECTOR.cmp_info.CmpIdx = cidx;
-	MY_VECTOR.cmp_info.num_native_events = ppc64_setup_native_table(  );
+	MY_VECTOR.cmp_info.num_native_events = aix_ppc64_setup_native_table(  );
 
 	procidx = pm_get_procindex(  );
 	switch ( procidx ) {
