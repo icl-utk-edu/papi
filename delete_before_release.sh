139ce8710316940186f46b16f1545a20f4f6b0a3
Dan Terpstra <terpstra@eecs.utk.edu>
2011-10-26 15:48:32 +0000

    Updates for the 4.2.0 release.

--- delete_before_release.sh
+++ delete_before_release.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 rm PAPI_FAQ.html
 rm release_procedure.txt
-rm doc/cat_progref.bat
 rm doc/DataRange.html
 rm doc/PAPI-C.html
-rm doc/papiprog.bat
-rm doc/PAPI_Prog_Ref.doc
-rm doc/papi_progref.pl
-rm doc/PAPI_USER_GUIDE.doc
-rm doc/procedure.txt
-rm doc/progref_template.doc
 rm doc/README
 rm delete_before_release.sh
 rm cvs2cl.pl
