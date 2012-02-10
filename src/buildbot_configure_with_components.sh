3dd2ba93cb63eb37fc0091047cbe2b516f4f4dd2
James Ralph <ralph@eecs.utk.edu>
2012-02-10 20:15:10 +0000

    take infiniband out of the buildbot test.

--- src/buildbot_configure_with_components.sh
+++ src/buildbot_configure_with_components.sh
@@ -1,2 +1,4 @@
 #!/bin/sh 
-./configure --with-debug --with-components="coretemp example infiniband lustre mx net" $1 
+# this is the configuration that goes into a fedora rpm
+#./configure --with-debug --with-components="coretemp example infiniband lustre mx net" $1 
+./configure --with-debug --with-components="coretemp example lustre mx net" $1 
