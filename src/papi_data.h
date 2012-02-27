f197d4ab48210c8b78ac1de4279e76da2dd5a1c7
Vince Weaver <vweaver1@eecs.utk.edu>
2012-02-27 12:11:16 -0500

    Remove unncessary extern declarations in papi_internal.c

--- src/papi_data.h
+++ src/papi_data.h
@@ -2,6 +2,8 @@
 #define PAPI_DATA_H
 
 extern hwi_presets_t _papi_hwi_presets;
+extern const hwi_preset_info_t _papi_hwi_preset_info[PAPI_MAX_PRESET_EVENTS];
+extern const unsigned int _papi_hwi_preset_type[PAPI_MAX_PRESET_EVENTS];
 
 int _papi_hwi_derived_type( char *derived, int *code );
 int _papi_hwi_derived_string( int type, char *derived, int len );
