ef484c00e973579773771a964fa530bb587dcbfa
Vince Weaver <vweaver1@eecs.utk.edu>
2012-05-17 13:39:33 -0400

    Fix typo in linux-timer.h

--- src/linux-timer.h
+++ src/linux-timer.h
@@ -1,6 +1,6 @@
 long long _linux_get_real_cycles( void );
 
-long long _linux_get_virt_usec_pttimers( void );
+long long _linux_get_virt_usec_pttimer( void );
 long long _linux_get_virt_usec_gettime( void );
 long long _linux_get_virt_usec_times( void );
 long long _linux_get_virt_usec_rusages( void );
