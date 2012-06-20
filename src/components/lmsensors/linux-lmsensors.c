caa94d64e8b4d8f6c1da45157e75a12428fc171c
Vince Weaver <vweaver1@eecs.utk.edu>
2012-06-20 15:27:13 -0400

    Update lmsensors component to actually compile.
    I finally found a machine with lmsensors installed.

--- src/components/lmsensors/linux-lmsensors.c
+++ src/components/lmsensors/linux-lmsensors.c
@@ -265,6 +265,7 @@ int
 _lmsensors_init_substrate( int cidx )
 {
     int res;
+    (void) cidx;
 
     /* Initialize libsensors library */
     if ( ( res = sensors_init( NULL ) ) != 0 ) {
@@ -288,7 +289,7 @@ _lmsensors_init_substrate( int cidx )
     if ( ( unsigned ) num_events != createNativeEvents(  ) ) {
        strncpy(_lmsensors_vector.cmp_info.disabled_reason,
 	      "LM_SENSOR number mismatch",PAPI_MAX_STR_LEN);
-       return PAPI_SBSTR;
+       return PAPI_ESBSTR;
     }
 
     _lmsensors_vector.cmp_info.num_native_events=num_events;
@@ -308,10 +309,10 @@ _lmsensors_init_control_state( hwd_control_state_t *ctl )
 	int i;
 
 	for ( i = 0; i < num_events; i++ )
-		( ( _lmsensors_control_state_t * ) ctrl )->counts[i] =
+		( ( _lmsensors_control_state_t * ) ctl )->counts[i] =
 			getEventValue( i );
 
-	( ( _lmsensors_control_state_t * ) ctrl )->lastupdate =
+	( ( _lmsensors_control_state_t * ) ctl )->lastupdate =
 		PAPI_get_real_usec(  );
 	return PAPI_OK;
 }
@@ -355,7 +356,7 @@ _lmsensors_read( hwd_context_t *ctx, hwd_control_state_t *ctl,
     long long start = PAPI_get_real_usec(  );
     int i;
  
-    _lmsensors_control_state_t *control=(_lmsensors_control_sate_t *)ctl;
+    _lmsensors_control_state_t *control=(_lmsensors_control_state_t *)ctl;
 
     if ( start - control->lastupdate > 200000 ) {	// cache refresh
        
@@ -540,7 +541,7 @@ papi_vector_t _lmsensors_vector = {
 	.version = "5.0",
 	.description = "Linux LMsensor statistics",
 	.num_mpx_cntrs = PAPI_MPX_DEF_DEG,
-	.num_cntrs = _lmsensors_MAX_COUNTERS,
+	.num_cntrs = LM_SENSORS_MAX_COUNTERS,
 	.default_domain = PAPI_DOM_USER,
 	//.available_domains = PAPI_DOM_USER,
 	.default_granularity = PAPI_GRN_THR,
