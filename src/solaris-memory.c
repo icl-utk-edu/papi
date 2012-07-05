681ef0274f8a6ad532d2d401a3672451bce8c219
Vince Weaver <vweaver1@eecs.utk.edu>
2012-07-05 15:39:56 -0400

    Merge solaris-memory.c and solaris-niagara2-memory.c

--- src/solaris-memory.c
+++ src/solaris-memory.c
@@ -8,6 +8,9 @@
 *
 * Mods:    Vince Weaver
 *          vweaver1@eecs.utk.edu
+*
+* Mods:    Fabian Gorsler 
+*          fabian.gorsler@smail.inf.h-bonn-rhein-sieg.de
 */
 
 #include "papi.h"
@@ -107,6 +110,9 @@ _solaris_get_memory_info( PAPI_hw_info_t * hw, int id )
 	return PAPI_OK;
 }
 
+
+
+
 int
 _solaris_get_dmem_info( PAPI_dmem_info_t * d )
 {
@@ -133,6 +139,56 @@ _solaris_get_dmem_info( PAPI_dmem_info_t * d )
 	d->locked = PAPI_EINVAL;
 	d->stack = PAPI_EINVAL;
 
-	return ( PAPI_OK );
+	return PAPI_OK;
 
+}
+
+int
+_niagara2_get_memory_info( PAPI_hw_info_t * hw, int id )
+{
+	PAPI_mh_level_t *mem = hw->mem_hierarchy.level;
+
+
+	/* I-Cache -> L1$ instruction */
+	/* FIXME: The policy used at this cache is unknown to PAPI. LSFR with random
+	   replacement. */
+	mem[0].cache[0].type = PAPI_MH_TYPE_INST;
+	mem[0].cache[0].size = 16 * 1024;	// 16 Kb
+	mem[0].cache[0].line_size = 32;
+	mem[0].cache[0].num_lines =
+		mem[0].cache[0].size / mem[0].cache[0].line_size;
+	mem[0].cache[0].associativity = 8;
+
+	/* D-Cache -> L1$ data */
+	mem[0].cache[1].type =
+		PAPI_MH_TYPE_DATA | PAPI_MH_TYPE_WT | PAPI_MH_TYPE_LRU;
+	mem[0].cache[1].size = 8 * 1024;	// 8 Kb
+	mem[0].cache[1].line_size = 16;
+	mem[0].cache[1].num_lines =
+		mem[0].cache[1].size / mem[0].cache[1].line_size;
+	mem[0].cache[1].associativity = 4;
+
+	/* ITLB -> TLB instruction */
+	mem[0].tlb[0].type = PAPI_MH_TYPE_INST | PAPI_MH_TYPE_PSEUDO_LRU;
+	mem[0].tlb[0].num_entries = 64;
+	mem[0].tlb[0].associativity = 64;
+
+	/* DTLB -> TLB data */
+	mem[0].tlb[1].type = PAPI_MH_TYPE_DATA | PAPI_MH_TYPE_PSEUDO_LRU;
+	mem[0].tlb[1].num_entries = 128;
+	mem[0].tlb[1].associativity = 128;
+
+	/* L2$ unified */
+	mem[1].cache[0].type = PAPI_MH_TYPE_UNIFIED | PAPI_MH_TYPE_WB
+		| PAPI_MH_TYPE_PSEUDO_LRU;
+	mem[1].cache[0].size = 4 * 1024 * 1024;	// 4 Mb
+	mem[1].cache[0].line_size = 64;
+	mem[1].cache[0].num_lines =
+		mem[1].cache[0].size / mem[1].cache[0].line_size;
+	mem[1].cache[0].associativity = 16;
+
+	/* Indicate we have two levels filled in the hierarchy */
+	hw->mem_hierarchy.levels = 2;
+
+	return PAPI_OK;
 }
