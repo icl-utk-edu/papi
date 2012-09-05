a9a3b343287e5f8164c1d363a9da8b24be3e84bb
William Cohen <wcohen@redhat.com>
2012-09-05 09:37:59 +0200

    simplify perf_util.c loop
    There were two separate loops in perf_setup_list_events() that were
    iterating pretty much the same way over a string.  To make it a bit
    easier to follow the second loop, a for-loop, was rewritten to be as
    similar as possible to the earlier while loop.  As a bonus, it keeps
    Coverity from complaining about the possible NULL access using the
    result of strchr.
    
    Signed-off-by: William Cohen <wcohen@redhat.com>

--- perf_examples/perf_util.c
+++ perf_examples/perf_util.c
@@ -137,13 +137,15 @@ perf_setup_list_events(const char *ev, perf_event_desc_t **fd, int *num_fds)
 		return -1;
 	}
 
-	for(i=0, q = events; i < num-2; i++, q = p + 1) {
-		p = strchr(q, ',');
+	i = 0; q = events;
+	while((p = strchr(q, ','))) {
 		*p = '\0';
-		argv[i] = q;
+		argv[i++] = q;
+		q = p + 1;
 	}
 	argv[i++] = q;
 	argv[i] = NULL;
+
 	ret = perf_setup_argv_events(argv, fd, num_fds);
 	free(argv);
 	free(events); /* strdup in perf_setup_argv_events() */
