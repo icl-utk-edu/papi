dfa802878a54d8ddcbf2665195c7a8573229d9a0
James Ralph <ralph@icl.utk.edu>
2013-01-15 14:33:23 -0500

    Buildbot configure script.
    Add cuda and nvml components, if configured, to the buildbot coverage test.
    Note: Script now checks for existance of Makefile.cuda and then Makefile.nvml
    so see if it can build the cuda component and then if it can build the nvml component.

--- src/buildbot_configure_with_components.sh
+++ src/buildbot_configure_with_components.sh
@@ -1,4 +1,12 @@
 #!/bin/sh 
 # this is the configuration that goes into a fedora rpm
 #./configure --with-debug --with-components="coretemp example infiniband lustre mx net" $1 
-./configure --with-components="coretemp example lustre mx net" $1 
+if [ -f components/cuda/Makefile.cuda ]; then
+	if [ -f components/nvml/Makefile.nvml ]; then
+		./configure --with-components="coretemp example lustre mx net cuda nvml" $1
+	else
+		./configure --with-components="coretemp example lustre mx net cuda" $1
+	fi
+else
+	./configure --with-components="coretemp example lustre mx net" $1
+fi
