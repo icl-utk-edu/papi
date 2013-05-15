6a61f9a2e388da2b43fa9172b3d095a473da1bc5
Vince Weaver <vincent.weaver@maine.edu>
2013-05-15 13:44:21 -0400

    more cleanup of the cpus.c file
    mostly formatting and added comments.

--- src/cpus.c
+++ src/cpus.c
@@ -19,75 +19,70 @@
 #include <string.h>
 #include <unistd.h>
 
-/*****************/
-/* BEGIN GLOBALS */
-/*****************/
+/* The list of cpus; this gets built as user apps set the cpu papi */
+/* option on an event set */
 
-/* The following globals get initialized and cleared by:
-   extern int _papi_hwi_init_global_cpus(void);
-   extern int _papi_hwi_shutdown_cpu(CpuInfo_t *cpu); */
-/* The list of cpus, gets built as user apps set the cpu papi option on an event set */
+static CpuInfo_t *_papi_hwi_cpu_head;
 
-/* Why is this volatile? */
-volatile CpuInfo_t *_papi_hwi_cpu_head;
-
-/*****************/
-/*  END  GLOBALS */
-/*****************/
 
 static CpuInfo_t *
 _papi_hwi_lookup_cpu( unsigned int cpu_num )
 {
-	THRDBG("Entry:\n");
-	CpuInfo_t *tmp;
+   APIDBG("Entry:\n");
 
-	_papi_hwi_lock( CPUS_LOCK );
+   CpuInfo_t *tmp;
 
-	tmp = ( CpuInfo_t * ) _papi_hwi_cpu_head;
-	while ( tmp != NULL ) {
-		THRDBG( "Examining cpu %#x at %p\n", tmp->cpu_num, tmp );
-		if ( tmp->cpu_num == cpu_num )
-			break;
-		tmp = tmp->next;
-		if ( tmp == _papi_hwi_cpu_head ) {
-			tmp = NULL;
-			break;
-		}
-	}
+   _papi_hwi_lock( CPUS_LOCK );
 
-	if ( tmp ) {
-		_papi_hwi_cpu_head = tmp;
-		THRDBG( "Found cpu %#x at %p\n", cpu_num, tmp );
-	} else {
-		THRDBG( "Did not find cpu %#x\n", cpu_num );
-	}
+   tmp = ( CpuInfo_t * ) _papi_hwi_cpu_head;
+   while ( tmp != NULL ) {
+      THRDBG( "Examining cpu %#x at %p\n", tmp->cpu_num, tmp );
+      if ( tmp->cpu_num == cpu_num ) {
+	 break;
+      }
+      tmp = tmp->next;
+      if ( tmp == _papi_hwi_cpu_head ) {
+	 tmp = NULL;
+	 break;
+      }
+   }
 
-	_papi_hwi_unlock( CPUS_LOCK );
-	return ( tmp );
+   if ( tmp ) {
+      _papi_hwi_cpu_head = tmp;
+      THRDBG( "Found cpu %#x at %p\n", cpu_num, tmp );
+   } else {
+      THRDBG( "Did not find cpu %#x\n", cpu_num );
+   }
+
+   _papi_hwi_unlock( CPUS_LOCK );
+	
+   return tmp;
 }
 
 int
-_papi_hwi_lookup_or_create_cpu( CpuInfo_t ** here, unsigned int cpu_num )
+_papi_hwi_lookup_or_create_cpu( CpuInfo_t **here, unsigned int cpu_num )
 {
-	THRDBG("Entry: here: %p\n", here);
-	CpuInfo_t *tmp = NULL;
-	int retval = PAPI_OK;
+   APIDBG("Entry: here: %p\n", here);
+	
+   CpuInfo_t *tmp = NULL;
+   int retval = PAPI_OK;
 
-	tmp = _papi_hwi_lookup_cpu(cpu_num);
-	if ( tmp == NULL )
-		retval = _papi_hwi_initialize_cpu( &tmp, cpu_num );
+   tmp = _papi_hwi_lookup_cpu(cpu_num);
+   if ( tmp == NULL ) {
+      retval = _papi_hwi_initialize_cpu( &tmp, cpu_num );
+   }
 
-	if ( retval == PAPI_OK )
-		*here = tmp;
+   if ( retval == PAPI_OK )
+      *here = tmp;
 
-	return ( retval );
+   return retval;
 }
 
 
 static CpuInfo_t *
 allocate_cpu( unsigned int cpu_num )
 {
-   THRDBG("Entry: cpu_num: %d\n", cpu_num);
+   APIDBG("Entry: cpu_num: %d\n", cpu_num);
 
    CpuInfo_t *cpu;
    int i;
@@ -129,7 +124,7 @@ allocate_cpu( unsigned int cpu_num )
 
    return cpu;
 
- error_free_contexts:
+error_free_contexts:
    for ( i--; i >= 0; i-- ) papi_free( cpu->context[i] );
 error_free_context:
    papi_free( cpu->context );
@@ -142,145 +137,160 @@ allocate_error:
 static void
 free_cpu( CpuInfo_t ** cpu )
 {
-   THRDBG( "Entry: *cpu: %p, cpu_num: %#x\n", *cpu, ( *cpu )->cpu_num);
-	int i;
-	for ( i = 0; i < papi_num_components; i++ ) {
-		if ( ( *cpu )->context[i] )
-			papi_free( ( *cpu )->context[i] );
-	}
+   APIDBG( "Entry: *cpu: %p, cpu_num: %#x\n", *cpu, ( *cpu )->cpu_num);
+	
+   int i;
+   for ( i = 0; i < papi_num_components; i++ ) {
+      if ( ( *cpu )->context[i] ) {
+	 papi_free( ( *cpu )->context[i] );
+      }
+   }
 
-	if ( ( *cpu )->context )
-		papi_free( ( *cpu )->context );
+   if ( ( *cpu )->context ) {
+      papi_free( ( *cpu )->context );
+   }
 
-	if ( ( *cpu )->running_eventset )
-		papi_free( ( *cpu )->running_eventset );
+   if ( ( *cpu )->running_eventset ) {
+      papi_free( ( *cpu )->running_eventset );
+   }
 
-	memset( *cpu, 0x00, sizeof ( CpuInfo_t ) );
-	papi_free( *cpu );
-	*cpu = NULL;
+   /* why do we clear this? */
+   memset( *cpu, 0x00, sizeof ( CpuInfo_t ) );
+   papi_free( *cpu );
+   *cpu = NULL;
 }
 
 static void
 insert_cpu( CpuInfo_t * entry )
 {
-   THRDBG("Entry: entry: %p\n", entry);
-	_papi_hwi_lock( CPUS_LOCK );
+   APIDBG("Entry: entry: %p\n", entry);
 
-	if ( _papi_hwi_cpu_head == NULL ) {	/* 0 elements */
-		THRDBG( "_papi_hwi_cpu_head is NULL\n" );
-		entry->next = entry;
-	} else if ( _papi_hwi_cpu_head->next == _papi_hwi_cpu_head ) {	/* 1 elements */
-		THRDBG( "_papi_hwi_cpu_head was cpu %d at %p\n",
-				_papi_hwi_cpu_head->cpu_num, _papi_hwi_cpu_head );
-		_papi_hwi_cpu_head->next = entry;
-		entry->next = ( CpuInfo_t * ) _papi_hwi_cpu_head;
-	} else {				 /* 2+ elements */
+   _papi_hwi_lock( CPUS_LOCK );
 
-		THRDBG( "_papi_hwi_cpu_head was cpu %d at %p\n",
-				_papi_hwi_cpu_head->cpu_num, _papi_hwi_cpu_head );
-		entry->next = _papi_hwi_cpu_head->next;
-		_papi_hwi_cpu_head->next = entry;
-	}
+   if ( _papi_hwi_cpu_head == NULL ) {	
+      /* 0 elements */
+      THRDBG( "_papi_hwi_cpu_head is NULL\n" );
+      entry->next = entry;
+   } else if ( _papi_hwi_cpu_head->next == _papi_hwi_cpu_head ) {
+      /* 1 element */
+      THRDBG( "_papi_hwi_cpu_head was cpu %d at %p\n",
+              _papi_hwi_cpu_head->cpu_num, _papi_hwi_cpu_head );
+      _papi_hwi_cpu_head->next = entry;
+      entry->next = ( CpuInfo_t * ) _papi_hwi_cpu_head;
+   } else {
+      /* 2+ elements */
+      THRDBG( "_papi_hwi_cpu_head was cpu %d at %p\n",
+	      _papi_hwi_cpu_head->cpu_num, _papi_hwi_cpu_head );
+      entry->next = _papi_hwi_cpu_head->next;
+      _papi_hwi_cpu_head->next = entry;
+   }
 
-	_papi_hwi_cpu_head = entry;
+   _papi_hwi_cpu_head = entry;
 
-	THRDBG( "_papi_hwi_cpu_head now cpu %d at %p\n",
-			_papi_hwi_cpu_head->cpu_num, _papi_hwi_cpu_head );
+   THRDBG( "_papi_hwi_cpu_head now cpu %d at %p\n",
+	   _papi_hwi_cpu_head->cpu_num, _papi_hwi_cpu_head );
 
-	_papi_hwi_unlock( CPUS_LOCK );
+   _papi_hwi_unlock( CPUS_LOCK );
 
 }
 
 static int
 remove_cpu( CpuInfo_t * entry )
 {
-   THRDBG("Entry: entry: %p\n", entry);
-	CpuInfo_t *tmp = NULL, *prev = NULL;
+   APIDBG("Entry: entry: %p\n", entry);
+	
+   CpuInfo_t *tmp = NULL, *prev = NULL;
 
-	_papi_hwi_lock( CPUS_LOCK );
+   _papi_hwi_lock( CPUS_LOCK );
 
-	THRDBG( "_papi_hwi_cpu_head was cpu %d at %p\n",
+   THRDBG( "_papi_hwi_cpu_head was cpu %d at %p\n",
 			_papi_hwi_cpu_head->cpu_num, _papi_hwi_cpu_head );
 
 	/* Find the preceding element and the matched element,
 	   short circuit if we've seen the head twice */
 
-	for ( tmp = ( CpuInfo_t * ) _papi_hwi_cpu_head;
-		  ( entry != tmp ) || ( prev == NULL ); tmp = tmp->next ) {
-		prev = tmp;
-	}
+   for ( tmp = ( CpuInfo_t * ) _papi_hwi_cpu_head;
+       ( entry != tmp ) || ( prev == NULL ); tmp = tmp->next ) {
+       prev = tmp;
+   }
 
-	if ( tmp != entry ) {
-		THRDBG( "Cpu %d at %p was not found in the cpu list!\n",
+   if ( tmp != entry ) {
+      THRDBG( "Cpu %d at %p was not found in the cpu list!\n",
 				entry->cpu_num, entry );
-		return ( PAPI_EBUG );
-	}
+      return PAPI_EBUG;
+   }
 
-	/* Only 1 element in list */
+   /* Only 1 element in list */
 
-	if ( prev == tmp ) {
-		_papi_hwi_cpu_head = NULL;
-		tmp->next = NULL;
-		THRDBG( "_papi_hwi_cpu_head now NULL\n" );
-	} else {
-		prev->next = tmp->next;
-		/* If we're removing the head, better advance it! */
-		if ( _papi_hwi_cpu_head == tmp ) {
-			_papi_hwi_cpu_head = tmp->next;
-			THRDBG( "_papi_hwi_cpu_head now cpu %d at %p\n",
-					_papi_hwi_cpu_head->cpu_num, _papi_hwi_cpu_head );
-		}
-		THRDBG( "Removed cpu %p from list\n", tmp );
-	}
+   if ( prev == tmp ) {
+      _papi_hwi_cpu_head = NULL;
+      tmp->next = NULL;
+      THRDBG( "_papi_hwi_cpu_head now NULL\n" );
+   } else {
+      prev->next = tmp->next;
+      /* If we're removing the head, better advance it! */
+      if ( _papi_hwi_cpu_head == tmp ) {
+	 _papi_hwi_cpu_head = tmp->next;
+	 THRDBG( "_papi_hwi_cpu_head now cpu %d at %p\n",
+		 _papi_hwi_cpu_head->cpu_num, _papi_hwi_cpu_head );
+      }
+      THRDBG( "Removed cpu %p from list\n", tmp );
+   }
 
-	_papi_hwi_unlock( CPUS_LOCK );
+   _papi_hwi_unlock( CPUS_LOCK );
 
-	return ( PAPI_OK );
+   return PAPI_OK;
 }
 
 int
 _papi_hwi_initialize_cpu( CpuInfo_t ** dest, unsigned int cpu_num )
 {
-   THRDBG("Entry: dest: %p, *dest: %p, cpu_num: %d\n", dest, *dest, cpu_num);
-	int retval;
-	CpuInfo_t *cpu;
-	int i;
+   APIDBG("Entry: dest: %p, *dest: %p, cpu_num: %d\n", dest, *dest, cpu_num);
 
-	if ( ( cpu = allocate_cpu(cpu_num) ) == NULL ) {
-		*dest = NULL;
-		return ( PAPI_ENOMEM );
-	}
+   int retval;
+   CpuInfo_t *cpu;
+   int i;
 
-	/* Call the component to fill in anything special. */
-	for ( i = 0; i < papi_num_components; i++ ) {
-		retval = _papi_hwd[i]->init_thread( cpu->context[i] );
-		if ( retval ) {
-			free_cpu( &cpu );
-			*dest = NULL;
-			return ( retval );
-		}
-	}
+   if ( ( cpu = allocate_cpu(cpu_num) ) == NULL ) {
+      *dest = NULL;
+      return PAPI_ENOMEM;
+   }
 
-	insert_cpu( cpu );
+   /* Call the component to fill in anything special. */
+   for ( i = 0; i < papi_num_components; i++ ) {
+      retval = _papi_hwd[i]->init_thread( cpu->context[i] );
+      if ( retval ) {
+	 free_cpu( &cpu );
+	 *dest = NULL;
+	 return retval;
+      }
+   }
 
-	*dest = cpu;
-	return ( PAPI_OK );
+   insert_cpu( cpu );
+
+   *dest = cpu;
+   return PAPI_OK;
 }
 
 int
-_papi_hwi_shutdown_cpu( CpuInfo_t * cpu )
+_papi_hwi_shutdown_cpu( CpuInfo_t *cpu )
 {
-   THRDBG("Entry: cpu: %p, cpu_num: %d\n", cpu, cpu->cpu_num);
-	int retval = PAPI_OK;
-	int i, failure = 0;
+   APIDBG("Entry: cpu: %p, cpu_num: %d\n", cpu, cpu->cpu_num);
 
-	remove_cpu( cpu );
-	THRDBG( "Shutting down cpu %d at %p\n", cpu->cpu_num, cpu );
-	for ( i = 0; i < papi_num_components; i++ ) {
-		retval = _papi_hwd[i]->shutdown_thread( cpu->context[i] );
-		if ( retval != PAPI_OK )
-			failure = retval;
-	}
-	free_cpu( &cpu );
-	return ( failure );
+   int retval = PAPI_OK;
+   int i, failure = 0;
+
+   remove_cpu( cpu );
+
+   THRDBG( "Shutting down cpu %d at %p\n", cpu->cpu_num, cpu );
+
+   for ( i = 0; i < papi_num_components; i++ ) {
+      retval = _papi_hwd[i]->shutdown_thread( cpu->context[i] );
+      if ( retval != PAPI_OK ) {
+	 failure = retval;
+      }
+   }
+   free_cpu( &cpu );
+
+   return failure;
 }
