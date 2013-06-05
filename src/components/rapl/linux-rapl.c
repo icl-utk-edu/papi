63074f8256e07994b690ce5e114b1ffa959b654d
James Ralph <ralph@icl.utk.edu>
2013-06-05 14:53:28 -0400

    rapl: Add Ivb-EP support
    The Intel docs are spotty on what is actually supported.
    They state:
    14.7.2 RAPL Domains and Platform Specificity
    The specific RAPL domains available in a platform varies across product segments.
    Platforms targeting client segment support the following RAPL domain hierarchy:
    	* Package
    	* Two power planes: PP0 and PP1 (PP1 may reflect to uncore devices)
    Platforms targeting server segment support the following RAPL domain hierarchy:
    	* Package
    	* Power plane: PP0
    	* DRAM

--- src/components/rapl/linux-rapl.c
+++ src/components/rapl/linux-rapl.c
@@ -322,6 +322,13 @@ _rapl_init_component( int cidx )
           pp1_avail=1;
           dram_avail=0;
        }	
+       else if (hw_info->cpuid_model==62) {
+	  /* IvyBridge-EP */
+          package_avail=1;
+          pp0_avail=1;
+          pp1_avail=0;
+          dram_avail=1;
+       }	
        else {
 	 /* not a supported model */
 	 strncpy(_rapl_vector.cmp_info.disabled_reason,
