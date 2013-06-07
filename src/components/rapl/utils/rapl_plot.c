d6505b76f6da6a66629ffda187d5d4933712930b
Vince Weaver <vincent.weaver@maine.edu>
2013-06-07 17:06:20 -0400

    rapl: update the rapl_plot utility
    Get the event names by enumerating the ones available with the
    RAPL component rather than having a hard-coded list.

--- src/components/rapl/utils/rapl_plot.c
+++ src/components/rapl/utils/rapl_plot.c
@@ -9,41 +9,26 @@
 
 #include "papi.h"
 
-#define NUM_EVENTS 6
+#define MAX_EVENTS 128
 
-char events[NUM_EVENTS][BUFSIZ]={
-  "rapl:::PACKAGE_ENERGY:PACKAGE0",
-  "rapl:::PACKAGE_ENERGY:PACKAGE1",
-  "rapl:::DRAM_ENERGY:PACKAGE0",
-  "rapl:::DRAM_ENERGY:PACKAGE1",
-  "rapl:::PP0_ENERGY:PACKAGE0",
-  "rapl:::PP0_ENERGY:PACKAGE1",
-};
+char events[MAX_EVENTS][BUFSIZ];
+char filenames[MAX_EVENTS][BUFSIZ];
 
-char filenames[NUM_EVENTS][BUFSIZ]={
-  "results.PACKAGE_ENERGY_PACKAGE0",
-  "results.PACKAGE_ENERGY_PACKAGE1",
-  "results.DRAM_ENERGY_PACKAGE0",
-  "results.DRAM_ENERGY_PACKAGE1",
-  "results.PP0_ENERGY_PACKAGE0",
-  "results.PP0_ENERGY_PACKAGE1",
-};
+FILE *fff[MAX_EVENTS];
 
-FILE *fff[NUM_EVENTS];
-
+static int num_events=0;
 
 int main (int argc, char **argv)
 {
 
     int retval,cid,rapl_cid=-1,numcmp;
     int EventSet = PAPI_NULL;
-    long long values[NUM_EVENTS];
-    int i;
+    long long values[MAX_EVENTS];
+    int i,code,enum_retval;
     const PAPI_component_info_t *cmpinfo = NULL;
     long long start_time,before_time,after_time;
     double elapsed_time,total_time;
-
-    
+    char event_name[BUFSIZ];
 
 	/* PAPI Initialization */
      retval = PAPI_library_init( PAPI_VER_CURRENT );
@@ -65,8 +50,9 @@ int main (int argc, char **argv)
 	   rapl_cid=cid;
 	   printf("Found rapl component at cid %d\n", rapl_cid);
 
-           if (cmpinfo->num_native_events==0) {
-	     fprintf(stderr,"No rapl events found\n");
+           if (cmpinfo->disabled) {
+	     fprintf(stderr,"No rapl events found: %s\n",
+		     cmpinfo->disabled_reason);
 	     exit(1);
            }
 	   break;
@@ -79,9 +65,40 @@ int main (int argc, char **argv)
         exit(1);
      }
 
+     /* Find Events */
+     code = PAPI_NATIVE_MASK;
+
+     enum_retval = PAPI_enum_cmp_event( &code, PAPI_ENUM_FIRST, cid );
+
+     while ( enum_retval == PAPI_OK ) {
+
+       retval = PAPI_event_code_to_name( code, event_name );
+       if ( retval != PAPI_OK ) {
+	  printf("Error translating %#x\n",code);
+	  exit(1);
+       }
+
+       printf("Found: %s\n",event_name);
+       strncpy(events[num_events],event_name,BUFSIZ);
+       sprintf(filenames[num_events],"results.%s",event_name);
+       num_events++;
+       
+       if (num_events==MAX_EVENTS) {
+	  printf("Too many events! %d\n",num_events);
+	  exit(1);
+       }
+
+       enum_retval = PAPI_enum_cmp_event( &code, PAPI_ENUM_EVENTS, cid );
+
+     }
+
+     if (num_events==0) {
+        printf("Error!  No RAPL events found!\n");
+	exit(1);
+     }
 
      /* Open output files */
-     for(i=0;i<NUM_EVENTS;i++) {
+     for(i=0;i<num_events;i++) {
         fff[i]=fopen(filenames[i],"w");
 	if (fff[i]==NULL) {
 	   fprintf(stderr,"Could not open %s\n",filenames[i]);
@@ -96,7 +113,7 @@ int main (int argc, char **argv)
         fprintf(stderr,"Error creating eventset!\n");
      }
 
-     for(i=0;i<NUM_EVENTS;i++) {
+     for(i=0;i<num_events;i++) {
 	
         retval = PAPI_add_named_event( EventSet, events[i] );
         if (retval != PAPI_OK) {
@@ -131,7 +148,7 @@ int main (int argc, char **argv)
         total_time=((double)(after_time-start_time))/1.0e9;
         elapsed_time=((double)(after_time-before_time))/1.0e9;
 
-        for(i=0;i<NUM_EVENTS;i++) {
+        for(i=0;i<num_events;i++) {
 
 	   fprintf(fff[i],"%.4f %.1f (* Average Power for %s *)\n",
 		   total_time,
