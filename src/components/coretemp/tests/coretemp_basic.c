99b5b68569845bdb7aa161cf65458fdad529e30c
Vince Weaver <vincent.weaver@maine.edu>
2013-06-10 13:15:49 -0400

    coretemp_basic: update test to properly enumerate events
    The code was old and was searching the entire native event list for
    ones that started with "hwmon".
    
    This updates the test to first find the coretemp component, then
    enumerate all events contained within.

--- src/components/coretemp/tests/coretemp_basic.c
+++ src/components/coretemp/tests/coretemp_basic.c
@@ -21,7 +21,7 @@
 int main (int argc, char **argv)
 {
 
-        int retval,cid,numcmp;
+        int retval,cid,numcmp,coretemp_cid=-1;
 	int EventSet = PAPI_NULL;
 	long long values[NUM_EVENTS];
 	int code;
@@ -40,81 +40,95 @@ int main (int argc, char **argv)
 	}
 
 	if (!TESTS_QUIET) {
-	   printf("Trying all hwmon* events\n");
+	   printf("Trying all coretemp events\n");
 	}
 
         numcmp = PAPI_num_components();
 
 	for(cid=0; cid<numcmp; cid++) {
 
-	   if (!TESTS_QUIET) {
-	      if ( (cmpinfo = PAPI_get_component_info(cid)) == NULL) {
-	         test_fail(__FILE__, __LINE__,"PAPI_get_component_info failed\n", 0);
-	      }
-	      printf("\tComponent %d - %s\n", cid, cmpinfo->name);
+	   if ( (cmpinfo = PAPI_get_component_info(cid)) == NULL) {
+	      test_fail(__FILE__, __LINE__,
+              "PAPI_get_component_info failed\n", 0);
 	   }
 
-	   code = PAPI_NATIVE_MASK;
+	   if (strstr(cmpinfo->name,"coretemp")) {
+	      coretemp_cid=cid;
+	      if (!TESTS_QUIET) {
+                 printf("Found coretemp component at cid %d\n", coretemp_cid);
+	      }
 
-           r = PAPI_enum_cmp_event( &code, PAPI_ENUM_FIRST, cid );
+	      if (cmpinfo->disabled) {
+	         fprintf(stderr,"Coretemp component disabled: %s\n",
+		         cmpinfo->disabled_reason);
+	         test_fail(__FILE__, __LINE__,
+                           "Component disabled\n", 0);
+	      }
+	   }
+	}
 
-	   while ( r == PAPI_OK ) {
-	      retval = PAPI_event_code_to_name( code, event_name );
-	      if ( retval != PAPI_OK ) {
-		 printf("Error translating %#x\n",code);
-	         test_fail( __FILE__, __LINE__, 
+	if (coretemp_cid==-1) {
+	   test_skip(__FILE__,__LINE__,"No coretemp component found",0);
+	}
+
+
+	code = PAPI_NATIVE_MASK;
+
+        r = PAPI_enum_cmp_event( &code, PAPI_ENUM_FIRST, coretemp_cid );
+
+	while ( r == PAPI_OK ) {
+	   retval = PAPI_event_code_to_name( code, event_name );
+	   if ( retval != PAPI_OK ) {
+	      printf("Error translating %#x\n",code);
+	      test_fail( __FILE__, __LINE__, 
                             "PAPI_event_code_to_name", retval );
-	      }
-
-	      if (!strncmp(event_name,"hwmon",5)) {
-	         if (!TESTS_QUIET) printf("%#x %s ",code,event_name);
-	     
-	         EventSet = PAPI_NULL;
-
-	         retval = PAPI_create_eventset( &EventSet );
-	         if (retval != PAPI_OK) {
-	            test_fail(__FILE__, __LINE__, 
-                              "PAPI_create_eventset()",retval);
-		 }
-
-	         retval = PAPI_add_event( EventSet, code );
-	         if (retval != PAPI_OK) {
-	            test_fail(__FILE__, __LINE__, 
-                                 "PAPI_add_event()",retval);
-		 }
-
-	         retval = PAPI_start( EventSet);
-	         if (retval != PAPI_OK) {
-	            test_fail(__FILE__, __LINE__, "PAPI_start()",retval);
-		 }
-
-	         retval = PAPI_stop( EventSet, values);
-	         if (retval != PAPI_OK) {
-	            test_fail(__FILE__, __LINE__, "PAPI_start()",retval);
-		 }
-
-	         if (!TESTS_QUIET) printf(" value: %lld\n",values[0]);
-
-	         retval = PAPI_cleanup_eventset( EventSet );
-	         if (retval != PAPI_OK) {
-	            test_fail(__FILE__, __LINE__, 
-                              "PAPI_cleanup_eventset()",retval);
-		 }
-
-	         retval = PAPI_destroy_eventset( &EventSet );
-	         if (retval != PAPI_OK) {
-	            test_fail(__FILE__, __LINE__, 
-                              "PAPI_destroy_eventset()",retval);
-		 }
-
-	         total_events++;
-	      }
-	      r = PAPI_enum_cmp_event( &code, PAPI_ENUM_EVENTS, cid );
 	   }
-        }
 
+	   if (!TESTS_QUIET) printf("%s ",event_name);
+	     
+	   EventSet = PAPI_NULL;
+
+	   retval = PAPI_create_eventset( &EventSet );
+	   if (retval != PAPI_OK) {
+	      test_fail(__FILE__, __LINE__, 
+                              "PAPI_create_eventset()",retval);
+	   }
+
+	   retval = PAPI_add_event( EventSet, code );
+	   if (retval != PAPI_OK) {
+	      test_fail(__FILE__, __LINE__, 
+                                 "PAPI_add_event()",retval);
+	   }
+
+	   retval = PAPI_start( EventSet);
+	   if (retval != PAPI_OK) {
+	      test_fail(__FILE__, __LINE__, "PAPI_start()",retval);
+	   }
+
+	   retval = PAPI_stop( EventSet, values);
+	   if (retval != PAPI_OK) {
+	      test_fail(__FILE__, __LINE__, "PAPI_start()",retval);
+	   }
+
+	   if (!TESTS_QUIET) printf(" value: %lld\n",values[0]);
+
+	   retval = PAPI_cleanup_eventset( EventSet );
+	   if (retval != PAPI_OK) {
+	      test_fail(__FILE__, __LINE__, 
+                              "PAPI_cleanup_eventset()",retval);
+	   }
+
+	   retval = PAPI_destroy_eventset( &EventSet );
+	   if (retval != PAPI_OK) {
+	      test_fail(__FILE__, __LINE__, 
+                              "PAPI_destroy_eventset()",retval);
+	   }
+
+	   total_events++;
+	   r = PAPI_enum_cmp_event( &code, PAPI_ENUM_EVENTS, coretemp_cid );
+	}
+       
 	if (total_events==0) {
-
 	   test_skip(__FILE__,__LINE__,"No coretemp events found",0);
 	}
 
