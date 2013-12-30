17e2794395fbd24584f06c9fae61ab58ef46aea1
James Ralph <ralph@icl.utk.edu>
2013-12-30 15:35:52 -0500

    coretemp_freebsd: Cleanups
    * Remove extraneous macro parameter that breaks compilation.
    * Do not attempt to malloc(0), if no coretemp OIDs found.
    
    Thanks to Gleb Smirnoff for the patch

--- src/components/coretemp_freebsd/coretemp_freebsd.c
+++ src/components/coretemp_freebsd/coretemp_freebsd.c
@@ -140,6 +140,9 @@ int coretemp_init_component ()
 		ret = sysctlnametomib (tmp, mib, &len);
 	}
 
+	if (CORETEMP_NUM_EVENTS == 0)
+		return PAPI_OK;
+
 	/* Allocate memory for the our event table */
 	coretemp_native_table = (coretemp_native_event_entry_t *)
 		papi_malloc (sizeof (coretemp_native_event_entry_t) * CORETEMP_NUM_EVENTS);
@@ -377,7 +380,7 @@ int coretemp_reset(hwd_context_t * ctx, hwd_control_state_t * ctrl)
 int coretemp_shutdown_component (void)
 {
 
-	SUBDBG( "coretemp_shutdown_component... %p\n", ctx );
+	SUBDBG( "coretemp_shutdown_component... %p\n");
 
 	/* Last chance to clean up */
 	papi_free (coretemp_native_table);
