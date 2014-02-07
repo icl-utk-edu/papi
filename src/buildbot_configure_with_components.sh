935a729274651652abb56d35a620513cd6529c38
James Ralph <ralph@icl.utk.edu>
2014-02-07 10:36:16 -0500

    buildbot_onfigure: Build more components

--- src/buildbot_configure_with_components.sh
+++ src/buildbot_configure_with_components.sh
@@ -3,10 +3,10 @@
 #./configure --with-debug --with-components="coretemp example infiniband lustre mx net" $1 
 if [ -f components/cuda/Makefile.cuda ]; then
 	if [ -f components/nvml/Makefile.nvml ]; then
-		./configure --with-components="coretemp example lustre mx net cuda nvml" $1
+		./configure --with-components="appio coretemp example lustre micpower mx net rapl stealtime cuda nvml" $1
 	else
-		./configure --with-components="coretemp example lustre mx net cuda" $1
+		./configure --with-components="appio coretemp example lustre micpower mx net rapl stealtime cuda" $1
 	fi
 else
-	./configure --with-components="coretemp example lustre mx net" $1
+	./configure --with-components="appio coretemp example lustre micpower mx net rapl stealtime" $1
 fi
