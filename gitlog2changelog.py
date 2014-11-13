806d87648474dee65dfd088738c5410bd5152e68
Heike McCraw <mccraw@icl.utk.edu>
2014-11-13 15:37:25 -0500

    The python script that is used to create a ChangeLog for the current release (gitlog2changelog.py) does only list commits that change more than one file.
    Fixed gitlog2changelog.py to also include commits where
    1 file is changed.

--- gitlog2changelog.py
+++ gitlog2changelog.py
@@ -77,7 +77,7 @@ for line in fin:
             else:
                 message = message + " " + line.strip()
     # If this line is hit all of the files have been stored for this commit
-    elif re.search('files changed', line) >= 0:
+    elif any(x in line for x in ['files changed', 'file changed']):
         filesFound = True
         continue
     # Collect the files for this commit. FIXME: Still need to add +/- to files
