91d761e23c8c594ba4c551261da98f4e412a1814
Asim YarKhan <yarkhan@icl.utk.edu>
2015-11-30 13:55:04 -0500

    For libmsr graph, clean up labels and size of axes in graph

--- src/components/libmsr/utils/libmsr_write_test.sh
+++ src/components/libmsr/utils/libmsr_write_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gpconfig="set grid; set key top left; set style line 1 lw 6; set style line 2 lw 1.5; set style line 3 lw 1.5; set style line 4 lw 1.5; set style line 5 lw 1.5; set style line 6 lw 1.5 lc rgb 'pink'; set style line 7 lw 1.5;"
+gpconfig="set grid; set key top left; set style line 1 lw 2 pt 3 ps 1.5; set style line 2 lw 1.5; set style line 3 lw 1.5;"
 set_term_postscript="set terminal postscript eps enhanced color font 'Helvetica,19' linewidth 1.5"
 
 ls_1=1
@@ -20,15 +20,16 @@ set title "Using PAPI libmsr component to read and set power caps\n 2x8 cores Xe
 ${gpconfig}
 set xlabel "Elapsed time (seconds)"
 set ylabel "Watts"
-set yrange [0:110]
-set y2range [0:0.4]
+set yrange [0:]
+set y2range [0:]
 set y2label "Unit Work Time (seconds)
-set xrange [0:33]
+set xrange [0:]
 set key bottom right
 plot \
-    'libmsr_write_test_output_tmp_set.dat' index 0 using (\$2):(\$9) title "Set/Request Avg Power Cap (watts in 1 sec)" smooth unique with points ls ${ls_1},\
-    'libmsr_write_test_output_tmp_read.dat' index 0 using (\$2):(\$6) title "Read Power Consumpution (watts)" smooth unique with linespoints ls ${ls_2}, \
-    'libmsr_write_test_output_tmp_read.dat' index 0 using (\$2):(\$3) axes x1y2 title "Time for Unit Work (seconds on y2 axis)" smooth unique with linespoints ls ${ls_3} 
+    'libmsr_write_test_output_tmp_read.dat' index 0 using (\$2):(\$6) title "Power Consumpution (watts)" smooth unique with linespoints ls ${ls_2}, \
+    'libmsr_write_test_output_tmp_read.dat' index 0 using (\$2):(\$3) axes x1y2 title "Time for Unit Work (seconds on y2 axis)" smooth unique with linespoints ls ${ls_3},\
+    'libmsr_write_test_output_tmp_set.dat' index 0 using (\$2):(\$9) title "Set Avg Power Cap (watts in 1 sec)" smooth unique with points ls ${ls_1}
+
 
 
 ${gp_pause}
