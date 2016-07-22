icl-utk-edu/papi

commit a31c90ed0aecdc3da5b47611d0068448cac38e5b

fix/add offcore_response:l3_miss alias for Intel SNB/IVB/HSW/BDW/SKL
This patch adds a L3_MISS alias for Intel Snb/IVB/HSW/BDW/SKL processors.
L3_MISS counts local and remote misses (if any).

Adds the corresponding validation tests.

Signed-off-by: Stephane Eranian <eranian@gmail.com>

lib/events/intel_snb_events.h
@@ -1765,6 +1765,13 @@ static const intel_x86_umask_t snb_offcore_response[]={
      .uequiv = "LLC_MISS_LOCAL_DRAM",
      .grpid = 1,
    },
+   { .uname  = "L3_MISS",
+     .udesc  = "Supplier: counts L3 misses to local DRAM",
+     .ucode  = 0x1ULL << (22+8),
+     .grpid  = 1,
+     .uequiv = "LLC_MISS_LOCAL",
+     .umodel = PFM_PMU_INTEL_SNB,
+   },
    { .uname  = "LLC_MISS_REMOTE",
      .udesc  = "Supplier: counts L3 misses to remote DRAM",
      .ucode = 0xffULL << (23+8),
@@ -1777,6 +1784,20 @@ static const intel_x86_umask_t snb_offcore_response[]={
      .ucode = 0xffULL << (23+8),
      .grpid = 1,
      .umodel = PFM_PMU_INTEL_SNB_EP,
+   },
+   { .uname  = "L3_MISS",
+     .udesc  = "Supplier: counts L3 misses to local or remote DRAM",
+     .ucode  = 0x3ULL << (22+8),
+     .uequiv = "LLC_MISS_LOCAL:LLC_MISS_REMOTE",
+     .umodel = PFM_PMU_INTEL_SNB_EP,
+     .grpid = 1,
+   },
+   { .uname  = "L3_MISS",
+     .udesc  = "Supplier: counts L3 misses to local or remote DRAM",
+     .ucode  = 0x3ULL << (22+8),
+     .uequiv = "LLC_MISS_LOCAL:LLC_MISS_REMOTE",
+     .umodel = PFM_PMU_INTEL_SNB_EP,
+     .grpid = 1,
    },
    { .uname  = "LLC_HITMESF",
      .udesc  = "Supplier: counts L3 hits in any state (M, E, S, F)",

lib/events/intel_bdw_events.h
@@ -1806,27 +1806,33 @@ static const intel_x86_umask_t bdw_offcore_response[]={
    { .uname  = "L3_MISS_LOCAL",
      .udesc  = "Supplier: counts L3 misses to local DRAM",
      .ucode  = 1ULL << (26+8),
-     .umodel = PFM_PMU_INTEL_BDW,
      .grpid = 1,
    },
    { .uname  = "LLC_MISS_LOCAL",
      .udesc  = "Supplier: counts L3 misses to local DRAM",
      .ucode  = 1ULL << (26+8),
      .uequiv = "L3_MISS_LOCAL",
-     .umodel = PFM_PMU_INTEL_BDW,
      .grpid = 1,
    },
    { .uname  = "LLC_MISS_LOCAL_DRAM",
      .udesc  = "Supplier: counts L3 misses to local DRAM",
      .ucode  = 1ULL << (26+8),
      .uequiv = "L3_MISS_LOCAL",
-     .umodel = PFM_PMU_INTEL_BDW,
      .grpid  = 1,
    },
    { .uname  = "L3_MISS",
-     .udesc  = "Supplier: counts L3 misses",
-     .ucode  = 0xfULL << (26+8),
+     .udesc  = "Supplier: counts L3 misses to local DRAM",
+     .ucode  = 1ULL << (26+8),
+     .uequiv = "L3_MISS_LOCAL",
      .grpid  = 1,
+     .umodel = PFM_PMU_INTEL_BDW,
+   },
+   { .uname  = "L3_MISS",
+     .udesc  = "Supplier: counts L3 misses to local or remote DRAM",
+     .ucode  = 0xfULL << (26+8),
+     .uequiv = "L3_MISS_LOCAL:L3_MISS_REMOTE_HOP0:L3_MISS_REMOTE_HOP1:L3_MISS_REMOTE_HOP2P",
+     .umodel = PFM_PMU_INTEL_BDW_EP,
+     .grpid = 1,
    },
    { .uname  = "L3_MISS_REMOTE_HOP0",
      .udesc  = "Supplier: counts L3 misses to remote DRAM with 0 hop",

lib/events/intel_hsw_events.h
@@ -1784,12 +1784,19 @@ static const intel_x86_umask_t hsw_offcore_response[]={
      .grpid = 1,
    },
    { .uname  = "L3_MISS",
+     .udesc  = "Supplier: counts L3 misses to local DRAM",
+     .ucode  = 0x1ULL << (22+8),
+     .uequiv = "L3_MISS_LOCAL",
+     .grpid = 1,
+     .umodel = PFM_PMU_INTEL_HSW,
+   },
+   { .uname  = "L3_MISS",
      .udesc  = "Supplier: counts L3 misses to local or remote DRAM",
-     .ucode = 0xfULL << (26+8),
+     .ucode = 0x7ULL << (27+8) | 0x1ULL << (22+8),
+     .uequiv = "L3_MISS_LOCAL:L3_MISS_REMOTE_HOP0:L3_MISS_REMOTE_HOP1:L3_MISS_REMOTE_HOP2P",
      .umodel = PFM_PMU_INTEL_HSW_EP,
      .grpid = 1,
    },
-
    { .uname  = "SPL_HIT",
      .udesc  = "Supplier: counts L3 supplier hit",
      .ucode = 0x1ULL << (30+8),

lib/events/intel_ivb_events.h
@@ -1732,6 +1732,20 @@ static const intel_x86_umask_t ivb_offcore_response[]={
      .umodel = PFM_PMU_INTEL_IVB_EP,
      .grpid = 1,
    },
+   { .uname  = "L3_MISS",
+     .udesc  = "Supplier: counts L3 misses to local DRAM",
+     .ucode  = 0x1ULL << (22+8),
+     .grpid  = 1,
+     .uequiv = "LLC_MISS_LOCAL",
+     .umodel = PFM_PMU_INTEL_IVB,
+   },
+   { .uname  = "L3_MISS",
+     .udesc  = "Supplier: counts L3 misses to local or remote DRAM",
+     .ucode  = 0x3ULL << (22+8),
+     .uequiv = "LLC_MISS_LOCAL:LLC_MISS_REMOTE",
+     .umodel = PFM_PMU_INTEL_IVB_EP,
+     .grpid = 1,
+   },
    { .uname  = "LLC_MISS_REMOTE_DRAM",
      .udesc  = "Supplier: counts L3 misses to remote DRAM",
      .ucode = 0xffULL << (23+8),

lib/events/intel_skl_events.h
@@ -1471,12 +1471,13 @@ static const intel_x86_umask_t skl_offcore_response[]={
    { .uname  = "L3_MISS_LOCAL",
      .udesc  = "Supplier: counts L3 misses to local DRAM",
      .ucode = 1ULL << (26+8),
-     .umodel = PFM_PMU_INTEL_SKL,
      .grpid = 1,
    },
    { .uname  = "L3_MISS",
      .udesc  = "Supplier: counts L3 misses",
-     .ucode = 0xfULL << (26+8),
+     .ucode = 0x1ULL << (26+8),
+     .uequiv = "L3_MISS_LOCAL",
+     .umodel = PFM_PMU_INTEL_SKL,
      .grpid = 1,
    },
    { .uname  = "SPL_HIT",

tests/validate_x86.c
@@ -998,6 +998,14 @@ static const test_event_t x86_test_events[]={
 	  .fstr = "snb::OFFCORE_RESPONSE_0:ANY_REQUEST:ANY_RESPONSE:k=1:u=1:e=0:i=0:c=0:t=0",
 	},
 	{ SRC_LINE,
+	  .name = "snb::offcore_response_0:l3_miss",
+	  .ret = PFM_SUCCESS,
+	  .count = 2,
+	  .codes[0] =0x5301b7,
+	  .codes[1] = 0x3f80408fffull,
+	  .fstr = "snb::OFFCORE_RESPONSE_0:ANY_REQUEST:LLC_MISS_LOCAL_DRAM:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0",
+	},
+	{ SRC_LINE,
 	  .name = "amd64_fam11h_turion::MAB_REQUESTS:DC_BUFFER_0",
 	  .ret  = PFM_ERR_NOTFOUND,
 	},
@@ -1151,6 +1159,14 @@ static const test_event_t x86_test_events[]={
 	  .ret  = PFM_SUCCESS,
 	  .count = 2,
 	  .codes[0] = 0x5301b7,
+	  .codes[1] = 0x3f80408fffull,
+	  .fstr = "ivb::OFFCORE_RESPONSE_0:ANY_REQUEST:LLC_MISS_LOCAL:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0",
+	},
+	{ SRC_LINE,
+	  .name = "ivb::offcore_response_0:l3_miss",
+	  .ret = PFM_SUCCESS,
+	  .count = 2,
+	  .codes[0] =0x5301b7,
 	  .codes[1] = 0x3f80408fffull,
 	  .fstr = "ivb::OFFCORE_RESPONSE_0:ANY_REQUEST:LLC_MISS_LOCAL:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0",
 	},
@@ -1777,6 +1793,15 @@ static const test_event_t x86_test_events[]={
 	  .fstr = "snb_ep::OFFCORE_RESPONSE_1:DMND_DATA_RD:DMND_RFO:DMND_IFETCH:WB:PF_DATA_RD:PF_RFO:PF_IFETCH:PF_LLC_DATA_RD:PF_LLC_RFO:PF_LLC_IFETCH:BUS_LOCKS:STRM_ST:OTHER:LLC_MISS_REMOTE_DRAM:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0",
 	},
 	{ SRC_LINE,
+	  .name = "snb_ep::offcore_response_0:l3_miss",
+	  .ret = PFM_SUCCESS,
+	  .count = 2,
+	  .codes[0] =0x5301b7,
+	  .codes[1] = 0x3fffc08fffull,
+	  .fstr = "snb_ep::OFFCORE_RESPONSE_0:ANY_REQUEST:LLC_MISS_LOCAL_DRAM:LLC_MISS_REMOTE_DRAM:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0",
+	},
+
+	{ SRC_LINE,
 	  .name = "snb_ep::mem_trans_retired:latency_above_threshold",
 	  .ret = PFM_SUCCESS,
 	  .count = 2,
@@ -2028,6 +2053,14 @@ static const test_event_t x86_test_events[]={
 	  .fstr = "ivb_ep::OFFCORE_RESPONSE_0:DMND_DATA_RD:DMND_RFO:DMND_IFETCH:WB:PF_DATA_RD:PF_RFO:PF_IFETCH:PF_LLC_DATA_RD:PF_LLC_RFO:PF_LLC_IFETCH:BUS_LOCKS:STRM_ST:OTHER:LLC_MISS_REMOTE_DRAM:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0"
 	},
 	{ SRC_LINE,
+	  .name = "ivb_ep::offcore_response_0:l3_miss",
+	  .ret = PFM_SUCCESS,
+	  .count = 2,
+	  .codes[0] =0x5301b7,
+	  .codes[1] = 0x3fffc08fffull,
+	  .fstr = "ivb_ep::OFFCORE_RESPONSE_0:ANY_REQUEST:LLC_MISS_LOCAL:LLC_MISS_REMOTE_DRAM:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0",
+	},
+	{ SRC_LINE,
 	  .name = "hsw::mem_trans_retired:latency_above_threshold:ldlat=3:u",
 	  .ret = PFM_SUCCESS,
 	  .count = 2,
@@ -2165,6 +2198,14 @@ static const test_event_t x86_test_events[]={
 	  .codes[0] = 0x5301b7,
 	  .codes[1] = 0x800100001ull,
 	  .fstr = "hsw::OFFCORE_RESPONSE_0:DMND_DATA_RD:L3_HITS:SNP_FWD:k=1:u=1:e=0:i=0:c=0:t=0:intx=0:intxcp=0",
+	},
+	{ SRC_LINE,
+	  .name = "hsw::offcore_response_0:l3_miss",
+	  .ret = PFM_SUCCESS,
+	  .count = 2,
+	  .codes[0] =0x5301b7,
+	  .codes[1] = 0x3f80408fffull,
+	  .fstr = "hsw::OFFCORE_RESPONSE_0:ANY_REQUEST:L3_MISS_LOCAL:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0:intx=0:intxcp=0",
 	},
 	{ SRC_LINE,
 	  .name = "ivb_unc_cbo0::unc_clockticks",
@@ -2906,6 +2947,14 @@ static const test_event_t x86_test_events[]={
 	  .fstr = "hsw_ep::OFFCORE_RESPONSE_0:DMND_DATA_RD:PF_DATA_RD:PF_L3_DATA_RD:L3_HITM:L3_HITE:L3_HITS:L3_HITF:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0:intx=0:intxcp=0",
 	},
 	{ SRC_LINE,
+	  .name = "hsw_ep::offcore_response_0:l3_miss",
+	  .ret = PFM_SUCCESS,
+	  .count = 2,
+	  .codes[0] =0x5301b7,
+	  .codes[1] = 0x3fb8408fffull,
+	  .fstr = "hsw_ep::OFFCORE_RESPONSE_0:ANY_REQUEST:L3_MISS_LOCAL:L3_MISS_REMOTE_HOP0:L3_MISS_REMOTE_HOP1:L3_MISS_REMOTE_HOP2P:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0:intx=0:intxcp=0",
+	},
+	{ SRC_LINE,
 	  .name = "bdw::mem_trans_retired:latency_above_threshold:ldlat=3:u",
 	  .ret = PFM_SUCCESS,
 	  .count = 2,
@@ -3046,8 +3095,8 @@ static const test_event_t x86_test_events[]={
 	  .ret = PFM_SUCCESS,
 	  .count = 2,
 	  .codes[0] =0x5301b7,
-	  .codes[1] = 0x3fbc008fffull,
-	  .fstr = "bdw::OFFCORE_RESPONSE_0:ANY_REQUEST:L3_MISS:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0:intx=0:intxcp=0",
+	  .codes[1] = 0x3f84008fffull,
+	  .fstr = "bdw::OFFCORE_RESPONSE_0:ANY_REQUEST:L3_MISS_LOCAL:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0:intx=0:intxcp=0",
 	},
 	{ SRC_LINE,
 	  .name = "bdw::offcore_response_1:any_data",
@@ -3063,7 +3112,7 @@ static const test_event_t x86_test_events[]={
 	  .count = 2,
 	  .codes[0] =0x5301b7,
 	  .codes[1] = 0x3fbc008fffull,
-	  .fstr = "bdw_ep::OFFCORE_RESPONSE_0:ANY_REQUEST:L3_MISS:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0:intx=0:intxcp=0",
+	  .fstr = "bdw_ep::OFFCORE_RESPONSE_0:ANY_REQUEST:L3_MISS_LOCAL:L3_MISS_REMOTE_HOP0:L3_MISS_REMOTE_HOP1:L3_MISS_REMOTE_HOP2P:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0:intx=0:intxcp=0",
 	},
 	{ SRC_LINE,
 	  .name = "bdw_ep::offcore_response_0:L3_MISS_REMOTE_HOP0_DRAM",
@@ -3934,6 +3983,15 @@ static const test_event_t x86_test_events[]={
 	  .codes[1] = 0x3f84008fff,
 	  .fstr = "skl::OFFCORE_RESPONSE_0:ANY_REQUEST:L3_MISS_LOCAL:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0:intx=0:intxcp=0",
 	},
+	{ SRC_LINE,
+	  .name = "skl::offcore_response_0:l3_miss",
+	  .ret = PFM_SUCCESS,
+	  .count = 2,
+	  .codes[0] =0x5301b7,
+	  .codes[1] = 0x3f84008fffull,
+	  .fstr = "skl::OFFCORE_RESPONSE_0:ANY_REQUEST:L3_MISS_LOCAL:SNP_ANY:k=1:u=1:e=0:i=0:c=0:t=0:intx=0:intxcp=0",
+	},
+
 	{ SRC_LINE,
 	  .name = "skl::cycle_activity:0x6:c=6",
 	  .count = 1,

commit a347a0a29389093e44c1049e351fb20e8702d040

remove duplicate offcore_response_*:l3_miss umask for SNB_EP
The L3_MISS was duplicated.

Bug introduced by:
a31c90ed0aec fix/add offcore_response:l3_miss alias for Intel SNB/IVB/HSW/BDW/SKL

Signed-off-by: Stephane Eranian <eranian@gmail.com>

lib/events/intel_snb_events.h
@@ -1792,13 +1792,6 @@ static const intel_x86_umask_t snb_offcore_response[]={
      .umodel = PFM_PMU_INTEL_SNB_EP,
      .grpid = 1,
    },
-   { .uname  = "L3_MISS",
-     .udesc  = "Supplier: counts L3 misses to local or remote DRAM",
-     .ucode  = 0x3ULL << (22+8),
-     .uequiv = "LLC_MISS_LOCAL:LLC_MISS_REMOTE",
-     .umodel = PFM_PMU_INTEL_SNB_EP,
-     .grpid = 1,
-   },
    { .uname  = "LLC_HITMESF",
      .udesc  = "Supplier: counts L3 hits in any state (M, E, S, F)",
      .ucode  = 0xfULL << (18+8),