f1d29592a65ccc3ad6a1fa6bcf91dbd2b6f366f4
Vince Weaver <vincent.weaver@maine.edu>
2017-03-06 14:08:01 -0500

    perf_event: add rdtsc() and rdpmc() inline-assembly

--- src/components/perf_event/perf_helpers.h
+++ src/components/perf_event/perf_helpers.h
@@ -17,6 +17,8 @@
 
 #endif
 
+#if defined(__x86_64__) || defined(__i386__)
+
 static long
 sys_perf_event_open( struct perf_event_attr *hw_event,
 		pid_t pid, int cpu, int group_fd, unsigned long flags )
@@ -28,3 +30,25 @@ sys_perf_event_open( struct perf_event_attr *hw_event,
 
 	return ret;
 }
+
+inline unsigned long long rdtsc(void) {
+
+	unsigned a,d;
+
+	__asm__ volatile("rdtsc" : "=a" (a), "=d" (d));
+
+	return ((unsigned long long)a) | (((unsigned long long)d) << 32);
+}
+
+inline unsigned long long rdpmc(unsigned int counter) {
+
+	unsigned int low, high;
+
+	__asm__ volatile("rdpmc" : "=a" (low), "=d" (high) : "c" (counter));
+
+	return (unsigned long long)low | ((unsigned long long)high) <<32;
+}
+
+#define barrier() __asm__ volatile("" ::: "memory")
+
+#endif
