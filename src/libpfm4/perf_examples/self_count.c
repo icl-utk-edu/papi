9088093b31d501aa2ff02ceea06355b66e12dcca
Asim YarKhan <yarkhan@icl.utk.edu>
2017-06-16 11:52:55 -0400

    Update libpfm4
    Current with
    
    commit ce5b320031f75f9a9881333c13902d5541f91cc8
    Author: Will Schmidt <will_schmidt@vnet.ibm.com>
    Date:   Tue Jun 6 11:09:17 2017 -0500
    
        add power9 entries to validate_power.c
    
        Hi,
    
        Update the validate_power test to include power9 entries.
    
        sniff-test run output:
        $ ./validate
        Libpfm structure tests:
                libpfm ABI version : 0
                pfm_pmu_info_t : Passed
                pfm_event_info_t : Passed
                pfm_event_attr_info_t : Passed
                pfm_pmu_encode_arg_t : Passed
                pfm_perf_encode_arg_t : Passed
        Libpfm internal table tests:
                <snip...>
                checking power9 (946 events): Passed
        Architecture specific tests:
                 20 PowerPC events: 0 errors
        All tests passed
    
        Signed-off-by:  Will Schmidt <will_schmidt@vnet.ibm.com>

--- src/libpfm4/perf_examples/self_count.c
+++ src/libpfm4/perf_examples/self_count.c
@@ -125,7 +125,6 @@ read_count(perf_event_desc_t *fds)
 	struct perf_event_mmap_page *hdr;
 	uint64_t values[3];
 	uint64_t offset = 0;
-	uint64_t val;
 	unsigned int seq;
 	ssize_t ret;
 	int idx = -1;
@@ -143,11 +142,12 @@ read_count(perf_event_desc_t *fds)
 			values[2] = hdr->time_running;
 			ret = 0;
 		} else {
-			offset = 0;
+			offset = -1;
 			idx = -1;
 			ret = read(fds->fd, values, sizeof(values));
 			if (ret < (ssize_t)sizeof(values))
 				errx(1, "cannot read values");
+			printf("using read\n");
 			break;
 		}
 		barrier();
@@ -160,10 +160,10 @@ read_count(perf_event_desc_t *fds)
 		values[2],
 		idx);
 
-	values[0] += offset;
+	/* subtract offset */
+	values[0] -= offset;
 
-	val   = perf_scale(values);
-	return val;
+	return perf_scale(values);
 }
 
 int
@@ -172,9 +172,8 @@ main(int argc, char **argv)
 	perf_event_desc_t *fds = NULL;
 	long lret;
 	size_t pgsz;
-	uint64_t val;
+	uint64_t val, prev_val;
 	int i, ret, num_fds = 0;
-	int n = 30;
 
 	lret = sysconf(_SC_PAGESIZE);
 	if (lret < 0)
@@ -216,17 +215,15 @@ main(int argc, char **argv)
 	ioctl(fds[0].fd, PERF_EVENT_IOC_ENABLE, 0);
 
 	alarm(10);
-
+	prev_val = 0;
 	for(;quit == 0;) {
-
-		for (i=0; i < num_fds; i++) {
+		for (i = 0; i < num_fds; i++) {
 			val = read_count(&fds[i]);
-			printf("%20"PRIu64" %s\n", val, fds[i].name);
+			/* print evnet deltas */
+			printf("%20"PRIu64" %s\n", val - prev_val, fds[i].name);
+			prev_val = val;
 		}
-		fib(n);
-		n += 5;
-		if (n > 35)
-			n = 30;
+		fib(35);
 	}
 	/*
 	 * disable all counters attached to this thread
