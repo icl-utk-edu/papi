6e606a9b42f77c8c750896c1e972849d0922b005
Vince Weaver <vincent.weaver@maine.edu>
2017-06-20 13:02:17 -0400

    utils: rename version.c to papi_version.c
    Also minor cleanups to the utility.

--- src/utils/papi_version.c
+++ src/utils/papi_version.c
@@ -1,10 +1,10 @@
 /**
-  * file version.c
+  * file papi_version.c
   *	@brief papi_version utility.
   * @page papi_version
   *	@section Name
   *	papi_version - provides version information for papi.
-  * 
+  *
   *	@section Synopsis
   *	papi_version
   *
@@ -13,7 +13,7 @@
   *	information about the current PAPI installation.
   *
   *	@section Bugs
-  *	There are no known bugs in this utility. 
+  *	There are no known bugs in this utility.
   *	If you find a bug, it should be reported to the PAPI Mailing List at <ptools-perfapi@ptools.org>.
   */
 /* This utility displays the current PAPI version number */
@@ -23,11 +23,14 @@
 #include "papi.h"
 
 int
-main(  )
+main( int argc, char **argv )
 {
+	(void) argc;
+	(void) argv;
+
 	printf( "PAPI Version: %d.%d.%d.%d\n", PAPI_VERSION_MAJOR( PAPI_VERSION ),
 			PAPI_VERSION_MINOR( PAPI_VERSION ),
 			PAPI_VERSION_REVISION( PAPI_VERSION ),
 			PAPI_VERSION_INCREMENT( PAPI_VERSION ) );
-	exit( 0 );
+	return 0;
 }
