355c91339eb965d71ae04da2c109898c6761ed16
Vince Weaver <vincent.weaver@maine.edu>
2017-08-19 13:27:07 -0400

    perf_event_uncore: tests: print a message indicating the problem on skip
    also some whitespace cleanups

--- src/components/perf_event_uncore/tests/perf_event_uncore.c
+++ src/components/perf_event_uncore/tests/perf_event_uncore.c
@@ -13,127 +13,142 @@
 
 int main( int argc, char **argv ) {
 
-   int retval;
-   int EventSet = PAPI_NULL;
-   long long values[1];
-   char *uncore_event=NULL;
-   char event_name[BUFSIZ];
-   int uncore_cidx=-1;
-   const PAPI_component_info_t *info;
+	int retval,quiet;
+	int EventSet = PAPI_NULL;
+	long long values[1];
+	char *uncore_event=NULL;
+	char event_name[BUFSIZ];
+	int uncore_cidx=-1;
+	const PAPI_component_info_t *info;
 
-   /* Set TESTS_QUIET variable */
-   tests_quiet( argc, argv );
+	/* Set TESTS_QUIET variable */
+	quiet = tests_quiet( argc, argv );
 
-   /* Init the PAPI library */
-   retval = PAPI_library_init( PAPI_VER_CURRENT );
-   if ( retval != PAPI_VER_CURRENT ) {
-      test_fail( __FILE__, __LINE__, "PAPI_library_init", retval );
-   }
+	/* Init the PAPI library */
+	retval = PAPI_library_init( PAPI_VER_CURRENT );
+	if ( retval != PAPI_VER_CURRENT ) {
+		test_fail( __FILE__, __LINE__, "PAPI_library_init", retval );
+	}
 
-   /* Find the uncore PMU */
-   uncore_cidx=PAPI_get_component_index("perf_event_uncore");
-   if (uncore_cidx<0) {
-      test_skip(__FILE__,__LINE__,"perf_event_uncore component not found",0);
-   }
+	/* Find the uncore PMU */
+	uncore_cidx=PAPI_get_component_index("perf_event_uncore");
+	if (uncore_cidx<0) {
+		if (!quiet) {
+			printf("perf_event_uncore component not found\n");
+		}
+		test_skip(__FILE__,__LINE__,"perf_event_uncore component not found",0);
+	}
 
-   /* Check if component disabled */
-   info=PAPI_get_component_info(uncore_cidx);
-   if (info->disabled) {
-      test_skip(__FILE__,__LINE__,"uncore component disabled",0);
-   }
+	/* Check if component disabled */
+	info=PAPI_get_component_info(uncore_cidx);
+	if (info->disabled) {
+		if (!quiet) {
+			printf("perf_event_uncore component is disabled\n");
+		}
+		test_skip(__FILE__,__LINE__,"uncore component disabled",0);
+	}
 
-   /* Get a relevant event name */
-   uncore_event=get_uncore_event(event_name, BUFSIZ);
-   if (uncore_event==NULL) {
-      test_skip( __FILE__, __LINE__,
-	        "PAPI does not support uncore on this processor", PAPI_ENOSUPP );
-   }
+	/* Get a relevant event name */
+	uncore_event=get_uncore_event(event_name, BUFSIZ);
+	if (uncore_event==NULL) {
+		if (!quiet) {
+			printf("uncore event name not available\n");
+		}
+		test_skip( __FILE__, __LINE__,
+			"PAPI does not support uncore on this processor",
+			PAPI_ENOSUPP );
+	}
 
-   /* Create an eventset */
-   retval = PAPI_create_eventset(&EventSet);
-   if (retval != PAPI_OK) {
-      test_fail(__FILE__, __LINE__, "PAPI_create_eventset",retval);
-   }
+	/* Create an eventset */
+	retval = PAPI_create_eventset(&EventSet);
+	if (retval != PAPI_OK) {
+		test_fail(__FILE__, __LINE__, "PAPI_create_eventset",retval);
+	}
 
-   /* Set a component for the EventSet */
-   retval = PAPI_assign_eventset_component(EventSet, uncore_cidx);
+	/* Set a component for the EventSet */
+	retval = PAPI_assign_eventset_component(EventSet, uncore_cidx);
 
-   /* we need to set to a certain cpu for uncore to work */
+	/* we need to set to a certain cpu for uncore to work */
 
-   PAPI_cpu_option_t cpu_opt;
+	PAPI_cpu_option_t cpu_opt;
 
-   cpu_opt.eventset=EventSet;
-   cpu_opt.cpu_num=0;
+	cpu_opt.eventset=EventSet;
+	cpu_opt.cpu_num=0;
 
-   retval = PAPI_set_opt(PAPI_CPU_ATTACH,(PAPI_option_t*)&cpu_opt);
-   if (retval != PAPI_OK) {
-      test_skip( __FILE__, __LINE__,
-		      "this test; trying to PAPI_CPU_ATTACH; need to run as root",
-		      retval);
-   }
+	retval = PAPI_set_opt(PAPI_CPU_ATTACH,(PAPI_option_t*)&cpu_opt);
+	if (retval != PAPI_OK) {
+		if (!quiet) {
+			printf("Could not cpu attach\n");
+		}
+		test_skip( __FILE__, __LINE__,
+			"this test; trying to PAPI_CPU_ATTACH; need to run as root",
+			retval);
+	}
 
-   /* we need to set the granularity to system-wide for uncore to work */
+	/* we need to set the granularity to system-wide for uncore to work */
 
-   PAPI_granularity_option_t gran_opt;
+	PAPI_granularity_option_t gran_opt;
 
-   gran_opt.def_cidx=0;
-   gran_opt.eventset=EventSet;
-   gran_opt.granularity=PAPI_GRN_SYS;
+	gran_opt.def_cidx=0;
+	gran_opt.eventset=EventSet;
+	gran_opt.granularity=PAPI_GRN_SYS;
 
-   retval = PAPI_set_opt(PAPI_GRANUL,(PAPI_option_t*)&gran_opt);
-   if (retval != PAPI_OK) {
-      test_skip( __FILE__, __LINE__,
-		      "this test; trying to set PAPI_GRN_SYS",
-		      retval);
-   }
+	retval = PAPI_set_opt(PAPI_GRANUL,(PAPI_option_t*)&gran_opt);
+	if (retval != PAPI_OK) {
+		test_skip( __FILE__, __LINE__,
+			"this test; trying to set PAPI_GRN_SYS",
+			retval);
+	}
 
-   /* we need to set domain to be as inclusive as possible */
+	/* we need to set domain to be as inclusive as possible */
 
-   PAPI_domain_option_t domain_opt;
+	PAPI_domain_option_t domain_opt;
 
-   domain_opt.def_cidx=0;
-   domain_opt.eventset=EventSet;
-   domain_opt.domain=PAPI_DOM_ALL;
+	domain_opt.def_cidx=0;
+	domain_opt.eventset=EventSet;
+	domain_opt.domain=PAPI_DOM_ALL;
 
-   retval = PAPI_set_opt(PAPI_DOMAIN,(PAPI_option_t*)&domain_opt);
-   if (retval != PAPI_OK) {
-      test_skip( __FILE__, __LINE__,
-		      "this test; trying to set PAPI_DOM_ALL; need to run as root",
-		      retval);
-   }
+	retval = PAPI_set_opt(PAPI_DOMAIN,(PAPI_option_t*)&domain_opt);
+	if (retval != PAPI_OK) {
+		if (!quiet) {
+			printf("could not set PAPI_DOM_ALL\n");
+		}
+		test_skip( __FILE__, __LINE__,
+			"this test; trying to set PAPI_DOM_ALL; need to run as root",
+			retval);
+	}
 
-   /* Add our uncore event */
-   retval = PAPI_add_named_event(EventSet, uncore_event);
-   if (retval != PAPI_OK) {
-      if ( !TESTS_QUIET ) {
-         fprintf(stderr,"Error trying to use event %s\n", uncore_event);
-      }
-      test_fail(__FILE__, __LINE__, "adding uncore event",retval);
-   }
+	/* Add our uncore event */
+	retval = PAPI_add_named_event(EventSet, uncore_event);
+	if (retval != PAPI_OK) {
+		if ( !quiet ) {
+			printf("Error trying to use event %s\n", uncore_event);
+		}
+		test_fail(__FILE__, __LINE__, "adding uncore event",retval);
+	}
 
+	/* Start PAPI */
+	retval = PAPI_start( EventSet );
+	if ( retval != PAPI_OK ) {
+		test_fail( __FILE__, __LINE__, "PAPI_start", retval );
+	}
 
-   /* Start PAPI */
-   retval = PAPI_start( EventSet );
-   if ( retval != PAPI_OK ) {
-      test_fail( __FILE__, __LINE__, "PAPI_start", retval );
-   }
+	/* our work code */
+	do_flops( NUM_FLOPS );
 
-   /* our work code */
-   do_flops( NUM_FLOPS );
+	/* Stop PAPI */
+	retval = PAPI_stop( EventSet, values );
+	if ( retval != PAPI_OK ) {
+		test_fail( __FILE__, __LINE__, "PAPI_stop", retval );
+	}
 
-   /* Stop PAPI */
-   retval = PAPI_stop( EventSet, values );
-   if ( retval != PAPI_OK ) {
-      test_fail( __FILE__, __LINE__, "PAPI_stop", retval );
-   }
+	if ( !quiet ) {
+		printf("Uncore test:\n");
+		printf("Using event %s\n",uncore_event);
+		printf("\t%s: %lld\n",uncore_event,values[0]);
+	}
 
-   if ( !TESTS_QUIET ) {
-      printf("Uncore test:\n");
-      printf("Using event %s\n",uncore_event);
-      printf("\t%s: %lld\n",uncore_event,values[0]);
-   }
+	test_pass( __FILE__ );
 
-   test_pass( __FILE__ );
-
-   return 0;
+	return 0;
 }
--- src/components/perf_event_uncore/tests/perf_event_uncore_cbox.c
+++ src/components/perf_event_uncore/tests/perf_event_uncore_cbox.c
@@ -17,41 +17,44 @@
 
 int main( int argc, char **argv ) {
 
-   int retval,i,j;
-   int EventSet[EVENTS_TO_TRY][MAX_PACKAGES];
-   long long values[EVENTS_TO_TRY][MAX_PACKAGES];
-   char event_name[BUFSIZ];
-   char uncore_base[BUFSIZ];
-   char uncore_event[BUFSIZ];
-   int uncore_cidx=-1;
-   int max_cbox=0;
-   int core_to_use=0;
+	int retval,i,j,quiet;
+	int EventSet[EVENTS_TO_TRY][MAX_PACKAGES];
+	long long values[EVENTS_TO_TRY][MAX_PACKAGES];
+	char event_name[BUFSIZ];
+	char uncore_base[BUFSIZ];
+	char uncore_event[BUFSIZ];
+	int uncore_cidx=-1;
+	int max_cbox=0;
+	int core_to_use=0;
 
-   const PAPI_hw_info_t *hwinfo;
+	const PAPI_hw_info_t *hwinfo;
 
-   /* Set TESTS_QUIET variable */
-   tests_quiet( argc, argv );
+	/* Set TESTS_QUIET variable */
+	quiet = tests_quiet( argc, argv );
 
-   /* Init the PAPI library */
-   retval = PAPI_library_init( PAPI_VER_CURRENT );
-   if ( retval != PAPI_VER_CURRENT ) {
-      test_fail( __FILE__, __LINE__, "PAPI_library_init", retval );
-   }
+	/* Init the PAPI library */
+	retval = PAPI_library_init( PAPI_VER_CURRENT );
+	if ( retval != PAPI_VER_CURRENT ) {
+		test_fail( __FILE__, __LINE__, "PAPI_library_init", retval );
+	}
 
-   /* Find the uncore PMU */
-   uncore_cidx=PAPI_get_component_index("perf_event_uncore");
-   if (uncore_cidx<0) {
-      test_skip(__FILE__,__LINE__,"perf_event_uncore component not found",0);
-   }
+	/* Find the uncore PMU */
+	uncore_cidx=PAPI_get_component_index("perf_event_uncore");
+	if (uncore_cidx<0) {
+		if (!quiet) {
+			printf("perf_event_uncore component not found\n");
+		}
+		test_skip(__FILE__,__LINE__,"perf_event_uncore component not found",0);
+	}
 
-   /* Get hardware info */
-   hwinfo = PAPI_get_hardware_info();
-   if ( hwinfo == NULL ) {
-        test_fail(__FILE__,__LINE__,"PAPI_get_hardware_info()",retval);
-   }
+	/* Get hardware info */
+	hwinfo = PAPI_get_hardware_info();
+	if ( hwinfo == NULL ) {
+		test_fail(__FILE__,__LINE__,"PAPI_get_hardware_info()",retval);
+	}
 
-   /* Get event to use */
-   if (hwinfo->vendor == PAPI_VENDOR_INTEL) {
+	/* Get event to use */
+	if (hwinfo->vendor == PAPI_VENDOR_INTEL) {
 
       if ( hwinfo->cpuid_family == 6) {
          switch(hwinfo->cpuid_model) {
@@ -83,106 +86,116 @@ int main( int argc, char **argv ) {
 
    }
 
-   if (!TESTS_QUIET) {
-      printf("Trying for %d sockets\n",hwinfo->sockets);
-      printf("threads %d cores %d ncpus %d\n", hwinfo->threads,hwinfo->cores,
-          hwinfo->ncpu);
-   }
+	if (!quiet) {
+		printf("Trying for %d sockets\n",hwinfo->sockets);
+		printf("threads %d cores %d ncpus %d\n", hwinfo->threads,hwinfo->cores,
+			hwinfo->ncpu);
+	}
 
-   for(i=0;i < hwinfo->sockets; i++) {
+	for(i=0;i < hwinfo->sockets; i++) {
 
-      /* perf_event provides which to use in "cpumask"    */
-      /* but libpfm4 doesn't report this back to us (yet) */
-      core_to_use=i*hwinfo->threads*hwinfo->cores;
-      if (!TESTS_QUIET) {
-         printf("Using core %d for socket %d\n",core_to_use,i);
-      }
+		/* perf_event provides which to use in "cpumask"    */
+		/* but libpfm4 doesn't report this back to us (yet) */
+		core_to_use=i*hwinfo->threads*hwinfo->cores;
+		if (!quiet) {
+			printf("Using core %d for socket %d\n",core_to_use,i);
+		}
 
-      for(j=0;j<EVENTS_TO_TRY;j++) {
+		for(j=0;j<EVENTS_TO_TRY;j++) {
 
-         /* Create an eventset */
-         EventSet[j][i]=PAPI_NULL;
-         retval = PAPI_create_eventset(&EventSet[j][i]);
-         if (retval != PAPI_OK) {
-            test_fail(__FILE__, __LINE__, "PAPI_create_eventset",retval);
-         }
+			/* Create an eventset */
+			EventSet[j][i]=PAPI_NULL;
+			retval = PAPI_create_eventset(&EventSet[j][i]);
+			if (retval != PAPI_OK) {
+				test_fail(__FILE__, __LINE__, "PAPI_create_eventset",retval);
+			}
 
-         /* Set a component for the EventSet */
-         retval = PAPI_assign_eventset_component(EventSet[j][i], uncore_cidx);
-         if (retval!=PAPI_OK) {
-            test_fail(__FILE__, __LINE__, "PAPI_assign_eventset_component",retval);
-         }
+			/* Set a component for the EventSet */
+			retval = PAPI_assign_eventset_component(EventSet[j][i], uncore_cidx);
+			if (retval!=PAPI_OK) {
+				test_fail(__FILE__, __LINE__, "PAPI_assign_eventset_component",retval);
+			}
 
-         /* we need to set to a certain cpu for uncore to work */
+			/* we need to set to a certain cpu for uncore to work */
 
-         PAPI_cpu_option_t cpu_opt;
+			PAPI_cpu_option_t cpu_opt;
 
-         cpu_opt.eventset=EventSet[j][i];
-         cpu_opt.cpu_num=core_to_use;
+			cpu_opt.eventset=EventSet[j][i];
+			cpu_opt.cpu_num=core_to_use;
 
-         retval = PAPI_set_opt(PAPI_CPU_ATTACH,(PAPI_option_t*)&cpu_opt);
-         if (retval != PAPI_OK) {
-            test_skip( __FILE__, __LINE__,
-		      "this test; trying to PAPI_CPU_ATTACH; need to run as root",
-		      retval);
-         }
+			retval = PAPI_set_opt(PAPI_CPU_ATTACH,(PAPI_option_t*)&cpu_opt);
+			if (retval != PAPI_OK) {
+				if (!quiet) {
+					printf("Problem with PAPI_CPU_ATTACH\n");
+				}
+				test_skip( __FILE__, __LINE__,
+					"this test; trying to PAPI_CPU_ATTACH; need to run as root",
+					retval);
+			}
 
-         /* Default Granularity should work */
+			/* Default Granularity should work */
 
-         /* Default domain should work */
+			/* Default domain should work */
 
-         /* Add our uncore event */
-	 sprintf(uncore_event,"%s%d::%s",uncore_base,j,event_name);
-         retval = PAPI_add_named_event(EventSet[j][i], uncore_event);
-         if (retval != PAPI_OK) {
-            max_cbox=j;
-	    break;
-         }
-	 if (!TESTS_QUIET) printf("Added %s for socket %d\n",uncore_event,i);
+			/* Add our uncore event */
+			sprintf(uncore_event,"%s%d::%s",uncore_base,j,event_name);
+			retval = PAPI_add_named_event(EventSet[j][i], uncore_event);
+			if (retval != PAPI_OK) {
+				max_cbox=j;
+				break;
+			}
+			if (!quiet) {
+				printf("Added %s for socket %d\n",uncore_event,i);
+			}
 
-     }
-   }
+		}
+	}
 
+	for(i=0;i < hwinfo->sockets; i++) {
 
-   for(i=0;i < hwinfo->sockets; i++) {
-      for(j=0;j<max_cbox;j++) {
-         if (!TESTS_QUIET) printf("Starting EventSet %d\n",EventSet[j][i]);
-         /* Start PAPI */
-         retval = PAPI_start( EventSet[j][i] );
-         if ( retval != PAPI_OK ) {
-	    printf("Error starting socket %d cbox %d\n",i,j);
-            test_fail( __FILE__, __LINE__, "PAPI_start", retval );
-         }
-     }
-   }
+		for(j=0;j<max_cbox;j++) {
+			if (!quiet) {
+				printf("Starting EventSet %d\n",EventSet[j][i]);
+			}
 
-   /* our work code */
-   do_flops( NUM_FLOPS );
+			/* Start PAPI */
+			retval = PAPI_start( EventSet[j][i] );
+			if ( retval != PAPI_OK ) {
+				printf("Error starting socket %d cbox %d\n",i,j);
+				test_fail( __FILE__, __LINE__, "PAPI_start", retval );
+			}
+		}
+	}
 
-   /* Stop PAPI */
-   for(i=0;i < hwinfo->sockets; i++) {
-      for(j=0;j<max_cbox;j++) {
-         retval = PAPI_stop( EventSet[j][i],&values[j][i] );
-         if ( retval != PAPI_OK ) {
-	    printf("Error stopping socket %d cbox %d\n",i,j);
-            test_fail( __FILE__, __LINE__, "PAPI_stop", retval );
-         }
-     }
-   }
+	/* our work code */
+	do_flops( NUM_FLOPS );
 
-   /* Print Results */
-   if ( !TESTS_QUIET ) {
-      for(i=0;i < hwinfo->sockets; i++) {
-         printf("Socket %d\n",i);
-         for(j=0;j<max_cbox;j++) {
-            printf("\t%s%d::%s %lld\n",uncore_base,j,event_name,values[j][i]);
-         }
-      }
-   }
+	/* Stop PAPI */
+	for(i=0;i < hwinfo->sockets; i++) {
+		for(j=0;j<max_cbox;j++) {
+			retval = PAPI_stop( EventSet[j][i],&values[j][i] );
+			if ( retval != PAPI_OK ) {
+				if (!quiet) {
+					printf("Error stopping socket %d cbox %d\n",i,j);
+				}
+				test_fail( __FILE__, __LINE__, "PAPI_stop", retval );
+			}
+		}
+	}
 
-   PAPI_shutdown();
+	/* Print Results */
+	if ( !quiet ) {
+		for(i=0;i < hwinfo->sockets; i++) {
+			printf("Socket %d\n",i);
+			for(j=0;j<max_cbox;j++) {
+				printf("\t%s%d::%s %lld\n",uncore_base,j,event_name,values[j][i]);
+			}
+		}
+	}
 
-   test_pass( __FILE__ );
+	PAPI_shutdown();
 
-   return 0;
+	test_pass( __FILE__ );
+
+	return 0;
 }
