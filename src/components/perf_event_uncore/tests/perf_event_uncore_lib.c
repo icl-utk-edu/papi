1d0560aa6aa957ca8fce52627c08447749e9fc6d
Vince Weaver <vincent.weaver@maine.edu>
2017-08-19 14:52:08 -0400

    perf_event_uncore: event list, add recent processors
    libpfm4 still doesn't support regular Haswell, Broadwell, or Skylake
    machines

--- src/components/perf_event_uncore/tests/perf_event_uncore_lib.c
+++ src/components/perf_event_uncore/tests/perf_event_uncore_lib.c
@@ -6,65 +6,99 @@
 
 char *get_uncore_event(char *event, int size) {
 
-   const PAPI_hw_info_t *hwinfo;
+	const PAPI_hw_info_t *hwinfo;
 
-   hwinfo = PAPI_get_hardware_info();
-   if ( hwinfo == NULL ) {
-	return NULL;
-   }
+	hwinfo = PAPI_get_hardware_info();
+	if ( hwinfo == NULL ) {
+		return NULL;
+	}
 
-   if (hwinfo->vendor == PAPI_VENDOR_INTEL) {
+	if (hwinfo->vendor == PAPI_VENDOR_INTEL) {
 
-      if ( hwinfo->cpuid_family == 6) {
-	switch(hwinfo->cpuid_model) {
-	   case 26:
-           case 30:
-	   case 31: /* Nehalem */
-	   case 46: /* Nehalem EX */
-	            strncpy(event,"nhm_unc::UNC_CLK_UNHALTED",size);
-		    return event;
-	            break;
-	   case 37:
-	   case 44: /* Westmere */
-	   case 47: /* Westmere EX */
-	            strncpy(event,"wsm_unc::UNC_CLK_UNHALTED",size);
-		    return event;
-	            break;
+		if ( hwinfo->cpuid_family == 6) {
 
-	   case 62: /* Ivy Trail */
-	   case 45: /* SandyBridge EP */
-	            strncpy(event,"snbep_unc_imc0::UNC_M_CLOCKTICKS",size);
-		    return event;
-		    break;
-	   case 42: /* SandyBridge */
-	            strncpy(event,"snb_unc_cbo0::UNC_CLOCKTICKS",size);
-		    return event;
-		    break;
-	   case 58: /* IvyBridge */
-	            strncpy(event,"ivb_unc_cbo0::UNC_CLOCKTICKS",size);
-		    return event;
-		    break;
-	   case 63: /*haswell EP*/
-		    strncpy(event,"hswep_unc_cbo0::UNC_C_CLOCKTICKS",size);
-		    return event;
-		    break;
+			switch(hwinfo->cpuid_model) {
 
-	   case 87: /*Knights Landing*/
+			case 26:
+			case 30:
+			case 31: /* Nehalem */
+			case 46: /* Nehalem EX */
+			strncpy(event,"nhm_unc::UNC_CLK_UNHALTED",size);
+			return event;
+			break;
+
+			case 37:
+			case 44: /* Westmere */
+			case 47: /* Westmere EX */
+			strncpy(event,"wsm_unc::UNC_CLK_UNHALTED",size);
+			return event;
+			break;
+
+			case 42: /* SandyBridge */
+			strncpy(event,"snb_unc_cbo0::UNC_CLOCKTICKS",size);
+			return event;
+			break;
+
+			case 58: /* IvyBridge */
+			strncpy(event,"ivb_unc_cbo0::UNC_CLOCKTICKS",size);
+			return event;
+			break;
+
+			case 62: /* Ivy Trail */
+			case 45: /* SandyBridge EP */
+			strncpy(event,"snbep_unc_imc0::UNC_M_CLOCKTICKS",size);
+			return event;
+			break;
+
+			case 60:
+			case 70:
+			case 69: /* Haswell: note libpfm4 has no haswell unc support */
+			return NULL;
+			break;
+
+			case 63: /*haswell EP*/
+			strncpy(event,"hswep_unc_cbo0::UNC_C_CLOCKTICKS",size);
+			return event;
+			break;
+
+			case 61:
+			case 71:
+			case 86: /* Broadwell: note libpfm4 has no broadwell unc support */
+			return NULL;
+			break;
+
+			case 79: /* Broadwell-EP */
+			strncpy(event,"bdx_unc_cbo0::UNC_C_CLOCKTICKS",size);
+			return event;
+			break;
+
+			case 78:
+			case 94: /* Skylake: note libpfm4 has no skylake unc support */
+			return NULL;
+			break;
+
+			case 85: /* Skylake-X */
+				/* note libpfm4 has no skylake-x unc support */
+			return NULL;
+			return event;
+			break;
+
+			case 87: /*Knights Landing*/
 			strncpy(event,"knl_unc_imc0::UNC_M_D_CLOCKTICKS",size);
 			return event;
 			break;
+			}
+		}
+		return NULL;
 	}
-      }
-      return NULL;
-   }
-   else if (hwinfo->vendor == PAPI_VENDOR_AMD) {
-      if ( hwinfo->cpuid_family == 21) {
-         /* For kernel 3.9 at least */
-	 strncpy(event,"DRAM_ACCESSES:ALL",size);
-         return event;
-      }
-      return NULL;
-   }
+	else if (hwinfo->vendor == PAPI_VENDOR_AMD) {
+		if ( hwinfo->cpuid_family == 21) {
+			/* For kernel 3.9 at least */
+			strncpy(event,"DRAM_ACCESSES:ALL",size);
+			return event;
+		}
+		return NULL;
+	}
 
-   return NULL;
+	return NULL;
 }
