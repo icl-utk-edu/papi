icl-utk-edu/papi

commit 77a5ac9d43b1e6d7fa2665949b285739d1556f91

add model field to intel_x86_entry_t
This patch adds a model field to the evnet entry for Intel x86.
This makes it possible to have mdoel-specific events when two
PMU models share the same table. This was supported for umasks
but not for event. This patch adds this flexibility.

Signed-off-by: Stephane Eranian <eranian@gmail.com>

lib/pfmlib_intel_x86.c
@@ -780,8 +780,12 @@ int
 pfm_intel_x86_get_event_first(void *this)
 {
 	pfmlib_pmu_t *p = this;
+	int idx = 0;
 
-	return p->pme_count ? 0 : -1;
+	/* skip event for different models */
+	while (!is_model_event(this, idx) && idx < p->pme_count) idx++;
+
+	return p->pme_count ? idx : -1;
 }
 
 int
@@ -789,17 +793,22 @@ pfm_intel_x86_get_event_next(void *this, int idx)
 {
 	pfmlib_pmu_t *p = this;
 
+	/* pme_count is always >= 1*/
 	if (idx >= (p->pme_count-1))
 		return -1;
 
-	return idx+1;
+	idx++;
+	/* skip event for different models */
+	while (!is_model_event(this, idx) && idx < p->pme_count) idx++;
+
+	return idx < p->pme_count ? idx : -1;
 }
 
 int
 pfm_intel_x86_event_is_valid(void *this, int pidx)
 {
 	pfmlib_pmu_t *p = this;
-	return pidx >= 0 && pidx < p->pme_count;
+	return pidx >= 0 && pidx < p->pme_count && is_model_event(this, pidx);
 }
 
 int
@@ -823,6 +832,9 @@ pfm_intel_x86_validate_table(void *this, FILE *fp)
 	}
 
 	for(i=0; i < pmu->pme_count; i++) {
+
+		if (!is_model_event(this, i))
+			continue;
 
 		if (!pe[i].name) {
 			fprintf(fp, "pmu: %s event%d: :: no name (prev event was %s)\n", pmu->name, i,
@@ -864,6 +876,10 @@ pfm_intel_x86_validate_table(void *this, FILE *fp)
 			fprintf(fp, "pmu: %s event%d: %s :: ngrp too big (max=%d)\n", pmu->name, i, pe[i].name, INTEL_X86_NUM_GRP);
 			error++;
 		}
+		if (pe[i].model >= PFM_PMU_MAX) {
+			fprintf(fp, "pmu: %s event%d: %s :: model too big (max=%d)\n", pmu->name, i, pe[i].name, PFM_PMU_MAX);
+			error++;
+		}
 
 		for (j=i+1; j < (int)pmu->pme_count; j++) {
 			if (pe[i].code == pe[j].code && !(pe[j].equiv || pe[i].equiv) && pe[j].cntmsk == pe[i].cntmsk) {
@@ -893,6 +909,10 @@ pfm_intel_x86_validate_table(void *this, FILE *fp)
 
 			if (pe[i].ngrp && pe[i].umasks[j].grpid >= pe[i].ngrp) {
 				fprintf(fp, "pmu: %s event%d: %s umask%d: %s :: invalid grpid %d (must be < %d)\n", pmu->name, i, pe[i].name, j, pe[i].umasks[j].uname, pe[i].umasks[j].grpid, pe[i].ngrp);
+				error++;
+			}
+			if (pe[i].umasks[j].umodel >= PFM_PMU_MAX) {
+				fprintf(fp, "pmu: %s event%d: %s umask%d: %s :: model too big (max=%d)\n", pmu->name, i, pe[i].name,  j, pe[i].umasks[j].uname, PFM_PMU_MAX);
 				error++;
 			}
 			if (pe[i].umasks[j].uflags & INTEL_X86_DFL)
@@ -980,6 +1000,11 @@ pfm_intel_x86_get_event_attr_info(void *this, int pidx, int attr_idx, pfmlib_eve
 	const pfmlib_attr_desc_t *atdesc = this_atdesc(this);
 	int numasks, idx;
 
+	if (!is_model_event(this, pidx)) {
+		DPRINT("invalid event index %d\n", pidx);
+		return PFM_ERR_INVAL;
+	}
+
 	numasks = intel_x86_num_umasks(this, pidx);
 	if (attr_idx < numasks) {
 		idx = intel_x86_attr2umask(this, pidx, attr_idx);
@@ -1017,6 +1042,11 @@ pfm_intel_x86_get_event_info(void *this, int idx, pfm_event_info_t *info)
 {
 	const intel_x86_entry_t *pe = this_pe(this);
 	pfmlib_pmu_t *pmu = this;
+
+	if (!is_model_event(this, idx)) {
+		DPRINT("invalid event index %d\n", idx);
+		return PFM_ERR_INVAL;
+	}
 
 	info->name  = pe[idx].name;
 	info->desc  = pe[idx].desc;

lib/pfmlib_intel_x86_priv.h
@@ -69,6 +69,7 @@ typedef struct {
 	unsigned int			modmsk;	/* bitmask of modifiers for this event */
 	unsigned int			modmsk_req; /* bitmask of required modifiers */
 	unsigned short			ngrp;	/* number of unit masks groups */
+	unsigned short			model;	/*  only available on this PMU model */
 	const intel_x86_umask_t		*umasks; /* umask desc */
 } intel_x86_entry_t;
 
@@ -262,6 +263,18 @@ intel_x86_eflag(void *this, int idx, int flag)
 {
 	const intel_x86_entry_t *pe = this_pe(this);
 	return !!(pe[idx].flags & flag);
+}
+
+static inline int
+is_model_event(void *this, int pidx)
+{
+	pfmlib_pmu_t *pmu = this;
+	const intel_x86_entry_t *pe = this_pe(this);
+	unsigned short model;
+
+	model = pe[pidx].model;
+
+	return model == 0 || model == pmu->pmu;
 }
 
 static inline int

commit a290dead7c1f351f8269a265c0d4a5f38a60ba29

fix usage of is_model_event() for Intel X86
This patch fixes a couple of problems introduced by
commit: 77a5ac9d43b1 add model field to intel_x86_entry_t

The code in pfm_intel_x86_get_event_first() was incorrect.
It was calling is_model_event() before checking if the index
was within bounds. It should have been the opposite. Same issue
in pfm_intel_x86_get_next_event(). This could cause SEGFAULT
as report by Phil Mucci.

The patch also fixes the return value of pfm_intel_x86_get_event_first().
It was not calculated correctly.

Reported-by: Phil Mucci <mucci@icl.utk.edu>
Signed-off-by: Stephane Eranian <eranian@gmail.com>

lib/pfmlib_intel_x86.c
@@ -783,9 +783,9 @@ pfm_intel_x86_get_event_first(void *this)
 	int idx = 0;
 
 	/* skip event for different models */
-	while (!is_model_event(this, idx) && idx < p->pme_count) idx++;
+	while (idx < p->pme_count && !is_model_event(this, idx)) idx++;
 
-	return p->pme_count ? idx : -1;
+	return idx < p->pme_count ? idx : -1;
 }
 
 int
@@ -799,7 +799,7 @@ pfm_intel_x86_get_event_next(void *this, int idx)
 
 	idx++;
 	/* skip event for different models */
-	while (!is_model_event(this, idx) && idx < p->pme_count) idx++;
+	while (idx < p->pme_count && !is_model_event(this, idx)) idx++;
 
 	return idx < p->pme_count ? idx : -1;
 }