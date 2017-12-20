17cdeb365174c993a355b85b2dca23942141f399
John Henry <jhenry@icl.utk.edu>
2017-12-20 16:26:41 -0500

    Updated version to 5.6.1 after the release

--- src/papi.h
+++ src/papi.h
@@ -221,7 +221,7 @@
 
 /* This is the official PAPI version */
 /* The final digit represents the patch count */
-#define PAPI_VERSION  			PAPI_VERSION_NUMBER(5,6,0,0)
+#define PAPI_VERSION  			PAPI_VERSION_NUMBER(5,6,1,0)
 #define PAPI_VER_CURRENT 		(PAPI_VERSION & 0xffff0000)
 
   /* Tests for checking event code type */
