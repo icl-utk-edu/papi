982184902c381cb27e2f49fb51b16abb7733b2a9
Stephane Eranian <eranian@gmail.com>
2019-01-03 02:38:29 -0800

    Add Intel CascadeLake X core PMU support
    CascadeLake X has the same core PMU as Intel Skylake X.
    In fact, they share the same model number and differ
    only in the steppings (CLX is stepping 5 and above).
    
    This patch adds support by sharing the event table
    with Skylake X. The detection code is custom because
    it needs to take into account the stepping.
    
    Signed-off-by: Stephane Eranian <eranian@gmail.com>

--- lib/pfmlib_intel_skl.c
+++ lib/pfmlib_intel_skl.c
@@ -40,6 +40,35 @@ static const int skx_models[] = {
 };
 
 static int
+pfm_skx_detect(void *this)
+{
+	int ret;
+
+	/* Detect SKX model numbers (skx_models) */
+	ret = pfm_intel_x86_model_detect(this);
+	if (ret != PFM_SUCCESS)
+		return ret;
+
+	/* SKX model with stepping < 5 */
+	return pfm_intel_x86_cfg.stepping < 5 ? PFM_SUCCESS : PFM_ERR_NOTSUPP;
+}
+
+
+static int
+pfm_clx_detect(void *this)
+{
+	int ret;
+
+	/* Detect SKX model numbers (skx_models) */
+	ret = pfm_intel_x86_model_detect(this);
+	if (ret != PFM_SUCCESS)
+		return ret;
+
+	/* CLX is SKX model with stepping >= 5 */
+	return pfm_intel_x86_cfg.stepping >= 5 ? PFM_SUCCESS : PFM_ERR_NOTSUPP;
+}
+
+static int
 pfm_skl_init(void *this)
 {
 	pfm_intel_x86_cfg.arch_version = 4;
@@ -75,6 +104,7 @@ pfmlib_pmu_t intel_skl_support={
 	 PFMLIB_VALID_PERF_PATTRS(pfm_intel_x86_perf_validate_pattrs),
 	.get_event_nattrs	= pfm_intel_x86_get_event_nattrs,
 	.can_auto_encode	= pfm_intel_x86_can_auto_encode,
+	.get_num_events		= pfm_intel_x86_get_num_events,
 };
 
 pfmlib_pmu_t intel_skx_support={
@@ -93,7 +123,7 @@ pfmlib_pmu_t intel_skx_support={
 				| INTEL_X86_PMU_FL_ECMASK,
 	.cpu_family		= 6,
 	.cpu_models		= skx_models,
-	.pmu_detect		= pfm_intel_x86_model_detect,
+	.pmu_detect		= pfm_skx_detect,
 	.pmu_init		= pfm_skl_init,
 	.get_event_encoding[PFM_OS_NONE] = pfm_intel_x86_get_encoding,
 	 PFMLIB_ENCODE_PERF(pfm_intel_x86_get_perf_encoding),
@@ -106,4 +136,37 @@ pfmlib_pmu_t intel_skx_support={
 	 PFMLIB_VALID_PERF_PATTRS(pfm_intel_x86_perf_validate_pattrs),
 	.get_event_nattrs	= pfm_intel_x86_get_event_nattrs,
 	.can_auto_encode	= pfm_intel_x86_can_auto_encode,
+	.get_num_events		= pfm_intel_x86_get_num_events,
+};
+
+pfmlib_pmu_t intel_clx_support={
+	.desc			= "Intel CascadeLake X",
+	.name			= "clx",
+	.pmu			= PFM_PMU_INTEL_CLX,
+	.pme_count		= LIBPFM_ARRAY_SIZE(intel_skl_pe),
+	.type			= PFM_PMU_TYPE_CORE,
+	.supported_plm		= INTEL_X86_PLM,
+	.num_cntrs		= 8, /* consider with HT off by default */
+	.num_fixed_cntrs	= 3,
+	.max_encoding		= 2, /* offcore_response */
+	.pe			= intel_skl_pe,
+	.atdesc			= intel_x86_mods,
+	.flags			= PFMLIB_PMU_FL_RAW_UMASK
+				| INTEL_X86_PMU_FL_ECMASK,
+	.cpu_family		= 6,
+	.cpu_models		= skx_models,
+	.pmu_detect		= pfm_clx_detect,
+	.pmu_init		= pfm_skl_init,
+	.get_event_encoding[PFM_OS_NONE] = pfm_intel_x86_get_encoding,
+	 PFMLIB_ENCODE_PERF(pfm_intel_x86_get_perf_encoding),
+	.get_event_first	= pfm_intel_x86_get_event_first,
+	.get_event_next		= pfm_intel_x86_get_event_next,
+	.event_is_valid		= pfm_intel_x86_event_is_valid,
+	.validate_table		= pfm_intel_x86_validate_table,
+	.get_event_info		= pfm_intel_x86_get_event_info,
+	.get_event_attr_info	= pfm_intel_x86_get_event_attr_info,
+	 PFMLIB_VALID_PERF_PATTRS(pfm_intel_x86_perf_validate_pattrs),
+	.get_event_nattrs	= pfm_intel_x86_get_event_nattrs,
+	.can_auto_encode	= pfm_intel_x86_can_auto_encode,
+	.get_num_events		= pfm_intel_x86_get_num_events,
 };
