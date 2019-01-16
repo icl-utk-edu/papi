0948515e25d1d815521bf96559bd1b343ecb324c
Anthony Castaldo <TonyCastaldo@icl.utk.edu>
2019-01-16 14:33:51 -0500

    Example script to run nvlink_all on PEAK.

--- src/components/cuda/tests/runAll.sh
+++ src/components/cuda/tests/runAll.sh
@@ -1,7 +1,9 @@
 # This runs both types of nvlink_all on the PEAK machine.
 # It should work on SUMMIT when that becomes available; 
 # you may wish to change the --gpu_per_rs to 4 (or whatever
-# the max per node may be). (rs =resource set).
+# the max per node may be). (rs =resource set). This is
+# not intended to run on all machines; but most clusters
+# will require something similar. 
 rm -f errAll1.txt errAll2.txt outAll1.txt outAll2.txt
-jsrun --nrs 1 --gpu_per_rs=2 --stdio_stderr errAll1.txt --stdio_stdout outAll1.txt ./nvlink_all --cpu-to-gpu
-jsrun --nrs 1 --gpu_per_rs=2 --stdio_stderr errAll2.txt --stdio_stdout outAll2.txt ./nvlink_all --gpu-to-gpu
+jsrun --nrs 1 --gpu_per_rs=2 ./nvlink_all --cpu-to-gpu >outAll1.txt 2>errAll1.txt
+jsrun --nrs 1 --gpu_per_rs=2 ./nvlink_all --gpu-to-gpu >outAll2.txt 2>errAll2.txt
