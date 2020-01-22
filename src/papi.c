icl-utk-edu/papi

commit db01193745da4486a9b2ad93f6d4272e1460e11d

Renamed papi_rate_stop to papi_stop_events.

src/papi.c
@@ -244,7 +244,7 @@ PAPI_flips_rate( int event, float *rtime, float *ptime, long long *flpins, float
   * @see PAPI_flips_rate()
   * @see PAPI_ipc()
   * @see PAPI_epc()
-  * @see PAPI_rate_stop()
+  * @see PAPI_stop_events()
  */
 int
 PAPI_flops_rate( int event, float *rtime, float *ptime, long long *flpops, float *mflops )
@@ -307,7 +307,7 @@ PAPI_flops_rate( int event, float *rtime, float *ptime, long long *flpops, float
   * @see PAPI_flips_rate()
   * @see PAPI_flops_rate()
   * @see PAPI_epc()
-  * @see PAPI_rate_stop()
+  * @see PAPI_stop_events()
  */
 int
 PAPI_ipc( float *rtime, float *ptime, long long *ins, float *ipc )
@@ -371,7 +371,7 @@ PAPI_ipc( float *rtime, float *ptime, long long *ins, float *ipc )
   * @see PAPI_flips_rate()
   * @see PAPI_flops_rate()
   * @see PAPI_ipc()
-  * @see PAPI_rate_stop()
+  * @see PAPI_stop_events()
  */
 int
 PAPI_epc( int event, float *rtime, float *ptime, long long *ref, long long *core, long long *evt, float *epc )
@@ -392,19 +392,19 @@ PAPI_epc( int event, float *rtime, float *ptime, long long *ref, long long *core
    return ( retval );
 }
 
-/** @class PAPI_rate_stop
-  * @brief Stop counting hardware events of rate functions.
+/** @class PAPI_stop_events
+  * @brief Stop counting hardware events of rate or high-level functions.
   *
   * @par C Interface: 
   * \#include <papi.h> @n
-  * int PAPI_rate_stop();
+  * int PAPI_stop_events();
   * 
   * @retval PAPI_ENOEVNT 
   * -- The EventSet is not started yet.
   * @retval PAPI_ENOMEM 
   * -- Insufficient memory to complete the operation. 
   *
-  * PAPI_rate_stop stops the counters of a rate function and destroys the event set.
+  * PAPI_stop_events stops the counters of a rate or high-level function.
   *
   * @see PAPI_flips_rate()
   * @see PAPI_flops_rate()
@@ -412,7 +412,7 @@ PAPI_epc( int event, float *rtime, float *ptime, long long *ref, long long *core
   * @see PAPI_epc()
  */
 int
-PAPI_rate_stop()
+PAPI_stop_events()
 {
    int retval;
    long long tmp_values[3];
@@ -441,10 +441,10 @@ PAPI_rate_stop()
 
 /* this internal function is called by PAPI_hl_region_begin */
 int
-_papi_rate_stop()
+_PAPI_stop_events()
 {
    int retval;
-   retval = PAPI_rate_stop();
+   retval = PAPI_stop_events();
    return retval;
 }
 

src/examples/PAPI_mix_hl_ll.c
@@ -36,7 +36,7 @@ int main()
   if ( (retval = PAPI_hl_region_end("slow_code")) < PAPI_OK )
     ERROR_RETURN(retval);
 
-  if ( (retval = PAPI_rate_stop()) < PAPI_OK )
+  if ( (retval = PAPI_stop_events()) < PAPI_OK )
     ERROR_RETURN(retval);
 
   /* get IPC using low-level API */

src/examples/PAPI_mix_hl_rate.c
@@ -56,7 +56,7 @@ int main()
   printf("Real_time: %f Proc_time: %f Instructions: %lld IPC: %f\n", 
          real_time, proc_time,ins,ipc);
 
-  if ( (retval = PAPI_rate_stop()) < PAPI_OK )
+  if ( (retval = PAPI_stop_events()) < PAPI_OK )
     ERROR_RETURN(retval);
 
   exit(0);

src/examples/PAPI_mix_ll_rate.c
@@ -36,7 +36,7 @@ int main()
   printf("Real_time: %f Proc_time: %f Instructions: %lld IPC: %f\n", 
          real_time, proc_time,ins,ipc);
 
-  if ( (retval = PAPI_rate_stop()) < PAPI_OK )
+  if ( (retval = PAPI_stop_events()) < PAPI_OK )
     ERROR_RETURN(retval);
 
   /* get IPC using low-level API */

src/papi.h
@@ -1108,7 +1108,7 @@ enum {
    int PAPI_flops_rate(int event, float *rtime, float *ptime, long long * flpops, float *mflops); /**< simplified call to get Mflops/s (floating point operation rate), real and processor time */
    int PAPI_ipc(float *rtime, float *ptime, long long * ins, float *ipc); /**< gets instructions per cycle, real and processor time */
    int PAPI_epc(int event, float *rtime, float *ptime, long long *ref, long long *core, long long *evt, float *epc); /**< gets (named) events per cycle, real and processor time, reference and core cycles */
-   int PAPI_rate_stop(); /**< stops a running event set from a rate function */
+   int PAPI_stop_events(); /**< stops an internal running event set from a rate or high-level function */
 
    /** @} */
 

commit e17b12cc24e6d2d628a80cb0a5d226da3d13b45b

Bug fix that was caused by commit db01193.

src/papi.c
@@ -441,11 +441,25 @@ PAPI_stop_events()
 
 /* this internal function is called by PAPI_hl_region_begin */
 int
-_PAPI_stop_events()
+_papi_rate_stop()
 {
    int retval;
-   retval = PAPI_stop_events();
-   return retval;
+   long long tmp_values[3];
+
+   if ( _papi_rate_events_running == 1 ) {
+      if ( _rate_state!= NULL ) {
+         if ( _rate_state->running > STOP ) {
+            retval = PAPI_stop( _rate_state->EventSet, tmp_values );
+            if ( retval == PAPI_OK ) {
+               PAPI_cleanup_eventset( _rate_state->EventSet );
+               _rate_state->running = STOP;
+            }
+            _papi_rate_events_running = 0;
+            return retval;
+         }
+      }
+   }
+   return ( PAPI_ENOEVNT );
 }
 
 static int