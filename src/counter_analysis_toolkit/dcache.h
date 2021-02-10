7f37a5bb6e2aee9978a6e51c0581ba2f30123bf0
Daniel Barry <dbarry@vols.utk.edu>
2021-02-10 13:53:12 -0500

    Removed the CAT data cache benchmarks from running in a separate, stand-alone thread.
    This is a necessary step to implement truly multi-threaded versions of the benchmarks.
    
    These changes were tested on the IBM POWER9 architecture.

--- src/counter_analysis_toolkit/dcache.h
+++ src/counter_analysis_toolkit/dcache.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int varyBufferSizes(int *values, double *rslts, double *counter, int line_size_in_bytes, float pages_per_block, int latency_only, int mode);
-void *thread_main(void *arg);
+int experiment_main(void *arg);
 void d_cache_driver(char* papi_event_name, int max_iter, char* outdir, int latency_only, int mode, int show_progress);
 void d_cache_test(int pattern, int max_iter, int line_size_in_bytes, float pages_per_block, char* papi_event_name, int latency_only, int mode, FILE* ofp);
 
