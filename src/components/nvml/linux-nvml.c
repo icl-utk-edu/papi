01da6c31a4ff83979646654b71b33c45dd6ab310
Giuseppe Congiu <gcongiu@icl.utk.edu>
2022-02-16 16:33:25 -0500

    nvml: change disabled_reason for delayed init

--- src/components/nvml/linux-nvml.c
+++ src/components/nvml/linux-nvml.c
@@ -1083,8 +1083,7 @@ static int _papi_nvml_init_component(int cidx)
     _nvml_vector.cmp_info.num_mpx_cntrs = -1;
 
      sprintf(_nvml_vector.cmp_info.disabled_reason,
-             "Not initialized, call PAPI_enum_cmp_event or any other "
-             "component event access function to force lazy init.");
+             "Not initialized. Access component events to initialize it.");
 
     return PAPI_EDELAY_INIT;
 }
