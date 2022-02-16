c27a6faaacac2164f5bd9f6bcd949e72b1fd825f
Giuseppe Congiu <gcongiu@icl.utk.edu>
2022-02-16 16:32:56 -0500

    rocm: change disabled_reason for delayed init

--- src/components/rocm/linux-rocm.c
+++ src/components/rocm/linux-rocm.c
@@ -731,8 +731,7 @@ static int _rocm_init_component(int cidx)
     _rocm_vector.cmp_info.num_mpx_cntrs = -1;
 
     sprintf(_rocm_vector.cmp_info.disabled_reason,
-            "Not initialized, call PAPI_enum_cmp_event or any other "
-            "component event access function to force lazy init.");
+            "Not initialized. Access component events to initialize it.");
 
     return PAPI_EDELAY_INIT;
 }
