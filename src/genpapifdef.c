deb6fb1c9eefd4afdb761d73ecdadb404088841b
AnustuvICL <anustuv@icl.utk.edu>
2022-05-17 15:08:58 -0400

    cuda: Raise error when adding metrics that need multiple pass
    - Add new error code `PAPI_EMULPASS`
    - Updated docs for `PAPI_add_event()` and `PAPI_add_named_event()`
    - Add test program in `components/cuda/test/test_multipass_event_fail.c`

--- src/genpapifdef.c
+++ src/genpapifdef.c
@@ -184,7 +184,8 @@ const hwi_describe_t _papi_def[] = {
     /*22 */ {PAPI_EATTR, "PAPI_EATTR", "Invalid or missing event attributes"},
     /*23 */ {PAPI_ECOUNT, "PAPI_ECOUNT", "Too many events or attributes"},
     /*24 */ {PAPI_ECOMBO, "PAPI_ECOMBO", "Bad combination of features"},
-    /*25 */ {PAPI_ECMP_DISABLED, "PAPI_ECMP_DISABLED", "Component containing event is disabled"}
+    /*25 */ {PAPI_ECMP_DISABLED, "PAPI_ECMP_DISABLED", "Component containing event is disabled"},
+    /*27 */ {PAPI_EMULPASS, "PAPI_EMULPASS", "Event exists, but cannot be counted due to multiple passes required by hardware"}
 };
 
 
