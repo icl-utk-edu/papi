9c6b28bb497d3b3e700c5407417dffae116c2743
Giuseppe Congiu <gcongiu@icl.utk.edu>
2022-05-28 07:25:39 -0400

    nvml: account for PAPI_EDELAY_INIT in tests
    Currently, nvml tests check for disable state of the component. Tests do
    not allow for PAPI_EDELAY_INIT error, introduced in commit 1f44a36,
    however. Thus, tests fail spuriously. This patch adds a force_nvml_init
    function that accesses the nvml events, forcing the component to init.

---
+++ src/components/nvml/tests/force_init.h
@@ -0,0 +1,10 @@
+#ifndef __FORCE_INIT_H__
+#define __FORCE_INIT_H__
+
+static inline void force_nvml_init(int cid)
+{
+    int ntv_code = PAPI_NATIVE_MASK;
+    PAPI_enum_cmp_event(&ntv_code, PAPI_ENUM_FIRST, cid);
+}
+
+#endif

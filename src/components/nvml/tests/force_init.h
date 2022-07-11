icl-utk-edu/papi

commit 1f44a360ab19dbf5a11dda5881d55ca5549c9620

papi_errno: add PAPI_EDELAY_INIT error for delayed init components
Delayed initialization components need a way for distinguishing delayed
initialization from disabled state.

src/papi.h
@@ -278,7 +278,8 @@ failure.
 #define PAPI_ECOUNT		-23    /**< Too many events or attributes */
 #define PAPI_ECOMBO		-24    /**< Bad combination of features */
 #define PAPI_ECMP_DISABLED	-25    /**< Component containing event is disabled */
-#define PAPI_NUM_ERRORS	 26    /**< Number of error messages specified in this API */
+#define PAPI_EDELAY_INIT -26
+#define PAPI_NUM_ERRORS	 27    /**< Number of error messages specified in this API */
 
 #define PAPI_NOT_INITED		0
 #define PAPI_LOW_LEVEL_INITED 	1       /* Low level has called library init */

commit 9c6b28bb497d3b3e700c5407417dffae116c2743

nvml: account for PAPI_EDELAY_INIT in tests
Currently, nvml tests check for disable state of the component. Tests do
not allow for PAPI_EDELAY_INIT error, introduced in commit 1f44a36,
however. Thus, tests fail spuriously. This patch adds a force_nvml_init
function that accesses the nvml events, forcing the component to init.

src/components/nvml/tests/force_init.h
@@ -1,0 +1,10 @@
+#ifndef __FORCE_INIT_H__
+#define __FORCE_INIT_H__
+
+static inline void force_nvml_init(int cid)
+{
+    int ntv_code = PAPI_NATIVE_MASK;
+    PAPI_enum_cmp_event(&ntv_code, PAPI_ENUM_FIRST, cid);
+}
+
+#endif

src/components/nvml/tests/nvml_power_limit_read_test.cu
@@ -26,6 +26,8 @@
 #include "papi.h"
 #include "papi_test.h"
 
+#include "force_init.h"
+
 // Host function
 int main( int argc, char** argv )
 {
@@ -71,6 +73,8 @@ int main( int argc, char** argv )
         fprintf(stderr, "NVML PAPI Component was not found.\n");       
         test_skip( __FILE__, __LINE__,"Component nvml is not present\n",-1 );
     }
+
+    force_nvml_init(cid);
 
     printf( "NVML found as Component %d of %d: %s: %d events\n", (1+cmpinfo->CmpIdx), numcmp, cmpinfo->name, cmpinfo->num_native_events );
     if (cmpinfo->disabled) {                                    // If disabled,

src/components/nvml/tests/nvml_power_limiting_test.cu
@@ -25,6 +25,8 @@
 #include "papi.h"
 #include "papi_test.h"
 
+#include "force_init.h"
+
 // Host function
 int main( int argc, char** argv )
 {
@@ -70,6 +72,8 @@ int main( int argc, char** argv )
         fprintf(stderr, "NVML PAPI Component was not found.\n");       
         test_skip( __FILE__, __LINE__,"Component nvml is not present\n",-1 );
     }
+
+    force_nvml_init(cid);
 
     printf( "NVML found as Component %d of %d: %s: %d events\n", (1+cmpinfo->CmpIdx), numcmp, cmpinfo->name, cmpinfo->num_native_events );
     if (cmpinfo->disabled) {                                    // If disabled,