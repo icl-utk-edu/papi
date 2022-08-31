73125962801f855d15804d35379d200f9f1cf10e
Giuseppe Congiu <gcongiu@icl.utk.edu>
2022-08-31 05:00:09 -0400

    mx: set disabled flag in cmp

--- src/components/mx/linux-mx.c
+++ src/components/mx/linux-mx.c
@@ -280,6 +280,7 @@ _mx_init_component( int cidx )
 	_mx_vector.cmp_info.CmpIdx = cidx;
 
   fn_exit:
+    _papi_hwd[cidx]->cmp_info.disabled = retval;
     return retval;
   fn_fail:
     goto fn_exit;
