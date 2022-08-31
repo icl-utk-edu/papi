5edc3f549672ad7a0ec294f6bf3e5c4e40847e35
Giuseppe Congiu <gcongiu@icl.utk.edu>
2022-08-31 05:01:53 -0400

    powercap_ppc: set disabled flag in cmp

--- src/components/powercap_ppc/linux-powercap-ppc.c
+++ src/components/powercap_ppc/linux-powercap-ppc.c
@@ -193,6 +193,7 @@ _powercap_ppc_init_component( int cidx )
     _powercap_ppc_vector.cmp_info.CmpIdx = cidx;
 
   fn_exit:
+    _papi_hwd[cidx]->cmp_info.disabled = retval;
     return retval;
   fn_fail:
     goto fn_exit;
