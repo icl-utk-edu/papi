58863cf9f1110b23322d0e768e3f8227ecb2767d
Giuseppe Congiu <gcongiu@icl.utk.edu>
2022-09-24 13:26:46 -0400

    sysdetect: add POWER10 cache info support

--- src/components/sysdetect/powerpc_cpu_utils.c
+++ src/components/sysdetect/powerpc_cpu_utils.c
@@ -139,6 +139,30 @@ _sysdetect_cache_level_info_t power9_cache_info[] = {
     },
 };
 
+_sysdetect_cache_level_info_t power10_cache_info[] = {
+    { // level 1 begins
+        2,
+        {
+            {PAPI_MH_TYPE_INST, 49152, 128, 384, 6},
+            {PAPI_MH_TYPE_DATA, 32768, 128, 256, 8}
+        }
+    },
+    { // level 2 begins
+        1,
+        {
+            {PAPI_MH_TYPE_UNIFIED, 1048576, 128, 8192, 8},
+            {PAPI_MH_TYPE_EMPTY, -1, -1, -1, -1}
+        }
+    },
+    { // level 3 begins
+        1,
+        {
+            {PAPI_MH_TYPE_UNIFIED, 4194304, 128, 32768, 16},
+            {PAPI_MH_TYPE_EMPTY, -1, -1, -1, -1}
+        }
+    },
+};
+
 #define SPRN_PVR            0x11F /* Processor Version Register */
 #define PVR_PROCESSOR_SHIFT 16
 
@@ -241,6 +265,9 @@ get_cache_info( CPU_attr_e attr, int level, int *value )
             break;
         case 0x4e:               /* POWER9 */
             clevel_ptr = power9_cache_info;
+            break;
+        case 0x80:               /* POWER10 */
+            clevel_ptr = power10_cache_info;
             break;
         default:
             return CPU_ERROR;
