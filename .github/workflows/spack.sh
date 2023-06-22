a88900e5443df1b60b7efdda99f82bac3190b8c5
G-Ragghianti <ragghianti@icl.utk.edu>
2023-06-22 11:05:05 -0400

    Only including simple spack test for now

--- .github/workflows/spack.sh
+++ .github/workflows/spack.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
-COMPONENT=$1
-COMPILER=$2
+COMPILER=$1
 
 [ -z "$COMPILER" ] && COMPILER=gcc@10 
 
@@ -12,26 +11,14 @@ trap 'echo "# $BASH_COMMAND"' DEBUG
 shopt -s expand_aliases
 
 export HOME=`pwd`
-git clone https://github.com/spack/spack ../spack || true
-(
-   cd ../spack
-   git pull
-)
-source ../spack/share/spack/setup-env.sh
+git clone https://github.com/spack/spack spack || true
+source spack/share/spack/setup-env.sh
 
-VARIANTS=""
-if [ "$COMPONENT" = "cuda" || "$COMPONENT" = "nvml" ]; then
-   VARIANTS="cuda_arch=70"
-elif [ "$COMPONENT" = "rocm" || "$COMPONENT" = "rocm_smi" ]; then
-   VARIANTS="amdgpu_target=gfx90a"
-fi
-
-SPEC="papi@master +$COMPONENT $VARIANTS %$COMPILER"
+SPEC="papi@master %$COMPILER"
 echo SPEC=$SPEC
 
-rm -rf .spack
 module load $COMPILER
 spack compiler find
-spack uninstall -a -y heffte || true
+#spack uninstall -a -y papi || true
 spack dev-build -i --fresh --test=root $SPEC
 spack test run papi
