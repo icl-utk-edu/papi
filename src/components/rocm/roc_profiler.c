icl-utk-edu/papi

commit 4991e161479587986a825cc0a1cea07b83b45823

rocm: get rid of asserts

src/components/rocm/roc_profiler.c
@@ -530,7 +530,10 @@ init_event_table(void)
     }
 
     ntv_table.events = papi_calloc(ntv_table.count, sizeof(ntv_event_t));
-    assert(ntv_table.events);
+    if (ntv_table.events == NULL) {
+        papi_errno = PAPI_ENOMEM;
+        goto fn_fail;
+    }
 
     struct ntv_arg arg;
     arg.count = 0;
@@ -1972,7 +1975,9 @@ get_context_counters(unsigned int *events_id, unsigned int dev_id, cb_context_no
                 break;
             }
         }
-        assert(j < rocp_ctx->u.intercept.feature_count);
+        if (j < rocp_ctx->u.intercept.feature_count) {
+            return PAPI_ECMP;
+        }
         rocp_ctx->u.intercept.counters[j] += n->counters[i];
     }
 

commit 05869059c1e3c29edc5913acca9242ee78789370

rocm: fix bug introduced by commit 4991e1614

src/components/rocm/roc_profiler.c
@@ -1981,7 +1981,7 @@ get_context_counters(unsigned int dev_id, cb_context_node_t *n, rocp_ctx_t rocp_
                 break;
             }
         }
-        if (j < rocp_ctx->u.intercept.feature_count) {
+        if (j >= rocp_ctx->u.intercept.feature_count) {
             return PAPI_ECMP;
         }
         rocp_ctx->u.intercept.counters[j] += n->counters[i];