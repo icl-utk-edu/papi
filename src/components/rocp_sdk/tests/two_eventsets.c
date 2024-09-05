7f99ac6de5d776544ba21b8070f5e9583a1d8eda
Anthony Danalis <adanalis@icl.utk.edu>
2024-09-05 20:06:20 +0000

    Better support for multiple eventsets and multiple devices.
    Now we create the dispatch profile when the user calls PAPI_start() not
    when the dispatch callback is invoked.
    Also, we create as many profiles as there are devices requested by the
    user events, and we associate each profile with the correct agent.

---
+++ src/components/rocp_sdk/tests/two_eventsets.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <papi.h>
+#include <papi_test.h>
+
+extern void launch_kernel(int device_id);
+
+int main(int argc, char *argv[])
+{
+    int papi_errno;
+#define NUM_EVENTS (5)
+    long long counters1[NUM_EVENTS] = { 0 };
+    long long counters2[NUM_EVENTS] = { 0 };
+    int eventset1 = PAPI_NULL;
+    int eventset2 = PAPI_NULL;
+    int exp1[NUM_EVENTS] = {1, 120000, 900, 500000, 1};
+    int exp2[NUM_EVENTS] = {120000, 1, 900, 1, 500000};
+    int exp3[NUM_EVENTS] = {1, 120000, 1, 1, 1};
+
+    const char *events1[NUM_EVENTS] = {
+                  "rocp_sdk:::SQ_CYCLES:device=0",
+                  "rocp_sdk:::SQ_CYCLES:device=1",
+                  "rocp_sdk:::SQ_BUSY_CYCLES:device=1",
+                  "rocp_sdk:::TCC_CYCLE:device=1",
+                  "rocp_sdk:::SQ_WAVES:device=1"
+    };
+
+    const char *events2[NUM_EVENTS] = {
+                  "rocp_sdk:::SQ_CYCLES:device=1",
+                  "rocp_sdk:::SQ_CYCLES:device=0",
+                  "rocp_sdk:::SQ_BUSY_CYCLES:device=1",
+                  "rocp_sdk:::SQ_WAVES:device=0",
+                  "rocp_sdk:::TCC_CYCLE:device=1"
+    };
+
+    papi_errno = PAPI_library_init(PAPI_VER_CURRENT);
+    if (papi_errno != PAPI_VER_CURRENT) {
+        test_fail(__FILE__, __LINE__, "PAPI_library_init", papi_errno);
+    }
+
+    papi_errno = PAPI_create_eventset(&eventset1);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_create_eventset", papi_errno);
+    }
+
+    for (int i = 0; i < NUM_EVENTS; ++i) {
+        papi_errno = PAPI_add_named_event(eventset1, events1[i]);
+        if (papi_errno != PAPI_OK) {
+            test_fail(__FILE__, __LINE__, "PAPI_add_named_event", papi_errno);
+        }
+    }
+
+    papi_errno = PAPI_create_eventset(&eventset2);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_create_eventset", papi_errno);
+    }
+
+    for (int i = 0; i < NUM_EVENTS; ++i) {
+        papi_errno = PAPI_add_named_event(eventset2, events2[i]);
+        if (papi_errno != PAPI_OK) {
+            test_fail(__FILE__, __LINE__, "PAPI_add_named_event", papi_errno);
+        }
+    }
+
+    printf("==================== FIRST EVENTSET - DEVICE 1 ====================\n");
+
+    papi_errno = PAPI_start(eventset1);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_start", papi_errno);
+    }
+    for(int rep=0; rep<=3; ++rep){
+
+        launch_kernel(1);
+
+        usleep(1000);
+
+        papi_errno = PAPI_read(eventset1, counters1);
+        if (papi_errno != PAPI_OK) {
+            test_fail(__FILE__, __LINE__, "PAPI_read", papi_errno);
+        }
+        printf("---------------------  PAPI_read()\n");
+
+        for (int i = 0; i < NUM_EVENTS; ++i) {
+            printf("%s: %lld (%.2lf)\n", events1[i], counters1[i], 1.0*counters1[i]/((1.0+rep)*exp1[i]));
+        }
+    }
+
+    papi_errno = PAPI_stop(eventset1, counters1);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_stop", papi_errno);
+    }
+
+    printf("---------------------  PAPI_stop()\n");
+
+    for (int i = 0; i < NUM_EVENTS; ++i) {
+        printf("%s: %lld (%.2lf)\n", events1[i], counters1[i], 1.0*counters1[i]/((1.0+3)*exp1[i]));
+    }
+
+    printf("==================== SECOND EVENTSET - DEVICE 1 ====================\n");
+
+
+    papi_errno = PAPI_start(eventset2);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_start", papi_errno);
+    }
+    for(int rep=0; rep<=3; ++rep){
+
+        launch_kernel(1);
+
+        usleep(1000);
+
+        papi_errno = PAPI_read(eventset2, counters2);
+        if (papi_errno != PAPI_OK) {
+            test_fail(__FILE__, __LINE__, "PAPI_read", papi_errno);
+        }
+        printf("---------------------  PAPI_read()\n");
+
+        for (int i = 0; i < NUM_EVENTS; ++i) {
+            printf("%s: %lld (%.2lf)\n", events2[i], counters2[i], 1.0*counters2[i]/((1.0+rep)*exp2[i]));
+        }
+    }
+
+    papi_errno = PAPI_stop(eventset2, counters2);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_stop", papi_errno);
+    }
+
+    printf("---------------------  PAPI_stop()\n");
+
+    for (int i = 0; i < NUM_EVENTS; ++i) {
+        printf("%s: %lld (%.2lf)\n", events2[i], counters2[i], 1.0*counters2[i]/((1.0+3)*exp2[i]));
+    }
+
+    printf("==================== SECOND EVENTSET - DEVICE 0 ====================\n");
+
+    papi_errno = PAPI_start(eventset2);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_start", papi_errno);
+    }
+    for(int rep=0; rep<=2; ++rep){
+
+        launch_kernel(0);
+
+        usleep(1000);
+
+        papi_errno = PAPI_read(eventset2, counters2);
+        if (papi_errno != PAPI_OK) {
+            test_fail(__FILE__, __LINE__, "PAPI_read", papi_errno);
+        }
+        printf("---------------------  PAPI_read()\n");
+
+        for (int i = 0; i < NUM_EVENTS; ++i) {
+            printf("%s: %lld (%.2lf)\n", events2[i], counters2[i], 1.0*counters2[i]/((1.0+rep)*exp3[i]));
+        }
+    }
+
+    papi_errno = PAPI_stop(eventset2, counters2);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_stop", papi_errno);
+    }
+
+    printf("---------------------  PAPI_stop()\n");
+
+    for (int i = 0; i < NUM_EVENTS; ++i) {
+        printf("%s: %lld (%.2lf)\n", events2[i], counters2[i], 1.0*counters2[i]/((1.0+2)*exp3[i]));
+    }
+
+    /* * * Cleanup * * */
+
+    papi_errno = PAPI_cleanup_eventset(eventset1);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_cleanup_eventset", papi_errno);
+    }
+
+    papi_errno = PAPI_destroy_eventset(&eventset1);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_destroy_eventset", papi_errno);
+    }
+
+    papi_errno = PAPI_cleanup_eventset(eventset2);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_cleanup_eventset", papi_errno);
+    }
+
+    papi_errno = PAPI_destroy_eventset(&eventset2);
+    if (papi_errno != PAPI_OK) {
+        test_fail(__FILE__, __LINE__, "PAPI_destroy_eventset", papi_errno);
+    }
+
+    PAPI_shutdown();
+    test_pass(__FILE__);
+    return 0;
+}
