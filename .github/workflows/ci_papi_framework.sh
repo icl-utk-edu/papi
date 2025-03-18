6c09b23683883b8838a47ff7db75b54a56b4061f
Treece Burgess <tburgess@icl.utk.edu>
2025-03-18 19:35:09 +0000

    Remove infiniband from the papi_components_comprehensive CI test

--- .github/workflows/ci_papi_framework.sh
+++ .github/workflows/ci_papi_framework.sh
@@ -50,7 +50,6 @@ esac
 # test linking with or without --with-shlib-tools 
 if [ "$SHLIB" = "without" ]; then
     ./configure --with-debug=$DEBUG --enable-warnings --with-components="$COMPONENTS"
-    ./configure --with-debug=$DEBUG --enable-warnings --with-components="$COMPONENTS" --with-shlib-tools
 else
     ./configure --with-debug=$DEBUG --enable-warnings --with-components="$COMPONENTS" --with-shlib-tools
 fi
@@ -62,7 +61,7 @@ utils/papi_component_avail
 
 # active component check
 CURRENT_ACTIVE_COMPONENTS=$(utils/papi_component_avail | grep -A1000 'Active components' | grep "Name:   " | awk '{printf "%s%s", sep, $2; sep=" "} END{print ""}')
-if [ "$COMPONENTS" = "cuda nvml rocm rocm_smi powercap powercap_ppc rapl sensors_ppc infiniband net appio io lustre stealtime coretemp lmsensors mx sde" ]; then 
+if [ "$COMPONENTS" = "cuda nvml rocm rocm_smi powercap powercap_ppc rapl sensors_ppc net appio io lustre stealtime coretemp lmsensors mx sde" ]; then 
     [ "$CURRENT_ACTIVE_COMPONENTS" = "perf_event perf_event_uncore cuda nvml powercap net appio io stealtime coretemp lmsensors sde sysdetect" ]
 elif [ "$COMPONENTS" = "rocm rocm_smi" ]; then
     [ "$CURRENT_ACTIVE_COMPONENTS" = "perf_event perf_event_uncore rocm rocm_smi sysdetect" ]
