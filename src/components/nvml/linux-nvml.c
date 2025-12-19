06007875b2513795d2d3b6fb09e081c761cbb2b0
Patrou, Maria <patroum@ornl.gov>
2025-12-19 14:49:11 -0500

    gpu energy, instant gpu and avg memory power nvml counters support added, papi_hl delta and average calculations added, issue with components fixed

--- src/components/nvml/linux-nvml.c
+++ src/components/nvml/linux-nvml.c
@@ -70,6 +70,8 @@ nvmlReturn_t DECLDIR nvmlDeviceGetFanSpeed(nvmlDevice_t, unsigned int *);
 nvmlReturn_t DECLDIR nvmlDeviceGetMemoryInfo(nvmlDevice_t, nvmlMemory_t *);
 nvmlReturn_t DECLDIR nvmlDeviceGetPerformanceState(nvmlDevice_t, nvmlPstates_t *);
 nvmlReturn_t DECLDIR nvmlDeviceGetPowerUsage(nvmlDevice_t, unsigned int *);
+nvmlReturn_t DECLDIR nvmlDeviceGetTotalEnergyConsumption(nvmlDevice_t, unsigned long long *);
+nvmlReturn_t DECLDIR nvmlDeviceGetFieldValues(nvmlDevice_t,int, nvmlFieldValue_t *);
 nvmlReturn_t DECLDIR nvmlDeviceGetTemperature(nvmlDevice_t, nvmlTemperatureSensors_t, unsigned int *);
 nvmlReturn_t DECLDIR nvmlDeviceGetTotalEccErrors(nvmlDevice_t, nvmlEccBitType_t, nvmlEccCounterType_t, unsigned long long *);
 nvmlReturn_t DECLDIR nvmlDeviceGetUtilizationRates(nvmlDevice_t, nvmlUtilization_t *);
@@ -92,6 +94,8 @@ static nvmlReturn_t (*nvmlDeviceGetFanSpeedPtr)(nvmlDevice_t, unsigned int *);
 static nvmlReturn_t (*nvmlDeviceGetMemoryInfoPtr)(nvmlDevice_t, nvmlMemory_t *);
 static nvmlReturn_t (*nvmlDeviceGetPerformanceStatePtr)(nvmlDevice_t, nvmlPstates_t *);
 static nvmlReturn_t (*nvmlDeviceGetPowerUsagePtr)(nvmlDevice_t, unsigned int *);
+static nvmlReturn_t (*nvmlDeviceGetTotalEnergyConsumptionPtr)(nvmlDevice_t, unsigned long long *);
+static nvmlReturn_t (*nvmlDeviceGetFieldValuesPtr)(nvmlDevice_t,int, nvmlFieldValue_t *);
 static nvmlReturn_t (*nvmlDeviceGetTemperaturePtr)(nvmlDevice_t, nvmlTemperatureSensors_t, unsigned int *);
 static nvmlReturn_t (*nvmlDeviceGetTotalEccErrorsPtr)(nvmlDevice_t, nvmlEccBitType_t, nvmlEccCounterType_t, unsigned long long *);
 static nvmlReturn_t (*nvmlDeviceGetUtilizationRatesPtr)(nvmlDevice_t, nvmlUtilization_t *);
@@ -316,6 +320,32 @@ getPState(nvmlDevice_t dev)
 }
 
 unsigned long long
+getEnergyConsumption(nvmlDevice_t dev)
+{
+    unsigned long long energy = 0;
+    nvmlReturn_t bad;
+    bad = (*nvmlDeviceGetTotalEnergyConsumptionPtr)(dev, &energy);
+
+    if (NVML_SUCCESS != bad) {
+        SUBDBG("something went wrong %s\n", (*nvmlErrorStringPtr)(bad));
+        return (unsigned long long) - 1;
+    }
+    return energy;
+}
+
+unsigned long long
+getDeviceFieldValue(nvmlDevice_t dev, int value_count, nvmlFieldValue_t* field_value)
+{
+    nvmlReturn_t bad;
+    bad = (*nvmlDeviceGetFieldValuesPtr)(dev, value_count,field_value);
+    if (NVML_SUCCESS != bad) {
+        SUBDBG("something went wrong %s\n", (*nvmlErrorStringPtr)(bad));
+        return (unsigned long long) - 1;
+    }
+    return field_value->value.ullVal;
+}
+
+unsigned long long
 getPowerUsage(nvmlDevice_t dev)
 {
     unsigned int power;
@@ -454,6 +484,8 @@ nvml_hardware_read(long long *value, int which_one)
     nvml_native_event_entry_t *entry;
     nvmlDevice_t handle;
     int cudaIdx = -1;
+    int value_count = 1;
+    nvmlFieldValue_t field_value[value_count];
 
     entry = &nvml_native_table[which_one];
     *value = (long long) - 1;
@@ -495,6 +527,19 @@ nvml_hardware_read(long long *value, int which_one)
     case FEATURE_POWER:
         *value = getPowerUsage(handle);
         break;
+    case FEATURE_ENERGY_CONSUMPTION:
+        *value = getEnergyConsumption(handle);
+        break;
+    case FEATURE_GPU_INST:
+        field_value->fieldId = NVML_FI_DEV_POWER_INSTANT;
+        field_value->scopeId = 0; // GPU-only scope
+        *value = getDeviceFieldValue(handle, value_count, field_value);
+        break;
+    case FEATURE_GPU_MEMORY_AVG:
+        field_value->fieldId = NVML_FI_DEV_POWER_AVERAGE;
+        field_value->scopeId = 2; // Module-wide scope
+        *value = getDeviceFieldValue(handle, value_count, field_value);
+	break;
     case FEATURE_TEMP:
         *value = getTemperature(handle);
         break;
@@ -691,6 +736,33 @@ detectDevices()
             num_events++;
         } else {
             SUBDBG("nvmlDeviceGetPowerUsage does not appear to be supported on this card. (nvml return code %d)\n", ret);
+        }
+
+        /*Check if energy consumption data are available  */
+        if (getEnergyConsumption(devices[i]) != (unsigned long long) - 1) {
+            features[i] |= FEATURE_ENERGY_CONSUMPTION;
+            num_events++;
+        }
+
+        
+	int value_count=1;
+        nvmlFieldValue_t field_value[value_count];
+
+        // GPU instant power
+        field_value->fieldId = NVML_FI_DEV_POWER_INSTANT;
+        field_value->scopeId = 0; // GPU-only scope
+        /* Check if the device field for gpu instant power data are available */
+        if (getDeviceFieldValue(devices[i],value_count, field_value) != (unsigned long long) - 1) {
+            features[i] |= FEATURE_GPU_INST;
+            num_events ++;
+        }
+        // GPU Memory average power
+        field_value->fieldId = NVML_FI_DEV_POWER_AVERAGE;
+        field_value->scopeId = 2; // Module-wide scope
+        /* Check if the device field for gpu memory data are available */
+        if (getDeviceFieldValue(devices[i],value_count, field_value) != (unsigned long long) - 1) {
+            features[i] |= FEATURE_GPU_MEMORY_AVG;
+            num_events ++;
         }
 
         /* Check if temperature data are available */
@@ -981,6 +1053,35 @@ createNativeEvents()
             strncpy(entry->units, "mW", PAPI_MIN_STR_LEN);
             strncpy(entry->description, "Power usage reading for the device, in miliwatts. This is the power draw (+/-5 watts) for the entire board: GPU, memory, etc.", PAPI_MAX_STR_LEN);
             entry->type = FEATURE_POWER;
+            entry++;
+            nvml_dev_id_table[devTableIdx] = i;
+            devTableIdx++;
+        }
+
+        if (HAS_FEATURE(features[i], FEATURE_ENERGY_CONSUMPTION)) {
+            sprintf(entry->name, "%s:energy_consumption", sanitized_name);
+            strncpy(entry->description, "Total energy consumption of the GPU in millijoules since the driver was last reloaded.", PAPI_MAX_STR_LEN);
+            entry->type = FEATURE_ENERGY_CONSUMPTION;
+            entry++;
+            nvml_dev_id_table[devTableIdx] = i;
+            devTableIdx++;
+        }
+
+        if (HAS_FEATURE(features[i], FEATURE_GPU_INST)) {
+            sprintf(entry->name, "%s:gpu_inst_power", sanitized_name);
+            strncpy(entry->units, "mW", PAPI_MIN_STR_LEN);
+            strncpy(entry->description, "Instateneous power usage for GPU.", PAPI_MAX_STR_LEN);
+            entry->type = FEATURE_GPU_INST;
+            entry++;
+            nvml_dev_id_table[devTableIdx] = i;
+            devTableIdx++;
+        }
+
+        if (HAS_FEATURE(features[i], FEATURE_GPU_MEMORY_AVG)) {
+            sprintf(entry->name, "%s:gpu_memory_avg_power", sanitized_name);
+            strncpy(entry->units, "mW", PAPI_MIN_STR_LEN);
+            strncpy(entry->description, "Average power usage for GPU Memory.", PAPI_MAX_STR_LEN);
+            entry->type = FEATURE_GPU_MEMORY_AVG;
             entry++;
             nvml_dev_id_table[devTableIdx] = i;
             devTableIdx++;
@@ -1439,6 +1540,16 @@ linkCudaLibraries()
     if (dlerror() != NULL) {
         strncpy(_nvml_vector.cmp_info.disabled_reason, "NVML function nvmlDeviceGetPowerUsage not found.", PAPI_MAX_STR_LEN);
         return (PAPI_ENOSUPP);
+    }
+    nvmlDeviceGetTotalEnergyConsumptionPtr = dlsym(dl3, "nvmlDeviceGetTotalEnergyConsumption");
+    if (dlerror() != NULL) {
+    	strncpy(_nvml_vector.cmp_info.disabled_reason, "NVML function nvmlDeviceGetTotalEnergyConsumption not found.", PAPI_MAX_STR_LEN);
+    	return (PAPI_ENOSUPP);
+    }
+    nvmlDeviceGetFieldValuesPtr = dlsym(dl3, "nvmlDeviceGetFieldValues");
+    if (dlerror() != NULL) {
+    	strncpy(_nvml_vector.cmp_info.disabled_reason, "NVML function nvmlDeviceGetFieldValues not found.", PAPI_MAX_STR_LEN);
+    	return (PAPI_ENOSUPP);
     }
     nvmlDeviceGetTemperaturePtr = dlsym(dl3, "nvmlDeviceGetTemperature");
     if (dlerror() != NULL) {
