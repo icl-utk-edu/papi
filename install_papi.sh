8e17b06491fd48e5a5fc7d9168438b84a62219d2
Treece-Burgess <tburges6@vols.utk.edu>
2026-05-20 15:46:01 -0700

    adding variables and restructing install script

--- install_papi.sh
+++ install_papi.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-# Collect the PAPI configure options
-papi_configure_options=
-for arg in "$@"
-do
-    papi_configure_options+="$arg "
-done
-
 ci_path_to_install="$PWD/ci-install"
-./configure --prefix="$ci_path_to_install" --with-debug=yes --enable-warnings $papi_configure_options
+./configure --prefix="$ci_path_to_install" "$@"
 make && make install
-
-utils/papi_component_avail
