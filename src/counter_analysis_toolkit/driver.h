60057bfd9f3e0c7c14780ac61cafb74edd28ec1c
Daniel Barry <dbarry@vols.utk.edu>
2026-08-05 20:32:57 +0000

    cat: add missing argument to function prototype
    The previous definition of the affected function was missing an
    argument, which led to a compile-time error. This commit adds the
    missing argument.
    
    These changes have been tested on a system containing the Intel Granite
    Rapids architecture.

--- src/counter_analysis_toolkit/driver.h
+++ src/counter_analysis_toolkit/driver.h
@@ -28,7 +28,7 @@ void trav_evts(evstock* stock, int pk, int* cards, int nevts, int selexnsize, in
 int perm(int n, int k);
 int comb(int n, int k);
 void testbench(char** allevts, int cmbtotal, hw_desc_t *hw_desc, cat_params_t params, int myid, int nprocs);
-void print_usage();
+void print_usage(char *name);
 static int parse_line(FILE *input, char **key, long long *value);
 static void read_conf_file(char *conf_file, hw_desc_t *hw_desc);
 static hw_desc_t *obtain_hardware_description(char *conf_file_name);
