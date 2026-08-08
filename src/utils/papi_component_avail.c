6c9dd081ecd77809213e0f0390e742b8c741d06a
Treece Burgess <tburges6@vols.utk.edu>
2026-08-08 02:13:03 +0000

    framework: properly handle the case of no components configured

--- src/utils/papi_component_avail.c
+++ src/utils/papi_component_avail.c
@@ -116,7 +116,13 @@ main( int argc, char **argv )
 	for ( cid = 0; cid < numcmp; cid++ ) {
 	  cmpinfo = PAPI_get_component_info( cid );
 
-	  printf( "Name:   %-23s %s\n", cmpinfo->name ,cmpinfo->description);
+	  if (strcasecmp(cmpinfo->name, "No Components Configured. ") != 0) {
+	      printf( "Name:   %-23s %s\n", cmpinfo->name ,cmpinfo->description);
+	  }
+	  else {
+	      printf( "Name:   %-23s\n", cmpinfo->name);
+	      continue;
+	  }
 
 	  if (cmpinfo->disabled == PAPI_EDELAY_INIT) {
 	      force_cmp_init(cid);
@@ -147,11 +153,17 @@ main( int argc, char **argv )
 	  cmpinfo = PAPI_get_component_info( cid );
 	  if (cmpinfo->disabled) continue;
 
-	  printf( "Name:   %-23s %s\n", cmpinfo->name ,cmpinfo->description);
-	  is_utility_cmp = check_for_utility_components(cmpinfo->name);
-	  if (!is_utility_cmp) {
-		printf( "        %-23s Native: %d, Preset: %d, Counters: %d\n",
-			" ", cmpinfo->num_native_events, cmpinfo->num_preset_events, cmpinfo->num_cntrs);
+	  if (strcasecmp(cmpinfo->name, "No Components Configured. ") != 0) {
+		printf( "Name:   %-23s %s\n", cmpinfo->name ,cmpinfo->description);
+		is_utility_cmp = check_for_utility_components(cmpinfo->name);
+		if (!is_utility_cmp) {
+			printf( "        %-23s Native: %d, Preset: %d, Counters: %d\n",
+				" ", cmpinfo->num_native_events, cmpinfo->num_preset_events, cmpinfo->num_cntrs);
+		}
+	  }
+	  else {
+		printf( "Name:   %-23s\n", cmpinfo->name);
+		continue;
 	  }
 
      int pmus=0;
